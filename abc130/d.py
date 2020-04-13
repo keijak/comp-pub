a7c1dc84aa234c48f21056400837041fba8d042a
Keiji Maekawa <m@keijak.com>
2020-04-13 12:36:04 +0200

    abc130

--- abc130/d.py
+++ abc130/d.py
@@ -1,9 +1,29 @@
-n,k = map(int,input().split())
-a = [int(x) for x in input().split()]
-assert len(a) == n
+N, K = map(int,input().split())
+A = [int(x) for x in input().split()]
+assert len(A) == N
 
 
-s0 = sum(a[:k])
+def incr_right(left, right, asum):
+    while asum < K:
+        if right >= N:
+            return False, None, None
+        asum += A[right]
+        right += 1
+    return True, right, asum
 
 
-print(a)
+def solve():
+    res = 0
+    left, right = 0, 0
+    asum = 0
+    for left in range(N):
+        if asum < K:
+            found, right, asum = incr_right(left, right, asum)
+            if not found:
+                break
+        res += N - right + 1
+        asum -= A[left]
+    return res
+
+
+print(solve())
