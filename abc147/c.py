8919cd536cbb49bb6c8f3e1e66c54aba814653e9
Keiji Maekawa <m@keijak.com>
2020-04-16 17:50:48 +0200

    abc147

---
+++ abc147/c.py
@@ -0,0 +1,36 @@
+N = int(input())
+edges = [[None] * N for _ in range(N)]
+for i in range(N):
+    num_prop = int(input())
+    for j in range(num_prop):
+        x, y = map(int, input().split())
+        edges[i][x-1] = y
+
+
+def check(hs, us):
+    for h in hs:
+        for h2 in hs:
+            if edges[h][h2] == 0:
+                return False
+        for u in us:
+            if edges[h][u] == 1:
+                return False
+    return True
+
+
+def search():
+    maxv = 0
+    for i in range(1 << N):
+        hs = []
+        us = []
+        for j in range(N):
+            if (i >> j) & 1 == 1:
+                hs.append(j)
+            else:
+                us.append(j)
+        if check(hs, us):
+            if maxv < len(hs):
+                maxv = len(hs)
+    return maxv
+
+print(search())
