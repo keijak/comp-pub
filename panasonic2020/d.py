ffb71fed09b0d10f54c98a0f5edeb6d44681c535
Keiji Maekawa <m@keijak.com>
2020-04-18 13:51:07 +0200

    panasonic2020

---
+++ panasonic2020/d.py
@@ -0,0 +1,13 @@
+N = int(input())
+
+ss = ['a']
+for i in range(1, N):
+    tmp = []
+    for s in ss:
+        newc = ord(max(s)) + 1
+        for j in range(ord('a'), newc+1):
+            tmp.append(s + chr(j))
+    ss = tmp
+
+for s in sorted(ss):
+    print(s)
