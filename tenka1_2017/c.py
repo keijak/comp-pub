5e503a55479236962c681f6230d70e0966df5ebb
Keiji Maekawa <m@keijak.com>
2020-04-18 18:21:10 +0200

    tenka1_2017

--- tenka1_2017/c.py
+++ tenka1_2017/c.py
@@ -2,7 +2,7 @@ import sys
 N=int(input())
 for h in range(3501):
     for n in range(h):
-        d=4*h*n-N*(h+n)
-        if d>0 and N*h*n%d == 0:
-            print(h,n,N*h*n//d)
+        d = 4*h*n - N*(h+n)
+        if d > 0 and N*h*n%d == 0:
+            print(h, n, N*h*n//d)
             sys.exit()
