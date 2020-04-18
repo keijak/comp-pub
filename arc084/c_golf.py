4bb4bafa0dfd80b506970f456e2fe5e597816f59
Keiji Maekawa <m@keijak.com>
2020-04-18 16:30:56 +0200

    arc084

--- arc084/c_golf.py
+++ arc084/c_golf.py
@@ -1,5 +1,3 @@
 from bisect import*
-(N,),A,B,C=[sorted(map(int,input().split()))for _ in range(4)]
-w,i=[0]*(N+1),N
-while i:i-=1;w[i]=w[i+1]+N-bisect(C,B[i])
-print(sum(w[bisect(B,a)]for a in A))
+(N,),A,B,C=[sorted(map(int,input().split()))for _ in[0]*4]
+print(sum(bisect_left(A,b)*(N-bisect(C,b))for b in B))
