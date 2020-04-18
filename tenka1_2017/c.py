72cc62b1fec5b7a514219c73530e46bae1e80279
Keiji Maekawa <m@keijak.com>
2020-04-18 18:20:07 +0200

    tenka1_2017

--- tenka1_2017/c.py
+++ tenka1_2017/c.py
@@ -1,5 +1,8 @@
-import sys;N=int(input())
+import sys
+N=int(input())
 for h in range(3501):
- for n in range(h):
-  d=4*h*n-N*(h+n)
-  if d>0 and N*h*n%d<1:print(h,n,N*h*n//d);sys.exit()
+    for n in range(h):
+        d=4*h*n-N*(h+n)
+        if d>0 and N*h*n%d == 0:
+            print(h,n,N*h*n//d)
+            sys.exit()
