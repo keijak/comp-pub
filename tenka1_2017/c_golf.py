839cdf1ee2dacfc0b4f9c242afc3a7495a0dfcd1
Keiji Maekawa <m@keijak.com>
2020-04-18 21:07:30 +0200

    sys.exit() is aliased to exit()

--- tenka1_2017/c_golf.py
+++ tenka1_2017/c_golf.py
@@ -1,5 +1,5 @@
-import sys;N=int(input())
+N=int(input())
 for h in range(3501):
  for n in range(h):
   d=4*h*n-N*(h+n)
-  if d>0 and N*h*n%d<1:print(h,n,N*h*n//d);sys.exit()
+  if d>0 and N*h*n%d<1:print(h,n,N*h*n//d);exit()
