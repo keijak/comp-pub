f6f8299b9ece508a6ae6c4b5ec4159e8176302df
Keiji Maekawa <m@keijak.com>
2020-04-20 19:29:16 +0200

    golf

--- abc048/c_golf.py
+++ abc048/c_golf.py
@@ -1,3 +1,3 @@
-(N,x),a=[list(map(int,input().split()))for _ in[0]*2];r=0
-for i in range(1,N):e=max(a[i]+a[i-1]-x,0);a[i]-=min(e,a[i]);r+=e
+n,x,*a=map(int,open(0).read().split());t=r=0
+for i in a:t=min(i,x-t);r+=i-t
 print(r)
