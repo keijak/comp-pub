b9f966b4a8a85dd7287280eb2ca6fde5f7485001
Keiji Maekawa <m@keijak.com>
2020-04-22 02:28:48 +0200

    sumitrust2019

--- sumitrust2019/e.py
+++ sumitrust2019/e.py
@@ -1,40 +1,34 @@
 import itertools
 import sys
-import numpy as np
 from collections import Counter
-import bisect
 
 sys.setrecursionlimit(10 ** 9)
 input = sys.stdin.readline
 M = 1000000007
 N = int(input())
 left = list(map(int, input().split()))
-assert len(left) == N
-assert left[0] == 0
-
 counter = Counter(left)
 
 
-def dp(t, ns):
-    cached = t.get(ns)
+def dp(t, n1, n2, n3):
+    cached = t.get((n1, n2, n3))
     if cached is not None:
         return cached
-    remaining = sum(ns)
+    remaining = n1 + n2 + n3
     assert remaining > 0
     last_cnt = left[remaining - 1] + 1
-    n1, n2, n3 = ns
     res = 0
     if last_cnt == n1:
-        res += dp(t, tuple(sorted([n1 - 1, n2, n3])))
+        res += dp(t, *sorted([n1 - 1, n2, n3]))
         res %= M
     if last_cnt == n2:
-        res += dp(t, tuple(sorted([n1, n2 - 1, n3])))
+        res += dp(t, *sorted([n1, n2 - 1, n3]))
         res %= M
     if last_cnt == n3:
-        res += dp(t, tuple(sorted([n1, n2, n3 - 1])))
+        res += dp(t, *sorted([n1, n2, n3 - 1]))
         res %= M
     # print(f"{remaining}: ({n1},{n2},{n3}) => {res}")
-    t[ns] = res
+    t[n1, n2, n3] = res
     return res
 
 
@@ -43,18 +37,19 @@ def solve():
     for i in range(N):
         k = counter[i]
         if k == 3:
-            h[0] = h[1] = h[2] = i + 1
+            h[2] = h[1] = h[0] = i + 1
         elif k == 2:
-            h[0] = h[1] = i + 1
+            h[2] = h[1] = i + 1
         elif k == 1:
-            h[0] = i + 1
+            h[2] = i + 1
         else:
             break
-    assert sum(h) == N
+    if sum(h) != N:
+        return 0
     t = dict()
     t[0, 0, 0] = 1
-    res = dp(t, tuple(sorted(h)))
-    return res * len(set(itertools.permutations(h))) % M
+    res = dp(t, *h)
+    return (res * len(set(itertools.permutations(h)))) % M
 
 
 print(solve())
