f433280bb403f54b117e9e2bcb5581da2f24aaa0
Keiji Maekawa <m@keijak.com>
2020-04-25 10:43:00 +0200

    abc082

--- abc082/d.py
+++ abc082/d.py
@@ -9,46 +9,52 @@ head, inst = 0, []
 while head < len(s) and s[head] == "F":
     head += 1
     rx += 1
+
+xlist, ylist = [], []
+heading_x = True
 while head < len(s):
     tcnt = 0
     while head < len(s) and s[head] == "T":
         tcnt += 1
         head += 1
+    if head == len(s):
+        break
     fcnt = 0
     while head < len(s) and s[head] == "F":
         fcnt += 1
         head += 1
-    inst.append((tcnt % 2, fcnt))
-
-f_total = sum([f for (_, f) in inst])
-f_rem = [0] * (len(inst) + 1)
-f_rem[len(inst)] = 0
-for i in range(len(inst) - 1, -1, -1):
-    f_rem[i] = f_rem[i + 1] + inst[i][1]
-assert f_rem[0] == f_total
+    if ((tcnt % 2) == 1) ^ heading_x:
+        heading_x = True
+        xlist.append(fcnt)
+    else:
+        heading_x = False
+        ylist.append(fcnt)
 
 
-visited = set()
-
-
-def reach(i, x, y, dx, dy):
-    dist = abs(gx - x) + abs(gy - y)
-    # print(f"i={i} ({x},{y}) dist={dist}: inst={inst[i:]}")
-    if i == len(inst):
-        return dist == 0
-    if (i, x, y, dx, dy) in visited:
+def fill(fs, goal_offset):
+    """Knapsack"""
+    f_total = sum(fs)
+    if f_total < goal_offset:
         return False
-    visited.add((i, x, y, dx, dy))
-    if dist > f_rem[i]:
+    if f_total == goal_offset:
+        return True
+    f_diff = f_total - goal_offset
+    if f_diff % 2 == 1:
         return False
-    t, f = inst[i]
-    if t:
-        dy, dx = dx, -dy
-    if reach(i + 1, x + f * dx, y + f * dy, dx, dy):
-        return True
-    if reach(i + 1, x - f * dx, y - f * dy, -dx, -dy):
-        return True
+    f_diff //= 2
+    diffs = {0}
+    for f in fs:
+        for d in list(diffs):
+            d2 = f + d
+            if d2 > f_diff:
+                continue
+            if d2 == f_diff:
+                return True
+            diffs.add(d2)
     return False
 
 
-print("Yes" if reach(0, rx, ry, 1, 0) else "No")
+if fill(xlist, abs(gx - rx)) and fill(ylist, abs(gy - ry)):
+    print("Yes")
+else:
+    print("No")
