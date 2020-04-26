e11914626f9c4f9b11a9a75df908b5e539f09eb6
Keiji Maekawa <m@keijak.com>
2020-04-26 22:23:22 +0200

    abc164

--- abc164/e.py
+++ abc164/e.py
@@ -1,4 +1,7 @@
+import sys
 import heapq
+
+input = sys.stdin.readline
 
 COST_MAX = 2501
 TIME_MAX = 10 ** 15
@@ -7,14 +10,12 @@ N, M, S = map(int, input().split())
 fare_adj = [[COST_MAX] * N for _ in range(N)]
 time_adj = [[TIME_MAX] * N for _ in range(N)]
 fare_max = 0
-fare_sum = 0
 for i in range(M):
     u, v, a, b = map(int, input().split())
     u -= 1
     v -= 1
     fare_adj[u][v] = fare_adj[v][u] = a
     time_adj[u][v] = time_adj[v][u] = b
-    fare_sum += a
     fare_max = max(fare_max, a)
 
 exchanges = []
@@ -24,27 +25,30 @@ for i in range(N):
     exchanges.append((c, d))
 
 
-mintime = [[TIME_MAX] * (fare_sum + 1) for _ in range(N)]
+WALLET_MAX = fare_max * (N - 1)
+mintime = [[TIME_MAX] * (WALLET_MAX + 1) for _ in range(N)]
 
 
 def search():
-    q = [(0, 0, S)]
+    q = [(0, 0, min(S, WALLET_MAX))]
     while q:
         t, loc, silvers = heapq.heappop(q)
-        silvers = min(silvers, fare_sum)
-        if t >= mintime[loc][silvers]:
+        if t > mintime[loc][silvers]:
             continue
         mintime[loc][silvers] = t
-        while silvers <= fare_sum:
+        while True:
             for nloc in range(N):
                 if nloc == loc:
                     continue
                 ntime = t + time_adj[loc][nloc]
-                nsilvers = min(silvers - fare_adj[loc][nloc], fare_sum)
+                nsilvers = min(silvers - fare_adj[loc][nloc], WALLET_MAX)
                 if nsilvers >= 0 and ntime < mintime[nloc][nsilvers]:
+                    mintime[nloc][nsilvers] = ntime
                     heapq.heappush(q, (ntime, nloc, nsilvers))
             rate, wait = exchanges[loc]
             t += wait
+            if silvers > WALLET_MAX:
+                break
             silvers += rate
 
 
