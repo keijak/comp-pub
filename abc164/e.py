cf19cd7f5b3c026e9d8080f12c07ba42f2ebff88
Keiji Maekawa <m@keijak.com>
2020-04-26 22:59:21 +0200

    abc164 dijkstra

--- abc164/e.py
+++ abc164/e.py
@@ -3,55 +3,51 @@ import heapq
 
 input = sys.stdin.readline
 
-COST_MAX = 2501
+NCOSTS = 2501
 TIME_MAX = 10 ** 15
 
 N, M, S = map(int, input().split())
-fare_adj = [[COST_MAX] * N for _ in range(N)]
+adj = [[] for _ in range(N * NCOSTS)]
+fare_adj = [[NCOSTS] * N for _ in range(N)]
 time_adj = [[TIME_MAX] * N for _ in range(N)]
 fare_max = 0
+
+
+def nodeid(city, silvers):
+    return silvers * N + city
+
+
 for i in range(M):
     u, v, a, b = map(int, input().split())
     u -= 1
     v -= 1
-    fare_adj[u][v] = fare_adj[v][u] = a
-    time_adj[u][v] = time_adj[v][u] = b
-    fare_max = max(fare_max, a)
+    for j in range(a, NCOSTS):
+        adj[nodeid(u, j)].append((nodeid(v, j - a), b))
+        adj[nodeid(v, j)].append((nodeid(u, j - a), b))
 
-exchanges = []
 for i in range(N):
     # g-s rate, waiting duration
     c, d = map(int, input().split())
-    exchanges.append((c, d))
+    for j in range(NCOSTS - c):
+        adj[nodeid(i, j)].append((nodeid(i, j + c), d))
 
 
-WALLET_MAX = fare_max * (N - 1)
-mintime = [[TIME_MAX] * (WALLET_MAX + 1) for _ in range(N)]
+mintime = [TIME_MAX for _ in range(N * NCOSTS)]
 
 
 def search():
-    q = [(0, 0, min(S, WALLET_MAX))]
+    q = [(0, nodeid(0, min(S, NCOSTS - 1)))]
     while q:
-        t, loc, silvers = heapq.heappop(q)
-        if t > mintime[loc][silvers]:
+        t, node1 = heapq.heappop(q)
+        if t > mintime[node1]:
             continue
-        mintime[loc][silvers] = t
-        while True:
-            for nloc in range(N):
-                if nloc == loc:
-                    continue
-                ntime = t + time_adj[loc][nloc]
-                nsilvers = min(silvers - fare_adj[loc][nloc], WALLET_MAX)
-                if nsilvers >= 0 and ntime < mintime[nloc][nsilvers]:
-                    mintime[nloc][nsilvers] = ntime
-                    heapq.heappush(q, (ntime, nloc, nsilvers))
-            rate, wait = exchanges[loc]
-            t += wait
-            if silvers > WALLET_MAX:
-                break
-            silvers += rate
+        for node2, tcost in adj[node1]:
+            ntime = t + tcost
+            if mintime[node2] > ntime:
+                mintime[node2] = ntime
+                heapq.heappush(q, (ntime, node2))
 
 
 search()
-for t in range(1, N):
-    print(min(mintime[t]))
+for i in range(1, N):
+    print(min(mintime[nodeid(i, j)] for j in range(NCOSTS)))
