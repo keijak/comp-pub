c4e4eb3b4778507c123bf73e25c396186ed9a927
Keiji Maekawa <m@keijak.com>
2020-04-29 21:51:13 +0200

    DP

--- dp/h.py
+++ dp/h.py
@@ -5,15 +5,30 @@ read = sys.stdin.buffer.read
 readline = sys.stdin.buffer.readline
 readlines = sys.stdin.buffer.readlines
 
+M = 10 ** 9 + 7
 H, W = map(int, readline().split())
 grid = []
 for i in range(H):
     grid.append(readline().decode("utf-8").rstrip())
+    assert len(grid[-1]) == W
 
 
 def solve():
-    print(grid)
+    t = [0] * W
+    t[0] = 1
+    for c in range(1, W):
+        if grid[0][c] == ".":
+            t[c] = t[c - 1]
+    for r in range(1, H):
+        for c in range(W):
+            if grid[r][c] == "#":
+                t[c] = 0
+            elif c > 0:
+                t[c] += t[c - 1]
+                t[c] %= M
+        print(r, "\t", t)
+    return t[-1]
 
 
 if __name__ == "__main__":
-    solve()
+    print(solve())
---
+++ dp/h_numpy.py
@@ -0,0 +1,34 @@
+import sys
+import numpy as np
+import numba
+
+sys.setrecursionlimit(10 ** 8)
+read = sys.stdin.buffer.read
+readline = sys.stdin.buffer.readline
+readlines = sys.stdin.buffer.readlines
+
+H, W = map(int, readline().split())
+grid = np.zeros((H, W), dtype=np.int64)
+for i in range(H):
+    s = readline().decode("utf-8").rstrip()
+    grid[i, :] = [c == "." for c in s]
+
+
+@numba.njit(numba.i8(numba.i4, numba.i4, numba.typeof(grid)), cache=True)
+def solve(H, W, grid):
+    M = 10 ** 9 + 7
+    t = grid[0, :].copy()
+    for c in range(1, W):
+        t[c] = min(t[c], t[c - 1])
+    for r in range(1, H):
+        mask = grid[r, :]
+        t[0] *= mask[0]
+        for c in range(1, W):
+            t[c] += t[c - 1]
+            t[c] *= mask[c]
+            t[c] %= M
+    return t[-1]
+
+
+if __name__ == "__main__":
+    print(solve(H, W, grid))
