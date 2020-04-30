94767d6180514101a13eedd5e69ac4688c2e6596
Keiji Maekawa <m@keijak.com>
2020-04-30 13:45:22 +0200

    DP

--- dp/j_numba.py
+++ dp/j_numba.py
@@ -1,5 +1,7 @@
 import collections
 import sys
+import numpy as np
+import numba
 
 sys.setrecursionlimit(10 ** 8)
 read = sys.stdin.buffer.read
@@ -10,13 +12,13 @@ N = int(readline())
 A = [int(x) for x in readline().split()]
 ac = collections.Counter(A)
 assert set(ac.keys()).issubset({1, 2, 3})
+C1, C2, C3 = ac[1], ac[2], ac[3]
 
 
+@numba.njit("f8(i4,i4,i4)", cache=True)
 def solve(c1, c2, c3):
     K = c1 + c2 + c3
-    L = K + 1
-    L2 = L ** 2
-    dp = [0.0] * L ** 3
+    dp = np.zeros((K + 3, c2 + c3 + 2, c3 + 1), dtype=np.float64)
     for i3 in range(c3 + 1):
         for i2 in range(c2 + c3 + 1):
             for i1 in range(N - i2 - i3 + 1):
@@ -28,14 +30,14 @@ def solve(c1, c2, c3):
                     continue
                 e = i0
                 if i1 > 0:
-                    e += i1 * (1.0 + dp[(i1 - 1) * L2 + i2 * L + i3])
+                    e += i1 * (1.0 + dp[i1 - 1, i2, i3])
                 if i2 > 0 and i1 <= K:
-                    e += i2 * (1.0 + dp[(i1 + 1) * L2 + (i2 - 1) * L + i3])
+                    e += i2 * (1.0 + dp[i1 + 1, i2 - 1, i3])
                 if i3 > 0 and i2 + 1 <= c2 + c3:
-                    e += i3 * (1.0 + dp[i1 * L2 + (i2 + 1) * L + i3 - 1])
-                dp[i1 * L2 + i2 * L + i3] = e / isum
-    return dp[c1 * L2 + c2 * L + c3]
+                    e += i3 * (1.0 + dp[i1, i2 + 1, i3 - 1])
+                dp[i1, i2, i3] = e / isum
+    return dp[c1, c2, c3]
 
 
 if __name__ == "__main__":
-    print(solve(ac[1], ac[2], ac[3]))
+    print(solve(C1, C2, C3))
