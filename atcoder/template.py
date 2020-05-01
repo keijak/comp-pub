8125524b695c348115122e37522404a9b6486237
Keiji Maekawa <m@keijak.com>
2020-05-01 09:26:38 +0200

    Add atcoder tools python template.

---
+++ atcoder/template.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+import sys
+
+sys.setrecursionlimit(10 ** 8)
+
+def iterate_tokens():
+    for line in sys.stdin.buffer:
+        for word in line.split():
+            yield word
+tokens = iterate_tokens()
+${input_part}
+
+
+def solve(${formal_arguments}):
+    pass
+
+
+def main():
+    print(solve(${actual_arguments}))
+
+
+if __name__ == '__main__':
+    main()
