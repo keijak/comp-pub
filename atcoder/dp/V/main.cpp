c5c7c9516dffea64543d26c3df5fc3cc6497f285
Keiji Maekawa <m@keijak.com>
2020-05-04 10:19:30 +0200

    dp

--- atcoder/dp/V/main.cpp
+++ atcoder/dp/V/main.cpp
@@ -1,28 +1,45 @@
-/* Time: $%Y%$-$%M%$-$%D%$ $%h%$:$%m%$:$%s%$ */
-
 #include <bits/stdc++.h>
 #define REP(i, n) for (decltype(n) i = 0; i < (n); ++i)
 using namespace std;
 using int64 = long long;
 
-
-
-void solve(long long N, long long M, std::vector<long long> x, std::vector<long long> y){
-
+void solve(int64 N, long long M, std::vector<vector<int>> adj) {
+  unordered_map<int64, int64> sub;
+  auto dfs = [&](auto rec, int node, int parent) -> int64 {
+    if (parent >= 0) {
+      auto it = sub.find(static_cast<int64>(node) * N + parent);
+      if (it != sub.end()) {
+        return it->second;
+      }
+    }
+    int64 r = 1;
+    for (int child : adj[node]) {
+      if (child == parent) continue;
+      r = r * (1 + rec(rec, child, node)) % M;
+    }
+    if (parent >= 0) {
+      sub[static_cast<int64>(node) * N + parent] = r;
+    }
+    return r;
+  };
+  for (int i = 0; i < N; ++i) {
+    cout << dfs(dfs, i, -1) << "\n";
+  }
 }
 
 int main() {
   ios::sync_with_stdio(false);
   cin.tie(0);
-  long long N;
-  scanf("%lld",&N);
-  long long M;
-  scanf("%lld",&M);
-  std::vector<long long> x(N-1);
-  std::vector<long long> y(N-1);
-  for(int i = 0 ; i < N-1 ; i++){
-    scanf("%lld",&x[i]);
-    scanf("%lld",&y[i]);
+  long long N, M;
+  cin >> N >> M;
+  vector<vector<int>> adj(N);
+  for (int i = 0; i < N - 1; i++) {
+    int x, y;
+    cin >> x >> y;
+    x--;
+    y--;
+    adj[x].push_back(y);
+    adj[y].push_back(x);
   }
-  solve(N, M, std::move(x), std::move(y));
+  solve(N, M, std::move(adj));
 }
