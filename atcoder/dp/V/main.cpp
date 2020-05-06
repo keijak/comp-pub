80fcf4ae383dbedd6b8b71df830390d588141a6e
Keiji Maekawa <m@keijak.com>
2020-05-06 06:45:02 +0200

    dp

--- atcoder/dp/V/main.cpp
+++ atcoder/dp/V/main.cpp
@@ -5,26 +5,55 @@ using Int = long long;
 Int N, M;
 vector<vector<int>> adj;
 vector<unordered_map<int, Int>> sub;
-
-Int dfs(int node, int parent) {
-  if (parent >= 0) {
-    auto it = sub[parent].find(node);
-    if (it != sub[parent].end()) return it->second;
-  }
-  Int r = 1;
-  for (int child : adj[node]) {
-    if (child == parent) continue;
-    r = r * (1 + dfs(child, node)) % M;
-  }
-  if (parent >= 0) {
-    sub[parent][node] = r;
-  }
-  return r;
-};
+vector<vector<Int>> lprod, rprod;
 
 void solve() {
+  vector<Int> ans(N);
+  auto dfs = [&](auto rec, int node, int parent) {
+    if (parent >= 0) {
+      auto it = sub[parent].find(node);
+      if (it != sub[parent].end()) return it->second;
+    }
+    Int r = 1;
+    vector<Int> cs(adj[node].size(), 1);
+    REP(i, adj[node].size()) {
+      int child = adj[node][i];
+      if (child == parent) {
+        cs[i] = 1;
+        continue;
+      }
+      Int c = 1 + rec(rec, child, node);
+      cs[i] = c;
+      r = r * c % M;
+    }
+    lprod[node].resize(cs.size() + 1, 1);
+    rprod[node].resize(cs.size() + 1, 1);
+    REP(i, cs.size()) {
+      lprod[node][i + 1] = lprod[node][i] * cs[i] % M;
+      rprod[node][cs.size() - 1 - i] =
+          rprod[node][cs.size() - i] * cs[cs.size() - 1 - i] % M;
+    }
+    if (parent >= 0) {
+      sub[parent][node] = r;
+    }
+    return r;
+  };
+  ans[0] = dfs(dfs, 0, -1);
+
+  auto dfs2 = [&](auto rec, int node, int parent, Int acc) -> void {
+    REP(i, adj[node].size()) {
+      int child = adj[node][i];
+      if (child == parent) continue;
+      ans[child] = sub[node][child];
+      Int q = lprod[node][i] * rprod[node][i + 1] % M * (1 + acc) % M;
+      ans[child] += sub[node][child] * q % M;
+      ans[child] %= M;
+      rec(rec, child, node, q);
+    }
+  };
+  dfs2(dfs2, 0, -1, 0);
   for (int i = 0; i < N; ++i) {
-    cout << dfs(i, -1) << "\n";
+    cout << ans[i] << "\n";
   }
 }
 
@@ -34,6 +63,8 @@ int main() {
   cin >> N >> M;
   adj.resize(N);
   sub.resize(N);
+  lprod.resize(N);
+  rprod.resize(N);
   for (int i = 0; i < N - 1; i++) {
     int x, y;
     cin >> x >> y;
