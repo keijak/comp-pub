dd9ae0391280dcba38c6f14a9a611156227bda8a
Keiji Maekawa <m@keijak.com>
2020-05-07 09:11:36 +0200

    dp

--- atcoder/dp/O/main.cpp
+++ atcoder/dp/O/main.cpp
@@ -1,27 +1,48 @@
-/* Time: $%Y%$-$%M%$-$%D%$ $%h%$:$%m%$:$%s%$ */
-
 #include <bits/stdc++.h>
 #define REP(i, n) for (decltype(n) i = 0; i < (n); ++i)
 using namespace std;
 using int64 = long long;
 
-
 const long long MOD = 1000000007;
 
-void solve(long long N, std::vector<std::vector<long long>> a){
-
+void solve(long long N, std::vector<std::vector<int>> a) {
+  vector<unordered_map<int, int64>> dp(N);
+  for (auto i : a[0]) {
+    dp[0][1 << i] = 1;
+  }
+  for (int i = 1; i < N; ++i) {
+    for (auto&& stcnt : dp[i - 1]) {
+      int state = stcnt.first;
+      int64 cnt = stcnt.second;
+      for (auto j : a[i]) {
+        int sj = 1 << j;
+        if (state & sj) continue;
+        dp[i][state | sj] += cnt;
+        dp[i][state | sj] %= MOD;
+      }
+    }
+  }
+  int64 ans = 0;
+  for (auto&& stcnt : dp[N - 1]) {
+    ans = (ans + stcnt.second) % MOD;
+  }
+  cout << ans << endl;
 }
 
 int main() {
-  ios::sync_with_stdio(false);
-  cin.tie(0);
-  long long N;
-  scanf("%lld",&N);
-  std::vector<std::vector<long long>> a(N, std::vector<long long>(N));
-  for(int i = 0 ; i < N ; i++){
-    for(int j = 0 ; j < N ; j++){
-      scanf("%lld",&a[i][j]);
+  int64 N;
+  cin >> N;
+  std::vector<std::vector<int>> a(N);
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < N; j++) {
+      int x;
+      cin >> x;
+      if (x) {
+        a[i].push_back(j);
+      }
     }
   }
+  sort(a.begin(), a.end(),
+       [](auto& v1, auto& v2) -> bool { return v1.size() < v2.size(); });
   solve(N, std::move(a));
 }
