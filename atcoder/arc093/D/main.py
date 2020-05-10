5c3d4cbcd45ab02293413fcb65416ceb834c7006
Keiji Maekawa <m@keijak.com>
2020-05-10 11:21:08 +0200

    arc093

--- atcoder/arc093/D/main.py
+++ atcoder/arc093/D/main.py
@@ -7,58 +7,35 @@ readline = sys.stdin.buffer.readline
 readlines = sys.stdin.buffer.readlines
 
 A, B = map(int, readline().split())
-color_flipped = False
-if A < B:
-    A, B = B, A
-    color_flipped = True
 
 
 def solve():
-    hb = min(B, 12)
-    h = (hb * 2 + 1) * 3
-    w = 100
-    # If not flipped, False -> White (A), True -> Black (B)
-    grid = [[True] * w for _ in range(h)]
-    black = 1
-    white = 0
-    for i in range(hb):
-        if black == B:
-            break
-        black += 1
-        white += 1
-        for c in range(w):
-            for j in range(3):
-                grid[6 * i + j + 3][c] = False
+    W = 100
+    hw = B // 25 + 2
+    hb = A // 25 + 2
+    H = hw + hb
+    grid = [[i >= hw] * W for i in range(H)]  # true => black
 
-    def paint_white(i):
-        nonlocal white
-        for c in range(0, w, 2):
-            if white == A:
-                return
-            grid[6 * i + 1][c] = False
-            white += 1
+    def paint_black():
+        black = 1
+        for r in range(0, hw - 1, 2):
+            for c in range(0, W, 2):
+                if black == B:
+                    return
+                grid[r][c] = True
+                black += 1
 
-    def paint_black(i):
-        nonlocal black
-        for c in range(0, w, 2):
-            if black == B:
-                return
-            grid[6 * i + 4][c] = True
-            black += 1
+    def paint_white():
+        white = 1
+        for r in range(hw + 1, H, 2):
+            for c in range(0, W, 2):
+                if white == A:
+                    return
+                grid[r][c] = False
+                white += 1
 
-    for i in range(hb):
-        paint_white(i)
-        paint_black(i)
-    assert black == B
-    while white < A:
-        grid.append([True] * w)
-        grid.append([True] * w)
-        for c in range(0, w, 2):
-            if white == A:
-                break
-            white += 1
-            grid[-1][c] = False
-
+    paint_black()
+    paint_white()
     return grid
 
 
@@ -67,7 +44,7 @@ def print_grid(grid):
     print(h, w)
     for r in range(h):
         for c in range(w):
-            if grid[r][c] ^ color_flipped:
+            if grid[r][c]:
                 print("#", end="")
             else:
                 print(".", end="")
