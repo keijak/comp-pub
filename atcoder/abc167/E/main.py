44ddfa24adc947f8de2dc2400b9f1a615c8f2bcf
Keiji Maekawa <m@keijak.com>
2020-05-11 14:43:52 +0200

    combmod

--- atcoder/abc167/E/main.py
+++ atcoder/abc167/E/main.py
@@ -8,35 +8,36 @@ readlines = sys.stdin.buffer.readlines
 N, M, K = map(int, readline().split())
 MOD = 998244353
 
-# Combination under a prime modulo: nCk % p
-def combmod(n, k, p=MOD):
+# Combination under a modulo: nCk % mod
+# Modulo must be a prime number.
+# Intermediate numerators and denominators are cached and reused.
+def combmod(n, k, p=MOD, numden=[(1, 1)]):
     if k > n:
         return 0
     if k > n - k:
         k = n - k
-    num = den = 1
-    for i in range(k):
-        num = num * (n - i) % p
-        den = den * (i + 1) % p
-    # By Fermat's little theorem,
-    # a^(p-1) ≡ 1 mod p
+    # Reuse precalculated numerators and denominators.
+    if k < len(numden):
+        num, den = numden[k]
+    else:
+        # Resume from len(numden).
+        num, den = numden[-1]
+        for i in range(len(numden) - 1, k):
+            num = num * (n - i) % p
+            den = den * (i + 1) % p
+            numden.append((num, den))
+    # a^(p-1) ≡ 1 mod p (by Fermat's little theorem)
     # i.e. a^(p-2) ≡ a^(-1) mod p
     inv_dev = pow(den, p - 2, p)
     return num * inv_dev % p
 
 
 def solve():
-    m1pows = [1 for i in range(K + 1)]
-    m1pows[0] = M * pow(M - 1, N - 1 - K, MOD) % MOD
-    for i in range(1, K + 1):
-        m1pows[i] = m1pows[i - 1] * (M - 1) % MOD
-    cs = 1
     ans = 0
     for i in range(K + 1):
-        ans += cs * m1pows[K - i]
+        cs = combmod(N - 1, i)
+        ans += cs * M % MOD * pow(M - 1, N - 1 - i, MOD) % MOD
         ans %= MOD
-        cs *= (N - 1 - i) * pow(i + 1, MOD - 2, MOD)
-        cs %= MOD
     return ans
 
 
--- lib/combmod.py
+++ lib/combmod.py
@@ -1,15 +1,24 @@
-# Combination under a prime modulo: nCk % p
-def combmod(n, k, p):
+# MOD = 998244353
+
+# Combination under a modulo: nCk % p
+# Modulo must be a prime number.
+# Intermediate numerators and denominators are cached and reused.
+def combmod(n, k, p=MOD, numden=[(1, 1)]):
     if k > n:
         return 0
     if k > n - k:
         k = n - k
-    num, den = 1, 1
-    for i in range(k):
-        num = num * (n - i) % p
-        den = den * (i + 1) % p
-    # By Fermat's little theorem,
-    # a^(p-1) ≡ 1 mod p
+    # Reuse precalculated numerators and denominators.
+    if k < len(numden):
+        num, den = numden[k]
+    else:
+        # Resume from len(numden).
+        num, den = numden[-1]
+        for i in range(len(numden) - 1, k):
+            num = num * (n - i) % p
+            den = den * (i + 1) % p
+            numden.append((num, den))
+    # a^(p-1) ≡ 1 mod p (by Fermat's little theorem)
     # i.e. a^(p-2) ≡ a^(-1) mod p
     inv_dev = pow(den, p - 2, p)
     return num * inv_dev % p
