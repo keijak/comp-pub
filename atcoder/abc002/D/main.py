cc691ccacbe883918c907aea121cb988498e06f7
Keiji Maekawa <m@keijak.com>
2020-05-19 09:07:37 +0200

    abc002

---
+++ atcoder/abc002/D/main.py
@@ -0,0 +1,38 @@
+import sys
+
+sys.setrecursionlimit(10 ** 8)
+ni = lambda: int(sys.stdin.readline())
+nm = lambda: map(int, sys.stdin.readline().split())
+nl = lambda: list(nm())
+ns = lambda: sys.stdin.readline().rstrip()
+
+N, M = nm()
+adj = [[False] * N for _ in range(N)]
+for i in range(M):
+    x, y = nm()
+    x -= 1
+    y -= 1
+    adj[x][y] = adj[y][x] = True
+
+
+def solve():
+    L = 1 << N
+    maxsize = 0
+    for s in range(L):
+        group = []
+        ok = True
+        for j in range(N):
+            if (s >> j) & 1:
+                if any(not adj[z][j] for z in group):
+                    ok = False
+                    break
+                group.append(j)
+        if ok:
+            if maxsize < len(group):
+                maxsize = len(group)
+                # print(f">> maxsize={maxsize}, group={group}")
+    return maxsize
+
+
+if __name__ == "__main__":
+    print(solve())
