0fa7b56604ab8d2e5b5b6eac408229153786cf41
Keiji Maekawa <m@keijak.com>
2020-05-21 11:02:43 +0200

    template

--- cf_template/io.cpp
+++ cf_template/io.cpp
@@ -1,8 +1,8 @@
 #include <bits/stdc++.h>
-#define REP(i, n) for (int i = 0; i < static_cast<int>(n); ++i)
 using namespace std;
 using i64 = long long;
 using u64 = unsigned long long;
+#define REP(i, n) for (int i = 0; (i64)(i) < (i64)(n); ++i)
 
 int main() {
   ios::sync_with_stdio(false);
