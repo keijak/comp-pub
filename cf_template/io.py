50a976bccec1ca2ef139b674b57ce9e732e24948
Keiji Maekawa <m@keijak.com>
2020-06-02 12:48:59 +0200

    cf template

--- cf_template/io.py
+++ cf_template/io.py
@@ -1,10 +1,10 @@
 import sys
 
 sys.setrecursionlimit(10 ** 8)
-ni = lambda: int(sys.stdin.readline())
-nm = lambda: map(int, sys.stdin.readline().split())
-nl = lambda: list(nm())
-ns = lambda: sys.stdin.readline().rstrip()
+ini = lambda: int(sys.stdin.readline())
+inm = lambda: map(int, sys.stdin.readline().split())
+inl = lambda: list(inm())
+ins = lambda: sys.stdin.readline().rstrip()
 
 
 def solve():
