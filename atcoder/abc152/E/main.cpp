0945ec37afde4ad65d430fbe4d6cbd5d953a30db
Keiji Maekawa <m@keijak.com>
2020-06-25 09:39:16 +0200

    factorize

---
+++ atcoder/abc152/E/main.cpp
@@ -0,0 +1,110 @@
+#define ENABLE_DEBUG 1
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+using u64 = unsigned long long;
+#define REP(i, n) for (int i = 0; (i64)(i) < (i64)(n); ++i)
+
+#if ENABLE_DEBUG
+template <typename T>
+void debug(T value) {
+  std::cerr << value;
+}
+template <typename T, typename... Ts>
+void debug(T value, Ts... args) {
+  std::cerr << value << ", ";
+  debug(args...);
+}
+#define DEBUG(...)                \
+  do {                            \
+    cerr << " \033[33m (L";       \
+    cerr << __LINE__ << ") ";     \
+    cerr << #__VA_ARGS__ << ": "; \
+    cerr << "\033[0m";            \
+    debug(__VA_ARGS__);           \
+    cerr << endl;                 \
+  } while (0)
+#else
+#define debug(...)
+#define DEBUG(...)
+#endif
+const i64 MOD = 1'000'000'007;
+
+struct mint {
+  long long x;
+  mint(long long x = 0) : x((x % MOD + MOD) % MOD) {}
+  mint operator-() const { return mint(-x); }
+  mint& operator+=(const mint a) {
+    if ((x += a.x) >= MOD) x -= MOD;
+    return *this;
+  }
+  mint& operator-=(const mint a) {
+    if ((x += MOD - a.x) >= MOD) x -= MOD;
+    return *this;
+  }
+  mint& operator*=(const mint a) {
+    (x *= a.x) %= MOD;
+    return *this;
+  }
+  mint operator+(const mint a) const { return mint(*this) += a; }
+  mint operator-(const mint a) const { return mint(*this) -= a; }
+  mint operator*(const mint a) const { return mint(*this) *= a; }
+  mint pow(long long t) const {
+    if (!t) return 1;
+    mint a = pow(t >> 1);
+    a *= a;
+    if (t & 1) a *= *this;
+    return a;
+  }
+
+  // for prime MOD
+  mint inv() const { return pow(MOD - 2); }
+  mint& operator/=(const mint a) { return *this *= a.inv(); }
+  mint operator/(const mint a) const { return mint(*this) /= a; }
+};
+istream& operator>>(istream& is, mint& a) { return is >> a.x; }
+ostream& operator<<(ostream& os, const mint& a) { return os << a.x; }
+
+vector<pair<i64, int>> factorize(i64 n) {
+  vector<pair<i64, int>> res;
+  for (i64 k = 2; k * k <= n; ++k) {
+    int cnt = 0;
+    while (n % k == 0) {
+      n /= k;
+      ++cnt;
+    }
+    if (cnt > 0) {
+      res.emplace_back(k, cnt);
+    }
+  }
+  if (n > 1) {
+    res.emplace_back(n, 1);
+  }
+  return res;
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int N;
+  cin >> N;
+  vector<i64> A(N);
+  for (auto& x : A) cin >> x;
+  map<i64, int> fs;
+  auto merge_factors = [&](const vector<pair<i64, int>>& fx) -> void {
+    for (auto [p, c] : fx) {
+      fs[p] = max(fs[p], c);
+    }
+  };
+  REP(i, N) { merge_factors(factorize(A[i])); }
+  mint allp = 1;
+  for (auto [p, c] : fs) {
+    allp *= mint(p).pow(c);
+  }
+  mint ans = 0;
+  REP(i, N) {
+    mint b = allp * mint(A[i]).inv();
+    ans += b;
+  }
+  cout << ans << endl;
+}
