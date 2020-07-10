17f97c096d0621a4ac0cdf3772f7e10d656a90fe
Keiji Maekawa <m@keijak.com>
2020-07-10 16:18:29 +0200

    matrix

--- lib/matrix.cpp
+++ lib/matrix.cpp
@@ -1,43 +1,112 @@
-// n * n Matrix
-using MatrixRow = vector<long long>;  // vector<mint>
-using Matrix = vector<MatrixRow>;
+template <class T>
+struct Matrix {
+  using Row = vector<T>;
+  vector<Row> A;
 
-Matrix matmul(const Matrix& a, const Matrix& b) {
-  int n = a.size();
-  //   assert(int(a[0].size()) == n);
-  //   assert(int(b.size()) == n);
-  //   assert(int(b[0].size()) == n);
+  Matrix() {}
 
-  Matrix ret(n, MatrixRow(n));
-  for (int i = 0; i < n; ++i) {
-    for (int j = 0; j < n; ++j) {
-      for (int k = 0; k < n; ++k) {
-        ret[i][j] += a[i][k] * b[k][j];
+  Matrix(int n, int m) : A(n, Row(m, 0)) {}
+
+  Matrix(int n) : A(n, Row(n, 0)){};
+
+  int height() const { return (int)(A.size()); }
+
+  int width() const { return (int)(A[0].size()); }
+
+  inline const Row &operator[](int k) const { return (A.at(k)); }
+
+  inline Row &operator[](int k) { return (A.at(k)); }
+
+  static Matrix I(int n) {
+    Matrix mat(n);
+    for (int i = 0; i < n; i++) mat[i][i] = 1;
+    return (mat);
+  }
+
+  Matrix &operator+=(const Matrix &B) {
+    size_t n = height(), m = width();
+    assert(n == B.height() && m == B.width());
+    for (int i = 0; i < n; i++)
+      for (int j = 0; j < m; j++) (*this)[i][j] += B[i][j];
+    return (*this);
+  }
+
+  Matrix &operator-=(const Matrix &B) {
+    size_t n = height(), m = width();
+    assert(n == B.height() && m == B.width());
+    for (int i = 0; i < n; i++)
+      for (int j = 0; j < m; j++) (*this)[i][j] -= B[i][j];
+    return (*this);
+  }
+
+  Matrix &operator*=(const Matrix &B) {
+    int n = height(), m = B.width(), p = width();
+    assert(p == B.height());
+    vector<Row> C(n, Row(m, 0));
+    for (int i = 0; i < n; i++)
+      for (int j = 0; j < m; j++)
+        for (int k = 0; k < p; k++)
+          C[i][j] = (C[i][j] + (*this)[i][k] * B[k][j]);
+    A.swap(C);
+    return (*this);
+  }
+
+  Matrix &operator^=(long long k) {
+    Matrix B = Matrix::I(height());
+    while (k > 0) {
+      if (k & 1) B *= *this;
+      *this *= *this;
+      k >>= 1LL;
+    }
+    A.swap(B.A);
+    return (*this);
+  }
+
+  Matrix operator+(const Matrix &B) const { return (Matrix(*this) += B); }
+
+  Matrix operator-(const Matrix &B) const { return (Matrix(*this) -= B); }
+
+  Matrix operator*(const Matrix &B) const { return (Matrix(*this) *= B); }
+
+  Matrix operator^(const long long k) const { return (Matrix(*this) ^= k); }
+
+  friend ostream &operator<<(ostream &os, Matrix &p) {
+    size_t n = p.height(), m = p.width();
+    for (int i = 0; i < n; i++) {
+      os << "[";
+      for (int j = 0; j < m; j++) {
+        os << p[i][j] << (j + 1 == m ? "]\n" : ",");
       }
     }
+    return (os);
   }
-  return ret;
-}
 
-Matrix matpow(const Matrix& a, long long k) {
-  int n = a.size();
-  //   assert(int(a[0].size()) == n);
-
-  if (k == 0) {
-    Matrix eye(n, MatrixRow(n));
-    for (int i = 0; i < n; ++i) {
-      eye[i][i] = 1;
+  T det() {
+    Matrix B(*this);
+    assert(width() == height());
+    T ret = 1;
+    for (int i = 0; i < width(); i++) {
+      int idx = -1;
+      for (int j = i; j < width(); j++) {
+        if (B[j][i] != 0) idx = j;
+      }
+      if (idx == -1) return (0);
+      if (i != idx) {
+        ret *= -1;
+        swap(B[i], B[idx]);
+      }
+      ret *= B[i][i];
+      T vv = B[i][i];
+      for (int j = 0; j < width(); j++) {
+        B[i][j] /= vv;
+      }
+      for (int j = i + 1; j < width(); j++) {
+        T a = B[j][i];
+        for (int k = 0; k < width(); k++) {
+          B[j][k] -= B[i][k] * a;
+        }
+      }
     }
-    return eye;
+    return (ret);
   }
-
-  if (k == 1) {
-    return a;
-  }
-
-  if (k & 1) {
-    return matmul(matpow(a, k - 1), a);
-  } else {
-    return matpow(matmul(a, a), k / 2);
-  }
-}
+};
