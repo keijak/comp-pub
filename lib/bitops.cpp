79bcd2b9edf69063a88eb5aaa5e141b204767fcb
Keiji Maekawa <m@keijak.com>
2020-07-16 22:15:55 +0200

    bit operations

--- lib/bitops.cpp
+++ lib/bitops.cpp
@@ -1,3 +1,6 @@
+// Turn off all bits of x except the rightmost one.
+int rightmost_bit(int x) { return x & -x; }
+
 // GCC Builtins.
 
 int popcount(unsigned int x) { return __builtin_popcount(x); }
@@ -11,7 +14,3 @@ int count_leading_zero(unsigned long long x) { return __builtin_clzll(x); }
 int count_trailing_zero(unsigned int x) { return __builtin_ctz(x); }
 int count_trailing_zero(unsigned long x) { return __builtin_ctzl(x); }
 int count_trailing_zero(unsigned long long x) { return __builtin_ctzll(x); }
-
-// Common Tricks
-
-int rightmost_bit(int x) { return x & -x; }
