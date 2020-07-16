e29575cbd31ac63417860cd72449a782ed82bb91
Keiji Maekawa <m@keijak.com>
2020-07-16 22:14:26 +0200

    bit operations

---
+++ lib/bitops.cpp
@@ -0,0 +1,17 @@
+// GCC Builtins.
+
+int popcount(unsigned int x) { return __builtin_popcount(x); }
+int popcount(unsigned long x) { return __builtin_popcountl(x); }
+int popcount(unsigned long long x) { return __builtin_popcountll(x); }
+
+int count_leading_zero(unsigned int x) { return __builtin_clz(x); }
+int count_leading_zero(unsigned long x) { return __builtin_clzl(x); }
+int count_leading_zero(unsigned long long x) { return __builtin_clzll(x); }
+
+int count_trailing_zero(unsigned int x) { return __builtin_ctz(x); }
+int count_trailing_zero(unsigned long x) { return __builtin_ctzl(x); }
+int count_trailing_zero(unsigned long long x) { return __builtin_ctzll(x); }
+
+// Common Tricks
+
+int rightmost_bit(int x) { return x & -x; }
