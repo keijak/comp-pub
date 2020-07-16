a582761448cc36519bb0134f925563dc9d06c70e
Keiji Maekawa <m@keijak.com>
2020-07-16 23:20:33 +0200

    ad hoc

---
+++ atcoder/agc002/C/main.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+using u64 = unsigned long long;
+#define REP(i, n) for (int i = 0; (i64)(i) < (i64)(n); ++i)
+
+#ifdef ENABLE_DEBUG
+template <typename T>
+void debug(T value) {
+  cerr << value;
+}
+template <typename T, typename... Ts>
+void debug(T value, Ts... args) {
+  cerr << value << ", ";
+  debug(args...);
+}
+#define DEBUG(...)                              \
+  do {                                          \
+    cerr << " \033[33m (L" << __LINE__ << ") "; \
+    cerr << #__VA_ARGS__ << ":\033[0m ";        \
+    debug(__VA_ARGS__);                         \
+    cerr << endl;                               \
+  } while (0)
+#else
+#define debug(...)
+#define DEBUG(...)
+#endif
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  i64 N, L;
+  cin >> N >> L;
+  vector<i64> A(N);
+  for (auto& x : A) cin >> x;
+  int p = -1;
+  for (int i = 0; i < N - 1; ++i) {
+    if (A[i] + A[i + 1] >= L) {
+      p = i;
+      break;
+    }
+  }
+  if (p == -1) {
+    cout << "Impossible\n";
+    return 0;
+  }
+  cout << "Possible\n";
+  for (int i = 0; i < p; ++i) {
+    cout << (i + 1) << "\n";
+  }
+  for (int i = N - 2; i >= p; --i) {
+    cout << (i + 1) << "\n";
+  }
+}
