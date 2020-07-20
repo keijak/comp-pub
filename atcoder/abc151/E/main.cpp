c84599a47c8f70f26cf41870da988906cad4553b
Keiji Maekawa <m@keijak.com>
2020-07-20 22:34:43 +0200

    counting

---
+++ atcoder/abc151/E/main.cpp
@@ -0,0 +1,110 @@
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+using u64 = unsigned long long;
+#define REP(i, n) for (int i = 0; (i64)(i) < (i64)(n); ++i)
+
+#ifdef ENABLE_DEBUG
+template <typename T>
+void debug(T value) {
+  cerr << value;
+}
+template <typename T, typename... Ts>
+void debug(T value, Ts... args) {
+  cerr << value << ", ";
+  debug(args...);
+}
+#define DEBUG(...)                              \
+  do {                                          \
+    cerr << " \033[33m (L" << __LINE__ << ") "; \
+    cerr << #__VA_ARGS__ << ":\033[0m ";        \
+    debug(__VA_ARGS__);                         \
+    cerr << endl;                               \
+  } while (0)
+#else
+#define debug(...)
+#define DEBUG(...)
+#endif
+
+const i64 MOD = 1'000'000'007;
+
+struct mint {
+  long long x;
+  mint(long long x = 0) : x((x % MOD + MOD) % MOD) {}
+  mint operator-() const { return mint(-x); }
+  mint& operator+=(const mint a) {
+    if ((x += a.x) >= MOD) x -= MOD;
+    return *this;
+  }
+  mint& operator-=(const mint a) {
+    if ((x += MOD - a.x) >= MOD) x -= MOD;
+    return *this;
+  }
+  mint& operator*=(const mint a) {
+    (x *= a.x) %= MOD;
+    return *this;
+  }
+  mint operator+(const mint a) const { return mint(*this) += a; }
+  mint operator-(const mint a) const { return mint(*this) -= a; }
+  mint operator*(const mint a) const { return mint(*this) *= a; }
+  mint pow(long long t) const {
+    if (!t) return 1;
+    mint a = pow(t >> 1);
+    a *= a;
+    if (t & 1) a *= *this;
+    return a;
+  }
+
+  // for prime MOD
+  mint inv() const { return pow(MOD - 2); }
+  mint& operator/=(const mint a) { return *this *= a.inv(); }
+  mint operator/(const mint a) const { return mint(*this) /= a; }
+};
+istream& operator>>(istream& is, mint& a) { return is >> a.x; }
+ostream& operator<<(ostream& os, const mint& a) { return os << a.x; }
+
+// Factorials over ModInts.
+struct Factorials {
+  // factorials and inverse factorials.
+  vector<mint> fact, ifact;
+
+  // n: max cached value.
+  Factorials(int n) : fact(n + 1), ifact(n + 1) {
+    assert(n < MOD);
+    fact[0] = 1;
+    for (int i = 1; i <= n; ++i) fact[i] = fact[i - 1] * i;
+    ifact[n] = fact[n].inv();
+    for (int i = n; i >= 1; --i) ifact[i - 1] = ifact[i] * i;
+  }
+
+  // Combination (nCk)
+  mint C(int n, int k) {
+    if (k < 0 || k > n) return 0;
+    return fact[n] * ifact[k] * ifact[n - k];
+  }
+
+  // Permutation (nPk)
+  mint P(int n, int k) {
+    if (k < 0 || k > n) return 0;
+    return fact[n] * ifact[n - k];
+  }
+};
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int n, k;
+  cin >> n >> k;
+  vector<i64> a(n);
+  for (auto& x : a) cin >> x;
+  sort(a.begin(), a.end());
+  mint ans = 0;
+  mint cx = 0;
+  Factorials fs(n);
+  REP(i, n - k + 1) {
+    cx += fs.C(k - 2 + i, i);
+    ans += cx * a[k - 1 + i];
+    ans -= cx * a[n - k - i];
+  }
+  cout << ans << endl;
+}
