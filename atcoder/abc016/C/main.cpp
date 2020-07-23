da1fe199e7b59936f608949195fe54a191ea5a45
Keiji Maekawa <m@keijak.com>
2020-07-23 21:03:14 +0200

    dfs

---
+++ atcoder/abc016/C/main.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+using u64 = unsigned long long;
+#define REP(i, n) for (int i = 0; (i64)(i) < (i64)(n); ++i)
+
+#ifdef ENABLE_DEBUG
+template <typename T>
+void debug(T value) {
+  cerr << value;
+}
+template <typename T, typename... Ts>
+void debug(T value, Ts... args) {
+  cerr << value << ", ";
+  debug(args...);
+}
+#define DEBUG(...)                              \
+  do {                                          \
+    cerr << " \033[33m (L" << __LINE__ << ") "; \
+    cerr << #__VA_ARGS__ << ":\033[0m ";        \
+    debug(__VA_ARGS__);                         \
+    cerr << endl;                               \
+  } while (0)
+#else
+#define debug(...)
+#define DEBUG(...)
+#endif
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int n, m;
+  cin >> n >> m;
+  vector<vector<int>> g(n);
+  REP(i, m) {
+    int a, b;
+    cin >> a >> b;
+    a--;
+    b--;
+    g[a].push_back(b);
+    g[b].push_back(a);
+  }
+  auto ff = [&](int v) -> int {
+    set<int> f1(g[v].begin(), g[v].end());
+    f1.insert(v);
+    set<int> f2;
+    for (auto u : g[v]) {
+      for (auto w : g[u]) {
+        if (f1.find(w) == f1.end()) {
+          f2.insert(w);
+        }
+      }
+    }
+    return f2.size();
+  };
+  REP(i, n) { cout << ff(i) << '\n'; }
+}
