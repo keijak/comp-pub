e5419aa617e8af6668bb22fde5978b7e07304045
Keiji Maekawa <m@keijak.com>
2020-07-29 08:25:58 +0200

    edpc

--- atcoder/dp/R/main.cpp
+++ atcoder/dp/R/main.cpp
@@ -1,29 +1,196 @@
-/* Time: $%Y%$-$%M%$-$%D%$ $%h%$:$%m%$:$%s%$ */
-
 #include <bits/stdc++.h>
-#define REP(i, n) for (decltype(n) i = 0; i < (n); ++i)
 using namespace std;
-using int64 = long long;
+using i64 = long long;
+using u64 = unsigned long long;
+#define REP(i, n) for (int i = 0; (i64)(i) < (i64)(n); ++i)
 
-
-const long long MOD = 1000000007;
-
-void solve(long long N, long long K, std::vector<std::vector<long long>> a){
-
+#ifdef ENABLE_DEBUG
+template <typename T>
+void debug(T value) {
+  cerr << value;
 }
+template <typename T, typename... Ts>
+void debug(T value, Ts... args) {
+  cerr << value << ", ";
+  debug(args...);
+}
+#define DEBUG(...)                              \
+  do {                                          \
+    cerr << " \033[33m (L" << __LINE__ << ") "; \
+    cerr << #__VA_ARGS__ << ":\033[0m ";        \
+    debug(__VA_ARGS__);                         \
+    cerr << endl;                               \
+  } while (0)
+#else
+#define debug(...)
+#define DEBUG(...)
+#endif
+
+const i64 MOD = 1'000'000'007;
+
+struct mint {
+  long long x;
+  mint(long long x = 0) : x((x % MOD + MOD) % MOD) {}
+  mint operator-() const { return mint(-x); }
+  mint &operator+=(const mint a) {
+    if ((x += a.x) >= MOD) x -= MOD;
+    return *this;
+  }
+  mint &operator-=(const mint a) {
+    if ((x += MOD - a.x) >= MOD) x -= MOD;
+    return *this;
+  }
+  mint &operator*=(const mint a) {
+    (x *= a.x) %= MOD;
+    return *this;
+  }
+  mint operator+(const mint a) const { return mint(*this) += a; }
+  mint operator-(const mint a) const { return mint(*this) -= a; }
+  mint operator*(const mint a) const { return mint(*this) *= a; }
+  mint pow(long long t) const {
+    if (!t) return 1;
+    mint a = pow(t >> 1);
+    a *= a;
+    if (t & 1) a *= *this;
+    return a;
+  }
+
+  // for prime MOD
+  mint inv() const { return pow(MOD - 2); }
+  mint &operator/=(const mint a) { return *this *= a.inv(); }
+  mint operator/(const mint a) const { return mint(*this) /= a; }
+};
+istream &operator>>(istream &is, mint &a) { return is >> a.x; }
+ostream &operator<<(ostream &os, const mint &a) { return os << a.x; }
+
+template <class T>
+struct Matrix {
+  using Row = vector<T>;
+  vector<Row> A;
+
+  Matrix() {}
+
+  Matrix(int n, int m) : A(n, Row(m, 0)) {}
+
+  Matrix(int n) : A(n, Row(n, 0)){};
+
+  int height() const { return (int)(A.size()); }
+
+  int width() const { return (int)(A[0].size()); }
+
+  inline const Row &operator[](int k) const { return (A.at(k)); }
+
+  inline Row &operator[](int k) { return (A.at(k)); }
+
+  static Matrix I(int n) {
+    Matrix mat(n);
+    for (int i = 0; i < n; i++) mat[i][i] = 1;
+    return (mat);
+  }
+
+  Matrix &operator+=(const Matrix &B) {
+    size_t n = height(), m = width();
+    assert(n == B.height() && m == B.width());
+    for (int i = 0; i < n; i++)
+      for (int j = 0; j < m; j++) (*this)[i][j] += B[i][j];
+    return (*this);
+  }
+
+  Matrix &operator-=(const Matrix &B) {
+    size_t n = height(), m = width();
+    assert(n == B.height() && m == B.width());
+    for (int i = 0; i < n; i++)
+      for (int j = 0; j < m; j++) (*this)[i][j] -= B[i][j];
+    return (*this);
+  }
+
+  Matrix &operator*=(const Matrix &B) {
+    int n = height(), m = B.width(), p = width();
+    assert(p == B.height());
+    vector<Row> C(n, Row(m, 0));
+    for (int i = 0; i < n; i++)
+      for (int j = 0; j < m; j++)
+        for (int k = 0; k < p; k++)
+          C[i][j] = (C[i][j] + (*this)[i][k] * B[k][j]);
+    A.swap(C);
+    return (*this);
+  }
+
+  Matrix &operator^=(long long k) {
+    Matrix B = Matrix::I(height());
+    while (k > 0) {
+      if (k & 1) B *= *this;
+      *this *= *this;
+      k >>= 1LL;
+    }
+    A.swap(B.A);
+    return (*this);
+  }
+
+  Matrix operator+(const Matrix &B) const { return (Matrix(*this) += B); }
+
+  Matrix operator-(const Matrix &B) const { return (Matrix(*this) -= B); }
+
+  Matrix operator*(const Matrix &B) const { return (Matrix(*this) *= B); }
+
+  Matrix operator^(const long long k) const { return (Matrix(*this) ^= k); }
+
+  friend ostream &operator<<(ostream &os, Matrix &p) {
+    size_t n = p.height(), m = p.width();
+    for (int i = 0; i < n; i++) {
+      os << "[";
+      for (int j = 0; j < m; j++) {
+        os << p[i][j] << (j + 1 == m ? "]\n" : ",");
+      }
+    }
+    return (os);
+  }
+
+  T det() {
+    Matrix B(*this);
+    assert(width() == height());
+    T ret = 1;
+    for (int i = 0; i < width(); i++) {
+      int idx = -1;
+      for (int j = i; j < width(); j++) {
+        if (B[j][i] != 0) idx = j;
+      }
+      if (idx == -1) return (0);
+      if (i != idx) {
+        ret *= -1;
+        swap(B[i], B[idx]);
+      }
+      ret *= B[i][i];
+      T vv = B[i][i];
+      for (int j = 0; j < width(); j++) {
+        B[i][j] /= vv;
+      }
+      for (int j = i + 1; j < width(); j++) {
+        T a = B[j][i];
+        for (int k = 0; k < width(); k++) {
+          B[j][k] -= B[i][k] * a;
+        }
+      }
+    }
+    return (ret);
+  }
+};
 
 int main() {
   ios::sync_with_stdio(false);
-  cin.tie(0);
-  long long N;
-  scanf("%lld",&N);
-  long long K;
-  scanf("%lld",&K);
-  std::vector<std::vector<long long>> a(N, std::vector<long long>(N));
-  for(int i = 0 ; i < N ; i++){
-    for(int j = 0 ; j < N ; j++){
-      scanf("%lld",&a[i][j]);
+  cin.tie(nullptr);
+
+  int N;
+  i64 K;
+  cin >> N >> K;
+  Matrix<mint> a(N);
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < N; j++) {
+      cin >> a[i][j];
     }
   }
-  solve(N, K, std::move(a));
+  a ^= K;
+  mint ans;
+  REP(i, N) REP(j, N) { ans += a[i][j]; }
+  cout << ans << endl;
 }
--- atcoder/dp/S/main.cpp
+++ atcoder/dp/S/main.cpp
@@ -1,23 +1,92 @@
-/* Time: $%Y%$-$%M%$-$%D%$ $%h%$:$%m%$:$%s%$ */
-
 #include <bits/stdc++.h>
-#define REP(i, n) for (decltype(n) i = 0; i < (n); ++i)
 using namespace std;
-using int64 = long long;
+using i64 = long long;
+using u64 = unsigned long long;
+#define REP(i, n) for (int i = 0; (i64)(i) < (i64)(n); ++i)
 
-
-const long long MOD = 1000000007;
-
-void solve(long long K, long long D){
-
+#ifdef ENABLE_DEBUG
+template <typename T>
+void debug(T value) {
+  cerr << value;
 }
+template <typename T, typename... Ts>
+void debug(T value, Ts... args) {
+  cerr << value << ", ";
+  debug(args...);
+}
+#define DEBUG(...)                              \
+  do {                                          \
+    cerr << " \033[33m (L" << __LINE__ << ") "; \
+    cerr << #__VA_ARGS__ << ":\033[0m ";        \
+    debug(__VA_ARGS__);                         \
+    cerr << endl;                               \
+  } while (0)
+#else
+#define debug(...)
+#define DEBUG(...)
+#endif
+
+const i64 MOD = 1'000'000'007;
+
+struct mint {
+  long long x;
+  mint(long long x = 0) : x((x % MOD + MOD) % MOD) {}
+  mint operator-() const { return mint(-x); }
+  mint& operator+=(const mint a) {
+    if ((x += a.x) >= MOD) x -= MOD;
+    return *this;
+  }
+  mint& operator-=(const mint a) {
+    if ((x += MOD - a.x) >= MOD) x -= MOD;
+    return *this;
+  }
+  mint& operator*=(const mint a) {
+    (x *= a.x) %= MOD;
+    return *this;
+  }
+  mint operator+(const mint a) const { return mint(*this) += a; }
+  mint operator-(const mint a) const { return mint(*this) -= a; }
+  mint operator*(const mint a) const { return mint(*this) *= a; }
+  mint pow(long long t) const {
+    if (!t) return 1;
+    mint a = pow(t >> 1);
+    a *= a;
+    if (t & 1) a *= *this;
+    return a;
+  }
+
+  // for prime MOD
+  mint inv() const { return pow(MOD - 2); }
+  mint& operator/=(const mint a) { return *this *= a.inv(); }
+  mint operator/(const mint a) const { return mint(*this) /= a; }
+};
+istream& operator>>(istream& is, mint& a) { return is >> a.x; }
+ostream& operator<<(ostream& os, const mint& a) { return os << a.x; }
 
 int main() {
   ios::sync_with_stdio(false);
-  cin.tie(0);
-  long long K;
-  scanf("%lld",&K);
-  long long D;
-  scanf("%lld",&D);
-  solve(K, D);
+  cin.tie(nullptr);
+  string K;
+  int D;
+  cin >> K >> D;
+
+  vector<vector<mint>> dp(2, vector<mint>(D, 0));
+  dp[0][0] = 1;
+  for (int i = 1; i <= int(K.size()); ++i) {
+    vector<vector<mint>> t(2, vector<mint>(D));
+    swap(t, dp);
+    int digit = K[i - 1] - '0';
+    REP(dsum, D) {
+      REP(d, 10) { dp[1][(dsum + d) % D] += t[1][dsum]; }
+      for (int d = 0; d < digit; ++d) {
+        dp[1][(dsum + d) % D] += t[0][dsum];
+      }
+      {
+        int d = digit;
+        dp[0][(dsum + d) % D] += t[0][dsum];
+      }
+    }
+  }
+  mint ans = dp[0][0] + dp[1][0] - 1;
+  cout << ans << endl;
 }
