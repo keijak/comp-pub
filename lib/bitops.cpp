9661bc25430e0d218f2c251f67d7876b91e2f601
Keiji Maekawa <m@keijak.com>
2020-07-30 08:44:01 +0200

    leftmost_bit

--- lib/bitops.cpp
+++ lib/bitops.cpp
@@ -1,6 +1,3 @@
-// Turn off all bits of x except the rightmost one.
-int rightmost_bit(int x) { return x & -x; }
-
 // GCC Builtins.
 
 int popcount(unsigned int x) { return __builtin_popcount(x); }
@@ -14,3 +11,14 @@ int count_leading_zero(unsigned long long x) { return __builtin_clzll(x); }
 int count_trailing_zero(unsigned int x) { return __builtin_ctz(x); }
 int count_trailing_zero(unsigned long x) { return __builtin_ctzl(x); }
 int count_trailing_zero(unsigned long long x) { return __builtin_ctzll(x); }
+
+// Bit extractors.
+
+// Turn off all bits of x except the rightmost (least significant) bit.
+int rightmost_bit(unsigned int x) { return x & -x; }
+
+// Turn off all bits of x except the leftmost (most significant) bit.
+int leftmost_bit(unsigned int x) {
+  int l = 31 - __builtin_clz(x);
+  return 1 << l;
+}
