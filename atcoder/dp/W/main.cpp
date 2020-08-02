0044b34340851edb8cbf8ff78d0fb28b769431c8
Keiji Maekawa <m@keijak.com>
2020-08-02 10:30:05 +0200

    edpc

--- atcoder/dp/W/main.cpp
+++ atcoder/dp/W/main.cpp
@@ -26,61 +26,6 @@ void debug(T value, Ts... args) {
 #define DEBUG(...)
 #endif
 
-template <class Monoid>
-struct SegTree {
-  using Func = function<Monoid(Monoid, Monoid)>;
-  const Func F;
-  const Monoid UNITY;
-  int size;
-  vector<Monoid> dat;
-
-  SegTree(int n, const Func f, const Monoid &unity)
-      : F(f), UNITY(unity), size(n), dat(2 * n, unity) {}
-
-  // Sets i-th value (0-indexed) to x for initial setup.
-  // build() must be called after set() calls.
-  void set(int i, const Monoid &x) { dat[size + i] = x; }
-  void build() {
-    for (int k = size - 1; k > 0; --k) {
-      dat[k] = F(dat[k * 2], dat[k * 2 + 1]);
-    }
-  }
-
-  // Sets i-th value (0-indexed) to x.
-  void update(int i, const Monoid &x) {
-    int k = size + i;
-    dat[k] = x;
-    while (k > 1) {
-      k >>= 1;
-      dat[k] = F(dat[k * 2], dat[k * 2 + 1]);
-    }
-  }
-
-  // Queries by [l,r) range (0-indexed, open interval).
-  Monoid fold(int l, int r) {
-    l += size;
-    r += size;
-    Monoid vleft = UNITY, vright = UNITY;
-    for (; l < r; l >>= 1, r >>= 1) {
-      if (l & 1) vleft = F(vleft, dat[l++]);
-      if (r & 1) vright = F(dat[--r], vright);
-    }
-    return F(vleft, vright);
-  }
-
-  // Queries by a single index (0-indexed).
-  Monoid operator[](int i) { return dat[size + i]; }
-
-  /* debug */
-  void print() {
-    for (int i = 0; i < size; ++i) {
-      cout << (*this)[i];
-      if (i != size - 1) cout << ",";
-    }
-    cout << endl;
-  }
-};
-
 template <class T>
 using MinHeap = priority_queue<T, vector<T>, greater<T>>;
 
@@ -120,9 +65,6 @@ int main() {
       this_score += a;
       heap_score += a;
     }
-    dp[0][i + 1] = max(dp[0][i], dp[1][i]);
-    dp[1][i+1] = max(dp[0][i] + heap_score,
-    dp[1][i] + heap_score
 
     if (heap.empty()) {
       dp[i + 1] = dp[i];
