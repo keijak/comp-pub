d96598482c4cdc0261909129bdd317dc5627c405
Keiji Maekawa <m@keijak.com>
2020-08-05 22:24:40 +0200

    cum sum

--- atcoder/agc023/A/main2.py
+++ atcoder/agc023/A/main2.py
@@ -1,7 +1,8 @@
 import itertools
 import collections
+
 N = int(input())
 A = list(map(int, input().split()))
-acum = [0] + list(itertools.accumulate(A))
+acum = list(itertools.accumulate(A, initial=0))
 c = collections.Counter(acum)
-print(sum(n*(n-1)//2 for n in c.values()))
+print(sum(n * (n - 1) // 2 for n in c.values()))  # nC2
