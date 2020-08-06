d1f3014e2b4c06bf94c790580258ef173437d0a5
Keiji Maekawa <m@keijak.com>
2020-08-07 00:01:04 +0200

    dp_with_fastmax

--- yukicoder/1117/main.cpp
+++ yukicoder/1117/main.cpp
@@ -2,6 +2,10 @@
 using namespace std;
 using i64 = long long;
 using u64 = unsigned long long;
+
+template <typename T>
+using V = vector<T>;
+
 #define REP(i, n) for (int i = 0; (i64)(i) < (i64)(n); ++i)
 
 #ifdef ENABLE_DEBUG
@@ -26,40 +30,88 @@ void debug(T value, Ts... args) {
 #define DEBUG(...)
 #endif
 
+#define chmax(x, y) x = max(x, y)
+const i64 INF = 1LL << 40;
+
+template <class Monoid>
+struct SegTree {
+  using Func = function<Monoid(Monoid, Monoid)>;
+  const Func F;
+  const Monoid UNITY;
+  int size;
+  vector<Monoid> dat;
+
+  SegTree(int n, const Func f, const Monoid &unity)
+      : F(f), UNITY(unity), size(n), dat(2 * n, unity) {}
+
+  // Sets i-th value (0-indexed) to x for initial setup.
+  // build() must be called after set() calls.
+  void set(int i, const Monoid &x) { dat[size + i] = x; }
+  void build() {
+    for (int k = size - 1; k > 0; --k) {
+      dat[k] = F(dat[k * 2], dat[k * 2 + 1]);
+    }
+  }
+
+  // Sets i-th value (0-indexed) to x.
+  void update(int i, const Monoid &x) {
+    int k = size + i;
+    dat[k] = x;
+    while (k > 1) {
+      k >>= 1;
+      dat[k] = F(dat[k * 2], dat[k * 2 + 1]);
+    }
+  }
+
+  // Queries by [l,r) range (0-indexed, open interval).
+  Monoid fold(int l, int r) {
+    l += size;
+    r += size;
+    Monoid vleft = UNITY, vright = UNITY;
+    for (; l < r; l >>= 1, r >>= 1) {
+      if (l & 1) vleft = F(vleft, dat[l++]);
+      if (r & 1) vright = F(dat[--r], vright);
+    }
+    return F(vleft, vright);
+  }
+
+  // Queries by a single index (0-indexed).
+  Monoid operator[](int i) { return dat[size + i]; }
+
+  /* debug */
+  void print() {
+    for (int i = 0; i < size; ++i) {
+      cout << (*this)[i];
+      if (i != size - 1) cout << ",";
+    }
+    cout << endl;
+  }
+};
+
 int main() {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
   i64 N, K, M;
   cin >> N >> K >> M;
-  vector<i64> A(N);
-  for (auto& x : A) cin >> x;
-  vector<i64> acum(N + 1), abscum(N + 1);
-  REP(i, N) {
-    acum[i + 1] = acum[i] + A[i];
-    abscum[i + 1] = abscum[i] + abs(A[i]);
+  V<i64> A(N);
+  for (auto &x : A) cin >> x;
+  V<i64> acum(N + 1);
+  REP(i, N) { acum[i + 1] = acum[i] + A[i]; }
+  V<i64> dp(N + 1, -INF);
+  dp[0] = 0;
+  auto mx = [](i64 x, i64 y) { return max(x, y); };
+  SegTree<i64> st1(N + 1, mx, -INF), st2(N + 1, mx, -INF);
+  st1.update(0, 0);
+  st2.update(0, 0);
+  for (int k = 1; k <= K; ++k) {
+    for (int i = 1; i <= N; ++i) {
+      st1.update(i, dp[i] - acum[i]);
+      st2.update(i, dp[i] + acum[i]);
+    }
+    for (int i = 1; i <= N; ++i) {
+      int start = max<int>(i - M, 0);
+      dp[i] = max(st1.fold(start, i) + acum[i], st2.fold(start, i) - acum[i]);
+    }
   }
-
-  vector<vector<i64>> memo(N + 1, vector<i64>(K + 1, -1));
-  auto f = [&](auto rec, int len, int block_count) -> i64 {
-    if (len == 0 || block_count == 0) return 0;
-    if (len < block_count) return 0;
-    if (memo[len][block_count] >= 0) return memo[len][block_count];
-    if (len == block_count) {
-      return (memo[len][block_count] = abscum[len]);
-    }
-    if (block_count == 1) {
-      return (memo[len][block_count] = abs(acum[len]));
-    }
-    int m = min<int>(M, len);
-    i64 max_score = 0;
-    i64 block_sum = 0;
-    for (int x = 1; x <= m; ++x) {
-      block_sum += A[len - x];
-      i64 r = rec(rec, len - x, block_count - 1);
-      max_score = max(max_score, r + abs(block_sum));
-    }
-    return (memo[len][block_count] = max_score);
-  };
-  i64 ans = f(f, N, K);
-  cout << ans << endl;
+  cout << dp[N] << endl;
 }
