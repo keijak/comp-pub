8e6fecffbd8c33c99d5800a8d073be9fae1d2ced
Keiji Maekawa <m@keijak.com>
2020-08-18 11:21:27 +0200

    min/max heap lib

---
+++ lib/heap.py
@@ -0,0 +1,39 @@
+class MinHeap(object):
+    def __init__(self):
+        self.h = []
+
+    def push(self, x):
+        heapq.heappush(self.h, x)
+
+    def pop(self):
+        return heapq.heappop(self.h)
+
+    def __getitem__(self, i):
+        return self.h[i]
+
+    def __len__(self):
+        return len(self.h)
+
+    def __bool__(self):
+        return bool(self.h)
+
+
+class MaxHeap(MinHeap):
+    class Negator(object):
+        def __init__(self, val):
+            self.val = val
+
+        def __lt__(self, other):
+            return self.val > other.val
+
+        def __eq__(self, other):
+            return self.val == other.val
+
+        def __str__(self):
+            return str(self.val)
+
+    def push(self, x):
+        heapq.heappush(self.h, self.Negator(x))
+
+    def pop(self):
+        return heapq.heappop(self.h).val
