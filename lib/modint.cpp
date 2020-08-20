98b7b904b7f05505bde45ae7acfc1432104571fe
Keiji Maekawa <m@keijak.com>
2020-08-20 10:07:49 +0200

    pascal triangle

--- lib/modint.cpp
+++ lib/modint.cpp
@@ -63,3 +63,18 @@ struct Factorials {
     return fact[n] * ifact[n - k];
   }
 };
+
+// Cretates a combination matrix in O(n^2) using Pascal's triangle.
+// C[n][k] = nCk
+vector<vector<mint>> gen_comb(int n) {
+  vector<vector<mint>> C(n + 1, vector<mint>(n + 1, 0));
+  for (int i = 0; i <= n; ++i) {
+    C[i][0] = C[i][i] = 1;
+  }
+  for (int i = 1; i <= n; ++i) {
+    for (int j = 1; j < i; ++j) {
+      C[i][j] = C[i - 1][j - 1] + C[i - 1][j];
+    }
+  }
+  return C;
+}
