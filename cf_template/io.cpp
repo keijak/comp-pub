59b4311ef44c7e3dd14d424f08b8f3b9b3aaecd0
Keiji Maekawa <m@keijak.com>
2020-08-22 13:57:24 +0200

    pprint

--- cf_template/io.cpp
+++ cf_template/io.cpp
@@ -15,17 +15,32 @@ template <typename... Ts>
 auto make_vec(size_t n, Ts... ts) {
   return vector<decltype(make_vec(ts...))>(n, make_vec(ts...));
 }
-template <typename T>
-ostream &operator<<(ostream &os, const vector<T> &v) {
-  os << '[';
-  for (auto &&e : v) os << e << ' ';
-  return os << ']';
+template <typename Container>
+ostream &pprint(const Container &a, string_view sep = " ",
+                string_view ends = "\n", ostream *os = nullptr) {
+  if (os == nullptr) os = &cout;
+  auto b = std::begin(a), e = std::end(a);
+  for (auto it = std::begin(a); it != e; ++it) {
+    if (it != b) *os << sep;
+    *os << *it;
+  }
+  return *os << ends;
 }
 template <typename T>
-ostream &operator<<(ostream &os, const set<T> &v) {
-  os << '[';
-  for (auto &&e : v) os << e << ' ';
-  return os << ']';
+ostream &operator<<(ostream &os, const vector<T> &a) {
+  return pprint(a, ", ", "", &(os << "[")) << "]";
+}
+template <typename T>
+ostream &operator<<(ostream &os, const set<T> &a) {
+  return pprint(a, ", ", "", &(os << "{")) << "}";
+}
+template <typename T, typename U>
+ostream &operator<<(ostream &os, const map<T, U> &a) {
+  return pprint(a, ", ", "", &(os << "{")) << "}";
+}
+template <typename T, typename U>
+ostream &operator<<(ostream &os, const pair<T, U> &a) {
+  return os << "(" << a.first << ", " << a.second << ")";
 }
 
 #ifndef DEBUGGING
