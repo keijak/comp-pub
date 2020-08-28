7877be38c5f50bcec37af54c5afe34f88d9dce1d
Keiji Maekawa <m@keijak.com>
2020-08-28 21:47:40 +0200

    joi

---
+++ atcoder/joi2018ho/B/main.cpp
@@ -0,0 +1,93 @@
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+using u64 = unsigned long long;
+#define REP(i, n) for (int i = 0; (i64)(i) < (i64)(n); ++i)
+
+template <typename T>
+using V = std::vector<T>;
+template <typename T>
+vector<T> make_vec(size_t n, T a) {
+  return vector<T>(n, a);
+}
+template <typename... Ts>
+auto make_vec(size_t n, Ts... ts) {
+  return vector<decltype(make_vec(ts...))>(n, make_vec(ts...));
+}
+template <typename Container>
+ostream &pprint(const Container &a, string_view sep = " ",
+                string_view ends = "\n", ostream *os = nullptr) {
+  if (os == nullptr) os = &cout;
+  auto b = std::begin(a), e = std::end(a);
+  for (auto it = std::begin(a); it != e; ++it) {
+    if (it != b) *os << sep;
+    *os << *it;
+  }
+  return *os << ends;
+}
+template <typename T>
+ostream &operator<<(ostream &os, const vector<T> &a) {
+  return pprint(a, ", ", "", &(os << "[")) << "]";
+}
+template <typename T>
+ostream &operator<<(ostream &os, const set<T> &a) {
+  return pprint(a, ", ", "", &(os << "{")) << "}";
+}
+template <typename T, typename U>
+ostream &operator<<(ostream &os, const map<T, U> &a) {
+  return pprint(a, ", ", "", &(os << "{")) << "}";
+}
+template <typename T, typename U>
+ostream &operator<<(ostream &os, const pair<T, U> &a) {
+  return os << "(" << a.first << ", " << a.second << ")";
+}
+
+#ifdef ENABLE_DEBUG
+template <typename T>
+void debug(T value) {
+  cerr << value;
+}
+template <typename T, typename... Ts>
+void debug(T value, Ts... args) {
+  cerr << value << ", ";
+  debug(args...);
+}
+#define DEBUG(...)                              \
+  do {                                          \
+    cerr << " \033[33m (L" << __LINE__ << ") "; \
+    cerr << #__VA_ARGS__ << ":\033[0m ";        \
+    debug(__VA_ARGS__);                         \
+    cerr << endl;                               \
+  } while (0)
+#else
+#define debug(...)
+#define DEBUG(...)
+#endif
+
+#define chmax(x, y) x = max(x, y)
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int N;
+  cin >> N;
+  V<i64> A(N), B(N), ids(N);
+  REP(i, N) cin >> A[i] >> B[i];
+  REP(i, N) ids[i] = i;
+  sort(ids.begin(), ids.end(), [&](int i, int j) { return A[i] > A[j]; });
+
+  i64 ans = B[ids[0]];
+  i64 rmax = B[ids[0]] - A[ids[0]];
+  DEBUG(ids[0], A[ids[0]], ans, rmax);
+  for (int i = 1; i < N; ++i) {
+    int j = ids[i];
+    chmax(ans, B[j]);
+    chmax(ans, B[j] + A[j] + rmax);
+
+    chmax(rmax, B[j] + rmax);
+    chmax(rmax, B[j] - A[j]);
+    DEBUG(j, A[j], B[j], ans, rmax);
+  }
+  cout << ans << endl;
+}
