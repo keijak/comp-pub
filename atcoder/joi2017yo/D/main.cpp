b4b4a6fc6d5c3ecc70425f3105e916dd9a231280
Keiji Maekawa <m@keijak.com>
2020-08-29 23:00:49 +0200

    joi

---
+++ atcoder/joi2017yo/D/main.cpp
@@ -0,0 +1,119 @@
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+using u64 = unsigned long long;
+#define REP(i, n) for (int i = 0; (i64)(i) < (i64)(n); ++i)
+#define SZ(a) int((a).size())
+
+template <class T>
+void chmax(T &a, T b) {
+  if (a < b) a = std::move(b);
+}
+template <class T>
+void chmin(T &a, T b) {
+  if (a > b) a = std::move(b);
+}
+
+template <typename T>
+using V = std::vector<T>;
+template <typename T>
+vector<T> make_vec(size_t n, T a) {
+  return vector<T>(n, a);
+}
+template <typename... Ts>
+auto make_vec(size_t n, Ts... ts) {
+  return vector<decltype(make_vec(ts...))>(n, make_vec(ts...));
+}
+template <typename Container>
+ostream &pprint(const Container &a, string_view sep = " ",
+                string_view ends = "\n", ostream *os = nullptr) {
+  if (os == nullptr) os = &cout;
+  auto b = std::begin(a), e = std::end(a);
+  for (auto it = std::begin(a); it != e; ++it) {
+    if (it != b) *os << sep;
+    *os << *it;
+  }
+  return *os << ends;
+}
+template <typename T>
+ostream &operator<<(ostream &os, const vector<T> &a) {
+  return pprint(a, ", ", "", &(os << "[")) << "]";
+}
+template <typename T>
+ostream &operator<<(ostream &os, const set<T> &a) {
+  return pprint(a, ", ", "", &(os << "{")) << "}";
+}
+template <typename T, typename U>
+ostream &operator<<(ostream &os, const map<T, U> &a) {
+  return pprint(a, ", ", "", &(os << "{")) << "}";
+}
+template <typename T, typename U>
+ostream &operator<<(ostream &os, const pair<T, U> &a) {
+  return os << "(" << a.first << ", " << a.second << ")";
+}
+
+#ifdef ENABLE_DEBUG
+template <typename T>
+void debug(T value) {
+  cerr << value;
+}
+template <typename T, typename... Ts>
+void debug(T value, Ts... args) {
+  cerr << value << ", ";
+  debug(args...);
+}
+#define DEBUG(...)                              \
+  do {                                          \
+    cerr << " \033[33m (L" << __LINE__ << ") "; \
+    cerr << #__VA_ARGS__ << ":\033[0m ";        \
+    debug(__VA_ARGS__);                         \
+    cerr << endl;                               \
+  } while (0)
+#else
+#define debug(...)
+#define DEBUG(...)
+#endif
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int N, M;
+  cin >> N >> M;
+  V<int> seq(N);
+  REP(i, N) {
+    int a;
+    cin >> a;
+    a--;
+    seq[i] = a;
+  }
+
+  // cum[type][index] = cumulative count
+  auto cum = make_vec(M, N + 1, 0);
+  REP(i, N) {
+    REP(j, M) { cum[j][i + 1] = cum[j][i]; }
+    cum[seq[i]][i + 1]++;
+  }
+
+  const int L = 1 << M;
+  auto dp = make_vec(L, N);
+  dp[0] = 0;
+  REP(s, L) {
+    int all_count = 0;
+    for (int i = 0; i < M; ++i) {
+      if (s & (1 << i)) {
+        all_count += cum[i][N];
+      }
+    }
+    for (int i = 0; i < M; ++i) {
+      if (s & (1 << i)) {
+        int l = all_count - cum[i][N];
+        int r = all_count;
+        int i_count = cum[i][r] - cum[i][l];
+        int cost = dp[s & ~(1 << i)] + (r - l) - i_count;
+        chmin(dp[s], cost);
+      }
+    }
+  }
+  cout << dp[L - 1] << endl;
+}
