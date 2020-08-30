09e71d6f8872da0a3dafec77a46d4ecafe1dc349
Keiji Maekawa <m@keijak.com>
2020-08-31 00:40:14 +0200

    kruskal

---
+++ atcoder/joisc2010/finals/main.cpp
@@ -0,0 +1,148 @@
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+using u64 = unsigned long long;
+#define REP(i, n) for (int i = 0; (i64)(i) < (i64)(n); ++i)
+#define SZ(a) int((a).size())
+
+template <class T>
+void chmax(T &a, T b) {
+  if (a < b) a = std::move(b);
+}
+template <class T>
+void chmin(T &a, T b) {
+  if (a > b) a = std::move(b);
+}
+
+template <typename T>
+using V = std::vector<T>;
+template <typename T>
+vector<T> make_vec(size_t n, T a) {
+  return vector<T>(n, a);
+}
+template <typename... Ts>
+auto make_vec(size_t n, Ts... ts) {
+  return vector<decltype(make_vec(ts...))>(n, make_vec(ts...));
+}
+template <typename T>
+istream &operator>>(istream &is, vector<T> &a) {
+  for (auto &x : a) is >> x;
+  return is;
+}
+template <typename Container>
+ostream &pprint(const Container &a, string_view sep = " ",
+                string_view ends = "\n", ostream *os = nullptr) {
+  if (os == nullptr) os = &cout;
+  auto b = std::begin(a), e = std::end(a);
+  for (auto it = std::begin(a); it != e; ++it) {
+    if (it != b) *os << sep;
+    *os << *it;
+  }
+  return *os << ends;
+}
+template <typename T>
+ostream &operator<<(ostream &os, const vector<T> &a) {
+  return pprint(a, ", ", "", &(os << "[")) << "]";
+}
+template <typename T>
+ostream &operator<<(ostream &os, const set<T> &a) {
+  return pprint(a, ", ", "", &(os << "{")) << "}";
+}
+template <typename T, typename U>
+ostream &operator<<(ostream &os, const map<T, U> &a) {
+  return pprint(a, ", ", "", &(os << "{")) << "}";
+}
+template <typename T, typename U>
+ostream &operator<<(ostream &os, const pair<T, U> &a) {
+  return os << "(" << a.first << ", " << a.second << ")";
+}
+
+#ifdef ENABLE_DEBUG
+template <typename T>
+void debug(T value) {
+  cerr << value;
+}
+template <typename T, typename... Ts>
+void debug(T value, Ts... args) {
+  cerr << value << ", ";
+  debug(args...);
+}
+#define DEBUG(...)                              \
+  do {                                          \
+    cerr << " \033[33m (L" << __LINE__ << ") "; \
+    cerr << #__VA_ARGS__ << ":\033[0m ";        \
+    debug(__VA_ARGS__);                         \
+    cerr << endl;                               \
+  } while (0)
+#else
+#define debug(...)
+#define DEBUG(...)
+#endif
+
+struct UnionFind {
+  vector<int> data;  // positive: parent, negative: size
+  int num_roots;
+  vector<int> weights;
+
+  explicit UnionFind(size_t sz) : data(sz, -1), num_roots(sz), weights(sz) {}
+
+  bool unite(int x, int y) {
+    x = find(x), y = find(y);
+    if (x == y) return false;
+    if (data[x] > data[y]) swap(x, y);
+    data[x] += data[y];
+    data[y] = x;
+    weights[x] += weights[y];
+    weights[y] = 0;
+    --num_roots;
+    return true;
+  }
+
+  int find(int k) {
+    if (data[k] < 0) return k;
+    return data[k] = find(data[k]);
+  }
+
+  int size(int k) { return -data[find(k)]; }
+
+  bool same(int x, int y) { return find(x) == find(y); }
+
+  vector<int> roots() {
+    vector<int> res;
+    res.reserve(num_roots);
+    for (int i = 0; i < (int)data.size(); ++i) {
+      if (data[i] < 0) res.push_back(i);
+    }
+    return res;
+  }
+};
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int N, M, K;
+  cin >> N >> M >> K;
+  V<tuple<int, int, int>> edges(M);
+  REP(i, M) {
+    int a, b, c;
+    cin >> a >> b >> c;
+    edges[i] = {c, a - 1, b - 1};
+  }
+  sort(edges.begin(), edges.end());
+  UnionFind uf(N);
+
+  // Kruskal.
+  for (const auto &[c, a, b] : edges) {
+    if (uf.num_roots == K) {
+      break;
+    }
+    if (uf.unite(a, b)) {
+      uf.weights[uf.find(a)] += c;
+    }
+  }
+  assert(uf.num_roots == K);
+  i64 ans = 0;
+  REP(i, N) { ans += uf.weights[i]; }
+  cout << ans << endl;
+}
