05eb2b6781a67b81ad8c721356b3df5a17e4aae8
Keiji Maekawa <m@keijak.com>
2020-08-30 15:56:19 +0200

    abc

--- atcoder/abc177/F/main.cpp
+++ atcoder/abc177/F/main.cpp
@@ -43,6 +43,10 @@ template <typename T>
 ostream &operator<<(ostream &os, const set<T> &a) {
   return pprint(a, ", ", "", &(os << "{")) << "}";
 }
+template <typename T>
+ostream &operator<<(ostream &os, const multiset<T> &a) {
+  return pprint(a, ", ", "", &(os << "{")) << "}";
+}
 template <typename T, typename U>
 ostream &operator<<(ostream &os, const map<T, U> &a) {
   return pprint(a, ", ", "", &(os << "{")) << "}";
@@ -85,13 +89,32 @@ int main() {
     A[i]--;
   }
 
-  set<int> st;
-  REP(i, W) { st.insert(i); }
-  int death_end = 0;
+  // start[cur] = rightmost start point to reach cur.
+  map<int, int> start;
+  multiset<int> moves;
+  REP(i, W) {
+    start[i] = i;
+    moves.insert(0);
+  }
 
   for (int i = 0; i < H; ++i) {
-    auto it1 = st.lower_bound(A[i]);
-    auto it2 = st.lower_bound(B[i]);
-    st.erase(it1, it2);
+    int l = A[i], r = B[i];
+    auto it = start.lower_bound(l);
+    int new_start = -1;
+    while (it != start.end() && (it->first <= r)) {
+      new_start = max(new_start, it->second);
+      int m = it->first - it->second;
+      moves.erase(moves.find(m));
+      it = start.erase(it);
+    }
+    if (new_start != -1 && r < W) {
+      start[r] = new_start;
+      moves.insert(r - new_start);
+    }
+    if (moves.empty()) {
+      cout << -1 << '\n';
+    } else {
+      cout << (*moves.begin()) + (i + 1) << '\n';
+    }
   }
 }
