f9093735519ba5dc364fb36b992e8ee2bc3f4b73
Keiji Maekawa <m@keijak.com>
2020-09-02 00:54:06 +0200

    joi

---
+++ atcoder/joi2007ho/D/main.cpp
@@ -0,0 +1,120 @@
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+using u64 = unsigned long long;
+#define REP(i, n) for (int i = 0; (i64)(i) < (i64)(n); ++i)
+#define SZ(a) int((a).size())
+
+template <class T>
+bool chmax(T &a, T b) {
+  if (a < b) return (a = move(b)), true;
+  return false;
+}
+template <class T>
+bool chmin(T &a, T b) {
+  if (a > b) return (a = move(b)), true;
+  return false;
+}
+
+template <typename T>
+using V = std::vector<T>;
+template <typename T>
+vector<T> make_vec(size_t n, T a) {
+  return vector<T>(n, a);
+}
+template <typename... Ts>
+auto make_vec(size_t n, Ts... ts) {
+  return vector<decltype(make_vec(ts...))>(n, make_vec(ts...));
+}
+template <typename T>
+istream &operator>>(istream &is, vector<T> &a) {
+  for (auto &x : a) is >> x;
+  return is;
+}
+template <typename Container>
+ostream &pprint(const Container &a, string_view sep = " ",
+                string_view ends = "\n", ostream *os = nullptr) {
+  if (os == nullptr) os = &cout;
+  auto b = std::begin(a), e = std::end(a);
+  for (auto it = std::begin(a); it != e; ++it) {
+    if (it != b) *os << sep;
+    *os << *it;
+  }
+  return *os << ends;
+}
+template <typename T>
+ostream &operator<<(ostream &os, const vector<T> &a) {
+  return pprint(a, ", ", "", &(os << "[")) << "]";
+}
+template <typename T>
+ostream &operator<<(ostream &os, const set<T> &a) {
+  return pprint(a, ", ", "", &(os << "{")) << "}";
+}
+template <typename T, typename U>
+ostream &operator<<(ostream &os, const map<T, U> &a) {
+  return pprint(a, ", ", "", &(os << "{")) << "}";
+}
+template <typename T, typename U>
+ostream &operator<<(ostream &os, const pair<T, U> &a) {
+  return os << "(" << a.first << ", " << a.second << ")";
+}
+
+#ifdef ENABLE_DEBUG
+template <typename T>
+void debug(T value) {
+  cerr << value;
+}
+template <typename T, typename... Ts>
+void debug(T value, Ts... args) {
+  cerr << value << ", ";
+  debug(args...);
+}
+#define DEBUG(...)                              \
+  do {                                          \
+    cerr << " \033[33m (L" << __LINE__ << ") "; \
+    cerr << #__VA_ARGS__ << ":\033[0m ";        \
+    debug(__VA_ARGS__);                         \
+    cerr << endl;                               \
+  } while (0)
+#else
+#define debug(...)
+#define DEBUG(...)
+#endif
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int N, M;  // N <= 5000, M <= 1e5
+  cin >> N >> M;
+  V<int> indeg(N);
+  V<V<int>> nex(N);
+  REP(i, M) {
+    int winner, loser;
+    cin >> winner >> loser;
+    winner--;
+    loser--;
+    indeg[loser]++;
+    nex[winner].push_back(loser);
+  }
+  bool ambiguous = false;
+  deque<int> q;
+  REP(i, N) {
+    if (indeg[i] == 0) {
+      q.emplace_back(i);
+    }
+  }
+  V<int> ranking;
+  while (q.size()) {
+    if (q.size() > 1) ambiguous = true;
+    auto t = q.front();
+    q.pop_front();
+    ranking.push_back(t + 1);
+    for (auto s : nex[t]) {
+      if (--indeg[s] == 0) {
+        q.push_back(s);
+      }
+    }
+  }
+  pprint(ranking, "\n");
+  cout << ambiguous << endl;
+}
