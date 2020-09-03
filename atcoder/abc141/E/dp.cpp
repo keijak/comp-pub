48d8d510fec458633b662aa0e0695d24ab4a790e
Keiji Maekawa <m@keijak.com>
2020-09-03 11:18:05 +0200

    Z Algorithm

--- atcoder/abc141/E/dp.cpp
+++ atcoder/abc141/E/dp.cpp
@@ -1,10 +1,8 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 using i64 = long long;
 using u64 = unsigned long long;
 #define REP(i, n) for (int i = 0; (i64)(i) < (i64)(n); ++i)
-#define SZ(a) int((a).size())
 
 template <class T>
 bool chmax(T &a, T b) {
@@ -28,47 +26,24 @@ auto make_vec(size_t n, Ts... ts) {
   return vector<decltype(make_vec(ts...))>(n, make_vec(ts...));
 }
 
-struct ZAlgorithm {
-  const int N;
-  vector<int> Z;
-  const string_view S;
-
-  explicit ZAlgorithm(string_view s) : N(s.size()), Z(s.size()), S(move(s)) {}
-
-  void calc() {
-    Z[0] = N;
-    int i = 1, j = 0;
-    while (i < N) {
-      while (i + j < N and S[j] == S[i + j]) ++j;
-      Z[i] = j;
-      if (j == 0) {
-        ++i;
-        continue;
-      }
-      int k = 1;
-      for (; k < j and k + Z[k] < j; ++k) {
-        Z[i + k] = Z[k];
-      }
-      i += k;
-      j -= k;
-    }
-  }
-};
-
 int main() {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
-
   int n;
   string s;
   cin >> n >> s;
-  string_view sv(s);
 
   int ans = 0;
-  REP(i, n) {
-    ZAlgorithm za(sv.substr(i));
-    za.calc();
-    REP(j, za.N) { chmax(ans, min(za.Z[j], j)); }
+  auto dp = make_vec(n + 1, 0);
+  for (int i = n - 2; i >= 0; --i) {
+    auto dp_next = make_vec(n + 1, 0);
+    for (int j = i + 1; j < n; ++j) {
+      if (s[i] == s[j]) {
+        dp_next[j] = dp[j + 1] + 1;
+        chmax(ans, min(dp_next[j], j - i));
+      }
+    }
+    swap(dp, dp_next);
   }
   cout << ans << endl;
 }
