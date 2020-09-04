50bb8790a1af3dba456a09fe0e5c50d4112d621e
Keiji Maekawa <m@keijak.com>
2020-09-04 14:51:26 +0200

    rollinghash

--- lib/rolling_hash.cpp
+++ lib/rolling_hash.cpp
@@ -2,25 +2,17 @@ struct RollingHash {
   using u64 = unsigned long long;
   using u128 = __uint128_t;
   static const u64 mod = (1ULL << 61) - 1;
-  const u64 base;
   vector<u64> hashed, power;
 
-  RollingHash(const string &s, u64 base) : base(base) {
+  RollingHash(string_view s) {
     int n = s.size();
     hashed.assign(n + 1, 0);
     power.assign(n + 1, 0);
     power[0] = 1;
     for (int i = 0; i < n; i++) {
-      power[i + 1] = mul(power[i], base);
-      hashed[i + 1] = add(mul(hashed[i], base), s[i]);
+      power[i + 1] = mul(power[i], base());
+      hashed[i + 1] = add(mul(hashed[i], base()), s[i]);
     }
-  }
-
-  static u64 gen_base() {
-    random_device seed_gen;
-    mt19937_64 engine(seed_gen());
-    uniform_int_distribution<u64> rand(1, mod - 1);
-    return rand(engine);
   }
 
   // Returns the hash value for the [l,r) interval.
@@ -29,10 +21,19 @@ struct RollingHash {
   }
 
  private:
+  static u64 base() {
+    static u64 val = []() -> u64 {
+      random_device seed_gen;
+      mt19937_64 engine(seed_gen());
+      uniform_int_distribution<u64> rand(1, mod - 1);
+      return rand(engine);
+    }();
+    return val;
+  }
+
   u64 add(u64 a, u64 b) {
-    if ((a += b) >= mod) {
-      a -= mod;
-    }
+    a += b;
+    if (a >= mod) a -= mod;
     return a;
   }
 
@@ -40,9 +41,8 @@ struct RollingHash {
     u128 t = (u128)a * b;
     u64 na = t >> 61;
     u64 nb = t & mod;
-    if ((na += nb) >= mod) {
-      na -= mod;
-    }
+    na += nb;
+    if (na >= mod) na -= mod;
     return na;
   }
 };
