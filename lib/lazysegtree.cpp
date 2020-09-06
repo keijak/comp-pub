57d839f7662b7c0e9ee6c1f04c89ce8ea7de4568
Keiji Maekawa <m@keijak.com>
2020-09-06 23:30:16 +0200

    lazysegtree

--- lib/lazysegtree.cpp
+++ lib/lazysegtree.cpp
@@ -4,7 +4,7 @@ struct LazySegmentTree {
   using UpdateFn = function<OperatorMonoid(OperatorMonoid, OperatorMonoid)>;
   using CombineFn = function<Monoid(Monoid, OperatorMonoid)>;
 
-  int sz, height;
+  int size, height;
   vector<Monoid> data;
   vector<OperatorMonoid> lazy;
   const AggFn agg_;          // aggregator
@@ -16,17 +16,17 @@ struct LazySegmentTree {
   LazySegmentTree(int n, const AggFn f, const UpdateFn g, const CombineFn h,
                   const Monoid &M1, const OperatorMonoid OM0)
       : agg_(f), update_(g), combine_(h), M1(M1), OM0(OM0) {
-    sz = 1;
+    size = 1;
     height = 0;
-    while (sz < n) sz <<= 1, height++;
-    data.assign(2 * sz, M1);
-    lazy.assign(2 * sz, OM0);
+    while (size < n) size <<= 1, height++;
+    data.assign(2 * size, M1);
+    lazy.assign(2 * size, OM0);
   }
 
-  void set(int k, const Monoid &x) { data[k + sz] = x; }
+  void set(int k, const Monoid &x) { data[k + size] = x; }
 
   void build() {
-    for (int k = sz - 1; k > 0; k--) {
+    for (int k = size - 1; k > 0; k--) {
       data[k] = agg_(data[2 * k + 0], data[2 * k + 1]);
     }
   }
@@ -54,8 +54,8 @@ struct LazySegmentTree {
 
   void update(int a, int b, const OperatorMonoid &x) {
     if (a >= b) return;
-    thrust(a += sz);
-    thrust(b += sz - 1);
+    thrust(a += size);
+    thrust(b += size - 1);
     for (int l = a, r = b + 1; l < r; l >>= 1, r >>= 1) {
       if (l & 1) lazy[l] = update_(lazy[l], x), ++l;
       if (r & 1) --r, lazy[r] = update_(lazy[r], x);
@@ -66,8 +66,8 @@ struct LazySegmentTree {
 
   Monoid fold(int a, int b) {
     if (a >= b) return M1;
-    thrust(a += sz);
-    thrust(b += sz - 1);
+    thrust(a += size);
+    thrust(b += size - 1);
     Monoid L = M1, R = M1;
     for (int l = a, r = b + 1; l < r; l >>= 1, r >>= 1) {
       if (l & 1) L = agg_(L, apply(l++));
@@ -80,7 +80,7 @@ struct LazySegmentTree {
 
   template <typename C>
   int find_subtree(int a, const C &check, Monoid &M, bool type) {
-    while (a < sz) {
+    while (a < size) {
       propagate(a);
       Monoid nxt =
           type ? agg_(apply(2 * a + type), M) : agg_(M, apply(2 * a + type));
@@ -89,7 +89,7 @@ struct LazySegmentTree {
       else
         M = nxt, a = 2 * a + 1 - type;
     }
-    return a - sz;
+    return a - size;
   }
 
   template <typename C>
@@ -99,9 +99,9 @@ struct LazySegmentTree {
       if (check(agg_(L, apply(1)))) return find_subtree(1, check, L, false);
       return -1;
     }
-    thrust(a + sz);
-    int b = sz;
-    for (a += sz, b += sz; a < b; a >>= 1, b >>= 1) {
+    thrust(a + size);
+    int b = size;
+    for (a += size, b += size; a < b; a >>= 1, b >>= 1) {
       if (a & 1) {
         Monoid nxt = agg_(L, apply(a));
         if (check(nxt)) return find_subtree(a, check, L, false);
@@ -115,13 +115,13 @@ struct LazySegmentTree {
   template <typename C>
   int find_last(int b, const C &check) {
     Monoid R = M1;
-    if (b >= sz) {
+    if (b >= size) {
       if (check(agg_(apply(1), R))) return find_subtree(1, check, R, true);
       return -1;
     }
-    thrust(b + sz - 1);
-    int a = sz;
-    for (b += sz; a < b; a >>= 1, b >>= 1) {
+    thrust(b + size - 1);
+    int a = size;
+    for (b += size; a < b; a >>= 1, b >>= 1) {
       if (b & 1) {
         Monoid nxt = agg_(apply(--b), R);
         if (check(nxt)) return find_subtree(b, check, R, true);
