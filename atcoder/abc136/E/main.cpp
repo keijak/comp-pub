a1ce22a793335fa2e22b59a490627f1b17e68cdb
Keiji Maekawa <m@keijak.com>
2020-09-08 01:29:10 +0200

    bruteforce

---
+++ atcoder/abc136/E/main.cpp
@@ -0,0 +1,130 @@
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+using u64 = unsigned long long;
+#define REP(i, n) for (int i = 0; i < (i64)(n); ++i)
+#define WHOLE(x) std::begin(x), std::end(x)
+#define LEN(a) int((a).size())
+
+template <class T>
+bool chmax(T &a, T b) {
+  if (a < b) return (a = move(b)), true;
+  return false;
+}
+template <class T>
+bool chmin(T &a, T b) {
+  if (a > b) return (a = move(b)), true;
+  return false;
+}
+
+template <typename T>
+using V = std::vector<T>;
+template <typename T>
+vector<T> make_vec(size_t n, T a) {
+  return vector<T>(n, a);
+}
+template <typename... Ts>
+auto make_vec(size_t n, Ts... ts) {
+  return vector<decltype(make_vec(ts...))>(n, make_vec(ts...));
+}
+template <typename T>
+istream &operator>>(istream &is, vector<T> &a) {
+  for (auto &x : a) is >> x;
+  return is;
+}
+template <typename Container>
+ostream &pprint(const Container &a, string_view sep = " ",
+                string_view ends = "\n", ostream *os = nullptr) {
+  if (os == nullptr) os = &cout;
+  auto b = std::begin(a), e = std::end(a);
+  for (auto it = std::begin(a); it != e; ++it) {
+    if (it != b) *os << sep;
+    *os << *it;
+  }
+  return *os << ends;
+}
+template <typename T>
+ostream &operator<<(ostream &os, const vector<T> &a) {
+  return pprint(a, ", ", "", &(os << "[")) << "]";
+}
+template <typename T>
+ostream &operator<<(ostream &os, const set<T> &a) {
+  return pprint(a, ", ", "", &(os << "{")) << "}";
+}
+template <typename T, typename U>
+ostream &operator<<(ostream &os, const map<T, U> &a) {
+  return pprint(a, ", ", "", &(os << "{")) << "}";
+}
+template <typename T, typename U>
+ostream &operator<<(ostream &os, const pair<T, U> &a) {
+  return os << "(" << a.first << ", " << a.second << ")";
+}
+
+#ifdef ENABLE_DEBUG
+template <typename T>
+void debug(T value) {
+  cerr << value;
+}
+template <typename T, typename... Ts>
+void debug(T value, Ts... args) {
+  cerr << value << ", ";
+  debug(args...);
+}
+#define DEBUG(...)                              \
+  do {                                          \
+    cerr << " \033[33m (L" << __LINE__ << ") "; \
+    cerr << #__VA_ARGS__ << ":\033[0m ";        \
+    debug(__VA_ARGS__);                         \
+    cerr << endl;                               \
+  } while (0)
+#else
+#define debug(...)
+#define DEBUG(...)
+#endif
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int N, K;
+  cin >> N >> K;
+  V<int> A(N);
+  i64 asum = 0;
+  REP(i, N) {
+    cin >> A[i];
+    asum += A[i];
+  }
+
+  i64 ans = 1;
+
+  auto check = [&](i64 k) -> void {
+    if (k <= ans) return;
+    V<i64> v(N);
+    REP(i, N) { v[i] = A[i] % k; }
+    sort(WHOLE(v));
+    V<i64> lcum(N + 1);
+    REP(i, N) { lcum[i + 1] = lcum[i] + v[i]; }
+    if (lcum[N] > 0) {
+      i64 rcum = 0;
+      for (int i = N - 1; i >= 0; --i) {
+        rcum += v[i] == 0 ? 0 : k - v[i];
+        if (rcum > K) return;
+        if (rcum > lcum[i]) return;
+        if (rcum == lcum[i]) {
+          break;  // ok
+        }
+      }
+    }
+    ans = k;
+  };
+
+  for (i64 k = 1; k * k <= asum; ++k) {
+    if (asum % k == 0) {
+      i64 r = asum / k;
+      check(r);
+      if (k != r) check(k);
+    }
+  }
+
+  cout << ans << endl;
+}
