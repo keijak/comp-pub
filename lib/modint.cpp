8ed596d12ed08eab4d29379c23ca24d779aab106
Keiji Maekawa <m@keijak.com>
2020-09-07 23:09:22 +0200

    modint

--- lib/modint.cpp
+++ lib/modint.cpp
@@ -1,9 +1,16 @@
 const int MOD = 1'000'000'007;
 
 struct mint {
-  using value_type = long long;
-  value_type x;
-  mint(value_type x = 0) : x((x % MOD + MOD) % MOD) {}
+  int x;
+
+  mint(long long val = 0) {
+    if (val < 0) {
+      long long k = (abs(val) + MOD - 1) / MOD;
+      val += k * MOD;
+    }
+    assert(val >= 0);
+    x = val % MOD;
+  }
   mint operator-() const { return mint(-x); }
   mint &operator+=(const mint &a) {
     if ((x += a.x) >= MOD) x -= MOD;
@@ -14,15 +21,19 @@ struct mint {
     return *this;
   }
   mint &operator*=(const mint &a) {
-    (x *= a.x) %= MOD;
+    x = (x * (long long)a.x) % MOD;
     return *this;
   }
-  mint pow(value_type t) const {
-    if (!t) return 1;
-    mint a = pow(t >> 1);
-    a *= a;
-    if (t & 1) a *= *this;
-    return a;
+  mint pow(long long t) const {
+    assert(t >= 0);
+    mint base = *this;
+    mint res = 1;
+    while (t) {
+      if (t & 1) res *= base;
+      base *= base;
+      t >>= 1;
+    }
+    return res;
   }
   // for prime MOD
   mint inv() const { return pow(MOD - 2); }
