cd6580731b38c0da89f64a0d4a48ab9ad9855e1a
Keiji Maekawa <m@keijak.com>
2020-09-07 09:38:58 +0200

    graph utility

--- lib/lca.cpp
+++ lib/lca.cpp
@@ -1,15 +1,13 @@
-// Lowest Common Ancestor
+// Lowest Common Ancestor of a tree.
 class LCA {
  public:
-  explicit LCA(int n)
-      : n(n), adj(n), parent(K, vector<int>(n, -1)), depth(n, -1) {}
+  const int n;  // number of nodes
+  const vector<vector<int>> &adj;
+  vector<vector<int>> parent;
+  vector<int> depth;
 
-  void add_edge(int x, int y) {
-    adj[x].push_back(y);
-    adj[y].push_back(x);
-  }
-
-  void build() {
+  explicit LCA(const Graph &g)
+      : n(g.n), adj(g.adj), parent(K, vector<int>(g.n, -1)), depth(g.n, -1) {
     dfs(0, -1, 0);
     for (int k = 0; k + 1 < K; k++) {
       REP(v, n) {
@@ -22,7 +20,8 @@ class LCA {
     }
   }
 
-  int query(int u, int v) {
+  // Returns the node ID of the lowest common ancestor.
+  int query(int u, int v) const {
     if (depth[u] > depth[v]) swap(u, v);
     for (int k = 0; k < K; k++) {
       if ((depth[v] - depth[u]) & (1 << k)) {
@@ -39,6 +38,12 @@ class LCA {
     return parent[0][u];
   }
 
+  // Returns the distance (number of edges) between two nodes.
+  int distance(int u, int v) const {
+    int p = query(u, v);
+    return (depth[u] - depth[p]) + (depth[v] - depth[p]);
+  }
+
  private:
   void dfs(int v, int p, int d) {
     parent[0][v] = p;
@@ -50,8 +55,4 @@ class LCA {
   }
 
   static const int K = 30;  // max parent lookup (2^K)
-  const int n;              // number of nodes
-  vector<vector<int>> adj;
-  vector<vector<int>> parent;
-  vector<int> depth;
 };
