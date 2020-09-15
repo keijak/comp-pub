4d4da6ab47d4e61743e0a6354bb2e0dac848c351
Keiji Maekawa <m@keijak.com>
2020-09-15 11:44:05 +0200

    factorial template

--- atcoder/abc178/D/dp.cpp
+++ atcoder/abc178/D/dp.cpp
@@ -1,38 +1,41 @@
 #include <bits/stdc++.h>
-using i64 = std::int64_t;
-using u64 = std::uint64_t;
+using namespace std;
+using i64 = long long;
+using u64 = unsigned long long;
 #define REP(i, n) for (int i = 0; i < (i64)(n); ++i)
 #define WHOLE(x) std::begin(x), std::end(x)
 #define LEN(a) int((a).size())
 
 template <class T>
 bool chmax(T &a, T b) {
-  return a >= b ? false : ((a = std::move(b)), true);
+  if (a < b) return (a = move(b)), true;
+  return false;
 }
 template <class T>
 bool chmin(T &a, T b) {
-  return a <= b ? false : ((a = std::move(b)), true);
+  if (a > b) return (a = move(b)), true;
+  return false;
 }
 
 template <typename T>
 using V = std::vector<T>;
 template <typename T>
-std::vector<T> make_vec(size_t n, T a) {
-  return std::vector<T>(n, a);
+vector<T> make_vec(size_t n, T a) {
+  return vector<T>(n, a);
 }
 template <typename... Ts>
 auto make_vec(size_t n, Ts... ts) {
-  return std::vector<decltype(make_vec(ts...))>(n, make_vec(ts...));
+  return vector<decltype(make_vec(ts...))>(n, make_vec(ts...));
 }
 template <typename T>
-std::istream &operator>>(std::istream &is, std::vector<T> &a) {
+istream &operator>>(istream &is, vector<T> &a) {
   for (auto &x : a) is >> x;
   return is;
 }
 template <typename Container>
-std::ostream &pprint(const Container &a, std::string_view sep = " ",
-                     std::string_view ends = "\n", std::ostream *os = nullptr) {
-  if (os == nullptr) os = &std::cout;
+ostream &pprint(const Container &a, string_view sep = " ",
+                string_view ends = "\n", ostream *os = nullptr) {
+  if (os == nullptr) os = &cout;
   auto b = std::begin(a), e = std::end(a);
   for (auto it = std::begin(a); it != e; ++it) {
     if (it != b) *os << sep;
@@ -40,47 +43,44 @@ std::ostream &pprint(const Container &a, std::string_view sep = " ",
   }
   return *os << ends;
 }
-template <typename T, typename = void>
-struct is_iterable : std::false_type {};
 template <typename T>
-struct is_iterable<T, std::void_t<decltype(std::begin(std::declval<T>())),
-                                  decltype(std::end(std::declval<T>()))>>
-    : std::true_type {};
-
-template <typename T,
-          typename = std::enable_if_t<is_iterable<T>::value &&
-                                      !std::is_same<T, std::string>::value>>
-std::ostream &operator<<(std::ostream &os, const T &a) {
+ostream &operator<<(ostream &os, const vector<T> &a) {
+  return pprint(a, ", ", "", &(os << "[")) << "]";
+}
+template <typename T>
+ostream &operator<<(ostream &os, const set<T> &a) {
   return pprint(a, ", ", "", &(os << "{")) << "}";
 }
 template <typename T, typename U>
-std::ostream &operator<<(std::ostream &os, const std::pair<T, U> &a) {
+ostream &operator<<(ostream &os, const map<T, U> &a) {
+  return pprint(a, ", ", "", &(os << "{")) << "}";
+}
+template <typename T, typename U>
+ostream &operator<<(ostream &os, const pair<T, U> &a) {
   return os << "(" << a.first << ", " << a.second << ")";
 }
 
 #ifdef ENABLE_DEBUG
 template <typename T>
-void pdebug(const T &value) {
-  std::cerr << value;
+void debug(T value) {
+  cerr << value;
 }
 template <typename T, typename... Ts>
-void pdebug(const T &value, const Ts &... args) {
-  pdebug(value);
-  std::cerr << ", ";
-  pdebug(args...);
+void debug(T value, Ts... args) {
+  cerr << value << ", ";
+  debug(args...);
 }
-#define DEBUG(...)                                   \
-  do {                                               \
-    std::cerr << " \033[33m (L" << __LINE__ << ") "; \
-    std::cerr << #__VA_ARGS__ << ":\033[0m ";        \
-    pdebug(__VA_ARGS__);                             \
-    std::cerr << std::endl;                          \
+#define DEBUG(...)                              \
+  do {                                          \
+    cerr << " \033[33m (L" << __LINE__ << ") "; \
+    cerr << #__VA_ARGS__ << ":\033[0m ";        \
+    debug(__VA_ARGS__);                         \
+    cerr << endl;                               \
   } while (0)
 #else
-#define pdebug(...)
+#define debug(...)
 #define DEBUG(...)
 #endif
-using namespace std;
 
 const int MOD = 1'000'000'007;
 
@@ -136,43 +136,16 @@ bool operator>=(const mint &a, const mint &b) { return a.x >= b.x; }
 istream &operator>>(istream &is, mint &a) { return is >> a.x; }
 ostream &operator<<(ostream &os, const mint &a) { return os << a.x; }
 
-template <class T = mint>
-struct Factorials {
-  // factorials and inverse factorials.
-  vector<T> fact, ifact;
-
-  // n: max cached value.
-  Factorials(int n) : fact(n + 1), ifact(n + 1) {
-    assert(n < MOD);
-    fact[0] = 1;
-    for (int i = 1; i <= n; ++i) fact[i] = fact[i - 1] * i;
-    ifact[n] = fact[n].inv();
-    for (int i = n; i >= 1; --i) ifact[i - 1] = ifact[i] * i;
-  }
-
-  // Combination (nCk)
-  T C(int n, int k) {
-    if (k < 0 || k > n) return 0;
-    return fact[n] * ifact[k] * ifact[n - k];
-  }
-
-  // Permutation (nPk)
-  T P(int n, int k) {
-    if (k < 0 || k > n) return 0;
-    return fact[n] * ifact[n - k];
-  }
-};
-
 int main() {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
   int S;
   cin >> S;
-  Factorials<> fs(S);
-  mint ans = 0;
-  for (int k = 1; 3 * k <= S; ++k) {
-    int r = S - 3 * k;
-    ans += fs.C(r + k - 1, r);
+  V<mint> dp(S + 1, mint(0));
+  dp[0] = 1;
+  for (int i = 3; i <= S; ++i) {
+    dp[i] = dp[i - 1];
+    if (i - 3 >= 0) dp[i] += dp[i - 3];
   }
-  cout << ans << endl;
+  cout << dp[S] << endl;
 }
