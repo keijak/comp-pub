9482951dc63e3b7e28f3072ff70104c131dc9a1b
Keiji Maekawa <m@keijak.com>
2020-09-20 00:14:21 +0200

    segtreeapi

--- lib/segtree.cpp
+++ lib/segtree.cpp
@@ -1,50 +1,47 @@
-template <class Monoid>
+template <typename Monoid>
 struct SegTree {
-  using Func = function<Monoid(Monoid, Monoid)>;
-  const Func F;
-  const Monoid UNITY;
+  using T = typename Monoid::T;
   int size;
-  vector<Monoid> dat;
+  std::vector<T> dat;
 
-  SegTree(int n, const Func f, const Monoid &unity)
-      : F(f), UNITY(unity), size(n), dat(2 * n, unity) {}
+  SegTree(int n) : size(n), dat(2 * n, Monoid::unity()) {}
 
   // Sets i-th value (0-indexed) to x for initial setup.
   // build() must be called after set() calls.
-  void set(int i, const Monoid &x) { dat[size + i] = x; }
+  void set(int i, const T &x) { dat[size + i] = x; }
   void build() {
     for (int k = size - 1; k > 0; --k) {
-      dat[k] = F(dat[k * 2], dat[k * 2 + 1]);
+      dat[k] = Monoid::op(dat[k * 2], dat[k * 2 + 1]);
     }
   }
 
   // Sets i-th value (0-indexed) to x.
-  void update(int i, const Monoid &x) {
+  void update(int i, const T &x) {
     int k = size + i;
     dat[k] = x;
     while (k > 1) {
       k >>= 1;
-      dat[k] = F(dat[k * 2], dat[k * 2 + 1]);
+      dat[k] = Monoid::op(dat[k * 2], dat[k * 2 + 1]);
     }
   }
 
   // Queries by [l,r) range (0-indexed, open interval).
-  Monoid fold(int l, int r) {
+  T fold(int l, int r) {
     l += size;
     r += size;
-    Monoid vleft = UNITY, vright = UNITY;
+    T vleft = Monoid::unity(), vright = Monoid::unity();
     for (; l < r; l >>= 1, r >>= 1) {
-      if (l & 1) vleft = F(vleft, dat[l++]);
-      if (r & 1) vright = F(dat[--r], vright);
+      if (l & 1) vleft = Monoid::op(vleft, dat[l++]);
+      if (r & 1) vright = Monoid::op(dat[--r], vright);
     }
-    return F(vleft, vright);
+    return Monoid::op(vleft, vright);
   }
 
   // Queries by a single index (0-indexed).
-  Monoid operator[](int i) const { return dat[size + i]; }
+  T operator[](int i) const { return dat[size + i]; }
 };
 template <typename T>
-ostream &operator<<(ostream &os, const SegTree<T> &st) {
+std::ostream &operator<<(std::ostream &os, const SegTree<T> &st) {
   os << "[";
   for (int i = 0; i < st.size; ++i) {
     if (i != 0) os << ", ";
@@ -52,3 +49,11 @@ ostream &operator<<(ostream &os, const SegTree<T> &st) {
   }
   return os << "]";
 }
+
+// Monoid
+
+struct AddOp {
+  using T = mint;
+  static T op(const T &x, const T &y) { return x + y; }
+  static T unity() { return 0; }
+};
