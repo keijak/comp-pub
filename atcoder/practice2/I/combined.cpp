9f5d8b75e1ad89437f8b4c841f008729d4aff0c5
Keiji Maekawa <m@keijak.com>
2020-09-23 15:16:44 +0200

    alpc

---
+++ atcoder/practice2/I/combined.cpp
@@ -0,0 +1,375 @@
+#include <bits/stdc++.h>
+
+
+#include <algorithm>
+#include <cassert>
+#include <numeric>
+#include <string>
+#include <vector>
+
+namespace atcoder {
+
+namespace internal {
+
+std::vector<int> sa_naive(const std::vector<int>& s) {
+    int n = int(s.size());
+    std::vector<int> sa(n);
+    std::iota(sa.begin(), sa.end(), 0);
+    std::sort(sa.begin(), sa.end(), [&](int l, int r) {
+        if (l == r) return false;
+        while (l < n && r < n) {
+            if (s[l] != s[r]) return s[l] < s[r];
+            l++;
+            r++;
+        }
+        return l == n;
+    });
+    return sa;
+}
+
+std::vector<int> sa_doubling(const std::vector<int>& s) {
+    int n = int(s.size());
+    std::vector<int> sa(n), rnk = s, tmp(n);
+    std::iota(sa.begin(), sa.end(), 0);
+    for (int k = 1; k < n; k *= 2) {
+        auto cmp = [&](int x, int y) {
+            if (rnk[x] != rnk[y]) return rnk[x] < rnk[y];
+            int rx = x + k < n ? rnk[x + k] : -1;
+            int ry = y + k < n ? rnk[y + k] : -1;
+            return rx < ry;
+        };
+        std::sort(sa.begin(), sa.end(), cmp);
+        tmp[sa[0]] = 0;
+        for (int i = 1; i < n; i++) {
+            tmp[sa[i]] = tmp[sa[i - 1]] + (cmp(sa[i - 1], sa[i]) ? 1 : 0);
+        }
+        std::swap(tmp, rnk);
+    }
+    return sa;
+}
+
+// SA-IS, linear-time suffix array construction
+// Reference:
+// G. Nong, S. Zhang, and W. H. Chan,
+// Two Efficient Algorithms for Linear Time Suffix Array Construction
+template <int THRESHOLD_NAIVE = 10, int THRESHOLD_DOUBLING = 40>
+std::vector<int> sa_is(const std::vector<int>& s, int upper) {
+    int n = int(s.size());
+    if (n == 0) return {};
+    if (n == 1) return {0};
+    if (n == 2) {
+        if (s[0] < s[1]) {
+            return {0, 1};
+        } else {
+            return {1, 0};
+        }
+    }
+    if (n < THRESHOLD_NAIVE) {
+        return sa_naive(s);
+    }
+    if (n < THRESHOLD_DOUBLING) {
+        return sa_doubling(s);
+    }
+
+    std::vector<int> sa(n);
+    std::vector<bool> ls(n);
+    for (int i = n - 2; i >= 0; i--) {
+        ls[i] = (s[i] == s[i + 1]) ? ls[i + 1] : (s[i] < s[i + 1]);
+    }
+    std::vector<int> sum_l(upper + 1), sum_s(upper + 1);
+    for (int i = 0; i < n; i++) {
+        if (!ls[i]) {
+            sum_s[s[i]]++;
+        } else {
+            sum_l[s[i] + 1]++;
+        }
+    }
+    for (int i = 0; i <= upper; i++) {
+        sum_s[i] += sum_l[i];
+        if (i < upper) sum_l[i + 1] += sum_s[i];
+    }
+
+    auto induce = [&](const std::vector<int>& lms) {
+        std::fill(sa.begin(), sa.end(), -1);
+        std::vector<int> buf(upper + 1);
+        std::copy(sum_s.begin(), sum_s.end(), buf.begin());
+        for (auto d : lms) {
+            if (d == n) continue;
+            sa[buf[s[d]]++] = d;
+        }
+        std::copy(sum_l.begin(), sum_l.end(), buf.begin());
+        sa[buf[s[n - 1]]++] = n - 1;
+        for (int i = 0; i < n; i++) {
+            int v = sa[i];
+            if (v >= 1 && !ls[v - 1]) {
+                sa[buf[s[v - 1]]++] = v - 1;
+            }
+        }
+        std::copy(sum_l.begin(), sum_l.end(), buf.begin());
+        for (int i = n - 1; i >= 0; i--) {
+            int v = sa[i];
+            if (v >= 1 && ls[v - 1]) {
+                sa[--buf[s[v - 1] + 1]] = v - 1;
+            }
+        }
+    };
+
+    std::vector<int> lms_map(n + 1, -1);
+    int m = 0;
+    for (int i = 1; i < n; i++) {
+        if (!ls[i - 1] && ls[i]) {
+            lms_map[i] = m++;
+        }
+    }
+    std::vector<int> lms;
+    lms.reserve(m);
+    for (int i = 1; i < n; i++) {
+        if (!ls[i - 1] && ls[i]) {
+            lms.push_back(i);
+        }
+    }
+
+    induce(lms);
+
+    if (m) {
+        std::vector<int> sorted_lms;
+        sorted_lms.reserve(m);
+        for (int v : sa) {
+            if (lms_map[v] != -1) sorted_lms.push_back(v);
+        }
+        std::vector<int> rec_s(m);
+        int rec_upper = 0;
+        rec_s[lms_map[sorted_lms[0]]] = 0;
+        for (int i = 1; i < m; i++) {
+            int l = sorted_lms[i - 1], r = sorted_lms[i];
+            int end_l = (lms_map[l] + 1 < m) ? lms[lms_map[l] + 1] : n;
+            int end_r = (lms_map[r] + 1 < m) ? lms[lms_map[r] + 1] : n;
+            bool same = true;
+            if (end_l - l != end_r - r) {
+                same = false;
+            } else {
+                while (l < end_l) {
+                    if (s[l] != s[r]) {
+                        break;
+                    }
+                    l++;
+                    r++;
+                }
+                if (l == n || s[l] != s[r]) same = false;
+            }
+            if (!same) rec_upper++;
+            rec_s[lms_map[sorted_lms[i]]] = rec_upper;
+        }
+
+        auto rec_sa =
+            sa_is<THRESHOLD_NAIVE, THRESHOLD_DOUBLING>(rec_s, rec_upper);
+
+        for (int i = 0; i < m; i++) {
+            sorted_lms[i] = lms[rec_sa[i]];
+        }
+        induce(sorted_lms);
+    }
+    return sa;
+}
+
+}  // namespace internal
+
+std::vector<int> suffix_array(const std::vector<int>& s, int upper) {
+    assert(0 <= upper);
+    for (int d : s) {
+        assert(0 <= d && d <= upper);
+    }
+    auto sa = internal::sa_is(s, upper);
+    return sa;
+}
+
+template <class T> std::vector<int> suffix_array(const std::vector<T>& s) {
+    int n = int(s.size());
+    std::vector<int> idx(n);
+    iota(idx.begin(), idx.end(), 0);
+    sort(idx.begin(), idx.end(), [&](int l, int r) { return s[l] < s[r]; });
+    std::vector<int> s2(n);
+    int now = 0;
+    for (int i = 0; i < n; i++) {
+        if (i && s[idx[i - 1]] != s[idx[i]]) now++;
+        s2[idx[i]] = now;
+    }
+    return internal::sa_is(s2, now);
+}
+
+std::vector<int> suffix_array(const std::string& s) {
+    int n = int(s.size());
+    std::vector<int> s2(n);
+    for (int i = 0; i < n; i++) {
+        s2[i] = s[i];
+    }
+    return internal::sa_is(s2, 255);
+}
+
+// Reference:
+// T. Kasai, G. Lee, H. Arimura, S. Arikawa, and K. Park,
+// Linear-Time Longest-Common-Prefix Computation in Suffix Arrays and Its
+// Applications
+template <class T>
+std::vector<int> lcp_array(const std::vector<T>& s,
+                           const std::vector<int>& sa) {
+    int n = int(s.size());
+    assert(n >= 1);
+    std::vector<int> rnk(n);
+    for (int i = 0; i < n; i++) {
+        rnk[sa[i]] = i;
+    }
+    std::vector<int> lcp(n - 1);
+    int h = 0;
+    for (int i = 0; i < n; i++) {
+        if (h > 0) h--;
+        if (rnk[i] == 0) continue;
+        int j = sa[rnk[i] - 1];
+        for (; j + h < n && i + h < n; h++) {
+            if (s[j + h] != s[i + h]) break;
+        }
+        lcp[rnk[i] - 1] = h;
+    }
+    return lcp;
+}
+
+std::vector<int> lcp_array(const std::string& s, const std::vector<int>& sa) {
+    int n = int(s.size());
+    std::vector<int> s2(n);
+    for (int i = 0; i < n; i++) {
+        s2[i] = s[i];
+    }
+    return lcp_array(s2, sa);
+}
+
+// Reference:
+// D. Gusfield,
+// Algorithms on Strings, Trees, and Sequences: Computer Science and
+// Computational Biology
+template <class T> std::vector<int> z_algorithm(const std::vector<T>& s) {
+    int n = int(s.size());
+    if (n == 0) return {};
+    std::vector<int> z(n);
+    z[0] = 0;
+    for (int i = 1, j = 0; i < n; i++) {
+        int& k = z[i];
+        k = (j + z[j] <= i) ? 0 : std::min(j + z[j] - i, z[i - j]);
+        while (i + k < n && s[k] == s[i + k]) k++;
+        if (j + z[j] < i + z[i]) j = i;
+    }
+    z[0] = n;
+    return z;
+}
+
+std::vector<int> z_algorithm(const std::string& s) {
+    int n = int(s.size());
+    std::vector<int> s2(n);
+    for (int i = 0; i < n; i++) {
+        s2[i] = s[i];
+    }
+    return z_algorithm(s2);
+}
+
+}  // namespace atcoder
+
+
+using i64 = std::int64_t;
+using u64 = std::uint64_t;
+#define REP(i, n) for (int i = 0; i < (i64)(n); ++i)
+#define ALL(x) std::begin(x), std::end(x)
+#define SIZE(a) (int)((a).size())
+
+template <class T>
+inline bool chmax(T &a, T b) {
+  return a < b and ((a = std::move(b)), true);
+}
+template <class T>
+inline bool chmin(T &a, T b) {
+  return a > b and ((a = std::move(b)), true);
+}
+
+template <typename T>
+using V = std::vector<T>;
+template <typename T>
+std::vector<T> make_vec(size_t n, T a) {
+  return std::vector<T>(n, a);
+}
+template <typename... Ts>
+auto make_vec(size_t n, Ts... ts) {
+  return std::vector<decltype(make_vec(ts...))>(n, make_vec(ts...));
+}
+template <typename T>
+std::istream &operator>>(std::istream &is, std::vector<T> &a) {
+  for (auto &x : a) is >> x;
+  return is;
+}
+template <typename Container>
+std::ostream &pprint(const Container &a, std::string_view sep = " ",
+                     std::string_view ends = "\n", std::ostream *os = nullptr) {
+  if (os == nullptr) os = &std::cout;
+  auto b = std::begin(a), e = std::end(a);
+  for (auto it = std::begin(a); it != e; ++it) {
+    if (it != b) *os << sep;
+    *os << *it;
+  }
+  return *os << ends;
+}
+template <typename T, typename = void>
+struct is_iterable : std::false_type {};
+template <typename T>
+struct is_iterable<T, std::void_t<decltype(std::begin(std::declval<T>())),
+                                  decltype(std::end(std::declval<T>()))>>
+    : std::true_type {};
+
+template <typename T,
+          typename = std::enable_if_t<is_iterable<T>::value &&
+                                      !std::is_same<T, std::string>::value>>
+std::ostream &operator<<(std::ostream &os, const T &a) {
+  return pprint(a, ", ", "", &(os << "{")) << "}";
+}
+template <typename T, typename U>
+std::ostream &operator<<(std::ostream &os, const std::pair<T, U> &a) {
+  return os << "(" << a.first << ", " << a.second << ")";
+}
+
+#ifdef ENABLE_DEBUG
+template <typename T>
+void pdebug(const T &value) {
+  std::cerr << value;
+}
+template <typename T, typename... Ts>
+void pdebug(const T &value, const Ts &... args) {
+  pdebug(value);
+  std::cerr << ", ";
+  pdebug(args...);
+}
+#define DEBUG(...)                                   \
+  do {                                               \
+    std::cerr << " \033[33m (L" << __LINE__ << ") "; \
+    std::cerr << #__VA_ARGS__ << ":\033[0m ";        \
+    pdebug(__VA_ARGS__);                             \
+    std::cerr << std::endl;                          \
+  } while (0)
+#else
+#define pdebug(...)
+#define DEBUG(...)
+#endif
+
+using namespace std;
+using atcoder::lcp_array;
+using atcoder::suffix_array;
+
+int main() {
+  cin.tie(nullptr);
+  ios::sync_with_stdio(false);
+  string s;
+  cin >> s;
+  int n = SIZE(s);
+  auto sa = suffix_array(s);
+  auto lcpa = lcp_array(s, sa);
+
+  DEBUG(s.size(), sa.size(), lcpa.size());
+  i64 ans = n * (i64)(n + 1) / 2;
+  REP(i, lcpa.size()) { ans -= lcpa[i]; }
+  cout << ans << endl;
+}
--- lib/segtree.hpp
+++ lib/segtree.hpp
@@ -1,49 +1,117 @@
+#include <iostream>
+#include <vector>
+
 template <typename Monoid>
 struct SegTree {
   using T = typename Monoid::T;
-  int size;
-  std::vector<T> dat;
 
-  SegTree(int n) : size(n), dat(2 * n, Monoid::unity()) {}
+  explicit SegTree(int n) : size_(n), data_(2 * n, Monoid::unity()) {}
 
-  // Sets i-th value (0-indexed) to x for initial setup.
-  // build() must be called after set() calls.
-  void set(int i, const T &x) { dat[size + i] = x; }
-  void build() {
-    for (int k = size - 1; k > 0; --k) {
-      dat[k] = Monoid::op(dat[k * 2], dat[k * 2 + 1]);
+  inline int size() const { return size_; }
+
+  // Initializes the data array.
+  void init(const std::vector<T> &leaves) {
+    assert(int(leaves.size()) == size_);
+    for (int i = 0; i < size_; ++i) {
+      data_[size_ + i] = leaves[i];
+    }
+    for (int k = size_ - 1; k > 0; --k) {
+      data_[k] = Monoid::op(data_[k * 2], data_[k * 2 + 1]);
     }
   }
 
   // Sets i-th value (0-indexed) to x.
   void update(int i, const T &x) {
-    int k = size + i;
-    dat[k] = x;
+    int k = size_ + i;
+    data_[k] = x;
     while (k > 1) {
       k >>= 1;
-      dat[k] = Monoid::op(dat[k * 2], dat[k * 2 + 1]);
+      data_[k] = Monoid::op(data_[k * 2], data_[k * 2 + 1]);
     }
   }
 
-  // Queries by [l,r) range (0-indexed, open interval).
-  T fold(int l, int r) {
-    l += size;
-    r += size;
+  // Queries by [l,r) range (0-indexed, half-open interval).
+  T fold(int l, int r) const {
+    l += size_;
+    r += size_;
     T vleft = Monoid::unity(), vright = Monoid::unity();
     for (; l < r; l >>= 1, r >>= 1) {
-      if (l & 1) vleft = Monoid::op(vleft, dat[l++]);
-      if (r & 1) vright = Monoid::op(dat[--r], vright);
+      if (l & 1) vleft = Monoid::op(vleft, data_[l++]);
+      if (r & 1) vright = Monoid::op(data_[--r], vright);
     }
     return Monoid::op(vleft, vright);
   }
 
   // Queries by a single index (0-indexed).
-  T operator[](int i) const { return dat[size + i]; }
+  T operator[](int i) const { return data_[size_ + i]; }
+
+  template <bool (*pred)(const T &)>
+  int max_right(int l) {
+    return max_right(l, [](const T &x) -> bool { return pred(x); });
+  }
+  template <class Predicate>
+  int max_right(int l, Predicate pred) {
+    assert(0 <= l && l <= size_);
+    assert(predicate(Monoid::unity()));
+    if (l == size_) return size_;
+    l += size_;
+    T sm = Monoid::unity();
+    do {
+      while (l % 2 == 0) l >>= 1;
+      if (!pred(Monoid::op(sm, data_[l]))) {
+        while (l < size_) {
+          l = (2 * l);
+          if (pred(Monoid::op(sm, data_[l]))) {
+            sm = Monoid::op(sm, data_[l]);
+            l++;
+          }
+        }
+        return l - size_;
+      }
+      sm = Monoid::op(sm, data_[l]);
+      l++;
+    } while ((l & -l) != l);
+    return size_;
+  }
+
+  template <bool (*pred)(const T &)>
+  int min_left(int r) {
+    return min_left(r, [](const T &x) -> bool { return pred(x); });
+  }
+  template <class Predicate>
+  int min_left(int r, Predicate pred) {
+    assert(0 <= r && r <= size_);
+    assert(pred(Monoid::unity()));
+    if (r == 0) return 0;
+    r += size_;
+    T sm = Monoid::unity();
+    do {
+      r--;
+      while (r > 1 && (r % 2)) r >>= 1;
+      if (!pred(Monoid::op(data_[r], sm))) {
+        while (r < size_) {
+          r = (2 * r + 1);
+          if (pred(Monoid::op(data_[r], sm))) {
+            sm = Monoid::op(data_[r], sm);
+            r--;
+          }
+        }
+        return r + 1 - size_;
+      }
+      sm = Monoid::op(data_[r], sm);
+    } while ((r & -r) != r);
+    return 0;
+  }
+
+ private:
+  int size_;
+  std::vector<T> data_;
 };
+
 template <typename T>
 std::ostream &operator<<(std::ostream &os, const SegTree<T> &st) {
   os << "[";
-  for (int i = 0; i < st.size; ++i) {
+  for (int i = 0; i < st.size_; ++i) {
     if (i != 0) os << ", ";
     os << st[i];
   }
