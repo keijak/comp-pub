3e6e28323d86cec5f3f566b6cbb5aab57d216a2c
Keiji Maekawa <m@keijak.com>
2020-09-23 10:31:21 +0200

    virus tree

---
+++ atcoder/abc133/E/main.cpp
@@ -0,0 +1,163 @@
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+using u64 = unsigned long long;
+#define REP(i, n) for (int i = 0; (i64)(i) < (i64)(n); ++i)
+
+template <typename T>
+using V = std::vector<T>;
+template <typename T>
+vector<T> make_vec(size_t n, T a) {
+  return vector<T>(n, a);
+}
+template <typename... Ts>
+auto make_vec(size_t n, Ts... ts) {
+  return vector<decltype(make_vec(ts...))>(n, make_vec(ts...));
+}
+template <typename Container>
+ostream &pprint(const Container &a, string_view sep = " ",
+                string_view ends = "\n", ostream *os = nullptr) {
+  if (os == nullptr) os = &cout;
+  auto b = std::begin(a), e = std::end(a);
+  for (auto it = std::begin(a); it != e; ++it) {
+    if (it != b) *os << sep;
+    *os << *it;
+  }
+  return *os << ends;
+}
+template <typename T>
+ostream &operator<<(ostream &os, const vector<T> &a) {
+  return pprint(a, ",", "", &(os << "[")) << "]";
+}
+template <typename T>
+ostream &operator<<(ostream &os, const set<T> &a) {
+  return pprint(a, ",", "", &(os << "{")) << "}";
+}
+template <typename T, typename U>
+ostream &operator<<(ostream &os, const map<T, U> &a) {
+  return pprint(a, ", ", "", &(os << "{")) << "}";
+}
+template <typename T, typename U>
+ostream &operator<<(ostream &os, const pair<T, U> &a) {
+  return os << "(" << a.first << ", " << a.second << ")";
+}
+
+#ifdef ENABLE_DEBUG
+template <typename T>
+void debug(T value) {
+  cerr << value;
+}
+template <typename T, typename... Ts>
+void debug(T value, Ts... args) {
+  cerr << value << ", ";
+  debug(args...);
+}
+#define DEBUG(...)                              \
+  do {                                          \
+    cerr << " \033[33m (L" << __LINE__ << ") "; \
+    cerr << #__VA_ARGS__ << ":\033[0m ";        \
+    debug(__VA_ARGS__);                         \
+    cerr << endl;                               \
+  } while (0)
+#else
+#define debug(...)
+#define DEBUG(...)
+#endif
+
+const i64 MOD = 1'000'000'007;
+
+struct mint {
+  long long x;
+  mint(long long x = 0) : x((x % MOD + MOD) % MOD) {}
+  mint operator-() const { return mint(-x); }
+  mint &operator+=(const mint &a) {
+    if ((x += a.x) >= MOD) x -= MOD;
+    return *this;
+  }
+  mint &operator-=(const mint &a) {
+    if ((x += MOD - a.x) >= MOD) x -= MOD;
+    return *this;
+  }
+  mint &operator*=(const mint &a) {
+    (x *= a.x) %= MOD;
+    return *this;
+  }
+  mint pow(long long t) const {
+    if (!t) return 1;
+    mint a = pow(t >> 1);
+    a *= a;
+    if (t & 1) a *= *this;
+    return a;
+  }
+  // for prime MOD
+  mint inv() const { return pow(MOD - 2); }
+  mint &operator/=(const mint &a) { return *this *= a.inv(); }
+};
+mint operator+(const mint &a, const mint &b) { return mint(a) += b; }
+mint operator-(const mint &a, const mint &b) { return mint(a) -= b; }
+mint operator*(const mint &a, const mint &b) { return mint(a) *= b; }
+mint operator/(const mint &a, const mint &b) { return mint(a) /= b; }
+bool operator==(const mint &a, const mint &b) { return a.x == b.x; }
+bool operator!=(const mint &a, const mint &b) { return a.x != b.x; }
+bool operator<(const mint &a, const mint &b) { return a.x < b.x; }
+bool operator>(const mint &a, const mint &b) { return a.x > b.x; }
+bool operator<=(const mint &a, const mint &b) { return a.x <= b.x; }
+bool operator>=(const mint &a, const mint &b) { return a.x >= b.x; }
+istream &operator>>(istream &is, mint &a) { return is >> a.x; }
+ostream &operator<<(ostream &os, const mint &a) { return os << a.x; }
+
+struct Factorials {
+  // factorials and inverse factorials.
+  vector<mint> fact, ifact;
+
+  // n: max cached value.
+  Factorials(int n) : fact(n + 1), ifact(n + 1) {
+    assert(n < MOD);
+    fact[0] = 1;
+    for (int i = 1; i <= n; ++i) fact[i] = fact[i - 1] * i;
+    ifact[n] = fact[n].inv();
+    for (int i = n; i >= 1; --i) ifact[i - 1] = ifact[i] * i;
+  }
+
+  // Combination (nCk)
+  mint C(int n, int k) {
+    if (k < 0 || k > n) return 0;
+    return fact[n] * ifact[k] * ifact[n - k];
+  }
+
+  // Permutation (nPk)
+  mint P(int n, int k) {
+    if (k < 0 || k > n) return 0;
+    return fact[n] * ifact[n - k];
+  }
+};
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int N, K;
+  cin >> N >> K;
+  V<V<int>> G(N);
+  REP(i, N - 1) {
+    int a, b;
+    cin >> a >> b;
+    a--;
+    b--;
+    G[a].push_back(b);
+    G[b].push_back(a);
+  }
+  Factorials fs(100010);
+  auto solve = [&](auto self, int v, int p) -> mint {
+    int m = int(G[v].size()) - (p >= 0 ? 1 : 0);
+    int n = K - (p >= 0 ? 2 : 1);
+    if (n < 0) return 0;
+    mint ans = fs.P(n, m);
+    for (int u : G[v]) {
+      if (u == p) continue;
+      ans *= self(self, u, v);
+    }
+    return ans;
+  };
+  mint ans = solve(solve, 0, -1) * mint(K);
+  cout << ans << endl;
+}
