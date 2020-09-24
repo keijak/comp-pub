b8ee1e34edb5ad9090498b13b2c1f131b896b38c
Keiji Maekawa <m@keijak.com>
2020-09-24 11:44:16 +0200

    modint extgcd

--- lib/modint.hpp
+++ lib/modint.hpp
@@ -1,33 +1,71 @@
-const int MOD = 1'000'000'007;
 
-struct mint {
-  int x;
+// Extended Euclidean algorithm
+// Returns gcd(a,b).
+// x and y are set to satisfy `a*x + b*y == gcd(a,b)`
+long long ext_gcd(long long a, long long b, long long &x, long long &y) {
+  if (b == 0) {
+    x = 1;
+    y = 0;
+    return a;
+  }
+  long long d = ext_gcd(b, a % b, y, x);
+  y -= a / b * x;
+  return d;
+}
 
-  constexpr mint(long long val = 0) : x(0) {
+template <unsigned int M>
+struct ModInt {
+  constexpr ModInt(long long val = 0) : _v(0) {
     if (val < 0) {
-      long long k = (abs(val) + MOD - 1) / MOD;
-      val += k * MOD;
+      long long k = (abs(val) + M - 1) / M;
+      val += k * M;
     }
     assert(val >= 0);
-    x = val % MOD;
+    _v = val % M;
   }
-  constexpr mint operator-() const { return mint(-x); }
-  constexpr mint &operator+=(const mint &a) {
-    if ((x += a.x) >= MOD) x -= MOD;
+
+  static constexpr int mod() { return M; }
+  static constexpr unsigned int umod() { return M; }
+  inline unsigned int val() const { return _v; }
+
+  ModInt &operator++() {
+    _v++;
+    if (_v == umod()) _v = 0;
     return *this;
   }
-  constexpr mint &operator-=(const mint &a) {
-    if ((x += MOD - a.x) >= MOD) x -= MOD;
+  ModInt &operator--() {
+    if (_v == 0) _v = umod();
+    _v--;
     return *this;
   }
-  constexpr mint &operator*=(const mint &a) {
-    x = (x * (long long)a.x) % MOD;
+  ModInt operator++(int) {
+    auto result = *this;
+    ++*this;
+    return result;
+  }
+  ModInt operator--(int) {
+    auto result = *this;
+    --*this;
+    return result;
+  }
+
+  constexpr ModInt operator-() const { return ModInt(-_v); }
+  constexpr ModInt &operator+=(const ModInt &a) {
+    if ((_v += a._v) >= M) _v -= M;
     return *this;
   }
-  constexpr mint pow(long long t) const {
+  constexpr ModInt &operator-=(const ModInt &a) {
+    if ((_v += M - a._v) >= M) _v -= M;
+    return *this;
+  }
+  constexpr ModInt &operator*=(const ModInt &a) {
+    _v = ((unsigned long long)(_v)*a._v) % M;
+    return *this;
+  }
+  constexpr ModInt pow(unsigned long long t) const {
     assert(t >= 0);
-    mint base = *this;
-    mint res = 1;
+    ModInt base = *this;
+    ModInt res = 1;
     while (t) {
       if (t & 1) res *= base;
       base *= base;
@@ -35,19 +73,62 @@ struct mint {
     }
     return res;
   }
-  // for prime MOD
-  constexpr mint inv() const { return pow(MOD - 2); }
-  constexpr mint &operator/=(const mint &a) { return *this *= a.inv(); }
+
+  // Inverse by Extended Euclidean algorithm.
+  // M doesn't need to be prime, but x and M must be coprime.
+  constexpr ModInt inv() const {
+    assert(_v != 0);
+    long long x, y;
+    long long g = ext_gcd(_v, M, x, y);
+    assert(g == 1LL);  // gcd(_v, M) must be 1.
+    return x;
+  }
+  constexpr ModInt &operator/=(const ModInt &a) { return *this *= a.inv(); }
+
+  friend constexpr ModInt operator+(const ModInt &a, const ModInt &b) {
+    return ModInt(a) += b;
+  }
+  friend constexpr ModInt operator-(const ModInt &a, const ModInt &b) {
+    return ModInt(a) -= b;
+  }
+  friend constexpr ModInt operator*(const ModInt &a, const ModInt &b) {
+    return ModInt(a) *= b;
+  }
+  friend constexpr ModInt operator/(const ModInt &a, const ModInt &b) {
+    return ModInt(a) /= b;
+  }
+  friend constexpr bool operator==(const ModInt &a, const ModInt &b) {
+    return a._v == b._v;
+  }
+  friend constexpr bool operator!=(const ModInt &a, const ModInt &b) {
+    return a._v != b._v;
+  }
+  friend constexpr bool operator<(const ModInt &a, const ModInt &b) {
+    return a._v < b._v;
+  }
+  friend constexpr bool operator>(const ModInt &a, const ModInt &b) {
+    return a._v > b._v;
+  }
+  friend constexpr bool operator<=(const ModInt &a, const ModInt &b) {
+    return a._v <= b._v;
+  }
+  friend constexpr bool operator>=(const ModInt &a, const ModInt &b) {
+    return a._v >= b._v;
+  }
+  friend std::istream &operator>>(std::istream &is, ModInt &a) {
+    return is >> a._v;
+  }
+  friend std::ostream &operator<<(std::ostream &os, const ModInt &a) {
+    return os << a._v;
+  }
+
+ private:
+  unsigned int _v;
 };
-constexpr mint operator+(const mint &a, const mint &b) { return mint(a) += b; }
-constexpr mint operator-(const mint &a, const mint &b) { return mint(a) -= b; }
-constexpr mint operator*(const mint &a, const mint &b) { return mint(a) *= b; }
-constexpr mint operator/(const mint &a, const mint &b) { return mint(a) /= b; }
-constexpr bool operator==(const mint &a, const mint &b) { return a.x == b.x; }
-constexpr bool operator!=(const mint &a, const mint &b) { return a.x != b.x; }
-constexpr bool operator<(const mint &a, const mint &b) { return a.x < b.x; }
-constexpr bool operator>(const mint &a, const mint &b) { return a.x > b.x; }
-constexpr bool operator<=(const mint &a, const mint &b) { return a.x <= b.x; }
-constexpr bool operator>=(const mint &a, const mint &b) { return a.x >= b.x; }
-istream &operator>>(istream &is, mint &a) { return is >> a.x; }
-ostream &operator<<(ostream &os, const mint &a) { return os << a.x; }
+const unsigned int MOD = 1'000'000'007;
+using Mint = ModInt<MOD>;
+
+// Runtime MOD:
+//   unsigned int MOD = 1'000'000'007;  // modifiable.
+//   template <unsigned int& M> struct ModInt { ... };
+//   using Mint = ModInt<MOD>;
