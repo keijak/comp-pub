79dcf5c14ff6c47e95f1d4b9078c578903396824
Keiji Maekawa <m@keijak.com>
2020-09-24 18:46:08 +0200

    lazysegtree

--- lib/monoids.hpp
+++ lib/monoids.hpp
@@ -1,23 +1,23 @@
 struct Min {
-  using T = int;
+  using T = long long;
   static T op(const T &x, const T &y) { return std::min(x, y); }
   static constexpr T id() { return std::numeric_limits<T>::max(); }
 };
 
 struct Max {
-  using T = int;
+  using T = long long;
   static T op(const T &x, const T &y) { return std::max(x, y); }
   static constexpr T id() { return std::numeric_limits<T>::min(); }
 };
 
 struct Sum {
-  using T = int;
+  using T = long long;
   static T op(const T &x, const T &y) { return x + y; }
   static constexpr T id() { return 0; }
 };
 
 struct Prod {
-  using T = int;
+  using T = long long;
   static T op(const T &x, const T &y) { return x * y; }
   static constexpr T id() { return 1; }
 };
@@ -31,7 +31,7 @@ struct Assign {
 };
 
 struct GCD {
-  using T = int;
+  using T = long long;
   static T op(const T &x, const T &y) {
     if (x == id()) return y;
     if (y == id()) return x;
@@ -41,97 +41,131 @@ struct GCD {
 };
 
 struct AddMin {
-  // Fold: Min
   using T = long long;
+  using F = long long;
+
+  // Fold: Min
   static T op(const T &x, const T &y) { return std::min(x, y); }
   static constexpr T id() { return std::numeric_limits<T>::max(); }
 
   // Update: Add
-  using F = long long;
-  static T apply(const F &f, const T &x) { return f + x; }
-  static F compose(const F &f, const F &g) { return f + g; }
-  static constexpr F id_f() { return 0; }
+  static T f_apply(const F &f, const T &x) { return f + x; }
+  static F f_compose(const F &f, const F &g) { return f + g; }
+  static constexpr F f_id() { return 0; }
 };
 
 struct AddMax {
+  using T = long long;
+  using F = long long;
+
   // Fold: Max
-  using T = int;
   static T op(const T &x, const T &y) { return std::max(x, y); }
   static constexpr T id() { return std::numeric_limits<T>::min(); }
 
   // Update: Add
-  using F = long long;
-  static T apply(const F &f, const T &x) { return f + x; }
-  static F compose(const F &f, const F &g) { return f + g; }
-  static constexpr F id_f() { return 0; }
+  static T f_apply(const F &f, const T &x) { return f + x; }
+  static F f_compose(const F &f, const F &g) { return f + g; }
+  static constexpr F f_id() { return 0; }
 };
 
 struct AddSum {
-  // Fold: Sum
   struct T {
     long long sum;
     int width;
   };
+  using F = long long;
+
+  // Fold: Sum
   static T op(const T &x, const T &y) {
     return {x.sum + y.sum, x.width + y.width};
   }
   static constexpr T id() { return {0, 0}; }
 
   // Update: Add
-  using F = long long;
-  static T apply(const F &f, const T &x) {
+  static T f_apply(const F &f, const T &x) {
     return {x.sum + f * x.width, x.width};
   }
-  static F compose(const F &f, const F &g) { return f + g; }
-  static constexpr F id_f() { return 0; }
+  static F f_compose(const F &f, const F &g) { return f + g; }
+  static constexpr F f_id() { return 0; }
 };
 
 struct AssignMin {
-  // Fold: Min
   using T = long long;
+  using F = std::optional<long long>;
+
+  // Fold: Min
   static T op(const T &x, const T &y) { return std::min(x, y); }
   static constexpr T id() { return std::numeric_limits<T>::max(); }
 
   // Update: Assign
-  using F = std::optional<long long>;
-  static T apply(const F &f, const T &x) {
+  static T f_apply(const F &f, const T &x) {
     return f.has_value() ? f.value() : x;
   }
-  static F compose(const F &f, const F &g) { return f.has_value() ? f : g; }
-  static constexpr F id_f() { return std::nullopt; }
+  static F f_compose(const F &f, const F &g) { return f.has_value() ? f : g; }
+  static constexpr F f_id() { return std::nullopt; }
 };
 
 struct AssignMax {
+  using T = long long;
+  using F = std::optional<long long>;
+
   // Fold: Max
-  using T = int;
   static T op(const T &x, const T &y) { return std::max(x, y); }
   static constexpr T id() { return std::numeric_limits<T>::min(); }
 
   // Update: Assign
-  using F = std::optional<long long>;
-  static T apply(const F &f, const T &x) {
+  static T f_apply(const F &f, const T &x) {
     return f.has_value() ? f.value() : x;
   }
-  static F compose(const F &f, const F &g) { return f.has_value() ? f : g; }
-  static constexpr F id_f() { return std::nullopt; }
+  static F f_compose(const F &f, const F &g) { return f.has_value() ? f : g; }
+  static constexpr F f_id() { return std::nullopt; }
 };
 
 struct AssignSum {
-  // Fold: Sum
   struct T {
     long long sum;
     int width;
   };
+  using F = std::optional<long long>;
+
+  // Fold: Sum
   static T op(const T &x, const T &y) {
     return {x.sum + y.sum, x.width + y.width};
   }
   static constexpr T id() { return {0, 0}; }
 
   // Update: Assign
-  using F = std::optional<long long>;
-  static T apply(const F &f, const T &x) {
+  static T f_apply(const F &f, const T &x) {
     return f.has_value() ? {f.value() * x.width, x.width} : x;
   }
-  static F compose(const F &f, const F &g) { return f.has_value() ? f : g; }
-  static constexpr F id_f() { return std::nullopt; }
+  static F f_compose(const F &f, const F &g) { return f.has_value() ? f : g; }
+  static constexpr F f_id() { return std::nullopt; }
+};
+
+struct MinMin {
+  using T = long long;
+  using F = long long;
+
+  // Fold: Min
+  static T op(const T &x, const T &y) { return std::min(x, y); }
+  static constexpr T id() { return std::numeric_limits<T>::max(); }
+
+  // Update: Min
+  static T f_apply(const F &f, const T &x) { return std::min(f, x); }
+  static F f_compose(const F &f, const F &g) { return std::min(f, g); }
+  static constexpr F f_id() { return std::numeric_limits<T>::max(); }
+};
+
+struct MaxMax {
+  using T = long long;
+  using F = long long;
+
+  // Fold: Max
+  static T op(const T &x, const T &y) { return std::max(x, y); }
+  static constexpr T id() { return std::numeric_limits<T>::min(); }
+
+  // Update: Max
+  static T f_apply(const F &f, const T &x) { return std::max(f, x); }
+  static F f_compose(const F &f, const F &g) { return std::max(f, g); }
+  static constexpr F f_id() { return std::numeric_limits<T>::min(); }
 };
