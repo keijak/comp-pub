4f8d7644de715e262b19f9c091dc66fec34be423
Keiji Maekawa <m@keijak.com>
2020-09-24 16:02:58 +0200

    lazysegtree

--- lib/lazysegtree.hpp
+++ lib/lazysegtree.hpp
@@ -1,133 +1,180 @@
-template <typename Monoid, typename OperatorMonoid = Monoid>
-struct LazySegmentTree {
-  using AggFn = function<Monoid(Monoid, Monoid)>;
-  using UpdateFn = function<OperatorMonoid(OperatorMonoid, OperatorMonoid)>;
-  using CombineFn = function<Monoid(Monoid, OperatorMonoid)>;
 
-  int size, height;
-  vector<Monoid> data;
-  vector<OperatorMonoid> lazy;
-  const AggFn agg_;          // aggregator
-  const UpdateFn update_;    // updater
-  const CombineFn combine_;  // combiner
-  const Monoid M1;
-  const OperatorMonoid OM0;
+template <typename OpMonoid>
+struct LazySegTree {
+  using T = typename OpMonoid::T;
+  using F = typename OpMonoid::F;
 
-  LazySegmentTree(int n, const AggFn f, const UpdateFn g, const CombineFn h,
-                  const Monoid &M1, const OperatorMonoid OM0)
-      : agg_(f), update_(g), combine_(h), M1(M1), OM0(OM0) {
-    size = 1;
-    height = 0;
-    while (size < n) size <<= 1, height++;
-    data.assign(2 * size, M1);
-    lazy.assign(2 * size, OM0);
-  }
+  inline int n() const { return n_; }
+  inline int offset() const { return offset_; }
 
-  void set(int k, const Monoid &x) { data[k + size] = x; }
+  explicit LazySegTree(int n)
+      : LazySegTree(std::vector<T>(n, OpMonoid::id())) {}
 
-  void build() {
-    for (int k = size - 1; k > 0; k--) {
-      data[k] = agg_(data[2 * k + 0], data[2 * k + 1]);
+  explicit LazySegTree(const std::vector<T> &v) : n_(int(v.size())) {
+    offset_ = 1;
+    for (bits_ = 0; offset_ < n_; ++bits_) {
+      offset_ <<= 1;
+    }
+    data_.assign(2 * offset_, OpMonoid::id());
+    lazy_ = std::vector<F>(offset_, OpMonoid::id_f());
+    for (int i = 0; i < n_; i++) data_[offset_ + i] = v[i];
+    for (int i = offset_ - 1; i >= 1; i--) {
+      update(i);
     }
   }
 
-  inline void propagate(int k) {
-    if (lazy[k] != OM0) {
-      lazy[2 * k + 0] = update_(lazy[2 * k + 0], lazy[k]);
-      lazy[2 * k + 1] = update_(lazy[2 * k + 1], lazy[k]);
-      data[k] = apply(k);
-      lazy[k] = OM0;
+  void set(int p, T x) {
+    assert(0 <= p && p < n_);
+    p += offset_;
+    for (int i = bits_; i >= 1; i--) push(p >> i);
+    data_[p] = x;
+    for (int i = 1; i <= bits_; i++) update(p >> i);
+  }
+
+  T &operator[](int p) {
+    assert(0 <= p && p < n_);
+    p += offset_;
+    for (int i = bits_; i >= 1; i--) push(p >> i);
+    return data_[p];
+  }
+
+  T fold(int l, int r) {
+    assert(0 <= l && l <= r && r <= n_);
+    if (l == r) return OpMonoid::id();
+
+    l += offset_;
+    r += offset_;
+
+    for (int i = bits_; i >= 1; i--) {
+      if (((l >> i) << i) != l) push(l >> i);
+      if (((r >> i) << i) != r) push(r >> i);
     }
-  }
 
-  inline Monoid apply(int k) {
-    return lazy[k] == OM0 ? data[k] : combine_(data[k], lazy[k]);
-  }
-
-  inline void recalc(int k) {
-    while (k >>= 1) data[k] = agg_(apply(2 * k + 0), apply(2 * k + 1));
-  }
-
-  inline void thrust(int k) {
-    for (int i = height; i > 0; i--) propagate(k >> i);
-  }
-
-  void update(int a, int b, const OperatorMonoid &x) {
-    if (a >= b) return;
-    thrust(a += size);
-    thrust(b += size - 1);
-    for (int l = a, r = b + 1; l < r; l >>= 1, r >>= 1) {
-      if (l & 1) lazy[l] = update_(lazy[l], x), ++l;
-      if (r & 1) --r, lazy[r] = update_(lazy[r], x);
+    T sml = OpMonoid::id(), smr = OpMonoid::id();
+    while (l < r) {
+      if (l & 1) sml = OpMonoid::op(sml, data_[l++]);
+      if (r & 1) smr = OpMonoid::op(data_[--r], smr);
+      l >>= 1;
+      r >>= 1;
     }
-    recalc(a);
-    recalc(b);
+
+    return OpMonoid::op(sml, smr);
   }
 
-  Monoid fold(int a, int b) {
-    if (a >= b) return M1;
-    thrust(a += size);
-    thrust(b += size - 1);
-    Monoid L = M1, R = M1;
-    for (int l = a, r = b + 1; l < r; l >>= 1, r >>= 1) {
-      if (l & 1) L = agg_(L, apply(l++));
-      if (r & 1) R = agg_(apply(--r), R);
-    }
-    return agg_(L, R);
+  void apply(int p, F f) {
+    assert(0 <= p && p < n_);
+    p += offset_;
+    for (int i = bits_; i >= 1; i--) push(p >> i);
+    data_[p] = OpMonoid::apply(f, data_[p]);
+    for (int i = 1; i <= bits_; i++) update(p >> i);
   }
+  void apply(int l, int r, F f) {
+    assert(0 <= l && l <= r && r <= n_);
+    if (l == r) return;
 
-  Monoid operator[](const int &k) { return fold(k, k + 1); }
+    l += offset_;
+    r += offset_;
 
-  template <typename C>
-  int find_subtree(int a, const C &check, Monoid &M, bool type) {
-    while (a < size) {
-      propagate(a);
-      Monoid nxt =
-          type ? agg_(apply(2 * a + type), M) : agg_(M, apply(2 * a + type));
-      if (check(nxt))
-        a = 2 * a + type;
-      else
-        M = nxt, a = 2 * a + 1 - type;
+    for (int i = bits_; i >= 1; i--) {
+      if (((l >> i) << i) != l) push(l >> i);
+      if (((r >> i) << i) != r) push((r - 1) >> i);
     }
-    return a - size;
-  }
 
-  template <typename C>
-  int find_first(int a, const C &check) {
-    Monoid L = M1;
-    if (a <= 0) {
-      if (check(agg_(L, apply(1)))) return find_subtree(1, check, L, false);
-      return -1;
-    }
-    thrust(a + size);
-    int b = size;
-    for (a += size, b += size; a < b; a >>= 1, b >>= 1) {
-      if (a & 1) {
-        Monoid nxt = agg_(L, apply(a));
-        if (check(nxt)) return find_subtree(a, check, L, false);
-        L = nxt;
-        ++a;
+    {
+      int l2 = l, r2 = r;
+      while (l < r) {
+        if (l & 1) all_apply(l++, f);
+        if (r & 1) all_apply(--r, f);
+        l >>= 1;
+        r >>= 1;
       }
+      l = l2;
+      r = r2;
     }
-    return -1;
+
+    for (int i = 1; i <= bits_; i++) {
+      if (((l >> i) << i) != l) update(l >> i);
+      if (((r >> i) << i) != r) update((r - 1) >> i);
+    }
   }
 
-  template <typename C>
-  int find_last(int b, const C &check) {
-    Monoid R = M1;
-    if (b >= size) {
-      if (check(agg_(apply(1), R))) return find_subtree(1, check, R, true);
-      return -1;
-    }
-    thrust(b + size - 1);
-    int a = size;
-    for (b += size; a < b; a >>= 1, b >>= 1) {
-      if (b & 1) {
-        Monoid nxt = agg_(apply(--b), R);
-        if (check(nxt)) return find_subtree(b, check, R, true);
-        R = nxt;
-      }
-    }
-    return -1;
+  template <bool (*g)(T)>
+  int max_right(int l) {
+    return max_right(l, [](T x) { return g(x); });
   }
+  template <class G>
+  int max_right(int l, G g) {
+    assert(0 <= l && l <= n_);
+    assert(g(OpMonoid::id()));
+    if (l == n_) return n_;
+    l += offset_;
+    for (int i = bits_; i >= 1; i--) push(l >> i);
+    T sm = OpMonoid::id();
+    do {
+      while (l % 2 == 0) l >>= 1;
+      if (!g(OpMonoid::op(sm, data_[l]))) {
+        while (l < offset_) {
+          push(l);
+          l = (2 * l);
+          if (g(OpMonoid::op(sm, data_[l]))) {
+            sm = OpMonoid::op(sm, data_[l]);
+            l++;
+          }
+        }
+        return l - offset_;
+      }
+      sm = OpMonoid::op(sm, data_[l]);
+      l++;
+    } while ((l & -l) != l);
+    return n_;
+  }
+
+  template <bool (*g)(T)>
+  int min_left(int r) {
+    return min_left(r, [](T x) { return g(x); });
+  }
+  template <class G>
+  int min_left(int r, G g) {
+    assert(0 <= r && r <= n_);
+    assert(g(OpMonoid::id()));
+    if (r == 0) return 0;
+    r += offset_;
+    for (int i = bits_; i >= 1; i--) push((r - 1) >> i);
+    T sm = OpMonoid::id();
+    do {
+      r--;
+      while (r > 1 && (r % 2)) r >>= 1;
+      if (!g(OpMonoid::op(data_[r], sm))) {
+        while (r < offset_) {
+          push(r);
+          r = (2 * r + 1);
+          if (g(OpMonoid::op(data_[r], sm))) {
+            sm = OpMonoid::op(data_[r], sm);
+            r--;
+          }
+        }
+        return r + 1 - offset_;
+      }
+      sm = OpMonoid::op(data_[r], sm);
+    } while ((r & -r) != r);
+    return 0;
+  }
+
+ private:
+  void update(int k) {
+    data_[k] = OpMonoid::op(data_[2 * k], data_[2 * k + 1]);
+  }
+  void all_apply(int k, F f) {
+    data_[k] = OpMonoid::apply(f, data_[k]);
+    if (k < offset_) lazy_[k] = OpMonoid::compose(f, lazy_[k]);
+  }
+  void push(int k) {
+    all_apply(2 * k, lazy_[k]);
+    all_apply(2 * k + 1, lazy_[k]);
+    lazy_[k] = OpMonoid::id_f();
+  }
+
+  int n_, offset_, bits_;
+  std::vector<T> data_;
+  std::vector<F> lazy_;
 };
--- lib/monoids.hpp
+++ lib/monoids.hpp
@@ -1,22 +1,22 @@
-struct MinOp {
+struct Min {
   using T = int;
   static T op(const T &x, const T &y) { return std::min(x, y); }
   static constexpr T id() { return std::numeric_limits<T>::max(); }
 };
 
-struct MaxOp {
+struct Max {
   using T = int;
   static T op(const T &x, const T &y) { return std::max(x, y); }
   static constexpr T id() { return std::numeric_limits<T>::min(); }
 };
 
-struct AddOp {
+struct Sum {
   using T = int;
   static T op(const T &x, const T &y) { return x + y; }
   static constexpr T id() { return 0; }
 };
 
-struct MulOp {
+struct Prod {
   using T = int;
   static T op(const T &x, const T &y) { return x * y; }
   static constexpr T id() { return 1; }
@@ -24,13 +24,13 @@ struct MulOp {
 
 // x := y
 // Override value by the second argument.
-struct AssignOp {
+struct Assign {
   using T = std::optional<int>;
   static T op(const T &x, const T &y) { return y.has_value() ? y : x; }
   static constexpr T id() { return std::nullopt; }
 };
 
-struct GCDOp {
+struct GCD {
   using T = int;
   static T op(const T &x, const T &y) {
     if (x == id()) return y;
@@ -38,4 +38,100 @@ struct GCDOp {
     return std::gcd(x, y);
   }
   static constexpr T id() { return -1; }
+};
+
+struct AddMin {
+  // Fold: Min
+  using T = long long;
+  static T op(const T &x, const T &y) { return std::min(x, y); }
+  static constexpr T id() { return std::numeric_limits<T>::max(); }
+
+  // Update: Add
+  using F = long long;
+  static T apply(const F &f, const T &x) { return f + x; }
+  static F compose(const F &f, const F &g) { return f + g; }
+  static constexpr F id_f() { return 0; }
+};
+
+struct AddMax {
+  // Fold: Max
+  using T = int;
+  static T op(const T &x, const T &y) { return std::max(x, y); }
+  static constexpr T id() { return std::numeric_limits<T>::min(); }
+
+  // Update: Add
+  using F = long long;
+  static T apply(const F &f, const T &x) { return f + x; }
+  static F compose(const F &f, const F &g) { return f + g; }
+  static constexpr F id_f() { return 0; }
+};
+
+struct AddSum {
+  // Fold: Sum
+  struct T {
+    long long sum;
+    int width;
+  };
+  static T op(const T &x, const T &y) {
+    return {x.sum + y.sum, x.width + y.width};
+  }
+  static constexpr T id() { return {0, 0}; }
+
+  // Update: Add
+  using F = long long;
+  static T apply(const F &f, const T &x) {
+    return {x.sum + f * x.width, x.width};
+  }
+  static F compose(const F &f, const F &g) { return f + g; }
+  static constexpr F id_f() { return 0; }
+};
+
+struct AssignMin {
+  // Fold: Min
+  using T = long long;
+  static T op(const T &x, const T &y) { return std::min(x, y); }
+  static constexpr T id() { return std::numeric_limits<T>::max(); }
+
+  // Update: Assign
+  using F = std::optional<long long>;
+  static T apply(const F &f, const T &x) {
+    return f.has_value() ? f.value() : x;
+  }
+  static F compose(const F &f, const F &g) { return f.has_value() ? f : g; }
+  static constexpr F id_f() { return std::nullopt; }
+};
+
+struct AssignMax {
+  // Fold: Max
+  using T = int;
+  static T op(const T &x, const T &y) { return std::max(x, y); }
+  static constexpr T id() { return std::numeric_limits<T>::min(); }
+
+  // Update: Assign
+  using F = std::optional<long long>;
+  static T apply(const F &f, const T &x) {
+    return f.has_value() ? f.value() : x;
+  }
+  static F compose(const F &f, const F &g) { return f.has_value() ? f : g; }
+  static constexpr F id_f() { return std::nullopt; }
+};
+
+struct AssignSum {
+  // Fold: Sum
+  struct T {
+    long long sum;
+    int width;
+  };
+  static T op(const T &x, const T &y) {
+    return {x.sum + y.sum, x.width + y.width};
+  }
+  static constexpr T id() { return {0, 0}; }
+
+  // Update: Assign
+  using F = std::optional<long long>;
+  static T apply(const F &f, const T &x) {
+    return f.has_value() ? {f.value() * x.width, x.width} : x;
+  }
+  static F compose(const F &f, const F &g) { return f.has_value() ? f : g; }
+  static constexpr F id_f() { return std::nullopt; }
 };
