aa62ec9e45af46696e49f24e66f6d7a8637138de
Keiji Maekawa <m@keijak.com>
2020-09-24 18:54:31 +0200

    lazysegtree

--- atcoder/practice2/L/main.cpp
+++ atcoder/practice2/L/main.cpp
@@ -82,24 +82,24 @@ void pdebug(const T &value, const Ts &... args) {
 #define DEBUG(...)
 #endif
 
-template <typename OpMonoid>
+template <typename LazyMonoid>
 struct LazySegTree {
-  using T = typename OpMonoid::T;
-  using F = typename OpMonoid::F;
+  using T = typename LazyMonoid::T;
+  using F = typename LazyMonoid::F;
 
   inline int n() const { return n_; }
   inline int offset() const { return offset_; }
 
   explicit LazySegTree(int n)
-      : LazySegTree(std::vector<T>(n, OpMonoid::id())) {}
+      : LazySegTree(std::vector<T>(n, LazyMonoid::id())) {}
 
   explicit LazySegTree(const std::vector<T> &v) : n_(int(v.size())) {
     offset_ = 1;
     for (bits_ = 0; offset_ < n_; ++bits_) {
       offset_ <<= 1;
     }
-    data_.assign(2 * offset_, OpMonoid::id());
-    lazy_ = std::vector<F>(offset_, OpMonoid::id_f());
+    data_.assign(2 * offset_, LazyMonoid::id());
+    lazy_ = std::vector<F>(offset_, LazyMonoid::f_id());
     for (int i = 0; i < n_; i++) data_[offset_ + i] = v[i];
     for (int i = offset_ - 1; i >= 1; i--) {
       update(i);
@@ -114,16 +114,16 @@ struct LazySegTree {
     for (int i = 1; i <= bits_; i++) update(p >> i);
   }
 
-  T &operator[](int p) {
+  const T &operator[](int p) const {
     assert(0 <= p && p < n_);
     p += offset_;
     for (int i = bits_; i >= 1; i--) push(p >> i);
     return data_[p];
   }
 
-  T fold(int l, int r) {
+  T fold(int l, int r) const {
     assert(0 <= l && l <= r && r <= n_);
-    if (l == r) return OpMonoid::id();
+    if (l == r) return LazyMonoid::id();
 
     l += offset_;
     r += offset_;
@@ -133,22 +133,22 @@ struct LazySegTree {
       if (((r >> i) << i) != r) push(r >> i);
     }
 
-    T sml = OpMonoid::id(), smr = OpMonoid::id();
+    T sml = LazyMonoid::id(), smr = LazyMonoid::id();
     while (l < r) {
-      if (l & 1) sml = OpMonoid::op(sml, data_[l++]);
-      if (r & 1) smr = OpMonoid::op(data_[--r], smr);
+      if (l & 1) sml = LazyMonoid::op(sml, data_[l++]);
+      if (r & 1) smr = LazyMonoid::op(data_[--r], smr);
       l >>= 1;
       r >>= 1;
     }
 
-    return OpMonoid::op(sml, smr);
+    return LazyMonoid::op(sml, smr);
   }
 
   void apply(int p, F f) {
     assert(0 <= p && p < n_);
     p += offset_;
     for (int i = bits_; i >= 1; i--) push(p >> i);
-    data_[p] = OpMonoid::apply(f, data_[p]);
+    data_[p] = LazyMonoid::apply(f, data_[p]);
     for (int i = 1; i <= bits_; i++) update(p >> i);
   }
   void apply(int l, int r, F f) {
@@ -182,84 +182,84 @@ struct LazySegTree {
   }
 
   template <bool (*g)(T)>
-  int max_right(int l) {
+  int max_right(int l) const {
     return max_right(l, [](T x) { return g(x); });
   }
   template <class G>
-  int max_right(int l, G g) {
+  int max_right(int l, G g) const {
     assert(0 <= l && l <= n_);
-    assert(g(OpMonoid::id()));
+    assert(g(LazyMonoid::id()));
     if (l == n_) return n_;
     l += offset_;
     for (int i = bits_; i >= 1; i--) push(l >> i);
-    T sm = OpMonoid::id();
+    T sm = LazyMonoid::id();
     do {
       while (l % 2 == 0) l >>= 1;
-      if (!g(OpMonoid::op(sm, data_[l]))) {
+      if (!g(LazyMonoid::op(sm, data_[l]))) {
         while (l < offset_) {
           push(l);
           l = (2 * l);
-          if (g(OpMonoid::op(sm, data_[l]))) {
-            sm = OpMonoid::op(sm, data_[l]);
+          if (g(LazyMonoid::op(sm, data_[l]))) {
+            sm = LazyMonoid::op(sm, data_[l]);
             l++;
           }
         }
         return l - offset_;
       }
-      sm = OpMonoid::op(sm, data_[l]);
+      sm = LazyMonoid::op(sm, data_[l]);
       l++;
     } while ((l & -l) != l);
     return n_;
   }
 
   template <bool (*g)(T)>
-  int min_left(int r) {
+  int min_left(int r) const {
     return min_left(r, [](T x) { return g(x); });
   }
   template <class G>
-  int min_left(int r, G g) {
+  int min_left(int r, G g) const {
     assert(0 <= r && r <= n_);
-    assert(g(OpMonoid::id()));
+    assert(g(LazyMonoid::id()));
     if (r == 0) return 0;
     r += offset_;
     for (int i = bits_; i >= 1; i--) push((r - 1) >> i);
-    T sm = OpMonoid::id();
+    T sm = LazyMonoid::id();
     do {
       r--;
       while (r > 1 && (r % 2)) r >>= 1;
-      if (!g(OpMonoid::op(data_[r], sm))) {
+      if (!g(LazyMonoid::op(data_[r], sm))) {
         while (r < offset_) {
           push(r);
           r = (2 * r + 1);
-          if (g(OpMonoid::op(data_[r], sm))) {
-            sm = OpMonoid::op(data_[r], sm);
+          if (g(LazyMonoid::op(data_[r], sm))) {
+            sm = LazyMonoid::op(data_[r], sm);
             r--;
           }
         }
         return r + 1 - offset_;
       }
-      sm = OpMonoid::op(data_[r], sm);
+      sm = LazyMonoid::op(data_[r], sm);
     } while ((r & -r) != r);
     return 0;
   }
 
  private:
   void update(int k) {
-    data_[k] = OpMonoid::op(data_[2 * k], data_[2 * k + 1]);
+    data_[k] = LazyMonoid::op(data_[2 * k], data_[2 * k + 1]);
   }
-  void all_apply(int k, F f) {
-    data_[k] = OpMonoid::apply(f, data_[k]);
-    if (k < offset_) lazy_[k] = OpMonoid::compose(f, lazy_[k]);
+  void all_apply(int k, F f) const {
+    data_[k] = LazyMonoid::f_apply(f, data_[k]);
+    if (k < offset_) lazy_[k] = LazyMonoid::f_compose(f, lazy_[k]);
   }
-  void push(int k) {
+  void push(int k) const {
     all_apply(2 * k, lazy_[k]);
     all_apply(2 * k + 1, lazy_[k]);
-    lazy_[k] = OpMonoid::id_f();
+    lazy_[k] = LazyMonoid::f_id();
   }
 
   int n_, offset_, bits_;
-  std::vector<T> data_;
-  std::vector<F> lazy_;
+  mutable std::vector<T> data_;
+  mutable std::vector<F> lazy_;
 };
 
 using namespace std;
@@ -271,6 +271,8 @@ struct FlipInversion {
     int size;
     i64 inversion;
   };
+  using F = bool;
+
   static T op(const T &x, const T &y) {
     return {
         x.ones + y.ones,
@@ -281,8 +283,7 @@ struct FlipInversion {
   static constexpr T id() { return {0, 0, 0}; }
 
   // Update: Flip
-  using F = bool;
-  static T apply(const F &f, const T &x) {
+  static T f_apply(const F &f, const T &x) {
     if (not f) return x;
     i64 inv = i64(x.size) * (x.size - 1) / 2LL;
     inv -= i64(x.ones) * (x.ones - 1) / 2LL;
@@ -291,8 +292,8 @@ struct FlipInversion {
     inv -= x.inversion;
     return {x.size - x.ones, x.size, inv};
   }
-  static F compose(const F &f, const F &g) { return f ^ g; }
-  static constexpr F id_f() { return false; }
+  static F f_compose(const F &f, const F &g) { return f ^ g; }
+  static constexpr F f_id() { return false; }
 };
 
 int main() {
