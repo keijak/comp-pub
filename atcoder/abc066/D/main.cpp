f39c5f17a96b9a008e2b53e57bd0ef0e05614e3c
Keiji Maekawa <m@keijak.com>
2020-10-04 00:31:50 +0200

    modint inverse

--- atcoder/abc066/D/main.cpp
+++ atcoder/abc066/D/main.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using i64 = std::int64_t;
 using u64 = std::uint64_t;
-#define REP(i, n) for (int i = 0; i < (i64)(n); ++i)
+#define REP(i, n) for (int i = 0, REP_N_ = int(n); i < REP_N_; ++i)
 #define ALL(x) std::begin(x), std::end(x)
 #define SIZE(a) (int)((a).size())
 
@@ -12,87 +12,6 @@ inline bool chmax(T &a, T b) {
 template <class T>
 inline bool chmin(T &a, T b) {
   return a > b and ((a = std::move(b)), true);
-}
-
-template <typename T>
-using V = std::vector<T>;
-template <typename T>
-std::vector<T> make_vec(size_t n, T a) {
-  return std::vector<T>(n, a);
-}
-template <typename... Ts>
-auto make_vec(size_t n, Ts... ts) {
-  return std::vector<decltype(make_vec(ts...))>(n, make_vec(ts...));
-}
-template <typename T>
-std::istream &operator>>(std::istream &is, std::vector<T> &a) {
-  for (auto &x : a) is >> x;
-  return is;
-}
-template <typename Container>
-std::ostream &pprint(const Container &a, std::string_view sep = " ",
-                     std::string_view ends = "\n", std::ostream *os = nullptr) {
-  if (os == nullptr) os = &std::cout;
-  auto b = std::begin(a), e = std::end(a);
-  for (auto it = std::begin(a); it != e; ++it) {
-    if (it != b) *os << sep;
-    *os << *it;
-  }
-  return *os << ends;
-}
-template <typename T, typename = void>
-struct is_iterable : std::false_type {};
-template <typename T>
-struct is_iterable<T, std::void_t<decltype(std::begin(std::declval<T>())),
-                                  decltype(std::end(std::declval<T>()))>>
-    : std::true_type {};
-
-template <typename T,
-          typename = std::enable_if_t<is_iterable<T>::value &&
-                                      !std::is_same<T, std::string>::value>>
-std::ostream &operator<<(std::ostream &os, const T &a) {
-  return pprint(a, ", ", "", &(os << "{")) << "}";
-}
-template <typename T, typename U>
-std::ostream &operator<<(std::ostream &os, const std::pair<T, U> &a) {
-  return os << "(" << a.first << ", " << a.second << ")";
-}
-
-#ifdef ENABLE_DEBUG
-template <typename T>
-void pdebug(const T &value) {
-  std::cerr << value;
-}
-template <typename T, typename... Ts>
-void pdebug(const T &value, const Ts &... args) {
-  pdebug(value);
-  std::cerr << ", ";
-  pdebug(args...);
-}
-#define DEBUG(...)                                   \
-  do {                                               \
-    std::cerr << " \033[33m (L" << __LINE__ << ") "; \
-    std::cerr << #__VA_ARGS__ << ":\033[0m ";        \
-    pdebug(__VA_ARGS__);                             \
-    std::cerr << std::endl;                          \
-  } while (0)
-#else
-#define pdebug(...)
-#define DEBUG(...)
-#endif
-
-// Extended Euclidean algorithm
-// Returns gcd(a,b).
-// x and y are set to satisfy `a*x + b*y == gcd(a,b)`
-long long ext_gcd(long long a, long long b, long long &x, long long &y) {
-  if (b == 0) {
-    x = 1;
-    y = 0;
-    return a;
-  }
-  long long d = ext_gcd(b, a % b, y, x);
-  y -= a / b * x;
-  return d;
 }
 
 template <unsigned int M>
@@ -145,7 +64,6 @@ struct ModInt {
     return *this;
   }
   constexpr ModInt pow(unsigned long long t) const {
-    assert(t >= 0);
     ModInt base = *this;
     ModInt res = 1;
     while (t) {
@@ -156,14 +74,18 @@ struct ModInt {
     return res;
   }
 
-  // Inverse by Extended Euclidean algorithm.
-  // M doesn't need to be prime, but x and M must be coprime.
   constexpr ModInt inv() const {
+    // Inverse by Extended Euclidean algorithm.
+    // M doesn't need to be prime, but x and M must be coprime.
     assert(_v != 0);
-    long long x, y;
-    long long g = ext_gcd(_v, M, x, y);
-    assert(g == 1LL);  // gcd(_v, M) must be 1.
+    auto [g, x, y] = ext_gcd(_v, M);
+    assert(g == 1LL);  // The GCD must be 1.
     return x;
+
+    // Inverse by Fermat's little theorem.
+    // M must be prime. It's often faster.
+    //
+    //     return pow(M - 2);
   }
   constexpr ModInt &operator/=(const ModInt &a) { return *this *= a.inv(); }
 
@@ -185,18 +107,6 @@ struct ModInt {
   friend constexpr bool operator!=(const ModInt &a, const ModInt &b) {
     return a._v != b._v;
   }
-  friend constexpr bool operator<(const ModInt &a, const ModInt &b) {
-    return a._v < b._v;
-  }
-  friend constexpr bool operator>(const ModInt &a, const ModInt &b) {
-    return a._v > b._v;
-  }
-  friend constexpr bool operator<=(const ModInt &a, const ModInt &b) {
-    return a._v <= b._v;
-  }
-  friend constexpr bool operator>=(const ModInt &a, const ModInt &b) {
-    return a._v >= b._v;
-  }
   friend std::istream &operator>>(std::istream &is, ModInt &a) {
     return is >> a._v;
   }
@@ -205,7 +115,27 @@ struct ModInt {
   }
 
  private:
-  unsigned int _v;
+  // Extended Euclidean algorithm
+  // Returns (gcd(a,b), x, y) where `a*x + b*y == gcd(a,b)`.
+  static std::tuple<int, int, int> ext_gcd(int a, int b) {
+    int ax = 1, ay = 0, bx = 0, by = 1;
+    for (;;) {
+      if (b == 0) break;
+      auto d = std::div(a, b);
+      int u = d.rem;
+      int ux = ax - bx * d.quot;
+      int uy = ay - by * d.quot;
+      a = b;
+      ax = bx;
+      ay = by;
+      b = u;
+      bx = ux;
+      by = uy;
+    }
+    return {a, ax, ay};
+  }
+
+  unsigned int _v;  // raw value
 };
 const unsigned int MOD = 1'000'000'007;
 using Mint = ModInt<MOD>;
@@ -215,6 +145,7 @@ using namespace std;
 int main() {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
+
   int n;
   cin >> n;
   vector<int> a(100001, -1);
--- atcoder/dp/Y/main.cpp
+++ atcoder/dp/Y/main.cpp
@@ -26,34 +26,58 @@ void debug(T value, Ts... args) {
 #define DEBUG(...)
 #endif
 
-const int MOD = 1'000'000'007;
-
-struct mint {
-  int x;
-  mint(long long val = 0) {
+template <unsigned int M>
+struct ModInt {
+  constexpr ModInt(long long val = 0) : _v(0) {
     if (val < 0) {
-      long long k = (abs(val) + MOD - 1) / MOD;
-      val += k * MOD;
+      long long k = (abs(val) + M - 1) / M;
+      val += k * M;
     }
-    x = val % MOD;
+    assert(val >= 0);
+    _v = val % M;
   }
-  mint operator-() const { return mint(-x); }
-  mint &operator+=(const mint &a) {
-    if ((x += a.x) >= MOD) x -= MOD;
+
+  static constexpr int mod() { return M; }
+  static constexpr unsigned int umod() { return M; }
+  inline unsigned int val() const { return _v; }
+
+  ModInt &operator++() {
+    _v++;
+    if (_v == umod()) _v = 0;
     return *this;
   }
-  mint &operator-=(const mint &a) {
-    if ((x += MOD - a.x) >= MOD) x -= MOD;
+  ModInt &operator--() {
+    if (_v == 0) _v = umod();
+    _v--;
     return *this;
   }
-  mint &operator*=(const mint &a) {
-    x = (x * (long long)a.x) % MOD;
+  ModInt operator++(int) {
+    auto result = *this;
+    ++*this;
+    return result;
+  }
+  ModInt operator--(int) {
+    auto result = *this;
+    --*this;
+    return result;
+  }
+
+  constexpr ModInt operator-() const { return ModInt(-_v); }
+  constexpr ModInt &operator+=(const ModInt &a) {
+    if ((_v += a._v) >= M) _v -= M;
     return *this;
   }
-  mint pow(long long t) const {
-    assert(t >= 0);
-    mint base = *this;
-    mint res = 1;
+  constexpr ModInt &operator-=(const ModInt &a) {
+    if ((_v += M - a._v) >= M) _v -= M;
+    return *this;
+  }
+  constexpr ModInt &operator*=(const ModInt &a) {
+    _v = ((unsigned long long)(_v)*a._v) % M;
+    return *this;
+  }
+  constexpr ModInt pow(unsigned long long t) const {
+    ModInt base = *this;
+    ModInt res = 1;
     while (t) {
       if (t & 1) res *= base;
       base *= base;
@@ -61,31 +85,82 @@ struct mint {
     }
     return res;
   }
-  // for prime MOD
-  mint inv() const { return pow(MOD - 2); }
-  mint &operator/=(const mint &a) { return *this *= a.inv(); }
+
+  constexpr ModInt inv() const {
+    // Inverse by Extended Euclidean algorithm.
+    // M doesn't need to be prime, but x and M must be coprime.
+    assert(_v != 0);
+    // int g, x, y;
+    // g = ext_gcd(_v, M, x, y);
+    auto [g, x, y] = ext_gcd(_v, M);
+    assert(g == 1LL);  // gcd(_v, M) must be 1.
+    return x;
+
+    // Inverse by Fermat's little theorem.
+    // M must be prime, but it's usually faster.
+    // return pow(M - 2);
+  }
+  constexpr ModInt &operator/=(const ModInt &a) { return *this *= a.inv(); }
+
+  friend constexpr ModInt operator+(const ModInt &a, const ModInt &b) {
+    return ModInt(a) += b;
+  }
+  friend constexpr ModInt operator-(const ModInt &a, const ModInt &b) {
+    return ModInt(a) -= b;
+  }
+  friend constexpr ModInt operator*(const ModInt &a, const ModInt &b) {
+    return ModInt(a) *= b;
+  }
+  friend constexpr ModInt operator/(const ModInt &a, const ModInt &b) {
+    return ModInt(a) /= b;
+  }
+  friend constexpr bool operator==(const ModInt &a, const ModInt &b) {
+    return a._v == b._v;
+  }
+  friend constexpr bool operator!=(const ModInt &a, const ModInt &b) {
+    return a._v != b._v;
+  }
+  friend std::istream &operator>>(std::istream &is, ModInt &a) {
+    return is >> a._v;
+  }
+  friend std::ostream &operator<<(std::ostream &os, const ModInt &a) {
+    return os << a._v;
+  }
+
+ private:
+  // Extended Euclidean algorithm
+  // Returns (gcd(a,b), x, y) where `a*x + b*y == gcd(a,b)`.
+  static std::tuple<int, int, int> ext_gcd(int a, int b) {
+    int ax = 1, ay = 0, bx = 0, by = 1;
+    for (;;) {
+      if (b == 0) break;
+      auto d = std::div(a, b);
+      int u = d.rem;
+      int ux = ax - bx * d.quot;
+      int uy = ay - by * d.quot;
+      a = b;
+      ax = bx;
+      ay = by;
+      b = u;
+      bx = ux;
+      by = uy;
+    }
+    return {a, ax, ay};
+  }
+
+  unsigned int _v;  // raw value
 };
-mint operator+(const mint &a, const mint &b) { return mint(a) += b; }
-mint operator-(const mint &a, const mint &b) { return mint(a) -= b; }
-mint operator*(const mint &a, const mint &b) { return mint(a) *= b; }
-mint operator/(const mint &a, const mint &b) { return mint(a) /= b; }
-bool operator==(const mint &a, const mint &b) { return a.x == b.x; }
-bool operator!=(const mint &a, const mint &b) { return a.x != b.x; }
-bool operator<(const mint &a, const mint &b) { return a.x < b.x; }
-bool operator>(const mint &a, const mint &b) { return a.x > b.x; }
-bool operator<=(const mint &a, const mint &b) { return a.x <= b.x; }
-bool operator>=(const mint &a, const mint &b) { return a.x >= b.x; }
-istream &operator>>(istream &is, mint &a) { return is >> a.x; }
-ostream &operator<<(ostream &os, const mint &a) { return os << a.x; }
+const unsigned int MOD = 1'000'000'007;
+using Mint = ModInt<MOD>;
 
 // Factorials over ModInts.
 struct Factorials {
   // factorials and inverse factorials.
-  vector<mint> fact, ifact;
+  vector<Mint> fact, ifact;
 
   // n: max cached value.
   Factorials(int n) : fact(n + 1), ifact(n + 1) {
-    assert(n < MOD);
+    assert(n < (int)MOD);
     fact[0] = 1;
     for (int i = 1; i <= n; ++i) fact[i] = fact[i - 1] * i;
     ifact[n] = fact[n].inv();
@@ -93,13 +168,13 @@ struct Factorials {
   }
 
   // Combination (nCk)
-  mint C(int n, int k) {
+  Mint C(int n, int k) {
     if (k < 0 || k > n) return 0;
     return fact[n] * ifact[k] * ifact[n - k];
   }
 
   // Permutation (nPk)
-  mint P(int n, int k) {
+  Mint P(int n, int k) {
     if (k < 0 || k > n) return 0;
     return fact[n] * ifact[n - k];
   }
@@ -121,7 +196,7 @@ int main() {
   sort(walls.begin(), walls.end());
   Factorials fs(H + W - 2);
   multimap<int, int> cs;
-  vector<vector<mint>> dp(N, vector<mint>(2));
+  vector<vector<Mint>> dp(N, vector<Mint>(2));
   REP(i, N) {
     auto [r, c] = walls[i];
     dp[i][1] += fs.C(r + c, r) * fs.C(H - 1 - r + W - 1 - c, H - 1 - r);
@@ -130,7 +205,7 @@ int main() {
       --it;
       int p = it->second;
       auto [pr, pc] = walls[p];
-      mint x = fs.C(r - pr + c - pc, r - pr) *
+      Mint x = fs.C(r - pr + c - pc, r - pr) *
                fs.C(H - 1 - r + W - 1 - c, H - 1 - r) *
                fs.C(H - 1 - pr + W - 1 - pc, H - 1 - pr).inv();
       dp[i][0] += dp[p][1] * x;
@@ -138,7 +213,7 @@ int main() {
     }
     cs.emplace(c, i);
   }
-  mint a0, a1;
+  Mint a0, a1;
   a0 = fs.C((H - 1) + (W - 1), H - 1);
   REP(i, N) {
     a0 += dp[i][0];
