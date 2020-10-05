57559be73de25577e1047f69861b15c56e22f4d2
Keiji Maekawa <m@keijak.com>
2020-10-05 12:17:45 +0200

    warshall floyd

---
+++ atcoder/abc074/D/main.cpp
@@ -0,0 +1,114 @@
+#include <bits/stdc++.h>
+using i64 = std::int64_t;
+using u64 = std::uint64_t;
+#define REP(i, n) for (int i = 0, REP_N_ = int(n); i < REP_N_; ++i)
+#define ALL(x) std::begin(x), std::end(x)
+#define SIZE(a) (int)((a).size())
+
+template <class T>
+inline bool chmax(T &a, T b) {
+  return a < b and ((a = std::move(b)), true);
+}
+template <class T>
+inline bool chmin(T &a, T b) {
+  return a > b and ((a = std::move(b)), true);
+}
+
+template <typename T>
+using V = std::vector<T>;
+template <typename T>
+std::istream &operator>>(std::istream &is, std::vector<T> &a) {
+  for (auto &x : a) is >> x;
+  return is;
+}
+template <typename Container>
+std::ostream &pprint(const Container &a, std::string_view sep = " ",
+                     std::string_view ends = "\n", std::ostream *os = nullptr) {
+  if (os == nullptr) os = &std::cout;
+  auto b = std::begin(a), e = std::end(a);
+  for (auto it = std::begin(a); it != e; ++it) {
+    if (it != b) *os << sep;
+    *os << *it;
+  }
+  return *os << ends;
+}
+template <typename T, typename = void>
+struct is_iterable : std::false_type {};
+template <typename T>
+struct is_iterable<T, std::void_t<decltype(std::begin(std::declval<T>())),
+                                  decltype(std::end(std::declval<T>()))>>
+    : std::true_type {};
+
+template <typename T,
+          typename = std::enable_if_t<is_iterable<T>::value &&
+                                      !std::is_same<T, std::string>::value>>
+std::ostream &operator<<(std::ostream &os, const T &a) {
+  return pprint(a, ", ", "", &(os << "{")) << "}";
+}
+template <typename T, typename U>
+std::ostream &operator<<(std::ostream &os, const std::pair<T, U> &a) {
+  return os << "(" << a.first << ", " << a.second << ")";
+}
+
+#ifdef ENABLE_DEBUG
+template <typename T>
+void pdebug(const T &value) {
+  std::cerr << value;
+}
+template <typename T, typename... Ts>
+void pdebug(const T &value, const Ts &... args) {
+  pdebug(value);
+  std::cerr << ", ";
+  pdebug(args...);
+}
+#define DEBUG(...)                                   \
+  do {                                               \
+    std::cerr << " \033[33m (L" << __LINE__ << ") "; \
+    std::cerr << #__VA_ARGS__ << ":\033[0m ";        \
+    pdebug(__VA_ARGS__);                             \
+    std::cerr << std::endl;                          \
+  } while (0)
+#else
+#define pdebug(...)
+#define DEBUG(...)
+#endif
+
+using namespace std;
+
+i64 solve() {
+  int n;
+  cin >> n;
+  auto A = vector(n, vector(n, 0LL));
+  REP(i, n) REP(j, n) { cin >> A[i][j]; }
+  REP(i, n) REP(j, n) {
+    if (A[i][j] != A[j][i]) return -1;
+  }
+
+  auto D = A;
+  REP(k, n) REP(i, n) REP(j, n) { chmin(D[i][j], D[i][k] + D[k][j]); }
+  REP(i, n) REP(j, n) {
+    if (A[i][j] != D[i][j]) return -1;
+  }
+
+  i64 dsum = 0;
+  REP(j, n) REP(i, j) {
+    bool indirect = false;
+    REP(k, n) {
+      auto x = D[i][k] + D[k][j];
+      if (i != k and j != k and D[i][j] == x) {
+        indirect = true;
+        break;
+      }
+    }
+    if (not indirect) {
+      dsum += D[i][j];
+    }
+  }
+  return dsum;
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout << solve() << endl;
+}
