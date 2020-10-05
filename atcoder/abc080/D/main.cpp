cb99234412188aa8899c929bda518fb18693e5cc
Keiji Maekawa <m@keijak.com>
2020-10-06 00:18:33 +0200

    imos

---
+++ atcoder/abc080/D/main.cpp
@@ -0,0 +1,120 @@
+#include <bits/stdc++.h>
+using i64 = std::int64_t;
+using u64 = std::uint64_t;
+#define REP(i, n) for (int i = 0, REP_N_ = int(n); i < REP_N_; ++i)
+#define ALL(x) std::begin(x), std::end(x)
+#define SIZE(a) (int)((a).size())
+
+template <class T>
+inline bool chmax(T &a, T b) {
+  return a < b and ((a = std::move(b)), true);
+}
+template <class T>
+inline bool chmin(T &a, T b) {
+  return a > b and ((a = std::move(b)), true);
+}
+
+template <typename T>
+using V = std::vector<T>;
+template <typename T>
+std::istream &operator>>(std::istream &is, std::vector<T> &a) {
+  for (auto &x : a) is >> x;
+  return is;
+}
+template <typename Container>
+std::ostream &pprint(const Container &a, std::string_view sep = " ",
+                     std::string_view ends = "\n", std::ostream *os = nullptr) {
+  if (os == nullptr) os = &std::cout;
+  auto b = std::begin(a), e = std::end(a);
+  for (auto it = std::begin(a); it != e; ++it) {
+    if (it != b) *os << sep;
+    *os << *it;
+  }
+  return *os << ends;
+}
+template <typename T, typename = void>
+struct is_iterable : std::false_type {};
+template <typename T>
+struct is_iterable<T, std::void_t<decltype(std::begin(std::declval<T>())),
+                                  decltype(std::end(std::declval<T>()))>>
+    : std::true_type {};
+
+template <typename T,
+          typename = std::enable_if_t<is_iterable<T>::value &&
+                                      !std::is_same<T, std::string>::value>>
+std::ostream &operator<<(std::ostream &os, const T &a) {
+  return pprint(a, ", ", "", &(os << "{")) << "}";
+}
+template <typename T, typename U>
+std::ostream &operator<<(std::ostream &os, const std::pair<T, U> &a) {
+  return os << "(" << a.first << ", " << a.second << ")";
+}
+
+#ifdef ENABLE_DEBUG
+template <typename T>
+void pdebug(const T &value) {
+  std::cerr << value;
+}
+template <typename T, typename... Ts>
+void pdebug(const T &value, const Ts &... args) {
+  pdebug(value);
+  std::cerr << ", ";
+  pdebug(args...);
+}
+#define DEBUG(...)                                   \
+  do {                                               \
+    std::cerr << " \033[33m (L" << __LINE__ << ") "; \
+    std::cerr << #__VA_ARGS__ << ":\033[0m ";        \
+    pdebug(__VA_ARGS__);                             \
+    std::cerr << std::endl;                          \
+  } while (0)
+#else
+#define pdebug(...)
+#define DEBUG(...)
+#endif
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int N, C;
+  cin >> N >> C;
+  V<V<pair<int, int>>> chan(C);
+
+  REP(i, N) {
+    int s, t, c;
+    cin >> s >> t >> c;
+    c--;
+    chan[c].emplace_back(s, t);
+  }
+  REP(c, C) {
+    sort(ALL(chan[c]));
+    V<pair<int, int>> q;
+    for (auto [s, t] : chan[c]) {
+      if (q.empty()) {
+        q.emplace_back(s, t);
+      } else if (q.back().second == s) {
+        q[q.size() - 1] = {q.back().first, t};
+      } else {
+        q.emplace_back(s, t);
+      }
+    }
+    swap(q, chan[c]);
+  }
+
+  const int TL = 100'005;
+  V<int> timeline(TL + 1);
+  REP(c, C) {
+    for (auto [s, t] : chan[c]) {
+      timeline[s - 1]++;
+      timeline[t]--;
+    }
+  }
+  for (int i = 1; i <= TL; ++i) {
+    timeline[i] += timeline[i - 1];
+  }
+  cout << *max_element(ALL(timeline)) << endl;
+  ;
+}
