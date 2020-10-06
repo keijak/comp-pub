bb0267f06ab62a735db1302ca85dc243965c1403
Keiji Maekawa <m@keijak.com>
2020-10-06 22:44:03 +0200

    observation

---
+++ atcoder/agc020/B/main.cpp
@@ -0,0 +1,115 @@
+#include <bits/stdc++.h>
+using i64 = std::int64_t;
+using u64 = std::uint64_t;
+#define REP(i, n) for (int i = 0, REP_N_ = int(n); i < REP_N_; ++i)
+#define ALL(x) std::begin(x), std::end(x)
+#define SIZE(a) (int)((a).size())
+
+template <class T>
+inline bool chmax(T &a, T b) {
+  return a < b and ((a = std::move(b)), true);
+}
+template <class T>
+inline bool chmin(T &a, T b) {
+  return a > b and ((a = std::move(b)), true);
+}
+
+template <typename T>
+using V = std::vector<T>;
+template <typename T>
+std::istream &operator>>(std::istream &is, std::vector<T> &a) {
+  for (auto &x : a) is >> x;
+  return is;
+}
+template <typename Container>
+std::ostream &pprint(const Container &a, std::string_view sep = " ",
+                     std::string_view ends = "\n", std::ostream *os = nullptr) {
+  if (os == nullptr) os = &std::cout;
+  auto b = std::begin(a), e = std::end(a);
+  for (auto it = std::begin(a); it != e; ++it) {
+    if (it != b) *os << sep;
+    *os << *it;
+  }
+  return *os << ends;
+}
+template <typename T, typename = void>
+struct is_iterable : std::false_type {};
+template <typename T>
+struct is_iterable<T, std::void_t<decltype(std::begin(std::declval<T>())),
+                                  decltype(std::end(std::declval<T>()))>>
+    : std::true_type {};
+
+template <typename T,
+          typename = std::enable_if_t<is_iterable<T>::value &&
+                                      !std::is_same<T, std::string>::value>>
+std::ostream &operator<<(std::ostream &os, const T &a) {
+  return pprint(a, ", ", "", &(os << "{")) << "}";
+}
+template <typename T, typename U>
+std::ostream &operator<<(std::ostream &os, const std::pair<T, U> &a) {
+  return os << "(" << a.first << ", " << a.second << ")";
+}
+
+#ifdef ENABLE_DEBUG
+template <typename T>
+void pdebug(const T &value) {
+  std::cerr << value;
+}
+template <typename T, typename... Ts>
+void pdebug(const T &value, const Ts &... args) {
+  pdebug(value);
+  std::cerr << ", ";
+  pdebug(args...);
+}
+#define DEBUG(...)                                   \
+  do {                                               \
+    std::cerr << " \033[33m (L" << __LINE__ << ") "; \
+    std::cerr << #__VA_ARGS__ << ":\033[0m ";        \
+    pdebug(__VA_ARGS__);                             \
+    std::cerr << std::endl;                          \
+  } while (0)
+#else
+#define pdebug(...)
+#define DEBUG(...)
+#endif
+
+using namespace std;
+using P = pair<i64, i64>;
+
+P filter(P ival, int val) {
+  P res;
+  auto [istart, iend] = ival;
+  i64 delta = (val - (istart % val)) % val;
+  i64 x = istart + delta;
+  if (x < iend) {
+    res.first = x;
+    res.second = x + val;
+  }
+  i64 y = iend - (iend % val);
+  if (y == iend) y -= val;
+  if (y >= istart) {
+    chmax(res.second, y + val);
+  }
+  return res;
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int K;
+  cin >> K;
+  V<int> A(K);
+  cin >> A;
+
+  P ival = {2, 3};
+  for (int i = K - 1; i >= 0; --i) {
+    ival = filter(move(ival), A[i]);
+    if (ival.first == ival.second) break;
+  }
+  if (ival.first == ival.second) {
+    cout << "-1\n";
+    exit(0);
+  }
+  cout << ival.first << ' ' << ival.second - 1 << '\n';
+}
