5bae59e8728be478c2c757c1596d8ff48d03f46b
Keiji Maekawa <m@keijak.com>
2020-10-07 13:21:00 +0200

    game

---
+++ atcoder/agc033/B/main.cpp
@@ -0,0 +1,121 @@
+#include <bits/stdc++.h>
+using i64 = long long;
+using u64 = unsigned long long;
+#define REP(i, n) for (int i = 0, REP_N_ = int(n); i < REP_N_; ++i)
+#define ALL(x) std::begin(x), std::end(x)
+#define SIZE(a) (int)((a).size())
+
+template <class T>
+inline bool chmax(T &a, T b) {
+  return a < b and ((a = std::move(b)), true);
+}
+template <class T>
+inline bool chmin(T &a, T b) {
+  return a > b and ((a = std::move(b)), true);
+}
+
+template <typename T>
+using V = std::vector<T>;
+template <typename T>
+std::istream &operator>>(std::istream &is, std::vector<T> &a) {
+  for (auto &x : a) is >> x;
+  return is;
+}
+template <typename Container>
+std::ostream &pprint(const Container &a, std::string_view sep = " ",
+                     std::string_view ends = "\n", std::ostream *os = nullptr) {
+  if (os == nullptr) os = &std::cout;
+  auto b = std::begin(a), e = std::end(a);
+  for (auto it = std::begin(a); it != e; ++it) {
+    if (it != b) *os << sep;
+    *os << *it;
+  }
+  return *os << ends;
+}
+template <typename T, typename = void>
+struct is_iterable : std::false_type {};
+template <typename T>
+struct is_iterable<T, std::void_t<decltype(std::begin(std::declval<T>())),
+                                  decltype(std::end(std::declval<T>()))>>
+    : std::true_type {};
+
+template <typename T,
+          typename = std::enable_if_t<is_iterable<T>::value &&
+                                      !std::is_same<T, std::string>::value>>
+std::ostream &operator<<(std::ostream &os, const T &a) {
+  return pprint(a, ", ", "", &(os << "{")) << "}";
+}
+template <typename T, typename U>
+std::ostream &operator<<(std::ostream &os, const std::pair<T, U> &a) {
+  return os << "(" << a.first << ", " << a.second << ")";
+}
+
+#ifdef ENABLE_DEBUG
+template <typename T>
+void pdebug(const T &value) {
+  std::cerr << value;
+}
+template <typename T, typename... Ts>
+void pdebug(const T &value, const Ts &... args) {
+  pdebug(value);
+  std::cerr << ", ";
+  pdebug(args...);
+}
+#define DEBUG(...)                                   \
+  do {                                               \
+    std::cerr << " \033[33m (L" << __LINE__ << ") "; \
+    std::cerr << #__VA_ARGS__ << ":\033[0m ";        \
+    pdebug(__VA_ARGS__);                             \
+    std::cerr << std::endl;                          \
+  } while (0)
+#else
+#define pdebug(...)
+#define DEBUG(...)
+#endif
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int H, W, N;
+  cin >> H >> W >> N;
+  int sr, sc;
+  cin >> sr >> sc;
+  string S, T;
+  cin >> S >> T;
+
+  auto is_alive = [&](int M, int start, char DECR, char INCR) -> bool {
+    int cl = 0, cr = 0;
+    REP(i, N) {
+      const int ri = N - 1 - i;
+      {
+        if (S[ri] == INCR) {
+          ++cr;
+        } else if (S[ri] == DECR) {
+          ++cl;
+        }
+      }
+      if (cl + cr >= M) return false;
+      if (ri - 1 >= 0) {
+        if (T[ri - 1] == INCR and cl > 0) {
+          --cl;
+        } else if (T[ri - 1] == DECR and cr > 0) {
+          --cr;
+        }
+      }
+    }
+    if (start < cl) return false;
+    if (start >= M - cr) return false;
+    return true;
+  };
+
+  auto ok1 = is_alive(W, sc - 1, 'L', 'R');
+  auto ok2 = is_alive(H, sr - 1, 'U', 'D');
+  if (ok1 and ok2) {
+    cout << "YES\n";
+  } else {
+    cout << "NO\n";
+  }
+}
