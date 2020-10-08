b53d25348d35a095dd110f7dcde4cf1a66152910
Keiji Maekawa <m@keijak.com>
2020-10-09 01:31:39 +0200

    bomber

---
+++ atcoder/abc023/C/main.cpp
@@ -0,0 +1,119 @@
+#include <bits/stdc++.h>
+using i64 = long long;
+using u64 = unsigned long long;
+#define REP(i, n) for (int i = 0, REP_N_ = int(n); i < REP_N_; ++i)
+#define ALL(x) std::begin(x), std::end(x)
+#define SIZE(a) (int)((a).size())
+
+template <class T>
+inline bool chmax(T &a, T b) {
+  return a < b and ((a = std::move(b)), true);
+}
+template <class T>
+inline bool chmin(T &a, T b) {
+  return a > b and ((a = std::move(b)), true);
+}
+
+template <typename T>
+using V = std::vector<T>;
+template <typename T>
+std::istream &operator>>(std::istream &is, std::vector<T> &a) {
+  for (auto &x : a) is >> x;
+  return is;
+}
+template <typename Container>
+std::ostream &pprint(const Container &a, std::string_view sep = " ",
+                     std::string_view ends = "\n", std::ostream *os = nullptr) {
+  if (os == nullptr) os = &std::cout;
+  auto b = std::begin(a), e = std::end(a);
+  for (auto it = std::begin(a); it != e; ++it) {
+    if (it != b) *os << sep;
+    *os << *it;
+  }
+  return *os << ends;
+}
+template <typename T, typename = void>
+struct is_iterable : std::false_type {};
+template <typename T>
+struct is_iterable<T, std::void_t<decltype(std::begin(std::declval<T>())),
+                                  decltype(std::end(std::declval<T>()))>>
+    : std::true_type {};
+
+template <typename T,
+          typename = std::enable_if_t<is_iterable<T>::value &&
+                                      !std::is_same<T, std::string>::value>>
+std::ostream &operator<<(std::ostream &os, const T &a) {
+  return pprint(a, ", ", "", &(os << "{")) << "}";
+}
+template <typename T, typename U>
+std::ostream &operator<<(std::ostream &os, const std::pair<T, U> &a) {
+  return os << "(" << a.first << ", " << a.second << ")";
+}
+
+#ifdef ENABLE_DEBUG
+template <typename T>
+void pdebug(const T &value) {
+  std::cerr << value;
+}
+template <typename T, typename... Ts>
+void pdebug(const T &value, const Ts &... args) {
+  pdebug(value);
+  std::cerr << ", ";
+  pdebug(args...);
+}
+#define DEBUG(...)                                   \
+  do {                                               \
+    std::cerr << " \033[33m (L" << __LINE__ << ") "; \
+    std::cerr << #__VA_ARGS__ << ":\033[0m ";        \
+    pdebug(__VA_ARGS__);                             \
+    std::cerr << std::endl;                          \
+  } while (0)
+#else
+#define pdebug(...)
+#define DEBUG(...)
+#endif
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int R, C, K, N;
+  cin >> R >> C >> K >> N;
+  V<pair<int, int>> candies(N);
+  map<int, V<int>> rows, cols;
+  REP(i, N) {
+    int r, c;
+    cin >> r >> c;
+    r--;
+    c--;
+    candies[i] = {r, c};
+    rows[r].push_back(i);
+    cols[c].push_back(i);
+  }
+
+  V<i64> col_freq(N + 1);
+  for (const auto &[c, indices] : cols) {
+    col_freq[SIZE(indices)]++;
+  }
+  col_freq[0] = C - SIZE(cols);
+  i64 num_zero_rows = R - SIZE(rows);
+
+  u64 ans = num_zero_rows * col_freq[K];
+  for (const auto &[r, indices] : rows) {
+    int k = SIZE(indices);
+    if (k > K) continue;
+    i64 tc0 = K - k;
+    i64 tc1 = K - k + 1;
+    i64 freq = col_freq[tc0];
+    for (auto i : indices) {
+      int c = candies[i].second;
+      int f = SIZE(cols[c]);
+      if (f == tc0) --freq;
+      if (f == tc1) ++freq;
+    }
+    ans += freq;
+  }
+  cout << ans << endl;
+}
