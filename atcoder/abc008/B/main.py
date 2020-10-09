194a764f18a7d1b0acb719849ff38ed121c16d75
Keiji Maekawa <m@keijak.com>
2020-10-10 01:58:19 +0200

    counter

---
+++ atcoder/abc008/B/main.py
@@ -0,0 +1,21 @@
+import sys
+from collections import Counter
+
+sys.setrecursionlimit(10 ** 8)
+ini = lambda: int(sys.stdin.readline())
+inl = lambda: [int(x) for x in sys.stdin.readline().split()]
+ins = lambda: sys.stdin.readline().rstrip()
+debug = lambda *a, **kw: print("\033[33m", *a, "\033[0m", **dict(file=sys.stderr, **kw))
+
+
+def solve():
+    n = ini()
+    c = Counter()
+    for _ in range(n):
+        s = ins()
+        c[s] += 1
+
+    return c.most_common(1)[0][0]
+
+
+print(solve())
