540269d820580ee9c858144e472825dd84f43d06
Keiji Maekawa <m@keijak.com>
2020-10-28 00:07:21 +0100

    memoize

---
+++ atcoder/abc037/D/main.cpp
@@ -0,0 +1,111 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/modint>
+using i64 = long long;
+using u64 = unsigned long long;
+#define REP(i, n) for (int i = 0, REP_N_ = int(n); i < REP_N_; ++i)
+#define ALL(x) std::begin(x), std::end(x)
+
+template <class T>
+inline bool chmax(T &a, T b) {
+  return a < b and ((a = std::move(b)), true);
+}
+template <class T>
+inline bool chmin(T &a, T b) {
+  return a > b and ((a = std::move(b)), true);
+}
+
+template <typename T>
+using V = std::vector<T>;
+template <typename T>
+std::istream &operator>>(std::istream &is, std::vector<T> &a) {
+  for (auto &x : a) is >> x;
+  return is;
+}
+template <typename Container>
+std::ostream &pprint(const Container &a, std::string_view sep = " ",
+                     std::string_view ends = "\n", std::ostream *os = nullptr) {
+  if (os == nullptr) os = &std::cout;
+  auto b = std::begin(a), e = std::end(a);
+  for (auto it = std::begin(a); it != e; ++it) {
+    if (it != b) *os << sep;
+    *os << *it;
+  }
+  return *os << ends;
+}
+template <typename T, typename = void>
+struct is_iterable : std::false_type {};
+template <typename T>
+struct is_iterable<T, std::void_t<decltype(std::begin(std::declval<T>())),
+                                  decltype(std::end(std::declval<T>()))>>
+    : std::true_type {};
+
+template <typename T,
+          typename = std::enable_if_t<is_iterable<T>::value &&
+                                      !std::is_same<T, std::string>::value>>
+std::ostream &operator<<(std::ostream &os, const T &a) {
+  return pprint(a, ", ", "", &(os << "{")) << "}";
+}
+template <typename T, typename U>
+std::ostream &operator<<(std::ostream &os, const std::pair<T, U> &a) {
+  return os << "(" << a.first << ", " << a.second << ")";
+}
+
+#ifdef ENABLE_DEBUG
+template <typename T>
+void pdebug(const T &value) {
+  std::cerr << value;
+}
+template <typename T, typename... Ts>
+void pdebug(const T &value, const Ts &... args) {
+  pdebug(value);
+  std::cerr << ", ";
+  pdebug(args...);
+}
+#define DEBUG(...)                                   \
+  do {                                               \
+    std::cerr << " \033[33m (L" << __LINE__ << ") "; \
+    std::cerr << #__VA_ARGS__ << ":\033[0m ";        \
+    pdebug(__VA_ARGS__);                             \
+    std::cerr << std::endl;                          \
+  } while (0)
+#else
+#define pdebug(...)
+#define DEBUG(...)
+#endif
+
+using namespace std;
+using mint = atcoder::modint1000000007;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int H, W;
+  cin >> H >> W;
+  auto A = vector(H, vector(W, 0));
+  REP(i, H) REP(j, W) { cin >> A[i][j]; }
+
+  array<int, 4> dx = {0, 0, 1, -1}, dy = {1, -1, 0, 0};
+
+  auto dp = vector(H, vector(W, optional<mint>()));
+  auto f = [&](auto self, int r, int c) -> mint {
+    auto &res = dp[r][c];
+    if (res.has_value()) return res.value();
+    int cur_val = A[r][c];
+    mint ret = 1;
+    REP(i, 4) {
+      int nr = r + dy[i];
+      int nc = c + dx[i];
+      if (nr < 0 or nr >= H or nc < 0 or nc >= W) continue;
+      if (A[nr][nc] < cur_val) {
+        ret += self(self, nr, nc);
+      }
+    }
+    res = ret;
+    return ret;
+  };
+  mint ans = 0;
+  REP(i, H) REP(j, W) { ans += f(f, i, j); }
+  cout << ans.val() << endl;
+}
