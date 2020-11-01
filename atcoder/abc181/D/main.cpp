0200d965326b96a0871a18206709911909ae480f
Keiji Maekawa <m@keijak.com>
2020-11-01 15:20:22 +0100

    abc

---
+++ atcoder/abc181/D/main.cpp
@@ -0,0 +1,118 @@
+#include <bits/stdc++.h>
+using i64 = long long;
+using u64 = unsigned long long;
+#define REP(i, n) for (int i = 0, REP_N_ = int(n); i < REP_N_; ++i)
+#define ALL(x) std::begin(x), std::end(x)
+
+template <class T>
+inline bool chmax(T &a, T b) {
+  return a < b and ((a = std::move(b)), true);
+}
+template <class T>
+inline bool chmin(T &a, T b) {
+  return a > b and ((a = std::move(b)), true);
+}
+
+template <typename T>
+using V = std::vector<T>;
+template <typename T>
+std::istream &operator>>(std::istream &is, std::vector<T> &a) {
+  for (auto &x : a) is >> x;
+  return is;
+}
+template <typename Container>
+std::ostream &pprint(const Container &a, std::string_view sep = " ",
+                     std::string_view ends = "\n", std::ostream *os = nullptr) {
+  if (os == nullptr) os = &std::cout;
+  auto b = std::begin(a), e = std::end(a);
+  for (auto it = std::begin(a); it != e; ++it) {
+    if (it != b) *os << sep;
+    *os << *it;
+  }
+  return *os << ends;
+}
+template <typename T, typename = void>
+struct is_iterable : std::false_type {};
+template <typename T>
+struct is_iterable<T, std::void_t<decltype(std::begin(std::declval<T>())),
+                                  decltype(std::end(std::declval<T>()))>>
+    : std::true_type {};
+
+template <typename T,
+          typename = std::enable_if_t<is_iterable<T>::value &&
+                                      !std::is_same<T, std::string>::value>>
+std::ostream &operator<<(std::ostream &os, const T &a) {
+  return pprint(a, ", ", "", &(os << "{")) << "}";
+}
+template <typename T, typename U>
+std::ostream &operator<<(std::ostream &os, const std::pair<T, U> &a) {
+  return os << "(" << a.first << ", " << a.second << ")";
+}
+
+#ifdef ENABLE_DEBUG
+template <typename T>
+void pdebug(const T &value) {
+  std::cerr << value;
+}
+template <typename T, typename... Ts>
+void pdebug(const T &value, const Ts &... args) {
+  pdebug(value);
+  std::cerr << ", ";
+  pdebug(args...);
+}
+#define DEBUG(...)                                   \
+  do {                                               \
+    std::cerr << " \033[33m (L" << __LINE__ << ") "; \
+    std::cerr << #__VA_ARGS__ << ":\033[0m ";        \
+    pdebug(__VA_ARGS__);                             \
+    std::cerr << std::endl;                          \
+  } while (0)
+#else
+#define pdebug(...)
+#define DEBUG(...)
+#endif
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  string s;
+  cin >> s;
+  int n = s.size();
+  map<int, int> c;
+  REP(i, n) {
+    int d = s[i] - '0';
+    c[d]++;
+  }
+  bool ok = [&]() -> bool {
+    if (n < 4) {
+      string t = s;
+      sort(ALL(t));
+      do {
+        u64 y;
+        stringstream ss;
+        ss << t;
+        ss >> y;
+        if (y % 8 == 0) return true;
+      } while (next_permutation(ALL(t)));
+      return false;
+    }
+    if (c[0] >= 3 and n > 3) return true;
+
+    for (i64 x = 8; x < 1000; x += 8) {
+      int d1 = x % 10;
+      int d2 = (x / 10) % 10;
+      int d3 = (x / 100) % 10;
+      c[d1]--;
+      c[d2]--;
+      c[d3]--;
+      if (c[d1] >= 0 and c[d2] >= 0 and c[d3] >= 0) return true;
+      c[d1]++;
+      c[d2]++;
+      c[d3]++;
+    }
+    return false;
+  }();
+  cout << (ok ? "Yes\n" : "No\n");
+}
