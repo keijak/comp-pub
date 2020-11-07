1b3d67aa76214fc7144caff5115b9b0feaa5645d
Keiji Maekawa <m@keijak.com>
2020-11-07 13:45:47 +0100

    grid_bfs

---
+++ atcoder/arc023/B/main.cpp
@@ -0,0 +1,133 @@
+#include <bits/stdc++.h>
+using i64 = long long;
+using u64 = unsigned long long;
+#define REP(i, n) for (int i = 0, REP_N_ = int(n); i < REP_N_; ++i)
+#define ALL(x) std::begin(x), std::end(x)
+
+template <class T>
+inline bool chmax(T &a, T b) {
+  return a < b and ((a = std::move(b)), true);
+}
+template <class T>
+inline bool chmin(T &a, T b) {
+  return a > b and ((a = std::move(b)), true);
+}
+
+template <typename T>
+using V = std::vector<T>;
+template <typename T>
+std::istream &operator>>(std::istream &is, std::vector<T> &a) {
+  for (auto &x : a) is >> x;
+  return is;
+}
+template <typename Container>
+std::ostream &pprint(const Container &a, std::string_view sep = " ",
+                     std::string_view ends = "\n", std::ostream *os = nullptr) {
+  if (os == nullptr) os = &std::cout;
+  auto b = std::begin(a), e = std::end(a);
+  for (auto it = std::begin(a); it != e; ++it) {
+    if (it != b) *os << sep;
+    *os << *it;
+  }
+  return *os << ends;
+}
+template <typename T, typename = void>
+struct is_iterable : std::false_type {};
+template <typename T>
+struct is_iterable<T, std::void_t<decltype(std::begin(std::declval<T>())),
+                                  decltype(std::end(std::declval<T>()))>>
+    : std::true_type {};
+
+template <typename T,
+          typename = std::enable_if_t<is_iterable<T>::value &&
+                                      !std::is_same<T, std::string>::value>>
+std::ostream &operator<<(std::ostream &os, const T &a) {
+  return pprint(a, ", ", "", &(os << "{")) << "}";
+}
+template <typename T, typename U>
+std::ostream &operator<<(std::ostream &os, const std::pair<T, U> &a) {
+  return os << "(" << a.first << ", " << a.second << ")";
+}
+
+#ifdef ENABLE_DEBUG
+template <typename T>
+void pdebug(const T &value) {
+  std::cerr << value;
+}
+template <typename T, typename... Ts>
+void pdebug(const T &value, const Ts &...args) {
+  pdebug(value);
+  std::cerr << ", ";
+  pdebug(args...);
+}
+#define DEBUG(...)                                   \
+  do {                                               \
+    std::cerr << " \033[33m (L" << __LINE__ << ") "; \
+    std::cerr << #__VA_ARGS__ << ":\033[0m ";        \
+    pdebug(__VA_ARGS__);                             \
+    std::cerr << std::endl;                          \
+  } while (0)
+#else
+#define pdebug(...)
+#define DEBUG(...)
+#endif
+
+using namespace std;
+
+struct State {
+  i64 cost;
+  int r, c;
+  //...
+};
+bool operator>(const State &x, const State &y) { return x.cost > y.cost; }
+
+const i64 INF = 1e18;
+
+std::vector<std::vector<i64>> grid_bfs(const std::vector<V<int>> &g,
+                                       int source_r, int source_c) {
+  const int H = g.size();
+  const int W = g[0].size();
+  array<int, 4> dx = {0, 0, 1, -1}, dy = {1, -1, 0, 0};
+  auto mincost = std::vector(H, std::vector(W, INF));
+  mincost[source_r][source_c] = 0LL;
+  std::deque<State> que;
+  que.push_back({0LL, source_r, source_c});
+  while (not que.empty()) {
+    State cur = std::move(que.front());
+    que.pop_front();
+    if (cur.cost != mincost[cur.r][cur.c]) {
+      continue;
+    }
+    for (int i = 0; i < 4; ++i) {
+      int nr = cur.r + dy[i];
+      int nc = cur.c + dx[i];
+      if (nr < 0 or nr >= H or nc < 0 or nc >= W) continue;
+      i64 new_cost = cur.cost + 1;
+      if (mincost[nr][nc] > new_cost) {
+        mincost[nr][nc] = new_cost;
+        que.push_back({new_cost, nr, nc});
+      }
+    }
+  }
+  return mincost;
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int R, C, D;
+  cin >> R >> C >> D;
+  auto grid = vector(R, vector(C, 0));
+  REP(i, R) REP(j, C) cin >> grid[i][j];
+
+  auto dist = grid_bfs(grid, 0, 0);
+  int ans = 0;
+  REP(i, R) REP(j, C) {
+    auto d = dist[i][j];
+    if (d % 2 != D % 2) continue;
+    if (d > D) continue;
+    chmax(ans, grid[i][j]);
+  }
+  cout << ans << endl;
+}
