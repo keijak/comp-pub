7e1527aaf58a1cf21eb61a329f5232c91acc68bc
Keiji Maekawa <m@keijak.com>
2020-11-07 11:43:58 +0100

    nim

---
+++ atcoder/arc013/C/main.cpp
@@ -0,0 +1,112 @@
+#include <bits/stdc++.h>
+using i64 = long long;
+using u64 = unsigned long long;
+#define REP(i, n) for (int i = 0, REP_N_ = int(n); i < REP_N_; ++i)
+#define ALL(x) std::begin(x), std::end(x)
+
+template <class T>
+inline bool chmax(T &a, T b) {
+  return a < b and ((a = std::move(b)), true);
+}
+template <class T>
+inline bool chmin(T &a, T b) {
+  return a > b and ((a = std::move(b)), true);
+}
+
+template <typename T>
+using V = std::vector<T>;
+template <typename T>
+std::istream &operator>>(std::istream &is, std::vector<T> &a) {
+  for (auto &x : a) is >> x;
+  return is;
+}
+template <typename Container>
+std::ostream &pprint(const Container &a, std::string_view sep = " ",
+                     std::string_view ends = "\n", std::ostream *os = nullptr) {
+  if (os == nullptr) os = &std::cout;
+  auto b = std::begin(a), e = std::end(a);
+  for (auto it = std::begin(a); it != e; ++it) {
+    if (it != b) *os << sep;
+    *os << *it;
+  }
+  return *os << ends;
+}
+template <typename T, typename = void>
+struct is_iterable : std::false_type {};
+template <typename T>
+struct is_iterable<T, std::void_t<decltype(std::begin(std::declval<T>())),
+                                  decltype(std::end(std::declval<T>()))>>
+    : std::true_type {};
+
+template <typename T,
+          typename = std::enable_if_t<is_iterable<T>::value &&
+                                      !std::is_same<T, std::string>::value>>
+std::ostream &operator<<(std::ostream &os, const T &a) {
+  return pprint(a, ", ", "", &(os << "{")) << "}";
+}
+template <typename T, typename U>
+std::ostream &operator<<(std::ostream &os, const std::pair<T, U> &a) {
+  return os << "(" << a.first << ", " << a.second << ")";
+}
+
+#ifdef ENABLE_DEBUG
+template <typename T>
+void pdebug(const T &value) {
+  std::cerr << value;
+}
+template <typename T, typename... Ts>
+void pdebug(const T &value, const Ts &...args) {
+  pdebug(value);
+  std::cerr << ", ";
+  pdebug(args...);
+}
+#define DEBUG(...)                                   \
+  do {                                               \
+    std::cerr << " \033[33m (L" << __LINE__ << ") "; \
+    std::cerr << #__VA_ARGS__ << ":\033[0m ";        \
+    pdebug(__VA_ARGS__);                             \
+    std::cerr << std::endl;                          \
+  } while (0)
+#else
+#define pdebug(...)
+#define DEBUG(...)
+#endif
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int N;
+  cin >> N;
+  V<int> stones;
+  stones.reserve(N * 6);
+  REP(i, N) {
+    int xl, yl, zl;
+    cin >> xl >> yl >> zl;
+    int m;
+    cin >> m;
+    int xmax = -1, xmin = xl + 1, ymax = -1, ymin = yl + 1, zmax = -1,
+        zmin = zl + 1;
+    REP(j, m) {
+      int x, y, z;
+      cin >> x >> y >> z;
+      chmin(xmin, x);
+      chmax(xmax, x);
+      chmin(ymin, y);
+      chmax(ymax, y);
+      chmin(zmin, z);
+      chmax(zmax, z);
+    }
+    stones.push_back(xmin);
+    stones.push_back(xl - 1 - xmax);
+    stones.push_back(ymin);
+    stones.push_back(yl - 1 - ymax);
+    stones.push_back(zmin);
+    stones.push_back(zl - 1 - zmax);
+  }
+
+  u64 xors = 0;
+  for (auto s : stones) xors ^= s;
+  cout << (xors == 0 ? "LOSE\n" : "WIN\n");
+}
