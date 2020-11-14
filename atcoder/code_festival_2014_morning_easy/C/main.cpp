55d1f29de3268325613cae906edf007a661f8788
Keiji Maekawa <m@keijak.com>
2020-11-14 22:38:36 +0100

    dijkstra

---
+++ atcoder/code_festival_2014_morning_easy/C/main.cpp
@@ -0,0 +1,161 @@
+#include <bits/stdc++.h>
+using i64 = long long;
+using u64 = unsigned long long;
+#define REP(i, n) for (int i = 0, REP_N_ = int(n); i < REP_N_; ++i)
+#define ALL(x) std::begin(x), std::end(x)
+
+template <class T>
+inline bool chmax(T &a, T b) {
+  return a < b and ((a = std::move(b)), true);
+}
+template <class T>
+inline bool chmin(T &a, T b) {
+  return a > b and ((a = std::move(b)), true);
+}
+template <class T>
+inline int ssize(const T &a) {
+  return (int)std::size(a);
+}
+
+template <typename T>
+using V = std::vector<T>;
+template <typename T>
+std::istream &operator>>(std::istream &is, std::vector<T> &a) {
+  for (auto &x : a) is >> x;
+  return is;
+}
+template <typename Container>
+std::ostream &pprint(const Container &a, std::string_view sep = " ",
+                     std::string_view ends = "\n", std::ostream *os = nullptr) {
+  if (os == nullptr) os = &std::cout;
+  auto b = std::begin(a), e = std::end(a);
+  for (auto it = std::begin(a); it != e; ++it) {
+    if (it != b) *os << sep;
+    *os << *it;
+  }
+  return *os << ends;
+}
+template <typename T, typename = void>
+struct is_iterable : std::false_type {};
+template <typename T>
+struct is_iterable<T, std::void_t<decltype(std::begin(std::declval<T>())),
+                                  decltype(std::end(std::declval<T>()))>>
+    : std::true_type {};
+
+template <typename T, typename = std::enable_if_t<
+                          is_iterable<T>::value &&
+                          !std::is_same<T, std::string_view>::value &&
+                          !std::is_same<T, std::string>::value>>
+std::ostream &operator<<(std::ostream &os, const T &a) {
+  return pprint(a, ", ", "", &(os << "{")) << "}";
+}
+template <typename T, typename U>
+std::ostream &operator<<(std::ostream &os, const std::pair<T, U> &a) {
+  return os << "(" << a.first << ", " << a.second << ")";
+}
+
+#ifdef ENABLE_DEBUG
+template <typename T>
+void pdebug(const T &value) {
+  std::cerr << value;
+}
+template <typename T, typename... Ts>
+void pdebug(const T &value, const Ts &...args) {
+  pdebug(value);
+  std::cerr << ", ";
+  pdebug(args...);
+}
+#define DEBUG(...)                                   \
+  do {                                               \
+    std::cerr << " \033[33m (L" << __LINE__ << ") "; \
+    std::cerr << #__VA_ARGS__ << ":\033[0m ";        \
+    pdebug(__VA_ARGS__);                             \
+    std::cerr << std::endl;                          \
+  } while (0)
+#else
+#define pdebug(...)
+#define DEBUG(...)
+#endif
+
+using namespace std;
+
+template <class T>
+using MinHeap = std::priority_queue<T, std::vector<T>, std::greater<T>>;
+
+struct Edge {
+  i64 cost;
+  int to;
+};
+
+struct State {
+  i64 cost;
+  int node;
+  //...
+};
+bool operator>(const State &x, const State &y) { return x.cost > y.cost; }
+
+const i64 INF = 1e18;
+
+std::vector<i64> dijkstra(const std::vector<std::vector<Edge>> &g, int source) {
+  const int n = g.size();
+  std::vector<i64> mincost(n, INF);
+  mincost[source] = 0LL;
+  MinHeap<State> que;
+  que.push({0LL, source});
+  while (not que.empty()) {
+    State cur = std::move(que.top());
+    que.pop();
+    if (cur.cost != mincost[cur.node]) {
+      continue;
+    }
+    for (const auto &e : g[cur.node]) {
+      i64 new_cost = cur.cost + e.cost;
+      if (mincost[e.to] > new_cost) {
+        mincost[e.to] = new_cost;
+        que.push({new_cost, e.to});
+      }
+    }
+  }
+  return mincost;
+}
+
+struct Graph {
+  int n;
+  V<V<Edge>> adj;
+  explicit Graph(int n) : n(n), adj(n) {}
+
+  void input(int m) {
+    for (int i = 0; i < m; ++i) {
+      int u, v, d;  // read as 1-indexed.
+      cin >> u >> v >> d;
+      u--, v--;  // to 0-indexed.
+      adj[u].push_back({d, v});
+      adj[v].push_back({d, u});
+    }
+  }
+};
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n, m;
+  cin >> n >> m;
+  int start, goal;
+  cin >> start >> goal;
+  --start, --goal;
+  Graph g(n);
+  g.input(m);
+
+  auto ds = dijkstra(g.adj, start);
+  auto dt = dijkstra(g.adj, goal);
+
+  REP(i, n) {
+    if (i == start or i == goal) continue;
+    if (ds[i] == dt[i] and ds[i] != INF) {
+      cout << i + 1 << endl;
+      exit(0);
+    }
+  }
+  cout << -1 << endl;
+}
