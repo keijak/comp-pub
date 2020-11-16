ab2eff2e4be151476c7a1074f96dff73ebdc2e1d
Keiji Maekawa <m@keijak.com>
2020-11-16 10:08:25 +0100

    codeforces div2 virtual

---
+++ cf/contest/701/D/main.cpp
@@ -0,0 +1,125 @@
+#include <bits/stdc++.h>
+using i64 = long long;
+using u64 = unsigned long long;
+using f128 = long double;
+#define REP(i, n) for (int i = 0, REP_N_ = (n); i < REP_N_; ++i)
+#define ALL(x) std::begin(x), std::end(x)
+
+template <class T>
+inline int ssize(const T &a) {
+  return (int)std::size(a);
+}
+template <class T>
+inline bool chmax(T &a, T b) {
+  return a < b and ((a = std::move(b)), true);
+}
+template <class T>
+inline bool chmin(T &a, T b) {
+  return a > b and ((a = std::move(b)), true);
+}
+
+template <typename T>
+using V = std::vector<T>;
+
+template <typename T>
+std::istream &operator>>(std::istream &is, std::vector<T> &a) {
+  for (auto &x : a) is >> x;
+  return is;
+}
+template <typename Container>
+std::ostream &pprint(const Container &a, std::string_view sep = " ",
+                     std::string_view ends = "\n", std::ostream *os = nullptr) {
+  if (os == nullptr) os = &std::cout;
+  auto b = std::begin(a), e = std::end(a);
+  for (auto it = std::begin(a); it != e; ++it) {
+    if (it != b) *os << sep;
+    *os << *it;
+  }
+  return *os << ends;
+}
+template <typename T, typename = void>
+struct is_iterable : std::false_type {};
+template <typename T>
+struct is_iterable<T, std::void_t<decltype(std::begin(std::declval<T>())),
+                                  decltype(std::end(std::declval<T>()))>>
+    : std::true_type {};
+
+template <typename T, typename = std::enable_if_t<
+                          is_iterable<T>::value &&
+                          !std::is_same<T, std::string_view>::value &&
+                          !std::is_same<T, std::string>::value>>
+std::ostream &operator<<(std::ostream &os, const T &a) {
+  return pprint(a, ", ", "", &(os << "{")) << "}";
+}
+template <typename T, typename U>
+std::ostream &operator<<(std::ostream &os, const std::pair<T, U> &a) {
+  return os << "(" << a.first << ", " << a.second << ")";
+}
+
+#ifdef ENABLE_DEBUG
+template <typename T>
+void pdebug(const T &value) {
+  std::cerr << value;
+}
+template <typename T, typename... Ts>
+void pdebug(const T &value, const Ts &... args) {
+  pdebug(value);
+  std::cerr << ", ";
+  pdebug(args...);
+}
+#define DEBUG(...)                                   \
+  do {                                               \
+    std::cerr << " \033[33m (L" << __LINE__ << ") "; \
+    std::cerr << #__VA_ARGS__ << ":\033[0m ";        \
+    pdebug(__VA_ARGS__);                             \
+    std::cerr << std::endl;                          \
+  } while (0)
+#else
+#define pdebug(...)
+#define DEBUG(...)
+#endif
+
+using namespace std;
+
+double solve() {
+  i64 n, l, v1, v2, k;
+  cin >> n >> l >> v1 >> v2 >> k;
+
+  auto check = [&](double x) {
+    // cerr << "===== x = " << x << " ====== \n";
+    if (v1 * x >= l) return true;
+    int rem = n;
+    double t2 = (l - v1 * x) / double(v2 - v1);
+    double t0 = 0;
+    for (int i = 0;; ++i) {
+      if (t0 + t2 > x) return false;
+      rem -= k;
+      DEBUG(i, rem);
+      if (rem <= 0) return true;
+      double tret = (v2 - v1) * t2 / (v1 + v2);
+      t0 += t2 + tret;
+      DEBUG(tret, t0);
+      if (t0 > x) return false;
+    }
+  };
+
+  double fv = 0.0, tv = n * 2 * double(l) / v2;
+  REP(iter, 80) {
+    double mid = (fv + tv) * 0.5;
+    if (check(mid)) {
+      // cerr << "    OK\n";
+      tv = mid;
+    } else {
+      // cerr << "    NG\n";
+      fv = mid;
+    }
+  }
+  return tv;
+}
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  cout << fixed << setprecision(16);
+  cout << solve() << endl;
+}
---
+++ cf/contest/701/E/main.cpp
@@ -0,0 +1,134 @@
+#include <bits/stdc++.h>
+using i64 = long long;
+using u64 = unsigned long long;
+#define REP(i, n) for (int i = 0, REP_N_ = (n); i < REP_N_; ++i)
+#define ALL(x) std::begin(x), std::end(x)
+
+template <class T>
+inline int ssize(const T &a) {
+  return (int)std::size(a);
+}
+template <class T>
+inline bool chmax(T &a, T b) {
+  return a < b and ((a = std::move(b)), true);
+}
+template <class T>
+inline bool chmin(T &a, T b) {
+  return a > b and ((a = std::move(b)), true);
+}
+
+template <typename T>
+using V = std::vector<T>;
+
+template <typename T>
+std::istream &operator>>(std::istream &is, std::vector<T> &a) {
+  for (auto &x : a) is >> x;
+  return is;
+}
+template <typename Container>
+std::ostream &pprint(const Container &a, std::string_view sep = " ",
+                     std::string_view ends = "\n", std::ostream *os = nullptr) {
+  if (os == nullptr) os = &std::cout;
+  auto b = std::begin(a), e = std::end(a);
+  for (auto it = std::begin(a); it != e; ++it) {
+    if (it != b) *os << sep;
+    *os << *it;
+  }
+  return *os << ends;
+}
+template <typename T, typename = void>
+struct is_iterable : std::false_type {};
+template <typename T>
+struct is_iterable<T, std::void_t<decltype(std::begin(std::declval<T>())),
+                                  decltype(std::end(std::declval<T>()))>>
+    : std::true_type {};
+
+template <typename T, typename = std::enable_if_t<
+                          is_iterable<T>::value &&
+                          !std::is_same<T, std::string_view>::value &&
+                          !std::is_same<T, std::string>::value>>
+std::ostream &operator<<(std::ostream &os, const T &a) {
+  return pprint(a, ", ", "", &(os << "{")) << "}";
+}
+template <typename T, typename U>
+std::ostream &operator<<(std::ostream &os, const std::pair<T, U> &a) {
+  return os << "(" << a.first << ", " << a.second << ")";
+}
+
+#ifdef ENABLE_DEBUG
+template <typename T>
+void pdebug(const T &value) {
+  std::cerr << value;
+}
+template <typename T, typename... Ts>
+void pdebug(const T &value, const Ts &... args) {
+  pdebug(value);
+  std::cerr << ", ";
+  pdebug(args...);
+}
+#define DEBUG(...)                                   \
+  do {                                               \
+    std::cerr << " \033[33m (L" << __LINE__ << ") "; \
+    std::cerr << #__VA_ARGS__ << ":\033[0m ";        \
+    pdebug(__VA_ARGS__);                             \
+    std::cerr << std::endl;                          \
+  } while (0)
+#else
+#define pdebug(...)
+#define DEBUG(...)
+#endif
+
+using namespace std;
+
+i64 solve() {
+  int n, k;
+  cin >> n >> k;
+  const int k2 = k * 2;
+  V<int> univ(k2);
+  cin >> univ;
+  V<bool> is_univ(n);
+  for (auto &x : univ) {
+    --x;
+    is_univ[x] = true;
+  }
+
+  V<V<int>> g(n);  // tree
+  REP(i, n - 1) {
+    int x, y;
+    cin >> x >> y;
+    --x, --y;
+    g[x].push_back(y);
+    g[y].push_back(x);
+  }
+
+  V<int> usize(n);
+  auto dfs1 = [&](auto self, int v, int p) -> int {
+    int sz = is_univ[v] ? 1 : 0;
+    for (auto w : g[v]) {
+      if (w == p) continue;
+      sz += self(self, w, v);
+    }
+    return (usize[v] = sz);
+  };
+  dfs1(dfs1, 0, -1);
+  assert(usize[0] == k2);
+
+  i64 ans = 0;
+  auto dfs2 = [&](auto self, int v, int p) -> void {
+    if (p >= 0) {
+      ans += min(usize[v], k2 - usize[v]);
+    }
+    for (auto w : g[v]) {
+      if (w == p) continue;
+      self(self, w, v);
+    }
+  };
+  dfs2(dfs2, 0, -1);
+  return ans;
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout << solve() << endl;
+}
