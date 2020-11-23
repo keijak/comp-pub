eabe0156e41e192459fb811e8dbaabe9760a7121
Keiji Maekawa <m@keijak.com>
2020-11-23 11:25:21 +0100

    dame

--- atcoder/abc184/F/twopointers.cpp
+++ atcoder/abc184/F/twopointers.cpp
@@ -1,23 +1,4 @@
 #include <bits/stdc++.h>
-#define REP(i, n) for (int i = 0, REP_N_ = (n); i < REP_N_; ++i)
-#define ALL(x) std::begin(x), std::end(x)
-
-template <class T>
-inline int ssize(const T &a) {
-  return (int)std::size(a);
-}
-template <class T>
-inline bool chmax(T &a, T b) {
-  return a < b and ((a = std::move(b)), true);
-}
-template <class T>
-inline bool chmin(T &a, T b) {
-  return a > b and ((a = std::move(b)), true);
-}
-
-template <typename T>
-using V = std::vector<T>;
-
 using namespace std;
 
 int tmp[(1 << 20) + 5];
@@ -25,47 +6,47 @@ int tmp[(1 << 20) + 5];
 int solve() {
   int N, T;
   cin >> N >> T;
-  V<int> A;
+  vector<int> A;
   A.reserve(N);
-  REP(i, N) {
+  for (int i = 0; i < N; ++i) {
     int x;
     cin >> x;
     if (x <= T) A.push_back(x);
   }
-  N = ssize(A);
-  sort(ALL(A));
+  N = A.size();
+  sort(A.begin(), A.end());
 
   int M1 = N / 2;
   int M2 = N - M1;
 
-  V<int> st, st2;
+  vector<int> st1, st2;
   {
-    st.reserve((1 << M1) + 5);
-    st.push_back(0);
-    REP(i, M1) {
-      int j = 0, t = 0, ai = A[i], sz1 = ssize(st);
+    st1.reserve((1 << M1) + 5);
+    st1.push_back(0);
+    for (int i = 0; i < M1; ++i) {
+      int j = 0, t = 0, ai = A[i], sz1 = st1.size();
       for (int k = 0; k < sz1;) {
-        if (j >= sz1 or st[k] <= st[j] + ai) {
-          tmp[t++] = st[k++];
+        if (j >= sz1 or st1[k] <= st1[j] + ai) {
+          tmp[t++] = st1[k++];
         } else if (j < sz1) {
-          int x = st[j] + ai;
+          int x = st1[j] + ai;
           if (x <= T) tmp[t++] = x;
           ++j;
         }
       }
       while (j < sz1) {
-        int x = st[j] + ai;
+        int x = st1[j] + ai;
         if (x <= T) tmp[t++] = x;
         ++j;
       }
-      st.assign(tmp, tmp + t);
+      st1.assign(tmp, tmp + t);
     }
   }
   {
     st2.reserve((1 << M2) + 5);
     st2.push_back(0);
-    REP(i, M2) {
-      int j = 0, t = 0, ai = A[M1 + i], sz2 = ssize(st2);
+    for (int i = 0; i < M2; ++i) {
+      int j = 0, t = 0, ai = A[M1 + i], sz2 = st2.size();
       for (int k = 0; k < sz2;) {
         if (j >= sz2 or st2[k] <= st2[j] + ai) {
           tmp[t++] = st2[k++];
@@ -82,15 +63,15 @@ int solve() {
       }
       st2.assign(tmp, tmp + t);
     }
-    reverse(ALL(st2));
+    reverse(st2.begin(), st2.end());
   }
 
-  int ans = 0, j = 0, sz2 = ssize(st2);
-  for (int i = 0, sz1 = ssize(st); i < sz1; ++i) {
-    int ub = T - st[i];
+  int ans = 0, j = 0, sz2 = st2.size();
+  for (int i = 0, sz1 = st1.size(); i < sz1; ++i) {
+    int ub = T - st1[i];
     while (j < sz2 and st2[j] > ub) ++j;
     if (j == sz2) break;
-    chmax(ans, st2[j] + st[i]);
+    if (int x = st1[i] + st2[j]; x > ans) ans = x;
   }
   return ans;
 }
@@ -98,5 +79,6 @@ int solve() {
 int main() {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
+
   cout << solve() << '\n';
 }
