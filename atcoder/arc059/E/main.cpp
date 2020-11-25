d4aabf03e762b02dd21b9bc3e14a657e46bb5df1
Keiji Maekawa <m@keijak.com>
2020-11-25 11:15:44 +0100

    fps

--- atcoder/arc059/E/main.cpp
+++ atcoder/arc059/E/main.cpp
@@ -1,85 +1,14 @@
 #include <bits/stdc++.h>
 
-#include <atcoder/convolution>
 #include <atcoder/modint>
+
 #define REP(i, n) for (int i = 0, REP_N_ = (n); i < REP_N_; ++i)
-#define ALL(x) std::begin(x), std::end(x)
-using i64 = long long;
-using u64 = unsigned long long;
-
-template <class T>
-inline int ssize(const T &a) {
-  return (int)std::size(a);
-}
-template <class T>
-inline bool chmax(T &a, T b) {
-  return a < b and ((a = std::move(b)), true);
-}
-template <class T>
-inline bool chmin(T &a, T b) {
-  return a > b and ((a = std::move(b)), true);
-}
-
-template <typename T>
-using V = std::vector<T>;
 
 template <typename T>
 std::istream &operator>>(std::istream &is, std::vector<T> &a) {
   for (auto &x : a) is >> x;
   return is;
 }
-template <typename Container>
-std::ostream &pprint(const Container &a, std::string_view sep = " ",
-                     std::string_view ends = "\n", std::ostream *os = nullptr) {
-  if (os == nullptr) os = &std::cout;
-  auto b = std::begin(a), e = std::end(a);
-  for (auto it = std::begin(a); it != e; ++it) {
-    if (it != b) *os << sep;
-    *os << *it;
-  }
-  return *os << ends;
-}
-template <typename T, typename = void>
-struct is_iterable : std::false_type {};
-template <typename T>
-struct is_iterable<T, std::void_t<decltype(std::begin(std::declval<T>())),
-                                  decltype(std::end(std::declval<T>()))>>
-    : std::true_type {};
-
-template <typename T, typename = std::enable_if_t<
-                          is_iterable<T>::value &&
-                          !std::is_same<T, std::string_view>::value &&
-                          !std::is_same<T, std::string>::value>>
-std::ostream &operator<<(std::ostream &os, const T &a) {
-  return pprint(a, ", ", "", &(os << "{")) << "}";
-}
-template <typename T, typename U>
-std::ostream &operator<<(std::ostream &os, const std::pair<T, U> &a) {
-  return os << "(" << a.first << ", " << a.second << ")";
-}
-
-#ifdef ENABLE_DEBUG
-template <typename T>
-void pdebug(const T &value) {
-  std::cerr << value;
-}
-template <typename T, typename... Ts>
-void pdebug(const T &value, const Ts &... args) {
-  pdebug(value);
-  std::cerr << ", ";
-  pdebug(args...);
-}
-#define DEBUG(...)                                   \
-  do {                                               \
-    std::cerr << " \033[33m (L" << __LINE__ << ") "; \
-    std::cerr << #__VA_ARGS__ << ":\033[0m ";        \
-    pdebug(__VA_ARGS__);                             \
-    std::cerr << std::endl;                          \
-  } while (0)
-#else
-#define pdebug(...)
-#define DEBUG(...)
-#endif
 
 // Formal Power Series (dense format).
 template <typename T, int DMAX>
@@ -90,6 +19,9 @@ struct DenseFPS {
   DenseFPS() : coeff_(1) {}  // zero-initialized
   explicit DenseFPS(std::vector<T> c) : coeff_(std::move(c)) {
     assert((int)c.size() <= DMAX + 1);
+  }
+  DenseFPS(std::initializer_list<T> init) : coeff_(init.begin(), init.end()) {
+    assert((int)init.size() <= DMAX + 1);
   }
 
   DenseFPS(const DenseFPS &other) : coeff_(other.coeff_) {}
@@ -155,6 +87,11 @@ struct DenseFPS {
     res *= scalar;
     return res;
   }
+  friend DenseFPS operator*(const T &scalar, const DenseFPS &y) {
+    DenseFPS res = {scalar};
+    res *= y;
+    return res;
+  }
   DenseFPS &operator*=(const DenseFPS &other) {
     *this = this->mul_naive(other);
     return *this;
@@ -172,11 +109,31 @@ struct DenseFPS {
     res /= scalar;
     return res;
   }
+  friend DenseFPS operator/(const T &scalar, const DenseFPS &y) {
+    DenseFPS res = {scalar};
+    res /= y;
+    return res;
+  }
   DenseFPS &operator/=(const DenseFPS &other) {
     return *this *= other.inv_naive();
   }
   friend DenseFPS operator/(const DenseFPS &x, const DenseFPS &y) {
     return x * y.inv_naive();
+  }
+
+  // Naive inverse. O(N^2)
+  DenseFPS inv_naive() const {
+    std::vector<T> res(DMAX + 1);
+    res[0] = coeff_[0].inv();
+    for (int i = 1; i <= DMAX; ++i) {
+      T s = 0;
+      const int mi = std::min(i + 1, size());
+      for (int j = 1; j < mi; ++j) {
+        s += coeff_[j] * res[i - j];
+      }
+      res[i] = -res[0] * s;
+    }
+    return DenseFPS(std::move(res));
   }
 
  private:
@@ -192,24 +149,11 @@ struct DenseFPS {
     }
     return DenseFPS(std::move(res));
   }
-
-  // Naive inverse. O(N^2)
-  DenseFPS inv_naive() const {
-    std::vector<T> res(size());
-    res[0] = coeff_[0].inv();
-    for (int i = 1; i < size(); ++i) {
-      T s = 0;
-      for (int j = 1; j <= i; ++j) {
-        s += coeff_[j] * res[i - j];
-      }
-      res[i] = -res[0] * s;
-    }
-    return DenseFPS(std::move(res));
-  }
 };
 
 using namespace std;
 using Mint = atcoder::modint1000000007;
+ostream &operator<<(ostream &os, const Mint &m) { return os << m.val(); }
 
 const int CMAX = 400;
 using FPS = DenseFPS<Mint, CMAX>;
@@ -217,20 +161,16 @@ using FPS = DenseFPS<Mint, CMAX>;
 Mint solve() {
   int N, C;
   cin >> N >> C;
-  V<int> A(N), B(N);
+  vector<int> A(N), B(N);
   cin >> A >> B;
 
-  FPS f({1});
+  FPS f = {1};
   REP(i, N) {
-    V<Mint> v(C + 1);
+    FPS g = {0};
     for (int x = A[i]; x <= B[i]; ++x) {
-      Mint a = 1;
-      REP(j, C + 1) {
-        v[j] += a;
-        a *= x;
-      }
+      g += 1 / FPS{1, -x};
     }
-    f *= FPS(v);
+    f *= g;
   }
   return f[C];
 }
@@ -238,5 +178,5 @@ Mint solve() {
 int main() {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
-  cout << solve().val() << endl;
+  cout << solve() << endl;
 }
