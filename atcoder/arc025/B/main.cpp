0e28154945e7ac5a8447b845509a00d7197d2fe8
Keiji Maekawa <m@keijak.com>
2020-11-30 02:49:51 +0100

    cumsum

---
+++ atcoder/arc025/B/main.cpp
@@ -0,0 +1,116 @@
+#include <bits/stdc++.h>
+#define REP(i, n) for (int i = 0, REP_N_ = (n); i < REP_N_; ++i)
+#define ALL(x) std::begin(x), std::end(x)
+using i64 = long long;
+using u64 = unsigned long long;
+
+template <class T>
+inline int ssize(const T &a) {
+  return (int)std::size(a);
+}
+template <class T>
+inline bool chmax(T &a, T b) {
+  return a < b and ((a = std::move(b)), true);
+}
+template <class T>
+inline bool chmin(T &a, T b) {
+  return a > b and ((a = std::move(b)), true);
+}
+
+template <typename T>
+std::istream &operator>>(std::istream &is, std::vector<T> &a) {
+  for (auto &x : a) is >> x;
+  return is;
+}
+template <typename Container>
+std::ostream &pprint(const Container &a, std::string_view sep = " ",
+                     std::string_view ends = "\n", std::ostream *os = nullptr) {
+  if (os == nullptr) os = &std::cout;
+  auto b = std::begin(a), e = std::end(a);
+  for (auto it = std::begin(a); it != e; ++it) {
+    if (it != b) *os << sep;
+    *os << *it;
+  }
+  return *os << ends;
+}
+template <typename T, typename = void>
+struct is_iterable : std::false_type {};
+template <typename T>
+struct is_iterable<T, std::void_t<decltype(std::begin(std::declval<T>())),
+                                  decltype(std::end(std::declval<T>()))>>
+    : std::true_type {};
+
+template <typename T, typename = std::enable_if_t<
+                          is_iterable<T>::value &&
+                          !std::is_same<T, std::string_view>::value &&
+                          !std::is_same<T, std::string>::value>>
+std::ostream &operator<<(std::ostream &os, const T &a) {
+  return pprint(a, ", ", "", &(os << "{")) << "}";
+}
+template <typename T, typename U>
+std::ostream &operator<<(std::ostream &os, const std::pair<T, U> &a) {
+  return os << "(" << a.first << ", " << a.second << ")";
+}
+
+#ifdef ENABLE_DEBUG
+template <typename T>
+void pdebug(const T &value) {
+  std::cerr << value;
+}
+template <typename T, typename... Ts>
+void pdebug(const T &value, const Ts &...args) {
+  pdebug(value);
+  std::cerr << ", ";
+  pdebug(args...);
+}
+#define DEBUG(...)                                   \
+  do {                                               \
+    std::cerr << " \033[33m (L" << __LINE__ << ") "; \
+    std::cerr << #__VA_ARGS__ << ":\033[0m ";        \
+    pdebug(__VA_ARGS__);                             \
+    std::cerr << std::endl;                          \
+  } while (0)
+#else
+#define pdebug(...)
+#define DEBUG(...)
+#endif
+
+using namespace std;
+
+i64 solve() {
+  int H, W;
+  cin >> H >> W;
+  auto grid = vector(H, vector(W, 0LL));
+  REP(i, H) REP(j, W) cin >> grid[i][j];
+
+  auto cumb = vector(H + 1, vector(W + 1, 0LL));
+  auto cumw = vector(H + 1, vector(W + 1, 0LL));
+  REP(i, H) {
+    REP(j, W) {
+      if ((i + j) % 2 == 0) {
+        int b = grid[i][j];
+        cumb[i + 1][j + 1] = cumb[i + 1][j] + cumb[i][j + 1] - cumb[i][j] + b;
+        cumw[i + 1][j + 1] = cumw[i + 1][j] + cumw[i][j + 1] - cumw[i][j];
+      } else {
+        int w = grid[i][j];
+        cumw[i + 1][j + 1] = cumw[i + 1][j] + cumw[i][j + 1] - cumw[i][j] + w;
+        cumb[i + 1][j + 1] = cumb[i + 1][j] + cumb[i][j + 1] - cumb[i][j];
+      }
+    }
+  }
+
+  i64 ans = 0;
+  REP(i, H + 1) REP(j, W + 1) REP(i2, i) REP(j2, j) {
+    i64 b = cumb[i][j] - cumb[i2][j] - cumb[i][j2] + cumb[i2][j2];
+    i64 w = cumw[i][j] - cumw[i2][j] - cumw[i][j2] + cumw[i2][j2];
+    if (b == w) {
+      chmax(ans, i64(i - i2) * i64(j - j2));
+    }
+  }
+  return ans;
+}
+
+int main() {
+  ios::sync_with_stdio(false), cin.tie(nullptr);
+  cout << solve() << endl;
+}
