9bbe7d3b340487fb52829d485d0a83409207dcf5
Keiji Maekawa <m@keijak.com>
2020-12-08 13:11:46 +0100

    parity

---
+++ atcoder/jsc2019_qual/C/main.cpp
@@ -0,0 +1,128 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/modint>
+
+#define REP(i, n) for (int i = 0, REP_N_ = (n); i < REP_N_; ++i)
+#define ALL(x) std::begin(x), std::end(x)
+using i64 = long long;
+using u64 = unsigned long long;
+
+template <class T>
+inline int ssize(const T &a) {
+  return (int)std::size(a);
+}
+template <class T>
+inline bool chmax(T &a, T b) {
+  return a < b and ((a = std::move(b)), true);
+}
+template <class T>
+inline bool chmin(T &a, T b) {
+  return a > b and ((a = std::move(b)), true);
+}
+
+template <typename T>
+std::istream &operator>>(std::istream &is, std::vector<T> &a) {
+  for (auto &x : a) is >> x;
+  return is;
+}
+template <typename Container>
+std::ostream &pprint(const Container &a, std::string_view sep = " ",
+                     std::string_view ends = "\n", std::ostream *os = nullptr) {
+  if (os == nullptr) os = &std::cout;
+  auto b = std::begin(a), e = std::end(a);
+  for (auto it = std::begin(a); it != e; ++it) {
+    if (it != b) *os << sep;
+    *os << *it;
+  }
+  return *os << ends;
+}
+template <typename T, typename = void>
+struct is_iterable : std::false_type {};
+template <typename T>
+struct is_iterable<T, std::void_t<decltype(std::begin(std::declval<T>())),
+                                  decltype(std::end(std::declval<T>()))>>
+    : std::true_type {};
+
+template <typename T, typename = std::enable_if_t<
+                          is_iterable<T>::value &&
+                          !std::is_same<T, std::string_view>::value &&
+                          !std::is_same<T, std::string>::value>>
+std::ostream &operator<<(std::ostream &os, const T &a) {
+  return pprint(a, ", ", "", &(os << "{")) << "}";
+}
+template <typename T, typename U>
+std::ostream &operator<<(std::ostream &os, const std::pair<T, U> &a) {
+  return os << "(" << a.first << ", " << a.second << ")";
+}
+
+#ifdef ENABLE_DEBUG
+template <typename T>
+void pdebug(const T &value) {
+  std::cerr << value;
+}
+template <typename T, typename... Ts>
+void pdebug(const T &value, const Ts &...args) {
+  pdebug(value);
+  std::cerr << ", ";
+  pdebug(args...);
+}
+#define DEBUG(...)                                   \
+  do {                                               \
+    std::cerr << " \033[33m (L" << __LINE__ << ") "; \
+    std::cerr << #__VA_ARGS__ << ":\033[0m ";        \
+    pdebug(__VA_ARGS__);                             \
+    std::cerr << std::endl;                          \
+  } while (0)
+#else
+#define pdebug(...)
+#define DEBUG(...)
+#endif
+
+using namespace std;
+using Mint = atcoder::modint1000000007;
+std::ostream &operator<<(std::ostream &os, const Mint &m) {
+  return os << m.val();
+}
+
+i64 solve() {
+  int n;
+  cin >> n;
+  const int n2 = n * 2;
+  string s;
+  cin >> s;
+  vector<bool> start(n2);
+  int scnt = 0;
+  REP(i, n2) {
+    bool odd_pos = i & 1;
+    bool black = s[i] == 'B';
+    start[i] = odd_pos ^ black;
+    if (start[i]) ++scnt;
+  }
+  DEBUG(start);
+  if (scnt != n) return 0;
+  if (not start[0]) return 0;
+  if (start[n2 - 1]) return 0;
+
+  Mint choice = 1;
+  scnt = 0;
+  REP(i, n2) {
+    if (start[i]) {
+      ++scnt;
+      continue;
+    }
+    if (scnt == 0) return 0;
+    choice *= scnt;
+    --scnt;
+  }
+  DEBUG(choice);
+  Mint perm = 1;
+  REP(i, n) { perm *= i + 1; }
+  DEBUG(perm);
+
+  return (choice * perm).val();
+}
+
+int main() {
+  ios::sync_with_stdio(false), cin.tie(nullptr);
+  cout << solve() << "\n";
+}
