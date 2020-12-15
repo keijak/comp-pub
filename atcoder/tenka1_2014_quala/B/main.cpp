73279f31305a6a00b37751358f207b72c2365500
Keiji Maekawa <m@keijak.com>
2020-12-15 15:25:22 +0100

    simulation

---
+++ atcoder/tenka1_2014_quala/B/main.cpp
@@ -0,0 +1,121 @@
+#include <bits/stdc++.h>
+#define REP(i, n) for (int i = 0, REP_N_ = (n); i < REP_N_; ++i)
+#define ALL(x) std::begin(x), std::end(x)
+using i64 = long long;
+using u64 = unsigned long long;
+
+template <typename T, typename U>
+inline bool chmax(T &a, U b) {
+  return a < b and ((a = std::move(b)), true);
+}
+template <typename T, typename U>
+inline bool chmin(T &a, U b) {
+  return a > b and ((a = std::move(b)), true);
+}
+template <typename T>
+inline int ssize(const T &a) {
+  return (int)std::size(a);
+}
+
+template <typename T>
+std::istream &operator>>(std::istream &is, std::vector<T> &a) {
+  for (auto &x : a) is >> x;
+  return is;
+}
+template <typename Container>
+std::ostream &print_seq(const Container &a, std::string_view sep = " ",
+                        std::string_view ends = "\n",
+                        std::ostream *os = nullptr) {
+  if (os == nullptr) os = &std::cout;
+  auto b = std::begin(a), e = std::end(a);
+  for (auto it = std::begin(a); it != e; ++it) {
+    if (it != b) *os << sep;
+    *os << *it;
+  }
+  return *os << ends;
+}
+template <typename T, typename = void>
+struct is_iterable : std::false_type {};
+template <typename T>
+struct is_iterable<T, std::void_t<decltype(std::begin(std::declval<T>())),
+                                  decltype(std::end(std::declval<T>()))>>
+    : std::true_type {};
+
+template <typename T, typename = std::enable_if_t<
+                          is_iterable<T>::value &&
+                          !std::is_same<T, std::string_view>::value &&
+                          !std::is_same<T, std::string>::value>>
+std::ostream &operator<<(std::ostream &os, const T &a) {
+  return print_seq(a, ", ", "", &(os << "{")) << "}";
+}
+template <typename T, typename U>
+std::ostream &operator<<(std::ostream &os, const std::pair<T, U> &a) {
+  return os << "(" << a.first << ", " << a.second << ")";
+}
+
+#ifdef ENABLE_DEBUG
+template <typename T>
+void pdebug(const T &value) {
+  std::cerr << value;
+}
+template <typename T, typename... Ts>
+void pdebug(const T &value, const Ts &...args) {
+  pdebug(value);
+  std::cerr << ", ";
+  pdebug(args...);
+}
+#define DEBUG(...)                                   \
+  do {                                               \
+    std::cerr << " \033[33m (L" << __LINE__ << ") "; \
+    std::cerr << #__VA_ARGS__ << ":\033[0m ";        \
+    pdebug(__VA_ARGS__);                             \
+    std::cerr << std::endl;                          \
+  } while (0)
+#else
+#define pdebug(...)
+#define DEBUG(...)
+#endif
+
+using namespace std;
+
+i64 solve() {
+  string command;
+  cin >> command;
+  int n = ssize(command);
+  i64 damage = 0, combo = 0;
+  vector<bool> charging(n + 10, false);
+  vector<int> reserved_combo(n + 10, 0);
+  vector<int> reserved_hand(n + 10, 0);
+  int hand = 5;
+  REP(i, n) {
+    combo += reserved_combo[i];
+    hand += reserved_hand[i];
+    if (charging[i]) continue;
+    char s = command[i];
+    if (s == '-') {
+      continue;
+    } else if (s == 'N') {
+      if (hand < 1) continue;
+      hand--;
+      i64 d = 10 + combo / 10;
+      damage += d;
+      reserved_combo[i + 2]++;
+      reserved_hand[i + 7]++;
+    } else {
+      assert(s == 'C');
+      if (hand < 3) continue;
+      hand -= 3;
+      i64 d = 50 + 5 * (combo / 10);
+      damage += d;
+      reserved_combo[i + 4]++;
+      reserved_hand[i + 9] += 3;
+      charging[i + 1] = charging[i + 2] = true;
+    }
+  }
+  return damage;
+}
+
+int main() {
+  std::ios::sync_with_stdio(false), cin.tie(nullptr);
+  cout << solve() << "\n";
+}
