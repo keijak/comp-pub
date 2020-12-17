b3e5a9f4ade063c2f29520c19e0bcb5f4bc201ee
Keiji Maekawa <m@keijak.com>
2020-12-17 23:36:30 +0100

    interval set

---
+++ atcoder/code_festival_2015_qualb/D/main.cpp
@@ -0,0 +1,123 @@
+#include <cassert>
+#include <iostream>
+#include <map>
+#include <vector>
+
+#define REP(i, n) for (int i = 0, REP_N_ = (n); i < REP_N_; ++i)
+#define ALL(x) std::begin(x), std::end(x)
+using i64 = long long;
+using u64 = unsigned long long;
+
+template <typename T, typename U>
+inline bool chmax(T &a, U b) {
+  return a < b and ((a = std::move(b)), true);
+}
+template <typename T, typename U>
+inline bool chmin(T &a, U b) {
+  return a > b and ((a = std::move(b)), true);
+}
+template <typename T>
+inline int ssize(const T &a) {
+  return (int)std::size(a);
+}
+
+template <typename T>
+std::istream &operator>>(std::istream &is, std::vector<T> &a) {
+  for (auto &x : a) is >> x;
+  return is;
+}
+template <typename Container>
+std::ostream &print_seq(const Container &a, std::string_view sep = " ",
+                        std::string_view ends = "\n",
+                        std::ostream *os = nullptr) {
+  if (os == nullptr) os = &std::cout;
+  auto b = std::begin(a), e = std::end(a);
+  for (auto it = std::begin(a); it != e; ++it) {
+    if (it != b) *os << sep;
+    *os << *it;
+  }
+  return *os << ends;
+}
+template <typename T, typename = void>
+struct is_iterable : std::false_type {};
+template <typename T>
+struct is_iterable<T, std::void_t<decltype(std::begin(std::declval<T>())),
+                                  decltype(std::end(std::declval<T>()))>>
+    : std::true_type {};
+
+template <typename T, typename = std::enable_if_t<
+                          is_iterable<T>::value &&
+                          !std::is_same<T, std::string_view>::value &&
+                          !std::is_same<T, std::string>::value>>
+std::ostream &operator<<(std::ostream &os, const T &a) {
+  return print_seq(a, ", ", "", &(os << "{")) << "}";
+}
+template <typename T, typename U>
+std::ostream &operator<<(std::ostream &os, const std::pair<T, U> &a) {
+  return os << "(" << a.first << ", " << a.second << ")";
+}
+
+#ifdef ENABLE_DEBUG
+template <typename T>
+void pdebug(const T &value) {
+  std::cerr << value;
+}
+template <typename T, typename... Ts>
+void pdebug(const T &value, const Ts &...args) {
+  pdebug(value);
+  std::cerr << ", ";
+  pdebug(args...);
+}
+#define DEBUG(...)                                   \
+  do {                                               \
+    std::cerr << " \033[33m (L" << __LINE__ << ") "; \
+    std::cerr << #__VA_ARGS__ << ":\033[0m ";        \
+    pdebug(__VA_ARGS__);                             \
+    std::cerr << std::endl;                          \
+  } while (0)
+#else
+#define pdebug(...)
+#define DEBUG(...)
+#endif
+
+using namespace std;
+const i64 INF = 1e16;
+
+void solve() {
+  int n;
+  cin >> n;
+  map<i64, i64> intervals;
+  intervals.emplace(INF, 1);
+  REP(i, n) {
+    i64 s, c;
+    cin >> s >> c;
+
+    auto it = intervals.lower_bound(s);
+    assert(it != intervals.end());
+    assert(it->first >= s);
+    i64 last_pos = s;
+    while (c > 0) {
+      i64 ied = it->first;
+      i64 ist = it->second;
+      i64 st = max(s, ist);
+      i64 ed = min(st + c - 1, ied);
+      it = intervals.erase(it);
+      if (ist < st) {
+        intervals.emplace(st - 1, ist);
+      }
+      if (ed < ied) {
+        intervals.emplace(ied, ed + 1);
+      }
+      last_pos = ed;
+      i64 len = ed - st + 1;
+      c -= len;
+      s = it->second;
+    }
+    cout << last_pos << '\n';
+  }
+}
+
+int main() {
+  std::ios::sync_with_stdio(false), cin.tie(nullptr);
+  solve();
+}
