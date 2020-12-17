1dd817057765fddbcb41ef980a9e458138f4ad97
Keiji Maekawa <m@keijak.com>
2020-12-17 09:47:48 +0100

    dijkstra + two pointers

---
+++ atcoder/arc025/C/main.cpp
@@ -0,0 +1,166 @@
+#include <bits/stdc++.h>
+
+#define REP(i, n) for (int i = 0, REP_N_ = (n); i < REP_N_; ++i)
+#define ALL(x) std::begin(x), std::end(x)
+using i64 = long long;
+using u64 = unsigned long long;
+
+template <typename T, typename U>
+inline bool chmax(T &a, U b) {
+  return a < b and ((a = std::move(b)), true);
+}
+template <typename T, typename U>
+inline bool chmin(T &a, U b) {
+  return a > b and ((a = std::move(b)), true);
+}
+template <typename T>
+inline int ssize(const T &a) {
+  return (int)std::size(a);
+}
+
+template <typename T>
+std::istream &operator>>(std::istream &is, std::vector<T> &a) {
+  for (auto &x : a) is >> x;
+  return is;
+}
+template <typename Container>
+std::ostream &print_seq(const Container &a, std::string_view sep = " ",
+                        std::string_view ends = "\n",
+                        std::ostream *os = nullptr) {
+  if (os == nullptr) os = &std::cout;
+  auto b = std::begin(a), e = std::end(a);
+  for (auto it = std::begin(a); it != e; ++it) {
+    if (it != b) *os << sep;
+    *os << *it;
+  }
+  return *os << ends;
+}
+template <typename T, typename = void>
+struct is_iterable : std::false_type {};
+template <typename T>
+struct is_iterable<T, std::void_t<decltype(std::begin(std::declval<T>())),
+                                  decltype(std::end(std::declval<T>()))>>
+    : std::true_type {};
+
+template <typename T, typename = std::enable_if_t<
+                          is_iterable<T>::value &&
+                          !std::is_same<T, std::string_view>::value &&
+                          !std::is_same<T, std::string>::value>>
+std::ostream &operator<<(std::ostream &os, const T &a) {
+  return print_seq(a, ", ", "", &(os << "{")) << "}";
+}
+template <typename T, typename U>
+std::ostream &operator<<(std::ostream &os, const std::pair<T, U> &a) {
+  return os << "(" << a.first << ", " << a.second << ")";
+}
+
+#ifdef ENABLE_DEBUG
+template <typename T>
+void pdebug(const T &value) {
+  std::cerr << value;
+}
+template <typename T, typename... Ts>
+void pdebug(const T &value, const Ts &...args) {
+  pdebug(value);
+  std::cerr << ", ";
+  pdebug(args...);
+}
+#define DEBUG(...)                                   \
+  do {                                               \
+    std::cerr << " \033[33m (L" << __LINE__ << ") "; \
+    std::cerr << #__VA_ARGS__ << ":\033[0m ";        \
+    pdebug(__VA_ARGS__);                             \
+    std::cerr << std::endl;                          \
+  } while (0)
+#else
+#define pdebug(...)
+#define DEBUG(...)
+#endif
+
+using namespace std;
+
+template <class T>
+using MinHeap = std::priority_queue<T, std::vector<T>, std::greater<T>>;
+
+struct Edge {
+  i64 cost;
+  int to;
+};
+
+struct State {
+  i64 cost;
+  int node;
+};
+bool operator>(const State &x, const State &y) { return x.cost > y.cost; }
+
+const i64 INF = 1e15;
+
+// Returns min distance from the source node to each node (if exists).
+std::vector<i64> dijkstra(const std::vector<std::vector<Edge>> &g, int source) {
+  const int n = g.size();
+  std::vector<i64> mincost(n, INF);
+  mincost[source] = 0LL;
+  MinHeap<State> que;
+  que.push({0LL, source});
+  while (not que.empty()) {
+    State cur = std::move(que.top());
+    que.pop();
+    if (cur.cost > mincost[cur.node]) {
+      continue;
+    }
+    for (const auto &e : g[cur.node]) {
+      i64 new_cost = cur.cost + e.cost;
+      if (mincost[e.to] > new_cost) {
+        mincost[e.to] = new_cost;
+        que.push({new_cost, e.to});
+      }
+    }
+  }
+  return mincost;
+}
+
+i64 solve() {
+  int N, M, R, T;
+  cin >> N >> M >> R >> T;
+
+  vector<vector<Edge>> g(N);
+  REP(i, M) {
+    int a, b, c;
+    cin >> a >> b >> c;
+    --a, --b;
+    g[a].push_back({c, b});
+    g[b].push_back({c, a});
+  }
+
+  i64 ans = 0;
+  REP(i, N) {
+    auto dists = dijkstra(g, i);
+    DEBUG(i, dists);
+    sort(ALL(dists));
+    int ti = 0;
+    for (int ri = 1; ri < N; ++ri) {
+      i64 rd = dists[ri];
+      assert(rd < INF);
+      assert(dists[ti] * R < rd * T);
+
+      while (ti + 1 < N) {
+        i64 td = dists[ti + 1];
+        if (td * R >= rd * T) break;
+        ++ti;
+      }
+      if (ti < ri) {
+        ans += ti;
+      } else {
+        ans += ti - 1;
+      }
+      DEBUG(rd, dists[ti], ti);
+    }
+  }
+
+  return ans;
+}
+
+int main() {
+  std::ios::sync_with_stdio(false), cin.tie(nullptr);
+  cout << solve() << "\n";
+}
