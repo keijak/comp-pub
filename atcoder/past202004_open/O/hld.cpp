e3997f7a8cbbc0c4e716383713c312592d419a97
Keiji Maekawa <m@keijak.com>
2020-12-25 03:49:42 +0100

    parallel binary search

---
+++ atcoder/past202004_open/O/hld.cpp
@@ -0,0 +1,373 @@
+#include <bits/stdc++.h>
+
+#define REP_(i, a_, b_, a, b, ...) \
+  for (int i = (a), _Z_##i = (b); i < _Z_##i; ++i)
+#define REP(i, ...) REP_(i, __VA_ARGS__, __VA_ARGS__, 0, __VA_ARGS__)
+#define ALL(x) std::begin(x), std::end(x)
+using i64 = long long;
+using u64 = unsigned long long;
+
+template <typename T, typename U>
+inline bool chmax(T &a, U b) {
+  return a < b and ((a = std::move(b)), true);
+}
+template <typename T, typename U>
+inline bool chmin(T &a, U b) {
+  return a > b and ((a = std::move(b)), true);
+}
+template <typename T>
+inline int ssize(const T &a) {
+  return (int)std::size(a);
+}
+
+template <typename T>
+std::istream &operator>>(std::istream &is, std::vector<T> &a) {
+  for (auto &x : a) is >> x;
+  return is;
+}
+template <typename Container>
+std::ostream &print_seq(const Container &a, std::string_view sep = " ",
+                        std::string_view ends = "\n",
+                        std::ostream *os = nullptr) {
+  if (os == nullptr) os = &std::cout;
+  auto b = std::begin(a), e = std::end(a);
+  for (auto it = std::begin(a); it != e; ++it) {
+    if (it != b) *os << sep;
+    *os << *it;
+  }
+  return *os << ends;
+}
+template <typename T, typename = void>
+struct is_iterable : std::false_type {};
+template <typename T>
+struct is_iterable<T, std::void_t<decltype(std::begin(std::declval<T>())),
+                                  decltype(std::end(std::declval<T>()))>>
+    : std::true_type {};
+
+template <typename T, typename = std::enable_if_t<
+                          is_iterable<T>::value &&
+                          !std::is_same<T, std::string_view>::value &&
+                          !std::is_same<T, std::string>::value>>
+std::ostream &operator<<(std::ostream &os, const T &a) {
+  return print_seq(a, ", ", "", &(os << "{")) << "}";
+}
+template <typename T, typename U>
+std::ostream &operator<<(std::ostream &os, const std::pair<T, U> &a) {
+  return os << "(" << a.first << ", " << a.second << ")";
+}
+
+#ifdef ENABLE_DEBUG
+template <typename T>
+void pdebug(const T &value) {
+  std::cerr << value;
+}
+template <typename T, typename... Ts>
+void pdebug(const T &value, const Ts &...args) {
+  pdebug(value);
+  std::cerr << ", ";
+  pdebug(args...);
+}
+#define DEBUG(...)                                   \
+  do {                                               \
+    std::cerr << " \033[33m (L" << __LINE__ << ") "; \
+    std::cerr << #__VA_ARGS__ << ":\033[0m ";        \
+    pdebug(__VA_ARGS__);                             \
+    std::cerr << std::endl;                          \
+  } while (0)
+#else
+#define pdebug(...)
+#define DEBUG(...)
+#endif
+
+using namespace std;
+
+struct UnionFind {
+  int n;
+  mutable std::vector<int> par;  // positive: parent, negative: size
+  int num_roots;
+
+  explicit UnionFind(int sz) : n(sz), par(sz, -1), num_roots(sz) {}
+
+  bool unite(int x, int y) {
+    x = find(x), y = find(y);
+    if (x == y) return false;
+    if (par[x] > par[y]) std::swap(x, y);
+    par[x] += par[y];
+    par[y] = x;
+    --num_roots;
+    return true;
+  }
+
+  int find(int k) const {
+    if (par[k] < 0) return k;
+    return par[k] = find(par[k]);
+  }
+
+  int size(int k) const { return -par[find(k)]; }
+
+  bool same(int x, int y) const { return find(x) == find(y); }
+
+  std::vector<int> roots() const {
+    std::vector<int> res;
+    res.reserve(num_roots);
+    for (int i = 0; i < (int)par.size(); ++i) {
+      if (par[i] < 0) res.push_back(i);
+    }
+    return res;
+  }
+};
+
+template <typename Monoid>
+struct SegTree {
+  using T = typename Monoid::T;
+
+  inline int n() const { return n_; }
+  inline int offset() const { return offset_; }
+
+  explicit SegTree(int n) : n_(n) {
+    offset_ = 1;
+    while (offset_ < n_) offset_ <<= 1;
+    data_.assign(2 * offset_, Monoid::id());
+  }
+
+  explicit SegTree(const std::vector<T> &leaves) : n_(leaves.size()) {
+    offset_ = 1;
+    while (offset_ < n_) offset_ <<= 1;
+    data_.assign(2 * offset_, Monoid::id());
+    for (int i = 0; i < n_; ++i) {
+      data_[offset_ + i] = leaves[i];
+    }
+    for (int i = offset_ - 1; i > 0; --i) {
+      data_[i] = Monoid::op(data_[i * 2], data_[i * 2 + 1]);
+    }
+  }
+
+  // Sets i-th value (0-indexed) to x.
+  void set(int i, const T &x) {
+    int k = offset_ + i;
+    data_[k] = x;
+    // Update its ancestors.
+    while (k > 1) {
+      k >>= 1;
+      data_[k] = Monoid::op(data_[k * 2], data_[k * 2 + 1]);
+    }
+  }
+
+  // Queries by [l,r) range (0-indexed, half-open interval).
+  T fold(int l, int r) const {
+    l = std::max(l, 0) + offset_;
+    r = std::min(r, offset_) + offset_;
+    T vleft = Monoid::id(), vright = Monoid::id();
+    for (; l < r; l >>= 1, r >>= 1) {
+      if (l & 1) vleft = Monoid::op(vleft, data_[l++]);
+      if (r & 1) vright = Monoid::op(data_[--r], vright);
+    }
+    return Monoid::op(vleft, vright);
+  }
+
+  T fold_all() const { return data_[1]; }
+
+  // Returns i-th value (0-indexed).
+  T operator[](int i) const { return data_[offset_ + i]; }
+
+  friend std::ostream &operator<<(std::ostream &os, const SegTree &st) {
+    os << "[";
+    for (int i = 0; i < st.n(); ++i) {
+      if (i != 0) os << ", ";
+      const auto &x = st[i];
+      os << x;
+    }
+    return os << "]";
+  }
+
+ private:
+  int n_;                // number of valid leaves.
+  int offset_;           // where leaves start
+  std::vector<T> data_;  // data size: 2*offset_
+};
+
+struct Max {
+  using T = long long;
+  static T op(const T &x, const T &y) { return std::max(x, y); }
+  static constexpr T id() { return std::numeric_limits<T>::lowest(); }
+};
+
+// Heavy-Light Decomposition
+struct HLD {
+  using NodeID = int;  // [0, n)
+  using G = std::vector<std::vector<int>>;
+  using IntervalVector = std::vector<std::pair<int, int>>;
+
+  int n;                                      // number of nodes in the tree
+  NodeID root;                                // root of the tree
+  G child;                                    // children node ids
+  std::vector<std::optional<NodeID>> parent;  // parent node id (or -1)
+  std::vector<int> subsize;                   // subtree size
+  // "ord" is preorder index in DFS traversal. [0, n)
+  std::vector<int> node_to_ord;     // node id to preorder index
+  std::vector<NodeID> ord_to_node;  // preorder index to node id
+  std::vector<NodeID> comp_root;    // node id to its heavy path component
+
+  explicit HLD(G g, NodeID root = 0)
+      : n(int(g.size())),
+        root(root),
+        child(g),
+        parent(n, -1),
+        subsize(n, 1),
+        node_to_ord(n, -1),
+        ord_to_node(n, -1),
+        comp_root(n, -1) {
+    dfs_subsize(root);
+    int counter = 0;
+    comp_root[root] = root;
+    dfs_hld(root, counter);
+  }
+
+  // Lowest Common Ancestor
+  NodeID lca(NodeID u, NodeID v) const {
+    for (;;) {
+      if (node_to_ord[u] > node_to_ord[v]) std::swap(u, v);
+      NodeID crv = comp_root[v];
+      if (comp_root[u] == crv) return u;
+      assert(parent[crv].has_value());
+      v = parent[crv].value();
+    }
+  }
+
+  // Returns the set of nodes on the u-v path, including both u and v.
+  //
+  // The return value is half-open intervals of the preorder indices of the
+  // nodes. One interval corresponds to one heavy path component.
+  IntervalVector node_ranges_on_path(NodeID u, NodeID v) const {
+    IntervalVector res;
+    for (;;) {
+      if (node_to_ord[u] > node_to_ord[v]) std::swap(u, v);
+      NodeID crv = comp_root[v];
+      res.emplace_back(std::max(node_to_ord[crv], node_to_ord[u]),
+                       node_to_ord[v] + 1);
+      if (comp_root[u] == crv) break;
+      assert(parent[crv].has_value());
+      v = parent[crv].value();
+    }
+    return res;
+  }
+
+  // Returns the set of edges in the u-v path.
+  //
+  // The return value is half-open intervals of the preorder indices of nodes
+  // corresponding to the deeper end (closer to leaves) of each edge in the
+  // path. Here we identify Edge(v, parent[v]) as v.
+  IntervalVector edge_ranges_on_path(NodeID u, NodeID v) const {
+    IntervalVector res;
+    for (;;) {
+      if (node_to_ord[u] > node_to_ord[v]) std::swap(u, v);
+      NodeID crv = comp_root[v];
+      if (comp_root[u] == crv) {
+        if (u != v) res.emplace_back(node_to_ord[u] + 1, node_to_ord[v] + 1);
+        break;
+      }
+      res.emplace_back(node_to_ord[crv], node_to_ord[v] + 1);
+      assert(parent[crv].has_value());
+      v = parent[crv].value();
+    }
+    return res;
+  }
+
+  // Distance (= number of edges) of the path between two nodes.
+  int distance(NodeID u, NodeID v) const {
+    int dist = 0;
+    for (auto [l, r] : edge_ranges_on_path(u, v)) {
+      dist += r - l;
+    }
+    return dist;
+  }
+
+ private:
+  // Fills `parent` and `subsize` and drops parent node ids from `child`.
+  void dfs_subsize(NodeID v) {
+    auto &edges = child[v];
+    if (parent[v].has_value()) {
+      auto it = std::find(edges.begin(), edges.end(), parent[v].value());
+      if (it != edges.end()) edges.erase(it);
+    }
+    for (NodeID &u : edges) {
+      parent[u] = v;
+      dfs_subsize(u);
+      subsize[v] += subsize[u];
+      if (subsize[u] > subsize[edges[0]]) {
+        std::swap(u, edges[0]);
+      }
+    }
+  }
+
+  // Fills `node_to_ord`, `ord_to_node`, and `comp_root`.
+  void dfs_hld(NodeID v, int &counter) {
+    node_to_ord[v] = counter++;
+    ord_to_node[node_to_ord[v]] = v;
+    for (NodeID u : child[v]) {
+      comp_root[u] = (u == child[v][0] ? comp_root[v] : u);
+      dfs_hld(u, counter);
+    }
+  }
+};
+
+int main() {
+  std::ios::sync_with_stdio(false), cin.tie(nullptr);
+  int n, m;
+  cin >> n >> m;
+  vector<tuple<i64, int, int>> edges(m);
+  map<pair<int, int>, int> edge_id;
+  REP(i, m) {
+    int a, b, c;
+    cin >> a >> b >> c;
+    --a, --b;
+    if (a > b) swap(a, b);
+    edges[i] = {c, a, b};
+    edge_id[{a, b}] = edge_id[{b, a}] = i;
+  }
+  auto sorted_edges = edges;
+  sort(ALL(sorted_edges));
+
+  UnionFind uf(n);
+  i64 opt_cost = 0;
+  set<int> opt_edge_ids;
+  vector<vector<int>> g(n);
+  for (auto [c, a, b] : sorted_edges) {
+    if (uf.same(a, b)) continue;
+    int e = edge_id[{a, b}];
+    opt_edge_ids.insert(e);
+    opt_cost += c;
+    uf.unite(a, b);
+
+    g[a].push_back(b);
+    g[b].push_back(a);
+  }
+
+  HLD hld(move(g));
+  SegTree<Max> seg(n);
+  REP(i, 1, n) {
+    auto par = hld.parent[i];
+    assert(par.has_value());
+    int p = par.value();
+    int e = edge_id[{i, p}];
+    auto [c, a, b] = edges[e];
+    assert(a == min(i, p));
+    assert(b == max(i, p));
+    int ix = hld.node_to_ord[i];
+    seg.set(ix, c);
+  }
+
+  REP(i, m) {
+    if (opt_edge_ids.count(i)) {
+      cout << opt_cost << '\n';
+    } else {
+      auto [c, a, b] = edges[i];
+      i64 mcost = 0;
+      for (auto [l, r] : hld.edge_ranges_on_path(a, b)) {
+        chmax(mcost, seg.fold(l, r));
+      }
+      i64 ans = opt_cost + c - mcost;
+      cout << ans << "\n";
+    }
+  }
+}
---
+++ atcoder/past202004_open/O/main.cpp
@@ -0,0 +1,172 @@
+#include <bits/stdc++.h>
+
+#define REP_(i, a_, b_, a, b, ...) \
+  for (int i = (a), _Z_##i = (b); i < _Z_##i; ++i)
+#define REP(i, ...) REP_(i, __VA_ARGS__, __VA_ARGS__, 0, __VA_ARGS__)
+#define ALL(x) std::begin(x), std::end(x)
+using i64 = long long;
+using u64 = unsigned long long;
+
+template <typename T, typename U>
+inline bool chmax(T &a, U b) {
+  return a < b and ((a = std::move(b)), true);
+}
+template <typename T, typename U>
+inline bool chmin(T &a, U b) {
+  return a > b and ((a = std::move(b)), true);
+}
+template <typename T>
+inline int ssize(const T &a) {
+  return (int)std::size(a);
+}
+
+template <typename T>
+std::istream &operator>>(std::istream &is, std::vector<T> &a) {
+  for (auto &x : a) is >> x;
+  return is;
+}
+template <typename Container>
+std::ostream &print_seq(const Container &a, std::string_view sep = " ",
+                        std::string_view ends = "\n",
+                        std::ostream *os = nullptr) {
+  if (os == nullptr) os = &std::cout;
+  auto b = std::begin(a), e = std::end(a);
+  for (auto it = std::begin(a); it != e; ++it) {
+    if (it != b) *os << sep;
+    *os << *it;
+  }
+  return *os << ends;
+}
+template <typename T, typename = void>
+struct is_iterable : std::false_type {};
+template <typename T>
+struct is_iterable<T, std::void_t<decltype(std::begin(std::declval<T>())),
+                                  decltype(std::end(std::declval<T>()))>>
+    : std::true_type {};
+
+template <typename T, typename = std::enable_if_t<
+                          is_iterable<T>::value &&
+                          !std::is_same<T, std::string_view>::value &&
+                          !std::is_same<T, std::string>::value>>
+std::ostream &operator<<(std::ostream &os, const T &a) {
+  return print_seq(a, ", ", "", &(os << "{")) << "}";
+}
+template <typename T, typename U>
+std::ostream &operator<<(std::ostream &os, const std::pair<T, U> &a) {
+  return os << "(" << a.first << ", " << a.second << ")";
+}
+
+#ifdef ENABLE_DEBUG
+template <typename T>
+void pdebug(const T &value) {
+  std::cerr << value;
+}
+template <typename T, typename... Ts>
+void pdebug(const T &value, const Ts &...args) {
+  pdebug(value);
+  std::cerr << ", ";
+  pdebug(args...);
+}
+#define DEBUG(...)                                   \
+  do {                                               \
+    std::cerr << " \033[33m (L" << __LINE__ << ") "; \
+    std::cerr << #__VA_ARGS__ << ":\033[0m ";        \
+    pdebug(__VA_ARGS__);                             \
+    std::cerr << std::endl;                          \
+  } while (0)
+#else
+#define pdebug(...)
+#define DEBUG(...)
+#endif
+
+using namespace std;
+
+struct UnionFind {
+  int n;
+  mutable std::vector<int> par;  // positive: parent, negative: size
+  int num_roots;
+
+  explicit UnionFind(int sz) : n(sz), par(sz, -1), num_roots(sz) {}
+
+  bool unite(int x, int y) {
+    x = find(x), y = find(y);
+    if (x == y) return false;
+    if (par[x] > par[y]) std::swap(x, y);
+    par[x] += par[y];
+    par[y] = x;
+    --num_roots;
+    return true;
+  }
+
+  int find(int k) const {
+    if (par[k] < 0) return k;
+    return par[k] = find(par[k]);
+  }
+
+  int size(int k) const { return -par[find(k)]; }
+
+  bool same(int x, int y) const { return find(x) == find(y); }
+
+  std::vector<int> roots() const {
+    std::vector<int> res;
+    res.reserve(num_roots);
+    for (int i = 0; i < (int)par.size(); ++i) {
+      if (par[i] < 0) res.push_back(i);
+    }
+    return res;
+  }
+};
+
+int main() {
+  std::ios::sync_with_stdio(false), cin.tie(nullptr);
+  int n, m;
+  cin >> n >> m;
+  vector<tuple<i64, int, int>> edges(m);
+  map<pair<int, int>, int> edge_id;
+  REP(i, m) {
+    int a, b, c;
+    cin >> a >> b >> c;
+    --a, --b;
+    if (a > b) swap(a, b);
+    edges[i] = {c, a, b};
+    edge_id[{a, b}] = edge_id[{b, a}] = i;
+  }
+  auto sorted_edges = edges;
+  sort(ALL(sorted_edges));
+
+  vector<int> fv(m, 0), tv(m, m);
+  i64 opt_cost = 0;
+  while (true) {
+    map<int, vector<int>> checks;
+    REP(i, m) {
+      if (tv[i] - fv[i] > 1) {
+        int mid = (tv[i] + fv[i]) / 2;
+        checks[mid].push_back(i);
+      }
+    }
+    if (checks.empty()) break;
+
+    UnionFind uf(n);
+    opt_cost = 0;
+    REP(i, m) {
+      for (auto j : checks[i]) {
+        auto [dcost, u, v] = edges[j];
+        if (uf.same(u, v)) {
+          tv[j] = i;
+        } else {
+          fv[j] = i;
+        }
+      }
+      auto [c, a, b] = sorted_edges[i];
+      if (uf.same(a, b)) continue;
+      uf.unite(a, b);
+      opt_cost += c;
+    }
+  }
+
+  REP(i, m) {
+    auto [xc, xa, xb] = edges[i];
+    auto [yc, ya, yb] = sorted_edges[tv[i] - 1];
+    cout << opt_cost - yc + xc << '\n';
+  }
+}
