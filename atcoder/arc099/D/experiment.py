3a711f03fa4ee879185dc85cd0a4aabfe0f4cdf7
Keiji Maekawa <m@keijak.com>
2020-12-26 22:55:51 +0100

    esper

---
+++ atcoder/arc099/D/experiment.py
@@ -0,0 +1,41 @@
+import sys
+from fractions import Fraction
+from collections import deque
+
+ini = lambda: int(sys.stdin.readline())
+inl = lambda: [int(x) for x in sys.stdin.readline().split()]
+ins = lambda: sys.stdin.readline().rstrip()
+# debug = lambda *a, **kw: print("\033[33m", *a, "\033[0m", **dict(file=sys.stderr, **kw))
+
+L = 10 ** 7
+
+
+def dsum(x):
+    s = 0
+    while x:
+        s += x % 10
+        x //= 10
+    return s
+
+
+def solve():
+    minr = Fraction(10 ** 18, 1)
+    ans = deque()
+    for x in range(L, 0, -1):
+        s = dsum(x)
+        r = Fraction(x, s)
+        if r <= minr:
+            if x != L:
+                ans.appendleft(x)
+            minr = r
+    print(*ans, sep="\n")
+
+
+try:
+    import frosch
+
+    frosch.hook()
+except ImportError:
+    pass
+
+solve()
