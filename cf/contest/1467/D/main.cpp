b2d74c21f855ac5ba112a47e8979a92b7a03f938
Keiji Maekawa <m@keijak.com>
2021-01-08 19:05:05 +0100

    codeforces div2

---
+++ cf/contest/1467/D/main.cpp
@@ -0,0 +1,307 @@
+#include <bits/stdc++.h>
+#define REP_(i, a_, b_, a, b, ...) \
+  for (int i = (a), _Z_##i = (b); i < _Z_##i; ++i)
+#define REP(i, ...) REP_(i, __VA_ARGS__, __VA_ARGS__, 0, __VA_ARGS__)
+#define ALL(x) std::begin(x), std::end(x)
+using i64 = long long;
+using u64 = unsigned long long;
+
+template <typename T, typename U>
+inline bool chmax(T &a, U b) {
+  return a < b and ((a = std::move(b)), true);
+}
+template <typename T, typename U>
+inline bool chmin(T &a, U b) {
+  return a > b and ((a = std::move(b)), true);
+}
+template <typename T>
+inline int ssize(const T &a) {
+  return (int)std::size(a);
+}
+
+template <typename T>
+std::istream &operator>>(std::istream &is, std::vector<T> &a) {
+  for (auto &x : a) is >> x;
+  return is;
+}
+template <typename Container>
+std::ostream &print_seq(const Container &a, std::string_view sep = " ",
+                        std::string_view ends = "\n",
+                        std::ostream *os = nullptr) {
+  if (os == nullptr) os = &std::cout;
+  auto b = std::begin(a), e = std::end(a);
+  for (auto it = std::begin(a); it != e; ++it) {
+    if (it != b) *os << sep;
+    *os << *it;
+  }
+  return *os << ends;
+}
+template <typename T, typename = void>
+struct is_iterable : std::false_type {};
+template <typename T>
+struct is_iterable<T, std::void_t<decltype(std::begin(std::declval<T>())),
+                                  decltype(std::end(std::declval<T>()))>>
+    : std::true_type {};
+
+template <typename T, typename = std::enable_if_t<
+                          is_iterable<T>::value &&
+                          !std::is_same<T, std::string_view>::value &&
+                          !std::is_same<T, std::string>::value>>
+std::ostream &operator<<(std::ostream &os, const T &a) {
+  return print_seq(a, ", ", "", &(os << "{")) << "}";
+}
+template <typename T, typename U>
+std::ostream &operator<<(std::ostream &os, const std::pair<T, U> &a) {
+  return os << "(" << a.first << ", " << a.second << ")";
+}
+
+#ifdef ENABLE_DEBUG
+template <typename T>
+void pdebug(const T &value) {
+  std::cerr << value;
+}
+template <typename T, typename... Ts>
+void pdebug(const T &value, const Ts &...args) {
+  pdebug(value);
+  std::cerr << ", ";
+  pdebug(args...);
+}
+#define DEBUG(...)                                   \
+  do {                                               \
+    std::cerr << " \033[33m (L" << __LINE__ << ") "; \
+    std::cerr << #__VA_ARGS__ << ":\033[0m ";        \
+    pdebug(__VA_ARGS__);                             \
+    std::cerr << std::endl;                          \
+  } while (0)
+#else
+#define pdebug(...)
+#define DEBUG(...)
+#endif
+
+using namespace std;
+
+template <unsigned M>
+struct ModInt {
+  constexpr ModInt(long long val = 0) : _v(0) {
+    if (val < 0) {
+      long long k = (abs(val) + M - 1) / M;
+      val += k * M;
+    }
+    assert(val >= 0);
+    _v = val % M;
+  }
+
+  static constexpr int mod() { return M; }
+  static constexpr unsigned umod() { return M; }
+  inline unsigned val() const { return _v; }
+
+  ModInt &operator++() {
+    _v++;
+    if (_v == umod()) _v = 0;
+    return *this;
+  }
+  ModInt &operator--() {
+    if (_v == 0) _v = umod();
+    _v--;
+    return *this;
+  }
+  ModInt operator++(int) {
+    auto result = *this;
+    ++*this;
+    return result;
+  }
+  ModInt operator--(int) {
+    auto result = *this;
+    --*this;
+    return result;
+  }
+
+  constexpr ModInt operator-() const { return ModInt(-_v); }
+  constexpr ModInt &operator+=(const ModInt &a) {
+    if ((_v += a._v) >= M) _v -= M;
+    return *this;
+  }
+  constexpr ModInt &operator-=(const ModInt &a) {
+    if ((_v += M - a._v) >= M) _v -= M;
+    return *this;
+  }
+  constexpr ModInt &operator*=(const ModInt &a) {
+    _v = ((unsigned long long)(_v)*a._v) % M;
+    return *this;
+  }
+  constexpr ModInt pow(unsigned long long t) const {
+    ModInt base = *this;
+    ModInt res = 1;
+    while (t) {
+      if (t & 1) res *= base;
+      base *= base;
+      t >>= 1;
+    }
+    return res;
+  }
+
+  constexpr ModInt inv() const {
+    // Inverse by Extended Euclidean algorithm.
+    // M doesn't need to be prime, but x and M must be coprime.
+    assert(_v != 0);
+    auto [g, x, y] = ext_gcd(_v, M);
+    assert(g == 1);  // The GCD must be 1.
+    return x;
+
+    // Inverse by Fermat's little theorem. M must be prime.
+    //
+    //     return pow(M - 2);
+  }
+  constexpr ModInt &operator/=(const ModInt &a) { return *this *= a.inv(); }
+
+  friend constexpr ModInt operator+(const ModInt &a, const ModInt &b) {
+    return ModInt(a) += b;
+  }
+  friend constexpr ModInt operator-(const ModInt &a, const ModInt &b) {
+    return ModInt(a) -= b;
+  }
+  friend constexpr ModInt operator*(const ModInt &a, const ModInt &b) {
+    return ModInt(a) *= b;
+  }
+  friend constexpr ModInt operator/(const ModInt &a, const ModInt &b) {
+    return ModInt(a) /= b;
+  }
+  friend constexpr bool operator==(const ModInt &a, const ModInt &b) {
+    return a._v == b._v;
+  }
+  friend constexpr bool operator!=(const ModInt &a, const ModInt &b) {
+    return a._v != b._v;
+  }
+  friend std::istream &operator>>(std::istream &is, ModInt &a) {
+    return is >> a._v;
+  }
+  friend std::ostream &operator<<(std::ostream &os, const ModInt &a) {
+    return os << a._v;
+  }
+
+ private:
+  // Etended Euclidean algorithm.
+  // Returns [g, x, y] where g = a*x + b*y = GCD(a, b).
+  std::array<unsigned, 3> ext_gcd(unsigned a, unsigned b) {
+    if (b == 0) return {a, 1, 0};
+    auto res = ext_gcd(b, a % b);  // = (g, x, y)
+    res[1] -= (a / b) * res[2];
+    std::swap(res[1], res[2]);
+    return res;  // = (g, y, x - (a/b)*y)
+  }
+
+  unsigned _v;  // raw value
+};
+const unsigned MOD = 1'000'000'007;
+using Mint = ModInt<MOD>;
+
+template <typename Monoid>
+struct SegTree {
+  using T = typename Monoid::T;
+
+ private:
+  int n_;                // number of valid leaves.
+  int offset_;           // where leaves start
+  std::vector<T> data_;  // data size: 2*offset_
+
+ public:
+  inline int n() const { return n_; }
+  inline int offset() const { return offset_; }
+
+  explicit SegTree(int n) : n_(n) {
+    offset_ = 1;
+    while (offset_ < n_) offset_ <<= 1;
+    data_.assign(2 * offset_, Monoid::id());
+  }
+
+  explicit SegTree(const std::vector<T> &leaves) : n_(leaves.size()) {
+    offset_ = 1;
+    while (offset_ < n_) offset_ <<= 1;
+    data_.assign(2 * offset_, Monoid::id());
+    for (int i = 0; i < n_; ++i) {
+      data_[offset_ + i] = leaves[i];
+    }
+    for (int i = offset_ - 1; i > 0; --i) {
+      data_[i] = Monoid::op(data_[i * 2], data_[i * 2 + 1]);
+    }
+  }
+
+  // Sets i-th value (0-indexed) to x.
+  void set(int i, const T &x) {
+    int k = offset_ + i;
+    data_[k] = x;
+    // Update its ancestors.
+    while (k > 1) {
+      k >>= 1;
+      data_[k] = Monoid::op(data_[k * 2], data_[k * 2 + 1]);
+    }
+  }
+
+  // Queries by [l,r) range (0-indexed, half-open interval).
+  T fold(int l, int r) const {
+    l = std::max(l, 0) + offset_;
+    r = std::min(r, offset_) + offset_;
+    T vleft = Monoid::id(), vright = Monoid::id();
+    for (; l < r; l >>= 1, r >>= 1) {
+      if (l & 1) vleft = Monoid::op(vleft, data_[l++]);
+      if (r & 1) vright = Monoid::op(data_[--r], vright);
+    }
+    return Monoid::op(vleft, vright);
+  }
+
+  T fold_all() const { return data_[1]; }
+
+  // Returns i-th value (0-indexed).
+  T operator[](int i) const { return data_[offset_ + i]; }
+
+  friend std::ostream &operator<<(std::ostream &os, const SegTree &st) {
+    os << "[";
+    for (int i = 0; i < st.n(); ++i) {
+      if (i != 0) os << ", ";
+      const auto &x = st[i];
+      os << x;
+    }
+    return os << "]";
+  }
+};
+
+struct Sum {
+  using T = Mint;
+  static T op(const T &x, const T &y) { return x + y; }
+  static constexpr T id() { return Mint(0); }
+};
+
+int main() {
+  ios_base::sync_with_stdio(false), cin.tie(nullptr);
+
+  i64 n, k, q;
+  cin >> n >> k >> q;
+  vector<i64> a(n);
+  cin >> a;
+
+  vector dp(k + 1, vector(n, Mint(0)));
+  REP(i, n) dp[0][i] = 1;
+  REP(i, 1, k + 1) {
+    REP(j, n) {
+      if (j - 1 >= 0) dp[i][j] += dp[i - 1][j - 1];
+      if (j + 1 < n) dp[i][j] += dp[i - 1][j + 1];
+    }
+  }
+  vector<Mint> c(n);
+  REP(i, k + 1) {
+    REP(j, n) { c[j] += dp[i][j] * dp[k - i][j]; }
+  }
+
+  vector<Mint> ini(n);
+  REP(i, n) ini[i] = a[i] * c[i];
+  SegTree<Sum> seg(move(ini));
+
+  REP(qi, q) {
+    int i, x;
+    cin >> i >> x;
+    --i;
+    a[i] = x;
+    seg.set(i, a[i] * c[i]);
+    cout << seg.fold_all() << '\n';
+  }
+}
