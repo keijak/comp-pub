ec6bbb139a656ca8227d0e41479b30fa1add0ed7
keijak <m@keijak.com>
2021-02-09 07:21:09 +0100

    math

--- atcoder/arc057/A/main.py
+++ atcoder/arc057/A/main.py
@@ -1,18 +1,16 @@
 import fractions
 
-# a[n+1] + 1/k = (k+1)(a[n] + 1/k)
-
 T = 2 * 10 ** 12
-a, k = map(int, input().split())
-if k == 0:
-    print(T - a)
-else:
+
+
+def solve():
+    a, k = map(int, input().split())
+    if k == 0:
+        return T - a
     rk = fractions.Fraction(1, k)
-    fv, tv = (0, 45)
-    while tv - fv > 1:
-        mid = (tv + fv) // 2
-        if ((k + 1) ** mid) * (a + rk) >= T + rk:
-            tv = mid
-        else:
-            fv = mid
-    print(tv)
+    for i in range(45):
+        if ((k + 1) ** i) * (a + rk) >= T + rk:
+            return i
+
+
+print(solve())
