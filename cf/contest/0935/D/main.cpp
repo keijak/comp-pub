eb9b7998ac3a6b405178e3ea84689f4f1b974d78
Keiji Maekawa <m@keijak.com>
2021-02-10 09:34:07 +0100

    codeforces div2 virtual

---
+++ cf/contest/0935/D/main.cpp
@@ -0,0 +1,255 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/math>
+#include <atcoder/modint>
+#define REP_(i, a_, b_, a, b, ...) \
+  for (int i = (a), _Z_##i = (b); i < _Z_##i; ++i)
+#define REP(i, ...) REP_(i, __VA_ARGS__, __VA_ARGS__, 0, __VA_ARGS__)
+#define ALL(x) std::begin(x), std::end(x)
+using i64 = long long;
+using u64 = unsigned long long;
+
+template <typename T, typename U>
+inline bool chmax(T &a, U b) {
+  return a < b and ((a = std::move(b)), true);
+}
+template <typename T, typename U>
+inline bool chmin(T &a, U b) {
+  return a > b and ((a = std::move(b)), true);
+}
+template <typename T>
+inline int ssize(const T &a) {
+  return (int)std::size(a);
+}
+
+template <typename T>
+std::istream &operator>>(std::istream &is, std::vector<T> &a) {
+  for (auto &x : a) is >> x;
+  return is;
+}
+template <typename Container>
+std::ostream &print_seq(const Container &a, std::string_view sep = " ",
+                        std::string_view ends = "\n",
+                        std::ostream *os = nullptr) {
+  if (os == nullptr) os = &std::cout;
+  auto b = std::begin(a), e = std::end(a);
+  for (auto it = std::begin(a); it != e; ++it) {
+    if (it != b) *os << sep;
+    *os << *it;
+  }
+  return *os << ends;
+}
+template <typename T, typename = void>
+struct is_iterable : std::false_type {};
+template <typename T>
+struct is_iterable<T, std::void_t<decltype(std::begin(std::declval<T>())),
+                                  decltype(std::end(std::declval<T>()))>>
+    : std::true_type {};
+
+template <typename T, typename = std::enable_if_t<
+                          is_iterable<T>::value &&
+                          !std::is_same<T, std::string_view>::value &&
+                          !std::is_same<T, std::string>::value>>
+std::ostream &operator<<(std::ostream &os, const T &a) {
+  return print_seq(a, ", ", "", &(os << "{")) << "}";
+}
+template <typename T, typename U>
+std::ostream &operator<<(std::ostream &os, const std::pair<T, U> &a) {
+  return os << "(" << a.first << ", " << a.second << ")";
+}
+
+#ifdef ENABLE_DEBUG
+template <typename T>
+void pdebug(const T &value) {
+  std::cerr << value;
+}
+template <typename T, typename... Ts>
+void pdebug(const T &value, const Ts &...args) {
+  pdebug(value);
+  std::cerr << ", ";
+  pdebug(args...);
+}
+#define DEBUG(...)                                   \
+  do {                                               \
+    std::cerr << " \033[33m (L" << __LINE__ << ") "; \
+    std::cerr << #__VA_ARGS__ << ":\033[0m ";        \
+    pdebug(__VA_ARGS__);                             \
+    std::cerr << std::endl;                          \
+  } while (0)
+#else
+#define pdebug(...)
+#define DEBUG(...)
+#endif
+
+using namespace std;
+// using Mint = atcoder::modint1000000007;
+// std::ostream &operator<<(std::ostream &os, const Mint &m) {
+//   return os << m.val();
+// }
+
+template <unsigned M>
+struct ModInt {
+  constexpr ModInt(long long val = 0) : _v(0) {
+    if (val < 0) {
+      long long k = (abs(val) + M - 1) / M;
+      val += k * M;
+    }
+    assert(val >= 0);
+    _v = val % M;
+  }
+
+  static constexpr int mod() { return M; }
+  static constexpr unsigned umod() { return M; }
+  inline unsigned val() const { return _v; }
+
+  ModInt &operator++() {
+    _v++;
+    if (_v == umod()) _v = 0;
+    return *this;
+  }
+  ModInt &operator--() {
+    if (_v == 0) _v = umod();
+    _v--;
+    return *this;
+  }
+  ModInt operator++(int) {
+    auto result = *this;
+    ++*this;
+    return result;
+  }
+  ModInt operator--(int) {
+    auto result = *this;
+    --*this;
+    return result;
+  }
+
+  constexpr ModInt operator-() const { return ModInt(-_v); }
+  constexpr ModInt &operator+=(const ModInt &a) {
+    if ((_v += a._v) >= M) _v -= M;
+    return *this;
+  }
+  constexpr ModInt &operator-=(const ModInt &a) {
+    if ((_v += M - a._v) >= M) _v -= M;
+    return *this;
+  }
+  constexpr ModInt &operator*=(const ModInt &a) {
+    _v = ((unsigned long long)(_v)*a._v) % M;
+    return *this;
+  }
+  constexpr ModInt pow(unsigned long long t) const {
+    ModInt base = *this;
+    ModInt res = 1;
+    while (t) {
+      if (t & 1) res *= base;
+      base *= base;
+      t >>= 1;
+    }
+    return res;
+  }
+
+  constexpr ModInt inv() const {
+    // Inverse by Extended Euclidean algorithm.
+    // M doesn't need to be prime, but x and M must be coprime.
+    assert(_v != 0);
+    auto [g, x, y] = ext_gcd(_v, M);
+    assert(g == 1);  // The GCD must be 1.
+    return x;
+
+    // Inverse by Fermat's little theorem. M must be prime.
+    //
+    //     return pow(M - 2);
+  }
+  constexpr ModInt &operator/=(const ModInt &a) { return *this *= a.inv(); }
+
+  friend constexpr ModInt operator+(const ModInt &a, const ModInt &b) {
+    return ModInt(a) += b;
+  }
+  friend constexpr ModInt operator-(const ModInt &a, const ModInt &b) {
+    return ModInt(a) -= b;
+  }
+  friend constexpr ModInt operator*(const ModInt &a, const ModInt &b) {
+    return ModInt(a) *= b;
+  }
+  friend constexpr ModInt operator/(const ModInt &a, const ModInt &b) {
+    return ModInt(a) /= b;
+  }
+  friend constexpr bool operator==(const ModInt &a, const ModInt &b) {
+    return a._v == b._v;
+  }
+  friend constexpr bool operator!=(const ModInt &a, const ModInt &b) {
+    return a._v != b._v;
+  }
+  friend std::istream &operator>>(std::istream &is, ModInt &a) {
+    return is >> a._v;
+  }
+  friend std::ostream &operator<<(std::ostream &os, const ModInt &a) {
+    return os << a._v;
+  }
+
+ private:
+  // Etended Euclidean algorithm.
+  // Returns [g, x, y] where g = a*x + b*y = GCD(a, b).
+  static constexpr std::array<int, 3> ext_gcd(int a, int b) {
+    if (b == 0) return {a, 1, 0};
+    auto res = ext_gcd(b, a % b);  // = (g, x, y)
+    res[1] -= (a / b) * res[2];
+    std::swap(res[1], res[2]);
+    return res;  // = (g, y, x - (a/b)*y)
+  }
+
+  unsigned _v;  // raw value
+};
+const unsigned MOD = 1'000'000'007;
+using Mint = ModInt<MOD>;
+
+// using Mint = long double;
+
+i64 solve() {
+  int n, m;
+  cin >> n >> m;
+  vector<int> a1(n), a2(n);
+  cin >> a1 >> a2;
+  vector<int> rc1(n + 1), rc2(n + 1);
+  for (int i = n - 1; i >= 0; --i) {
+    rc1[i] = rc1[i + 1] + int(a1[i] == 0 ? 1 : 0);
+    rc2[i] = rc2[i + 1] + int(a2[i] == 0 ? 1 : 0);
+  }
+  REP(i, n) {
+    if (a1[i] == 0 or a2[i] == 0) break;
+    if (a1[i] > a2[i]) return 1;
+    if (a1[i] < a2[i]) return 0;
+  }
+
+  Mint nume = 0;
+  Mint same = 1;
+  REP(i, n) {
+    Mint rest = Mint(m).pow(rc1[i + 1] + rc2[i + 1]) * same;
+    // Mint rest = pow((Mint)m, rc1[i + 1] + rc2[i + 1]);
+    if (a1[i] == 0 and a2[i] == 0) {
+      nume += Mint(m) * Mint(m - 1) / 2 * rest;
+      same *= m;
+    } else if (a1[i] == 0) {
+      nume += Mint(m - a2[i]) * rest;
+    } else if (a2[i] == 0) {
+      nume += Mint(a1[i] - 1) * rest;
+    } else {
+      if (a1[i] > a2[i]) {
+        nume += rest;
+        break;
+      }
+      if (a1[i] < a2[i]) {
+        break;
+      }
+    }
+  }
+
+  Mint deno = Mint(m).pow(rc1[0] + rc2[0]);
+  // Mint deno = pow((Mint)m, rc1[0] + rc2[0]);
+  DEBUG(nume, deno);
+  return (nume / deno).val();
+}
+
+int main() {
+  ios_base::sync_with_stdio(false), cin.tie(nullptr);
+  cout << solve() << "\n";
+}
---
+++ cf/contest/0935/E/main.cpp
@@ -0,0 +1,155 @@
+#include <bits/stdc++.h>
+#define REP_(i, a_, b_, a, b, ...) \
+  for (int i = (a), _Z_##i = (b); i < _Z_##i; ++i)
+#define REP(i, ...) REP_(i, __VA_ARGS__, __VA_ARGS__, 0, __VA_ARGS__)
+#define ALL(x) std::begin(x), std::end(x)
+using i64 = long long;
+using u64 = unsigned long long;
+
+template <typename T, typename U>
+inline bool chmax(T &a, U b) {
+  return a < b and ((a = std::move(b)), true);
+}
+template <typename T, typename U>
+inline bool chmin(T &a, U b) {
+  return a > b and ((a = std::move(b)), true);
+}
+template <typename T>
+inline int ssize(const T &a) {
+  return (int)std::size(a);
+}
+
+template <typename T>
+std::istream &operator>>(std::istream &is, std::vector<T> &a) {
+  for (auto &x : a) is >> x;
+  return is;
+}
+template <typename Container>
+std::ostream &print_seq(const Container &a, std::string_view sep = " ",
+                        std::string_view ends = "\n",
+                        std::ostream *os = nullptr) {
+  if (os == nullptr) os = &std::cout;
+  auto b = std::begin(a), e = std::end(a);
+  for (auto it = std::begin(a); it != e; ++it) {
+    if (it != b) *os << sep;
+    *os << *it;
+  }
+  return *os << ends;
+}
+template <typename T, typename = void>
+struct is_iterable : std::false_type {};
+template <typename T>
+struct is_iterable<T, std::void_t<decltype(std::begin(std::declval<T>())),
+                                  decltype(std::end(std::declval<T>()))>>
+    : std::true_type {};
+
+template <typename T, typename = std::enable_if_t<
+                          is_iterable<T>::value &&
+                          !std::is_same<T, std::string_view>::value &&
+                          !std::is_same<T, std::string>::value>>
+std::ostream &operator<<(std::ostream &os, const T &a) {
+  return print_seq(a, ", ", "", &(os << "{")) << "}";
+}
+template <typename T, typename U>
+std::ostream &operator<<(std::ostream &os, const std::pair<T, U> &a) {
+  return os << "(" << a.first << ", " << a.second << ")";
+}
+
+#ifdef ENABLE_DEBUG
+template <typename T>
+void pdebug(const T &value) {
+  std::cerr << value;
+}
+template <typename T, typename... Ts>
+void pdebug(const T &value, const Ts &...args) {
+  pdebug(value);
+  std::cerr << ", ";
+  pdebug(args...);
+}
+#define DEBUG(...)                                   \
+  do {                                               \
+    std::cerr << " \033[33m (L" << __LINE__ << ") "; \
+    std::cerr << #__VA_ARGS__ << ":\033[0m ";        \
+    pdebug(__VA_ARGS__);                             \
+    std::cerr << std::endl;                          \
+  } while (0)
+#else
+#define pdebug(...)
+#define DEBUG(...)
+#endif
+
+using namespace std;
+
+struct Node {
+  vector<i64> maxval;
+  vector<i64> minval;
+};
+
+const i64 INF = 1e15;
+
+i64 solve() {
+  string E;
+  cin >> E;
+  int n = ssize(E);
+  int P, M;
+  cin >> P >> M;
+  bool plus_state = true;
+  int limit = P;
+  if (M < P) {
+    plus_state = false;
+    limit = M;
+  }
+
+  auto parse = [&](auto rec, int i) -> pair<Node, int> {
+    char ch = E[i];
+    if (ch == '(') {
+      auto [a1, j] = rec(rec, i + 1);
+      assert(E[j] == '?');
+      auto [a2, k] = rec(rec, j + 1);
+      assert(E[k] == ')');
+      Node res;
+      res.maxval.assign(limit + 1, -INF);
+      res.minval.assign(limit + 1, INF);
+      if (plus_state) {
+        for (int p = 0; p <= limit; ++p) {
+          for (int q = 0; p + q <= limit; ++q) {
+            chmax(res.maxval[p + q], a1.maxval[p] - a2.minval[q]);
+            chmin(res.minval[p + q], a1.minval[p] - a2.maxval[q]);
+          }
+          for (int q = 0; p + q + 1 <= limit; ++q) {
+            chmax(res.maxval[p + q + 1], a1.maxval[p] + a2.maxval[q]);
+            chmin(res.minval[p + q + 1], a1.minval[p] + a2.minval[q]);
+          }
+        }
+      } else {
+        for (int p = 0; p <= limit; ++p) {
+          for (int q = 0; p + q <= limit; ++q) {
+            chmax(res.maxval[p + q], a1.maxval[p] + a2.maxval[q]);
+            chmin(res.minval[p + q], a1.minval[p] + a2.minval[q]);
+          }
+          for (int q = 0; p + q + 1 <= limit; ++q) {
+            chmax(res.maxval[p + q + 1], a1.maxval[p] - a2.minval[q]);
+            chmin(res.minval[p + q + 1], a1.minval[p] - a2.maxval[q]);
+          }
+        }
+      }
+      return {move(res), k + 1};
+    } else {
+      int d = ch - '0';
+      DEBUG(i, i + 1, d);
+      Node res;
+      res.maxval.assign(limit + 1, -INF);
+      res.minval.assign(limit + 1, INF);
+      res.maxval[0] = res.minval[0] = d;
+      return {move(res), i + 1};
+    }
+  };
+  auto [t, i] = parse(parse, 0);
+  assert(i == n);
+  return t.maxval[limit];
+}
+
+int main() {
+  ios_base::sync_with_stdio(false), cin.tie(nullptr);
+  cout << solve() << "\n";
+}
