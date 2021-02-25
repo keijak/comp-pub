b1e7be503efca2ebe68509748bb1c3d1f34932f4
Keiji Maekawa <m@keijak.com>
2021-02-25 22:23:37 +0100

    cf div2 virtual

---
+++ cf/contest/0989/C/main.cpp
@@ -0,0 +1,131 @@
+#include <bits/stdc++.h>
+#define REP_(i, a_, b_, a, b, ...) \
+  for (int i = (a), _Z_##i = (b); i < _Z_##i; ++i)
+#define REP(i, ...) REP_(i, __VA_ARGS__, __VA_ARGS__, 0, __VA_ARGS__)
+#define ALL(x) std::begin(x), std::end(x)
+using i64 = long long;
+using u64 = unsigned long long;
+
+template <typename T, typename U>
+inline bool chmax(T &a, U b) {
+  return a < b and ((a = std::move(b)), true);
+}
+template <typename T, typename U>
+inline bool chmin(T &a, U b) {
+  return a > b and ((a = std::move(b)), true);
+}
+template <typename T>
+inline int ssize(const T &a) {
+  return (int)std::size(a);
+}
+
+template <typename T>
+std::istream &operator>>(std::istream &is, std::vector<T> &a) {
+  for (auto &x : a) is >> x;
+  return is;
+}
+template <typename Container>
+std::ostream &print_seq(const Container &a, std::string_view sep = " ",
+                        std::string_view ends = "\n",
+                        std::ostream *os = nullptr) {
+  if (os == nullptr) os = &std::cout;
+  auto b = std::begin(a), e = std::end(a);
+  for (auto it = std::begin(a); it != e; ++it) {
+    if (it != b) *os << sep;
+    *os << *it;
+  }
+  return *os << ends;
+}
+template <typename T, typename = void>
+struct is_iterable : std::false_type {};
+template <typename T>
+struct is_iterable<T, std::void_t<decltype(std::begin(std::declval<T>())),
+                                  decltype(std::end(std::declval<T>()))>>
+    : std::true_type {};
+
+template <typename T, typename = std::enable_if_t<
+                          is_iterable<T>::value &&
+                          !std::is_same<T, std::string_view>::value &&
+                          !std::is_same<T, std::string>::value>>
+std::ostream &operator<<(std::ostream &os, const T &a) {
+  return print_seq(a, ", ", "", &(os << "{")) << "}";
+}
+template <typename T, typename U>
+std::ostream &operator<<(std::ostream &os, const std::pair<T, U> &a) {
+  return os << "(" << a.first << ", " << a.second << ")";
+}
+
+#ifdef ENABLE_DEBUG
+template <typename T>
+void pdebug(const T &value) {
+  std::cerr << value;
+}
+template <typename T, typename... Ts>
+void pdebug(const T &value, const Ts &...args) {
+  pdebug(value);
+  std::cerr << ", ";
+  pdebug(args...);
+}
+#define DEBUG(...)                                   \
+  do {                                               \
+    std::cerr << " \033[33m (L" << __LINE__ << ") "; \
+    std::cerr << #__VA_ARGS__ << ":\033[0m ";        \
+    pdebug(__VA_ARGS__);                             \
+    std::cerr << std::endl;                          \
+  } while (0)
+#else
+#define pdebug(...)
+#define DEBUG(...)
+#endif
+
+using namespace std;
+
+void solve() {
+  int a, b, c, d;
+  cin >> a >> b >> c >> d;
+  --c;
+  --d;
+  int n = 40;
+  int m = 20;
+  vector<string> grid(n);
+  REP(i, m) {
+    grid[i].resize(n);
+    REP(j, n) { grid[i][j] = 'C'; }
+  }
+  REP(i, m, n) {
+    grid[i].resize(n);
+    REP(j, n) { grid[i][j] = 'D'; }
+  }
+
+  deque<char> q;
+  REP(i, d) q.push_back('D');
+  REP(i, a) q.push_back('A');
+  REP(i, b) q.push_back('B');
+
+  for (int i = 0; i < m; i += 2) {
+    for (int j = 0; j < n; j += 2) {
+      if (q.empty()) goto phase2;
+      char ch = q.front();
+      q.pop_front();
+      grid[i][j] = ch;
+    }
+  }
+phase2:
+  REP(i, c) q.push_front('C');
+  for (int i = m + 1; i < n; i += 2) {
+    for (int j = 0; j < n; j += 2) {
+      if (q.empty()) goto phase3;
+      char ch = q.front();
+      q.pop_front();
+      grid[i][j] = ch;
+    }
+  }
+phase3:
+  cout << n << " " << n << "\n";
+  REP(i, n) { cout << grid[i] << "\n"; }
+}
+
+int main() {
+  ios_base::sync_with_stdio(false), cin.tie(nullptr);
+  solve();
+}
