0715c26ad687ac5a6ef1ed3172960d5abb21bb53
Keiji Maekawa <m@keijak.com>
2021-03-03 02:49:48 +0100

    cf div2

--- cf/contest/1494/D/main.cpp
+++ cf/contest/1494/D/main.cpp
@@ -3,85 +3,18 @@
   for (int i = (a), _Z_##i = (b); i < _Z_##i; ++i)
 #define REP(i, ...) REP_(i, __VA_ARGS__, __VA_ARGS__, 0, __VA_ARGS__)
 #define ALL(x) std::begin(x), std::end(x)
-using i64 = long long;
-using u64 = unsigned long long;
 
-template <typename T, typename U>
-inline bool chmax(T &a, U b) {
-  return a < b and ((a = std::move(b)), true);
-}
-template <typename T, typename U>
-inline bool chmin(T &a, U b) {
-  return a > b and ((a = std::move(b)), true);
-}
-template <typename T>
-inline int ssize(const T &a) {
-  return (int)std::size(a);
-}
-
-template <typename T>
-std::istream &operator>>(std::istream &is, std::vector<T> &a) {
-  for (auto &x : a) is >> x;
-  return is;
-}
 template <typename Container>
 std::ostream &print_seq(const Container &a, std::string_view sep = " ",
                         std::string_view ends = "\n",
-                        std::ostream *os = nullptr) {
-  if (os == nullptr) os = &std::cout;
+                        std::ostream &os = std::cout) {
   auto b = std::begin(a), e = std::end(a);
   for (auto it = std::begin(a); it != e; ++it) {
-    if (it != b) *os << sep;
-    *os << *it;
+    if (it != b) os << sep;
+    os << *it;
   }
-  return *os << ends;
+  return os << ends;
 }
-template <typename T, typename = void>
-struct is_iterable : std::false_type {};
-template <typename T>
-struct is_iterable<T, std::void_t<decltype(std::begin(std::declval<T>())),
-                                  decltype(std::end(std::declval<T>()))>>
-    : std::true_type {};
-
-template <typename T, typename = std::enable_if_t<
-                          is_iterable<T>::value &&
-                          !std::is_same<T, std::string_view>::value &&
-                          !std::is_same<T, std::string>::value>>
-std::ostream &operator<<(std::ostream &os, const T &a) {
-  return print_seq(a, ", ", "", &(os << "{")) << "}";
-}
-template <typename T, typename U>
-std::ostream &operator<<(std::ostream &os, const std::pair<T, U> &a) {
-  return os << "(" << a.first << ", " << a.second << ")";
-}
-
-#ifdef ENABLE_DEBUG
-template <typename T>
-void pdebug(const T &value) {
-  std::cerr << value;
-}
-template <typename T, typename... Ts>
-void pdebug(const T &value, const Ts &...args) {
-  pdebug(value);
-  std::cerr << ", ";
-  pdebug(args...);
-}
-#define DEBUG(...)                                   \
-  do {                                               \
-    std::cerr << " \033[33m (L" << __LINE__ << ") "; \
-    std::cerr << #__VA_ARGS__ << ":\033[0m ";        \
-    pdebug(__VA_ARGS__);                             \
-    std::cerr << std::endl;                          \
-  } while (0)
-#else
-#define pdebug(...)
-#define DEBUG(...)
-#endif
-
-using namespace std;
-
-template <class T>
-using MinHeap = priority_queue<T, vector<T>, greater<T>>;
 
 struct UnionFind {
   int n;
@@ -104,121 +37,50 @@ struct UnionFind {
     if (parent[v] < 0) return v;
     return parent[v] = find(parent[v]);
   }
-
-  int size(int v) const { return -parent[find(v)]; }
-
-  bool same(int x, int y) const { return find(x) == find(y); }
-
-  std::vector<int> roots() const {
-    std::vector<int> res;
-    res.reserve(num_roots);
-    for (int i = 0; i < n; ++i) {
-      if (parent[i] < 0) res.push_back(i);
-    }
-    return res;
-  }
 };
 
-struct Min {
-  using T = int;
-  static T op(const T &x, const T &y) { return std::min(x, y); }
-  static constexpr T id() { return std::numeric_limits<T>::max(); }
-};
-
-template <typename Abelian>  // Abelian Monoid
-struct AggregatableUnionFind : public UnionFind {
-  using T = typename Abelian::T;
-  std::vector<T> agg_data;
-
-  explicit AggregatableUnionFind(std::vector<T> data)
-      : UnionFind(data.size()), agg_data(std::move(data)) {}
-
-  bool unite(int x, int y) {
-    int rx = this->find(x), ry = this->find(y);
-    if (not UnionFind::unite(x, y)) return false;
-    int r = this->find(x);
-    agg_data[r] = Abelian::op(agg_data[rx], agg_data[ry]);
-    return true;
-  }
-
-  const T &agg(int x) const { return agg_data[this->find(x)]; }
-};
+using namespace std;
 
 void solve() {
   int n;
   cin >> n;
-  vector a(n, vector(n, 0LL));
+  vector a(n, vector(n, 0));
   REP(i, n) { REP(j, n) cin >> a[i][j]; }
 
-  vector<i64> salary;
-  salary.reserve(n * 2);
-  salary.resize(n, 0LL);
-  REP(i, n) { salary[i] = a[i][i]; }  // leaf salary
-  DEBUG(salary);
+  vector<int> salaries(n, 0);
+  REP(i, n) { salaries[i] = a[i][i]; }  // leaf salaries
 
-  map<pair<i64, int>, int> bossid;
-  vector<vector<int>> ancestor(n);
-  REP(i, n) { ancestor[i].push_back(i); }
   map<int, int> parent;
+  map<int, int> bossid;
+  REP(i, n) { bossid[i] = i; }
 
-  deque<tuple<i64, int, int>> dq;
-  REP(i, n) REP(j, i + 1, n) { dq.emplace_back(a[i][j], i, j); }
-  sort(ALL(dq));
+  vector<tuple<int, int, int>> q;
+  REP(i, n) REP(j, i + 1, n) { q.emplace_back(a[i][j], i, j); }
+  sort(ALL(q));
 
-  vector<int> eid(n);
-  REP(i, n) eid[i] = i;
-  AggregatableUnionFind<Min> uf(eid);
-
-  while (not dq.empty()) {
-    auto [sal, i, j] = dq.front();
-    dq.pop_front();
-    assert(i < j);
-
-    int ri = uf.agg(i);
-    int rj = uf.agg(j);
-    int r = min(ri, rj);
-
-    pair<i64, int> bkey = {sal, r};
-    auto it = bossid.find(bkey);
-    if (it == bossid.end()) {
-      int bid = salary.size();
-      bossid[bkey] = bid;
-      int p = ancestor[ri].back();
-      if (salary[p] < sal) {
-        parent[p] = bid;
-      }
-      if (ri != rj) {
-        int p = ancestor[rj].back();
-        if (salary[p] < sal) parent[p] = bid;
-      }
-      ancestor[r].push_back(bid);
-      salary.push_back(sal);
-    } else {
-      int bid = it->second;
-      for (int rx : {ri, rj}) {
-        int p = ancestor[rx].back();
-        if (salary[p] < sal) {
-          parent[p] = bid;
-        }
-      }
-    }
+  UnionFind uf(n);
+  for (auto [sal, i, j] : q) {
+    int ri = uf.find(i), rj = uf.find(j);
+    int bi = bossid[ri], bj = bossid[rj];
     uf.unite(i, j);
-  }
-
-  int k = salary.size();
-  cout << k << "\n";
-  print_seq(salary);
-  int root = -1;
-  REP(i, n) { assert(parent.find(i) != parent.end()); }
-  REP(i, n, k) {
-    if (parent.find(i) == parent.end()) {
-      root = i;
-      break;
+    int rm = uf.find(i);
+    if (salaries[bi] == sal and salaries[bj] == sal) {
+      continue;
+    } else if (salaries[bi] == sal) {
+      bossid[rm] = parent[bj] = bi;
+    } else if (salaries[bj] == sal) {
+      bossid[rm] = parent[bi] = bj;
+    } else {
+      int new_boss = salaries.size();
+      salaries.push_back(sal);
+      bossid[rm] = parent[bi] = parent[bj] = new_boss;
     }
   }
-  cout << root + 1 << "\n";
-  DEBUG(parent);
-  // assert(ssize(parent) == k - 1);
+
+  int k = salaries.size();
+  cout << k << "\n";
+  print_seq(salaries);
+  cout << k << "\n";
   for (auto [v, u] : parent) {
     cout << v + 1 << " " << u + 1 << "\n";
   }
