da87c4ab0f14743bfbad29a0306a4f71f8a1eb1b
Keiji Maekawa <m@keijak.com>
2021-03-14 11:01:50 +0100

    cf div2 virtual

--- cf/contest/1062/E/main.cpp
+++ cf/contest/1062/E/main.cpp
@@ -19,64 +19,6 @@ inline int ssize(const T &a) {
   return (int)std::size(a);
 }
 
-template <typename T>
-std::istream &operator>>(std::istream &is, std::vector<T> &a) {
-  for (auto &x : a) is >> x;
-  return is;
-}
-template <typename Container>
-std::ostream &print_seq(const Container &a, std::string_view sep = " ",
-                        std::string_view ends = "\n",
-                        std::ostream &os = std::cout) {
-  auto b = std::begin(a), e = std::end(a);
-  for (auto it = std::begin(a); it != e; ++it) {
-    if (it != b) os << sep;
-    os << *it;
-  }
-  return os << ends;
-}
-template <typename T, typename = void>
-struct is_iterable : std::false_type {};
-template <typename T>
-struct is_iterable<T, std::void_t<decltype(std::begin(std::declval<T>())),
-                                  decltype(std::end(std::declval<T>()))>>
-    : std::true_type {};
-
-template <typename T, typename = std::enable_if_t<
-                          is_iterable<T>::value &&
-                          !std::is_same<T, std::string_view>::value &&
-                          !std::is_same<T, std::string>::value>>
-std::ostream &operator<<(std::ostream &os, const T &a) {
-  return print_seq(a, ", ", "", (os << "{")) << "}";
-}
-template <typename T, typename U>
-std::ostream &operator<<(std::ostream &os, const std::pair<T, U> &a) {
-  return os << "(" << a.first << ", " << a.second << ")";
-}
-
-#ifdef ENABLE_DEBUG
-template <typename T>
-void pdebug(const T &value) {
-  std::cerr << value;
-}
-template <typename T, typename... Ts>
-void pdebug(const T &value, const Ts &...args) {
-  pdebug(value);
-  std::cerr << ", ";
-  pdebug(args...);
-}
-#define DEBUG(...)                                   \
-  do {                                               \
-    std::cerr << " \033[33m (L" << __LINE__ << ") "; \
-    std::cerr << #__VA_ARGS__ << ":\033[0m ";        \
-    pdebug(__VA_ARGS__);                             \
-    std::cerr << std::endl;                          \
-  } while (0)
-#else
-#define pdebug(...)
-#define DEBUG(...)
-#endif
-
 using namespace std;
 
 // tour: preorder node ids
@@ -118,74 +60,49 @@ struct EulerTour {
   }
 };
 
-template <typename Monoid>
-struct SegTree {
-  using T = typename Monoid::T;
+template <class SemiLattice>
+struct SparseTable {
+  using T = typename SemiLattice::T;
 
- private:
-  int n_;                // number of valid leaves.
-  int offset_;           // where leaves start
-  std::vector<T> data_;  // data size: 2*offset_
-
- public:
-  inline int n() const { return n_; }
-  inline int offset() const { return offset_; }
-
-  explicit SegTree(int n) : n_(n) {
-    offset_ = 1;
-    while (offset_ < n_) offset_ <<= 1;
-    data_.assign(2 * offset_, Monoid::id());
-  }
-
-  explicit SegTree(const std::vector<T> &leaves) : n_(leaves.size()) {
-    offset_ = 1;
-    while (offset_ < n_) offset_ <<= 1;
-    data_.assign(2 * offset_, Monoid::id());
-    for (int i = 0; i < n_; ++i) {
-      data_[offset_ + i] = leaves[i];
-    }
-    for (int i = offset_ - 1; i > 0; --i) {
-      data_[i] = Monoid::op(data_[i * 2], data_[i * 2 + 1]);
-    }
-  }
-
-  // Sets i-th value (0-indexed) to x.
-  void set(int i, const T &x) {
-    int k = offset_ + i;
-    data_[k] = x;
-    // Update its ancestors.
-    while (k > 1) {
-      k >>= 1;
-      data_[k] = Monoid::op(data_[k * 2], data_[k * 2 + 1]);
-    }
-  }
+  explicit SparseTable(const std::vector<T> &vec) { init(vec); }
 
   // Queries by [l,r) range (0-indexed, half-open interval).
   T fold(int l, int r) const {
-    l = std::max(l, 0) + offset_;
-    r = std::min(r, offset_) + offset_;
-    T vleft = Monoid::id(), vright = Monoid::id();
-    for (; l < r; l >>= 1, r >>= 1) {
-      if (l & 1) vleft = Monoid::op(vleft, data_[l++]);
-      if (r & 1) vright = Monoid::op(data_[--r], vright);
+    l = max(l, 0);
+    r = min<int>(r, n_);
+    if (l >= r) {
+      return SemiLattice::id();
     }
-    return Monoid::op(vleft, vright);
+    const T &lval = data_[height_[r - l]][l];
+    const T &rval = data_[height_[r - l]][r - (1 << height_[r - l])];
+    return SemiLattice::op(lval, rval);
   }
-
-  T fold_all() const { return data_[1]; }
 
   // Returns i-th value (0-indexed).
-  T operator[](int i) const { return data_[offset_ + i]; }
+  T operator[](int i) const { return data_[height_[1]][i]; }
 
-  friend std::ostream &operator<<(std::ostream &os, const SegTree &st) {
-    os << "[";
-    for (int i = 0; i < st.n(); ++i) {
-      if (i != 0) os << ", ";
-      const auto &x = st[i];
-      os << x;
+ private:
+  void init(const std::vector<T> &vec) {
+    int n = vec.size(), h = 0;
+    n_ = n;
+    while ((1 << h) <= n) ++h;
+    data_.assign(h, std::vector<T>(1 << h, SemiLattice::id()));
+    height_.assign(n + 1, 0);
+    for (int i = 2; i <= n; i++) {
+      height_[i] = height_[i >> 1] + 1;
     }
-    return os << "]";
+    for (int i = 0; i < n; ++i) {
+      data_[0][i] = vec[i];
+    }
+    for (int i = 1; i < h; ++i)
+      for (int j = 0; j < n; ++j)
+        data_[i][j] = SemiLattice::op(
+            data_[i - 1][j], data_[i - 1][std::min(j + (1 << (i - 1)), n - 1)]);
   }
+
+  int n_;  // number of elements.
+  std::vector<std::vector<T>> data_;
+  std::vector<int> height_;
 };
 
 const int BIG = 1000000;
@@ -216,54 +133,37 @@ int main() {
     parent[i + 1] = p;
     children[p].push_back(i + 1);
   }
-  REP(i, n) {
-    DEBUG(i, parent[i]);
-    DEBUG(i, children[i]);
-  }
   EulerTour et(children, 0);
-  DEBUG(et.tour);
-  DEBUG(et.pre_index);
-  DEBUG(et.post_index);
-  DEBUG(et.level);
 
-  SegTree<Min> minseg(et.pre_index);
-  SegTree<Max> maxseg(et.pre_index);
-  SegTree<Min> levelseg(et.level);
+  SparseTable<Min> minst(et.pre_index);
+  SparseTable<Max> maxst(et.pre_index);
+  SparseTable<Min> levelst(et.level);
 
   REP(iq, q) {
     int l, r;
     cin >> l >> r;
     --l;
-    DEBUG(l, r);
-    int imin = minseg.fold(l, r);
-    DEBUG(imin, et.tour[imin]);
-    int imax = maxseg.fold(l, r);
-    DEBUG(imax, et.tour[imax]);
-
-    int manlv = -1;
+    int imin = minst.fold(l, r);
+    int imax = maxst.fold(l, r);
+    int manager_level = -1;
     int excluded = -1;
     {  // exclude imin
       int ei = et.tour[imin];
       int mi = BIG;
-      chmin(mi, minseg.fold(l, ei));
-      chmin(mi, minseg.fold(ei + 1, r));
-      assert(mi < BIG);
-      DEBUG(imin, ei, mi);
-      manlv = levelseg.fold(mi, imax + 1);
+      chmin(mi, minst.fold(l, ei));
+      chmin(mi, minst.fold(ei + 1, r));
+      manager_level = levelst.fold(mi, imax + 1);
       excluded = ei;
     }
     {  // exclude imax
       int ei = et.tour[imax];
       int mi = -BIG;
-      chmax(mi, maxseg.fold(l, ei));
-      chmax(mi, maxseg.fold(ei + 1, r));
-      assert(mi > -BIG);
-      if (chmax(manlv, levelseg.fold(imin, mi + 1))) {
+      chmax(mi, maxst.fold(l, ei));
+      chmax(mi, maxst.fold(ei + 1, r));
+      if (chmax(manager_level, levelst.fold(imin, mi + 1))) {
         excluded = ei;
       }
     }
-    assert(excluded != -1);
-    assert(manlv != -1);
-    cout << excluded + 1 << " " << manlv << "\n";
+    cout << excluded + 1 << " " << manager_level << "\n";
   }
 }
