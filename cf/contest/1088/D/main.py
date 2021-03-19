891fe97fb9dc0f13fe06a245a202d05af4634ed4
keijak <m@keijak.com>
2021-03-19 18:15:22 +0100

    interactive

--- cf/contest/1088/D/main.py
+++ cf/contest/1088/D/main.py
@@ -6,63 +6,67 @@ def ask(c, d):
     return int(input())
 
 
-def solve_same():
-    ans = 0
-    for i in range(29, -1, -1):
-        print(f">> {i=} {ans=}", file=sys.stderr)
-        bit = 1 << i
-        res1 = ask(ans ^ bit, ans)
-        res2 = ask(ans, ans ^ bit)
-        if res1 == -1 and res2 == 1:
-            ans |= bit
-    return ans
+c = d = 0
 
 
-def solve1():
-    print("solve1")
-    a = b = 0
-    c = d = 0
-    for i in range(29, -1, -1):
-        print(f">> {i=} {a=} {b=} {c=} {d=}", file=sys.stderr)
-        bit = 1 << i
-        res1 = ask(c ^ bit, d ^ bit)
-        if res1 == -1:
-            a |= bit
-            c |= bit
-            res2 = ask(c, d)
-        else:
-            res2 = ask(c ^ bit, d)
-            if res2 == -1:
-                a |= bit
-                b |= bit
-    return a, b
-
-
-def solve2():
-    print("solve2")
-    a = b = 0
-    c = d = 0
-    for i in range(29, -1, -1):
-        print(f">> {i=} {a=} {b=} {c=} {d=}", file=sys.stderr)
-        bit = 1 << i
-        res1 = ask(c ^ bit, d ^ bit)
-        if res1 == 1:
-            b |= bit
-            d |= bit
-        else:
+def solve(mi, base):
+    def solve_same():
+        global c, d
+        print("# solve_same", file=sys.stderr)
+        for i in range(mi, -1, -1):
+            print(f">> {i=} {c=} {d=}", file=sys.stderr)
+            bit = 1 << i
+            res1 = ask(c ^ bit, d)
             res2 = ask(c, d ^ bit)
-            if res2 == 1:
-                a |= bit
-                b |= bit
-    return a, b
+            if res1 == -1 and res2 == 1:
+                c |= bit
+                d |= bit
+
+    def solve1():
+        global c, d
+        print("# solve1", file=sys.stderr)
+        for i in range(mi, -1, -1):
+            print(f">> {i=} {c=} {d=}", file=sys.stderr)
+            bit = 1 << i
+            res1 = ask(c ^ bit, d ^ bit)
+            if res1 == -1:
+                # a[i] == 1, b[i] == 0
+                c |= bit
+                return solve(i - 1, ask(c, d))
+            else:
+                # a[i] == b[i]
+                res2 = ask(c ^ bit, d)
+                if res2 == -1:
+                    # a[i] == b[i] == 1
+                    c |= bit
+                    d |= bit
+
+    def solve2():
+        global c, d
+        print("# solve2", file=sys.stderr)
+        for i in range(mi, -1, -1):
+            print(f">> {i=} {c=} {d=}", file=sys.stderr)
+            bit = 1 << i
+            res1 = ask(c ^ bit, d ^ bit)
+            if res1 == 1:
+                # a[i] == 0, b[i] == 1
+                d |= bit
+                return solve(i - 1, ask(c, d))
+            else:
+                # a[i] == b[i]
+                res2 = ask(c, d ^ bit)
+                if res2 == 1:
+                    # a[i] == b[i] == 1
+                    c |= bit
+                    d |= bit
+
+    if base == 0:
+        solve_same()
+    elif base == 1:
+        solve1()
+    else:
+        solve2()
 
 
-base = ask(0, 0)
-if base == 0:
-    a = solve_same()
-    b = a
-elif base == 1:
-    a, b = solve1()
-else:
-    a, b = solve2()
-print("! {} {}".format(a, b))
+solve(29, ask(0, 0))
+print("! {} {}".format(c, d))
