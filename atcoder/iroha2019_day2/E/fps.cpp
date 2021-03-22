c0480ca6c1f925f556c719854c1a0982af8a13d4
Keiji Maekawa <m@keijak.com>
2021-03-22 19:53:31 +0100

    fps

---
+++ atcoder/iroha2019_day2/E/fps.cpp
@@ -0,0 +1,351 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/modint>
+using Mint = atcoder::modint1000000007;
+std::ostream &operator<<(std::ostream &os, const Mint &m) {
+  return os << m.val();
+}
+
+using namespace std;
+using i64 = long long;
+
+template <class T = Mint>
+struct Factorials {
+  // factorials and inverse factorials.
+  std::vector<T> fact, ifact;
+
+  // n: max cached value.
+  Factorials(int n) : fact(n + 1), ifact(n + 1) {
+    assert(n > 0 and n < T::mod());
+    fact[0] = 1;
+    for (int i = 1; i <= n; ++i) fact[i] = fact[i - 1] * i;
+    ifact[n] = fact[n].inv();
+    for (int i = n; i >= 1; --i) ifact[i - 1] = ifact[i] * i;
+  }
+
+  // Combination (nCk)
+  T C(int n, int k) const {
+    if (k < 0 || k > n) return 0;
+    return fact[n] * ifact[k] * ifact[n - k];
+  }
+};
+
+namespace FastFourierTransform {
+using real = double;
+
+struct C {
+  real x, y;
+
+  C() : x(0), y(0) {}
+
+  C(real x, real y) : x(x), y(y) {}
+
+  inline C operator+(const C &c) const { return C(x + c.x, y + c.y); }
+
+  inline C operator-(const C &c) const { return C(x - c.x, y - c.y); }
+
+  inline C operator*(const C &c) const {
+    return C(x * c.x - y * c.y, x * c.y + y * c.x);
+  }
+
+  inline C conj() const { return C(x, -y); }
+};
+
+const real PI = acosl(-1);
+int base = 1;
+vector<C> rts = {{0, 0}, {1, 0}};
+vector<int> rev = {0, 1};
+
+void ensure_base(int nbase) {
+  if (nbase <= base) return;
+  rev.resize(1 << nbase);
+  rts.resize(1 << nbase);
+  for (int i = 0; i < (1 << nbase); i++) {
+    rev[i] = (rev[i >> 1] >> 1) + ((i & 1) << (nbase - 1));
+  }
+  while (base < nbase) {
+    real angle = PI * 2.0 / (1 << (base + 1));
+    for (int i = 1 << (base - 1); i < (1 << base); i++) {
+      rts[i << 1] = rts[i];
+      real angle_i = angle * (2 * i + 1 - (1 << base));
+      rts[(i << 1) + 1] = C(cos(angle_i), sin(angle_i));
+    }
+    ++base;
+  }
+}
+
+void fft(vector<C> &a, int n) {
+  assert((n & (n - 1)) == 0);
+  int zeros = __builtin_ctz(n);
+  ensure_base(zeros);
+  int shift = base - zeros;
+  for (int i = 0; i < n; i++) {
+    if (i < (rev[i] >> shift)) {
+      swap(a[i], a[rev[i] >> shift]);
+    }
+  }
+  for (int k = 1; k < n; k <<= 1) {
+    for (int i = 0; i < n; i += 2 * k) {
+      for (int j = 0; j < k; j++) {
+        C z = a[i + j + k] * rts[j + k];
+        a[i + j + k] = a[i + j] - z;
+        a[i + j] = a[i + j] + z;
+      }
+    }
+  }
+}
+
+vector<int64_t> multiply(const vector<int> &a, const vector<int> &b) {
+  int need = (int)a.size() + (int)b.size() - 1;
+  int nbase = 1;
+  while ((1 << nbase) < need) nbase++;
+  ensure_base(nbase);
+  int sz = 1 << nbase;
+  vector<C> fa(sz);
+  for (int i = 0; i < sz; i++) {
+    int x = (i < (int)a.size() ? a[i] : 0);
+    int y = (i < (int)b.size() ? b[i] : 0);
+    fa[i] = C(x, y);
+  }
+  fft(fa, sz);
+  C r(0, -0.25 / (sz >> 1)), s(0, 1), t(0.5, 0);
+  for (int i = 0; i <= (sz >> 1); i++) {
+    int j = (sz - i) & (sz - 1);
+    C z = (fa[j] * fa[j] - (fa[i] * fa[i]).conj()) * r;
+    fa[j] = (fa[i] * fa[i] - (fa[j] * fa[j]).conj()) * r;
+    fa[i] = z;
+  }
+  for (int i = 0; i < (sz >> 1); i++) {
+    C A0 = (fa[i] + fa[i + (sz >> 1)]) * t;
+    C A1 = (fa[i] - fa[i + (sz >> 1)]) * t * rts[(sz >> 1) + i];
+    fa[i] = A0 + A1 * s;
+  }
+  fft(fa, sz >> 1);
+  vector<int64_t> ret(need);
+  for (int i = 0; i < need; i++) {
+    ret[i] = llround(i & 1 ? fa[i >> 1].y : fa[i >> 1].x);
+  }
+  return ret;
+}
+};  // namespace FastFourierTransform
+
+template <typename T>
+struct ArbitraryModConvolution {
+  using real = FastFourierTransform::real;
+  using C = FastFourierTransform::C;
+  using value_type = T;
+
+  static vector<T> multiply(const vector<T> &a, const vector<T> &b,
+                            int need = -1) {
+    if (need == -1) need = a.size() + b.size() - 1;
+    int nbase = 0;
+    while ((1 << nbase) < need) nbase++;
+    FastFourierTransform::ensure_base(nbase);
+    int sz = 1 << nbase;
+    vector<C> fa(sz);
+    for (int i = 0; i < (int)a.size(); i++) {
+      fa[i] = C(a[i].val() & ((1 << 15) - 1), a[i].val() >> 15);
+    }
+    fft(fa, sz);
+    vector<C> fb(sz);
+    if (a == b) {
+      fb = fa;
+    } else {
+      for (int i = 0; i < (int)b.size(); i++) {
+        fb[i] = C(b[i].val() & ((1 << 15) - 1), b[i].val() >> 15);
+      }
+      fft(fb, sz);
+    }
+    real ratio = 0.25 / sz;
+    C r2(0, -1), r3(ratio, 0), r4(0, -ratio), r5(0, 1);
+    for (int i = 0; i <= (sz >> 1); i++) {
+      int j = (sz - i) & (sz - 1);
+      C a1 = (fa[i] + fa[j].conj());
+      C a2 = (fa[i] - fa[j].conj()) * r2;
+      C b1 = (fb[i] + fb[j].conj()) * r3;
+      C b2 = (fb[i] - fb[j].conj()) * r4;
+      if (i != j) {
+        C c1 = (fa[j] + fa[i].conj());
+        C c2 = (fa[j] - fa[i].conj()) * r2;
+        C d1 = (fb[j] + fb[i].conj()) * r3;
+        C d2 = (fb[j] - fb[i].conj()) * r4;
+        fa[i] = c1 * d1 + c2 * d2 * r5;
+        fb[i] = c1 * d2 + c2 * d1;
+      }
+      fa[j] = a1 * b1 + a2 * b2 * r5;
+      fb[j] = a1 * b2 + a2 * b1;
+    }
+    fft(fa, sz);
+    fft(fb, sz);
+    vector<T> ret(need);
+    for (int i = 0; i < need; i++) {
+      int64_t aa = llround(fa[i].x);
+      int64_t bb = llround(fb[i].x);
+      int64_t cc = llround(fa[i].y);
+      aa = T(aa).val(), bb = T(bb).val(), cc = T(cc).val();
+      ret[i] = aa + (bb << 15) + (cc << 30);
+    }
+    return ret;
+  }
+};
+
+template <typename T, int DMAX>
+struct ArbitraryModMult {
+  using value_type = T;
+  static constexpr int dmax() { return DMAX; }
+
+  static std::vector<T> multiply(const std::vector<T> &x,
+                                 const std::vector<T> &y) {
+    // NOTE: Setting `need=DMAX+1` gives incorrect results.
+    auto res = ArbitraryModConvolution<T>::multiply(x, y);
+    if (res.size() > DMAX + 1) res.resize(DMAX + 1);  // shrink
+    return res;
+  }
+};
+
+// Formal Power Series (dense format).
+template <typename Mult>
+struct DenseFPS {
+  using T = typename Mult::value_type;
+  static const int DMAX = Mult::dmax();
+
+  // Coefficients of terms from x^0 to x^DMAX.
+  std::vector<T> coeff_;
+
+  DenseFPS() : coeff_(1) {}  // zero-initialized
+  explicit DenseFPS(std::vector<T> c) : coeff_(std::move(c)) {
+    assert((int)c.size() <= DMAX + 1);
+  }
+  DenseFPS(std::initializer_list<T> init) : coeff_(init.begin(), init.end()) {
+    assert((int)init.size() <= DMAX + 1);
+  }
+
+  DenseFPS(const DenseFPS &other) : coeff_(other.coeff_) {}
+  DenseFPS(DenseFPS &&other) : coeff_(std::move(other.coeff_)) {}
+  DenseFPS &operator=(const DenseFPS &other) {
+    coeff_ = other.coeff_;
+    return *this;
+  }
+  DenseFPS &operator=(DenseFPS &&other) {
+    coeff_ = std::move(other.coeff_);
+    return *this;
+  }
+
+  int size() const { return (int)coeff_.size(); }
+
+  // Returns the coefficient of x^k.
+  T operator[](int k) const {
+    if (k >= size()) return 0;
+    return coeff_[k];
+  }
+
+  DenseFPS &operator+=(const T &scalar) {
+    coeff_[0] += scalar;
+    return *this;
+  }
+  friend DenseFPS operator+(const DenseFPS &x, const T &scalar) {
+    DenseFPS res = x;
+    res += scalar;
+    return res;
+  }
+  DenseFPS &operator+=(const DenseFPS &other) {
+    if (size() < other.size()) {
+      coeff_.resize(other.size());
+    }
+    for (int i = 0; i < other.size(); ++i) coeff_[i] += other[i];
+    return *this;
+  }
+  friend DenseFPS operator+(const DenseFPS &x, const DenseFPS &y) {
+    DenseFPS res = x;
+    res += y;
+    return res;
+  }
+
+  DenseFPS &operator-=(const DenseFPS &other) {
+    if (size() < other.size()) {
+      coeff_.resize(other.size());
+    }
+    for (int i = 0; i < other.size(); ++i) coeff_[i] -= other[i];
+    return *this;
+  }
+  friend DenseFPS operator-(const DenseFPS &x, const DenseFPS &y) {
+    DenseFPS res = x;
+    res -= y;
+    return res;
+  }
+
+  DenseFPS &operator*=(const T &scalar) {
+    for (auto &x : coeff_) x *= scalar;
+    return *this;
+  }
+  friend DenseFPS operator*(const DenseFPS &x, const T &scalar) {
+    DenseFPS res = x;
+    res *= scalar;
+    return res;
+  }
+  friend DenseFPS operator*(const T &scalar, const DenseFPS &y) {
+    DenseFPS res = {scalar};
+    res *= y;
+    return res;
+  }
+  DenseFPS &operator*=(const DenseFPS &other) {
+    *this = DenseFPS(Mult::multiply(std::move(this->coeff_), other.coeff_));
+    return *this;
+  }
+  friend DenseFPS operator*(const DenseFPS &x, const DenseFPS &y) {
+    return DenseFPS(Mult::multiply(x.coeff_, y.coeff_));
+  }
+
+  DenseFPS &operator/=(const T &scalar) {
+    for (auto &x : coeff_) x /= scalar;
+    return *this;
+  }
+  friend DenseFPS operator/(const DenseFPS &x, const T &scalar) {
+    DenseFPS res = x;
+    res /= scalar;
+    return res;
+  }
+  friend DenseFPS operator/(const T &scalar, const DenseFPS &y) {
+    DenseFPS res = {scalar};
+    res /= y;
+    return res;
+  }
+  DenseFPS &operator/=(const DenseFPS &other) {
+    *this *= DenseFPS(Mult::invert(other.coeff_));
+    return *this;
+  }
+  friend DenseFPS operator/(const DenseFPS &x, const DenseFPS &y) {
+    return x * DenseFPS(Mult::invert(y.coeff_));
+  }
+
+  DenseFPS pow(i64 t) {
+    DenseFPS res;
+    DenseFPS base = std::move(*this);
+    res.coeff_[0] = 1;
+    while (t) {
+      if (t & 1) res *= base;
+      base *= base;
+      t >>= 1;
+    }
+    return res;
+  }
+};
+
+const int D = 100000;
+using FPS = DenseFPS<ArbitraryModMult<Mint, D>>;
+
+Mint solve() {
+  int n, m;
+  cin >> n >> m;
+  if (n < 3) return 0;
+
+  Factorials fs(n + m);
+  Mint total = fs.C(n + m - 2, n - 1);
+  FPS a = FPS{1, 1} * FPS{1, 1, 1}.pow(m - 1);
+  return total - a[n - 1];
+}
+
+int main() {
+  ios_base::sync_with_stdio(false), cin.tie(nullptr);
+  cout << solve() << "\n";
+}
