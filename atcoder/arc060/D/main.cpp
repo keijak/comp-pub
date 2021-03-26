92bf917a91fb4767a9030906764130cfce035cab
keijak <m@keijak.com>
2021-03-26 10:46:22 +0100

    digit sum

---
+++ atcoder/arc060/D/main.cpp
@@ -0,0 +1,126 @@
+#include <bits/stdc++.h>
+#define REP_(i, a_, b_, a, b, ...) \
+  for (int i = (a), _Z_##i = (b); i < _Z_##i; ++i)
+#define REP(i, ...) REP_(i, __VA_ARGS__, __VA_ARGS__, 0, __VA_ARGS__)
+#define ALL(x) std::begin(x), std::end(x)
+using i64 = long long;
+using u64 = unsigned long long;
+
+template <typename T, typename U>
+inline bool chmax(T &a, U b) {
+  return a < b and ((a = std::move(b)), true);
+}
+template <typename T, typename U>
+inline bool chmin(T &a, U b) {
+  return a > b and ((a = std::move(b)), true);
+}
+template <typename T>
+inline int ssize(const T &a) {
+  return (int)std::size(a);
+}
+
+template <typename T>
+std::istream &operator>>(std::istream &is, std::vector<T> &a) {
+  for (auto &x : a) is >> x;
+  return is;
+}
+template <typename Container>
+std::ostream &print_seq(const Container &a, std::string_view sep = " ",
+                        std::string_view ends = "\n",
+                        std::ostream &os = std::cout) {
+  auto b = std::begin(a), e = std::end(a);
+  for (auto it = std::begin(a); it != e; ++it) {
+    if (it != b) os << sep;
+    os << *it;
+  }
+  return os << ends;
+}
+template <typename T, typename = void>
+struct is_iterable : std::false_type {};
+template <typename T>
+struct is_iterable<T, std::void_t<decltype(std::begin(std::declval<T>())),
+                                  decltype(std::end(std::declval<T>()))>>
+    : std::true_type {};
+
+template <typename T, typename = std::enable_if_t<
+                          is_iterable<T>::value &&
+                          !std::is_same<T, std::string_view>::value &&
+                          !std::is_same<T, std::string>::value>>
+std::ostream &operator<<(std::ostream &os, const T &a) {
+  return print_seq(a, ", ", "", (os << "{")) << "}";
+}
+template <typename T, typename U>
+std::ostream &operator<<(std::ostream &os, const std::pair<T, U> &a) {
+  return os << "(" << a.first << ", " << a.second << ")";
+}
+
+#ifdef ENABLE_DEBUG
+template <typename T>
+void pdebug(const T &value) {
+  std::cerr << value;
+}
+template <typename T, typename... Ts>
+void pdebug(const T &value, const Ts &...args) {
+  pdebug(value);
+  std::cerr << ", ";
+  pdebug(args...);
+}
+#define DEBUG(...)                                   \
+  do {                                               \
+    std::cerr << " \033[33m (L" << __LINE__ << ") "; \
+    std::cerr << #__VA_ARGS__ << ":\033[0m ";        \
+    pdebug(__VA_ARGS__);                             \
+    std::cerr << std::endl;                          \
+  } while (0)
+#else
+#define pdebug(...)
+#define DEBUG(...)
+#endif
+
+using namespace std;
+
+const int L = 1000000;
+const i64 BIG = 1e15;
+using Int = __int128_t;
+
+i64 solve() {
+  i64 n, s;
+  cin >> n >> s;
+  DEBUG(n, s);
+  if (n < s) return -1;
+  if (n == s) return n + 1;
+  for (i64 b = 2; b * b <= n; ++b) {
+    i64 x = n;
+    i64 dsum = 0;
+    while (x) {
+      const auto &[q, r] = std::div(x, b);
+      dsum += r;
+      x = q;
+    }
+    if (dsum == s) return b;
+  }
+
+  i64 ans = BIG;
+  if (s == 1) chmin(ans, n);
+
+  i64 b = n - 1;
+  for (i64 k = 1; b > n / b; ++k) {
+    assert(n / b == k);
+    i64 dsum_min = k + (n % b);
+    i64 q = n / (k + 1);
+    i64 r = n % (q + 1);
+    assert(n / (q + 1) == k);
+    i64 dsum_max = k + r;
+    if (dsum_min <= s and s <= dsum_max and (s - dsum_min) % k == 0) {
+      i64 d = (s - dsum_min) / k;
+      chmin(ans, b - d);
+    }
+    b = q;
+  }
+  return (ans == BIG) ? -1 : ans;
+}
+
+int main() {
+  ios_base::sync_with_stdio(false), cin.tie(nullptr);
+  cout << solve() << "\n";
+}
