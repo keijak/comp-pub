bb327ba840347313b8bedb539dc3bd39008cb65a
keijak <m@keijak.com>
2021-03-27 08:45:07 +0100

    yukicoder

---
+++ yukicoder/1441/main.cpp
@@ -0,0 +1,341 @@
+#include <bits/stdc++.h>
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tag_and_trait.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+template <class Key>
+using rb_tree_set =
+    __gnu_pbds::tree<Key, __gnu_pbds::null_type, std::less<Key>,
+                     __gnu_pbds::rb_tree_tag,
+                     __gnu_pbds::tree_order_statistics_node_update>;
+
+#define REP_(i, a_, b_, a, b, ...) \
+  for (int i = (a), _Z_##i = (b); i < _Z_##i; ++i)
+#define REP(i, ...) REP_(i, __VA_ARGS__, __VA_ARGS__, 0, __VA_ARGS__)
+#define ALL(x) std::begin(x), std::end(x)
+using i64 = long long;
+using u64 = unsigned long long;
+
+template <typename T, typename U>
+inline bool chmax(T &a, U b) {
+  return a < b and ((a = std::move(b)), true);
+}
+template <typename T, typename U>
+inline bool chmin(T &a, U b) {
+  return a > b and ((a = std::move(b)), true);
+}
+template <typename T>
+inline int ssize(const T &a) {
+  return (int)std::size(a);
+}
+
+template <typename T>
+std::istream &operator>>(std::istream &is, std::vector<T> &a) {
+  for (auto &x : a) is >> x;
+  return is;
+}
+template <typename Container>
+std::ostream &print_seq(const Container &a, std::string_view sep = " ",
+                        std::string_view ends = "\n",
+                        std::ostream &os = std::cout) {
+  auto b = std::begin(a), e = std::end(a);
+  for (auto it = std::begin(a); it != e; ++it) {
+    if (it != b) os << sep;
+    os << *it;
+  }
+  return os << ends;
+}
+template <typename T, typename = void>
+struct is_iterable : std::false_type {};
+template <typename T>
+struct is_iterable<T, std::void_t<decltype(std::begin(std::declval<T>())),
+                                  decltype(std::end(std::declval<T>()))>>
+    : std::true_type {};
+
+template <typename T, typename = std::enable_if_t<
+                          is_iterable<T>::value &&
+                          !std::is_same<T, std::string_view>::value &&
+                          !std::is_same<T, std::string>::value>>
+std::ostream &operator<<(std::ostream &os, const T &a) {
+  return print_seq(a, ", ", "", (os << "{")) << "}";
+}
+template <typename T, typename U>
+std::ostream &operator<<(std::ostream &os, const std::pair<T, U> &a) {
+  return os << "(" << a.first << ", " << a.second << ")";
+}
+
+#ifdef ENABLE_DEBUG
+template <typename T>
+void pdebug(const T &value) {
+  std::cerr << value;
+}
+template <typename T, typename... Ts>
+void pdebug(const T &value, const Ts &...args) {
+  pdebug(value);
+  std::cerr << ", ";
+  pdebug(args...);
+}
+#define DEBUG(...)                                   \
+  do {                                               \
+    std::cerr << " \033[33m (L" << __LINE__ << ") "; \
+    std::cerr << #__VA_ARGS__ << ":\033[0m ";        \
+    pdebug(__VA_ARGS__);                             \
+    std::cerr << std::endl;                          \
+  } while (0)
+#else
+#define pdebug(...)
+#define DEBUG(...)
+#endif
+
+using namespace std;
+
+template <typename LazyMonoid>
+struct LazySegTree {
+  using T = typename LazyMonoid::T;
+  using F = typename LazyMonoid::F;
+
+  inline int n() const { return n_; }
+  inline int offset() const { return offset_; }
+
+  explicit LazySegTree(int n)
+      : LazySegTree(std::vector<T>(n, LazyMonoid::id())) {}
+
+  explicit LazySegTree(const std::vector<T> &v) : n_(int(v.size())) {
+    offset_ = 1;
+    for (bits_ = 0; offset_ < n_; ++bits_) {
+      offset_ <<= 1;
+    }
+    data_.assign(2 * offset_, LazyMonoid::id());
+    lazy_ = std::vector<F>(offset_, LazyMonoid::f_id());
+    for (int i = 0; i < n_; i++) data_[offset_ + i] = v[i];
+    for (int i = offset_ - 1; i >= 1; i--) {
+      update(i);
+    }
+  }
+
+  void set(int p, T x) {
+    assert(0 <= p && p < n_);
+    p += offset_;
+    // Update the leaf.
+    for (int i = bits_; i >= 1; i--) push(p >> i);
+    data_[p] = x;
+    // Update its ancestors.
+    for (int i = 1; i <= bits_; i++) update(p >> i);
+  }
+
+  const T &operator[](int p) const {
+    assert(0 <= p && p < n_);
+    p += offset_;
+    for (int i = bits_; i >= 1; i--) push(p >> i);
+    return data_[p];
+  }
+
+  T fold(int l, int r) const {
+    assert(0 <= l && l <= r && r <= n_);
+    if (l == r) return LazyMonoid::id();
+
+    l += offset_;
+    r += offset_;
+
+    for (int i = bits_; i >= 1; i--) {
+      if (((l >> i) << i) != l) push(l >> i);
+      if (((r >> i) << i) != r) push(r >> i);
+    }
+
+    T sml = LazyMonoid::id(), smr = LazyMonoid::id();
+    while (l < r) {
+      if (l & 1) sml = LazyMonoid::op(sml, data_[l++]);
+      if (r & 1) smr = LazyMonoid::op(data_[--r], smr);
+      l >>= 1;
+      r >>= 1;
+    }
+
+    return LazyMonoid::op(sml, smr);
+  }
+
+  T fold_all() const { return data_[1]; }
+
+  void apply(int p, F f) {
+    assert(0 <= p && p < n_);
+    p += offset_;
+    for (int i = bits_; i >= 1; i--) push(p >> i);
+    data_[p] = LazyMonoid::apply(f, data_[p]);
+    for (int i = 1; i <= bits_; i++) update(p >> i);
+  }
+  void apply(int l, int r, F f) {
+    assert(0 <= l && l <= r && r <= n_);
+    if (l == r) return;
+
+    l += offset_;
+    r += offset_;
+
+    for (int i = bits_; i >= 1; i--) {
+      if (((l >> i) << i) != l) push(l >> i);
+      if (((r >> i) << i) != r) push((r - 1) >> i);
+    }
+
+    {
+      int l2 = l, r2 = r;
+      while (l < r) {
+        if (l & 1) apply_all(l++, f);
+        if (r & 1) apply_all(--r, f);
+        l >>= 1;
+        r >>= 1;
+      }
+      l = l2;
+      r = r2;
+    }
+
+    for (int i = 1; i <= bits_; i++) {
+      if (((l >> i) << i) != l) update(l >> i);
+      if (((r >> i) << i) != r) update((r - 1) >> i);
+    }
+  }
+
+  template <bool (*g)(T)>
+  int max_right(int l) const {
+    return max_right(l, [](T x) { return g(x); });
+  }
+  template <class G>
+  int max_right(int l, G g) const {
+    assert(0 <= l && l <= n_);
+    assert(g(LazyMonoid::id()));
+    if (l == n_) return n_;
+    l += offset_;
+    for (int i = bits_; i >= 1; i--) push(l >> i);
+    T sm = LazyMonoid::id();
+    do {
+      while (l % 2 == 0) l >>= 1;
+      if (!g(LazyMonoid::op(sm, data_[l]))) {
+        while (l < offset_) {
+          push(l);
+          l = (2 * l);
+          if (g(LazyMonoid::op(sm, data_[l]))) {
+            sm = LazyMonoid::op(sm, data_[l]);
+            l++;
+          }
+        }
+        return l - offset_;
+      }
+      sm = LazyMonoid::op(sm, data_[l]);
+      l++;
+    } while ((l & -l) != l);
+    return n_;
+  }
+
+  template <bool (*g)(T)>
+  int min_left(int r) const {
+    return min_left(r, [](T x) { return g(x); });
+  }
+  template <class G>
+  int min_left(int r, G g) const {
+    assert(0 <= r && r <= n_);
+    assert(g(LazyMonoid::id()));
+    if (r == 0) return 0;
+    r += offset_;
+    for (int i = bits_; i >= 1; i--) push((r - 1) >> i);
+    T sm = LazyMonoid::id();
+    do {
+      r--;
+      while (r > 1 && (r % 2)) r >>= 1;
+      if (!g(LazyMonoid::op(data_[r], sm))) {
+        while (r < offset_) {
+          push(r);
+          r = (2 * r + 1);
+          if (g(LazyMonoid::op(data_[r], sm))) {
+            sm = LazyMonoid::op(data_[r], sm);
+            r--;
+          }
+        }
+        return r + 1 - offset_;
+      }
+      sm = LazyMonoid::op(data_[r], sm);
+    } while ((r & -r) != r);
+    return 0;
+  }
+
+  friend std::ostream &operator<<(std::ostream &os, const LazySegTree &st) {
+    os << "[";
+    for (int i = 0; i < st.n(); ++i) {
+      if (i != 0) os << ", ";
+      const auto &x = st[i];
+      os << x;
+    }
+    return os << "]";
+  }
+
+ private:
+  void update(int k) {
+    data_[k] = LazyMonoid::op(data_[2 * k], data_[2 * k + 1]);
+  }
+  void apply_all(int k, F f) const {
+    data_[k] = LazyMonoid::f_apply(f, data_[k]);
+    if (k < offset_) lazy_[k] = LazyMonoid::f_compose(f, lazy_[k]);
+  }
+  void push(int k) const {
+    apply_all(2 * k, lazy_[k]);
+    apply_all(2 * k + 1, lazy_[k]);
+    lazy_[k] = LazyMonoid::f_id();
+  }
+
+  int n_, offset_, bits_;
+  mutable std::vector<T> data_;
+  mutable std::vector<F> lazy_;
+};
+
+struct AssignSum {
+  struct T {
+    long long sum;
+    int width;
+  };
+  using F = std::optional<long long>;
+
+  // Fold: Sum
+  static T op(const T &x, const T &y) {
+    return {x.sum + y.sum, x.width + y.width};
+  }
+  static constexpr T id() { return {0, 0}; }
+
+  // Update: Assign
+  static T f_apply(const F &f, const T &x) {
+    return f ? T{(*f) * x.width, x.width} : x;
+  }
+  static F f_compose(const F &f, const F &g) { return f ? f : g; }
+  static constexpr F f_id() { return std::nullopt; }
+};
+
+int main() {
+  ios_base::sync_with_stdio(false), cin.tie(nullptr);
+  int n, q;
+  cin >> n >> q;
+  vector<i64> a(n);
+  cin >> a;
+
+  vector<AssignSum::T> b(n);
+  REP(i, n) { b[i] = {a[i], 1}; }
+  LazySegTree<AssignSum> seg(move(b));
+
+  rb_tree_set<int> index;
+  REP(i, n) index.insert(i);
+  index.insert(n);
+
+  REP(qi, q) {
+    int t, l, r;
+    cin >> t >> l >> r;
+    --l;
+    auto pl = index.find_by_order(l);
+    auto pr = index.find_by_order(r);
+    auto res = seg.fold(*pl, *pr);
+    if (t == 2) {
+      cout << res.sum << "\n";
+      continue;
+    }
+    if (*pl != *pr) {
+      seg.apply(*pl, *pr, 0LL);
+      seg.set(*pl, {res.sum, 1LL});
+      for (++pl; pl != pr;) {
+        pl = index.erase(pl);
+      }
+    }
+  }
+}
---
+++ yukicoder/1442/main.cpp
@@ -0,0 +1,319 @@
+#include <bits/stdc++.h>
+#define REP_(i, a_, b_, a, b, ...) \
+  for (int i = (a), _Z_##i = (b); i < _Z_##i; ++i)
+#define REP(i, ...) REP_(i, __VA_ARGS__, __VA_ARGS__, 0, __VA_ARGS__)
+#define ALL(x) std::begin(x), std::end(x)
+using i64 = long long;
+using u64 = unsigned long long;
+
+template <typename T, typename U>
+inline bool chmax(T &a, U b) {
+  return a < b and ((a = std::move(b)), true);
+}
+template <typename T, typename U>
+inline bool chmin(T &a, U b) {
+  return a > b and ((a = std::move(b)), true);
+}
+template <typename T>
+inline int ssize(const T &a) {
+  return (int)std::size(a);
+}
+
+template <typename T>
+std::istream &operator>>(std::istream &is, std::vector<T> &a) {
+  for (auto &x : a) is >> x;
+  return is;
+}
+template <typename Container>
+std::ostream &print_seq(const Container &a, std::string_view sep = " ",
+                        std::string_view ends = "\n",
+                        std::ostream &os = std::cout) {
+  auto b = std::begin(a), e = std::end(a);
+  for (auto it = std::begin(a); it != e; ++it) {
+    if (it != b) os << sep;
+    os << *it;
+  }
+  return os << ends;
+}
+template <typename T, typename = void>
+struct is_iterable : std::false_type {};
+template <typename T>
+struct is_iterable<T, std::void_t<decltype(std::begin(std::declval<T>())),
+                                  decltype(std::end(std::declval<T>()))>>
+    : std::true_type {};
+
+template <typename T, typename = std::enable_if_t<
+                          is_iterable<T>::value &&
+                          !std::is_same<T, std::string_view>::value &&
+                          !std::is_same<T, std::string>::value>>
+std::ostream &operator<<(std::ostream &os, const T &a) {
+  return print_seq(a, ", ", "", (os << "{")) << "}";
+}
+template <typename T, typename U>
+std::ostream &operator<<(std::ostream &os, const std::pair<T, U> &a) {
+  return os << "(" << a.first << ", " << a.second << ")";
+}
+
+#ifdef ENABLE_DEBUG
+template <typename T>
+void pdebug(const T &value) {
+  std::cerr << value;
+}
+template <typename T, typename... Ts>
+void pdebug(const T &value, const Ts &...args) {
+  pdebug(value);
+  std::cerr << ", ";
+  pdebug(args...);
+}
+#define DEBUG(...)                                   \
+  do {                                               \
+    std::cerr << " \033[33m (L" << __LINE__ << ") "; \
+    std::cerr << #__VA_ARGS__ << ":\033[0m ";        \
+    pdebug(__VA_ARGS__);                             \
+    std::cerr << std::endl;                          \
+  } while (0)
+#else
+#define pdebug(...)
+#define DEBUG(...)
+#endif
+
+using namespace std;
+
+const i64 BIG = 1e16;
+
+template <class T>
+using MinHeap = priority_queue<T, vector<T>, greater<T>>;
+
+struct Edge {
+  i64 cost;
+  int to;
+};
+
+struct State {
+  i64 cost;
+  int node;
+};
+bool operator>(const State &x, const State &y) { return x.cost > y.cost; }
+
+// Returns min distance from the source node to each node (if exists).
+auto search(const vector<vector<Edge>> &g, int source) {
+  const int n = g.size();
+  vector mincost(n, BIG);
+  MinHeap<State> que;
+  auto push = [&](i64 cost, int node) -> bool {
+    if (chmin(mincost[node], cost)) {
+      que.push({cost, node});
+      return true;
+    }
+    return false;
+  };
+  assert(push(0LL, source));
+
+  while (not que.empty()) {
+    State cur = move(que.top());
+    que.pop();
+    if (cur.cost != mincost[cur.node]) {
+      continue;
+    }
+    for (const auto &e : g[cur.node]) {
+      i64 new_cost = cur.cost + e.cost;
+      push(new_cost, e.to);
+    }
+  }
+  return mincost;
+}
+
+// Heavy-Light Decomposition
+struct HLD {
+  using NodeID = int;                       // [0, n)
+  using G = std::vector<std::vector<int>>;  // undirected graph
+  // half-open intervals of preorder indices of nodes.
+  using IntervalVec = std::vector<std::pair<int, int>>;
+
+  int n;                                      // number of nodes in the tree
+  NodeID root;                                // root of the tree
+  G child;                                    // children node ids
+  std::vector<std::optional<NodeID>> parent;  // parent node id (or -1)
+  std::vector<int> subsize;                   // subtree size
+  // "ord" is preorder index in DFS traversal. [0, n)
+  std::vector<int> node_to_ord;     // node id to preorder index
+  std::vector<NodeID> ord_to_node;  // preorder index to node id
+  std::vector<NodeID> comp_root;    // node id to its heavy path component
+
+  explicit HLD(G g, NodeID root = 0)
+      : n(int(g.size())),
+        root(root),
+        child(g),
+        parent(n, -1),
+        subsize(n, 1),
+        node_to_ord(n, -1),
+        ord_to_node(n, -1),
+        comp_root(n, -1) {
+    dfs_subsize(root);
+    int counter = 0;
+    comp_root[root] = root;
+    dfs_hld(root, counter);
+  }
+
+  // Lowest Common Ancestor
+  NodeID lca(NodeID u, NodeID v) const {
+    for (;;) {
+      if (node_to_ord[u] > node_to_ord[v]) std::swap(u, v);
+      NodeID crv = comp_root[v];
+      if (comp_root[u] == crv) return u;
+      assert(parent[crv].has_value());
+      v = parent[crv].value();
+    }
+  }
+
+  // Returns the set of nodes on the u-v path, including both u and v.
+  //
+  // The return value is half-open intervals of the preorder indices of the
+  // nodes. One interval corresponds to one heavy path component.
+  IntervalVec node_ranges_on_path(NodeID u, NodeID v) const {
+    IntervalVec res;
+    for (;;) {
+      if (node_to_ord[u] > node_to_ord[v]) std::swap(u, v);
+      NodeID crv = comp_root[v];
+      res.emplace_back(std::max(node_to_ord[crv], node_to_ord[u]),
+                       node_to_ord[v] + 1);
+      if (comp_root[u] == crv) break;
+      assert(parent[crv].has_value());
+      v = parent[crv].value();
+    }
+    return res;
+  }
+
+  // Returns the set of edges in the u-v path.
+  //
+  // The return value is half-open intervals of the preorder indices of nodes
+  // corresponding to the deeper end (closer to leaves) of each edge in the
+  // path. Here we identify Edge(v, parent[v]) as v.
+  IntervalVec edge_ranges_on_path(NodeID u, NodeID v) const {
+    IntervalVec res;
+    for (;;) {
+      if (node_to_ord[u] > node_to_ord[v]) std::swap(u, v);
+      NodeID crv = comp_root[v];
+      if (comp_root[u] == crv) {
+        if (u != v) res.emplace_back(node_to_ord[u] + 1, node_to_ord[v] + 1);
+        break;
+      }
+      res.emplace_back(node_to_ord[crv], node_to_ord[v] + 1);
+      assert(parent[crv].has_value());
+      v = parent[crv].value();
+    }
+    return res;
+  }
+
+  // Distance (= number of edges) of the path between two nodes.
+  int distance(NodeID u, NodeID v) const {
+    int dist = 0;
+    for (auto [l, r] : edge_ranges_on_path(u, v)) {
+      dist += r - l;
+    }
+    return dist;
+  }
+
+ private:
+  // Fills `parent` and `subsize` and drops parent node ids from `child`.
+  void dfs_subsize(NodeID v) {
+    auto &edges = child[v];
+    if (parent[v].has_value()) {
+      auto it = std::find(edges.begin(), edges.end(), parent[v].value());
+      if (it != edges.end()) edges.erase(it);
+    }
+    for (NodeID &u : edges) {
+      parent[u] = v;
+      dfs_subsize(u);
+      subsize[v] += subsize[u];
+      if (subsize[u] > subsize[edges[0]]) {
+        std::swap(u, edges[0]);
+      }
+    }
+  }
+
+  // Fills `node_to_ord`, `ord_to_node`, and `comp_root`.
+  void dfs_hld(NodeID v, int &counter) {
+    node_to_ord[v] = counter++;
+    ord_to_node[node_to_ord[v]] = v;
+    for (NodeID u : child[v]) {
+      comp_root[u] = (u == child[v][0] ? comp_root[v] : u);
+      dfs_hld(u, counter);
+    }
+  }
+};
+
+int main() {
+  ios_base::sync_with_stdio(false), cin.tie(nullptr);
+
+  int n, k;
+  cin >> n >> k;
+  vector<vector<Edge>> g(n + k);
+  vector<vector<int>> g2(n);
+  map<pair<int, int>, i64> edge_to_cost;
+  REP(i, n - 1) {
+    int a, b;
+    i64 c;
+    cin >> a >> b >> c;
+    --a, --b;
+    g[a].push_back({c, b});
+    g[b].push_back({c, a});
+    g2[a].push_back(b);
+    g2[b].push_back(a);
+    edge_to_cost[pair{min(a, b), max(a, b)}] = c;
+  }
+
+  vector<i64> flight_cost(k);
+  vector<vector<i64>> mincost_table(k);
+  REP(i, k) {
+    i64 m, p;
+    cin >> m >> p;
+    flight_cost[i] = p;
+    int sup = n + i;
+    REP(j, m) {
+      int x;
+      cin >> x;
+      --x;
+      g[sup].push_back({0, x});
+      g[x].push_back({p, sup});
+    }
+  }
+  REP(i, k) {
+    int sup = n + i;
+    i64 p = flight_cost[i];
+    mincost_table[i] = search(g, sup);
+  }
+  HLD hld(g2);
+  vector<i64> base_cost(n), acc(n + 1, 0LL);
+  REP(i, n) {
+    auto p = hld.parent[i];
+    if (not p.has_value()) continue;
+    i64 c = edge_to_cost[pair{min(i, *p), max(i, *p)}];
+    int j = hld.node_to_ord[i];
+    base_cost[j] = c;
+  }
+  REP(i, n) acc[i + 1] = acc[i] + base_cost[i];
+
+  int q;
+  cin >> q;
+  REP(qi, q) {
+    int u, v;
+    cin >> u >> v;
+    --u, --v;
+
+    i64 ans = BIG;
+    REP(i, k) {
+      i64 cu = mincost_table[i][u];
+      i64 cv = mincost_table[i][v];
+      chmin(ans, cu + cv + flight_cost[i]);
+    }
+    {
+      i64 cost = 0;
+      for (auto [l, r] : hld.edge_ranges_on_path(u, v)) {
+        cost += acc[r] - acc[l];
+      }
+      chmin(ans, cost);
+    }
+    cout << ans << "\n";
+  }
+}
