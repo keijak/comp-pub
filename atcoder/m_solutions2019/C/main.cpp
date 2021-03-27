619d149ad3d052fd7c3cfc0f09ff904c5d74f8bf
Keiji Maekawa <m@keijak.com>
2021-03-27 12:54:17 +0100

    probability

---
+++ atcoder/m_solutions2019/C/main.cpp
@@ -0,0 +1,148 @@
+#include <bits/stdc++.h>
+#define REP_(i, a_, b_, a, b, ...) \
+  for (int i = (a), _Z_##i = (b); i < _Z_##i; ++i)
+#define REP(i, ...) REP_(i, __VA_ARGS__, __VA_ARGS__, 0, __VA_ARGS__)
+#define ALL(x) std::begin(x), std::end(x)
+using i64 = long long;
+using u64 = unsigned long long;
+
+template <typename T, typename U>
+inline bool chmax(T &a, U b) {
+  return a < b and ((a = std::move(b)), true);
+}
+template <typename T, typename U>
+inline bool chmin(T &a, U b) {
+  return a > b and ((a = std::move(b)), true);
+}
+template <typename T>
+inline int ssize(const T &a) {
+  return (int)std::size(a);
+}
+
+template <typename T>
+std::istream &operator>>(std::istream &is, std::vector<T> &a) {
+  for (auto &x : a) is >> x;
+  return is;
+}
+template <typename Container>
+std::ostream &print_seq(const Container &a, std::string_view sep = " ",
+                        std::string_view ends = "\n",
+                        std::ostream &os = std::cout) {
+  auto b = std::begin(a), e = std::end(a);
+  for (auto it = std::begin(a); it != e; ++it) {
+    if (it != b) os << sep;
+    os << *it;
+  }
+  return os << ends;
+}
+template <typename T, typename = void>
+struct is_iterable : std::false_type {};
+template <typename T>
+struct is_iterable<T, std::void_t<decltype(std::begin(std::declval<T>())),
+                                  decltype(std::end(std::declval<T>()))>>
+    : std::true_type {};
+
+template <typename T, typename = std::enable_if_t<
+                          is_iterable<T>::value &&
+                          !std::is_same<T, std::string_view>::value &&
+                          !std::is_same<T, std::string>::value>>
+std::ostream &operator<<(std::ostream &os, const T &a) {
+  return print_seq(a, ", ", "", (os << "{")) << "}";
+}
+template <typename T, typename U>
+std::ostream &operator<<(std::ostream &os, const std::pair<T, U> &a) {
+  return os << "(" << a.first << ", " << a.second << ")";
+}
+
+#ifdef ENABLE_DEBUG
+template <typename T>
+void pdebug(const T &value) {
+  std::cerr << value;
+}
+template <typename T, typename... Ts>
+void pdebug(const T &value, const Ts &...args) {
+  pdebug(value);
+  std::cerr << ", ";
+  pdebug(args...);
+}
+#define DEBUG(...)                                   \
+  do {                                               \
+    std::cerr << " \033[33m (L" << __LINE__ << ") "; \
+    std::cerr << #__VA_ARGS__ << ":\033[0m ";        \
+    pdebug(__VA_ARGS__);                             \
+    std::cerr << std::endl;                          \
+  } while (0)
+#else
+#define pdebug(...)
+#define DEBUG(...)
+#endif
+
+#include <atcoder/modint>
+using Mint = atcoder::modint1000000007;
+std::ostream &operator<<(std::ostream &os, const Mint &m) {
+  return os << m.val();
+}
+
+// using Mint = long double;
+
+template <class T = Mint>
+struct Factorials {
+  // factorials and inverse factorials.
+  std::vector<T> fact, ifact;
+
+  // n: max cached value.
+  Factorials(int n) : fact(n + 1), ifact(n + 1) {
+    assert(n > 0 and n < T::mod());
+    fact[0] = 1;
+    for (int i = 1; i <= n; ++i) fact[i] = fact[i - 1] * i;
+    ifact[n] = fact[n].inv();
+    for (int i = n; i >= 1; --i) ifact[i - 1] = ifact[i] * i;
+  }
+
+  // Combination (nCk)
+  T C(int n, int k) const {
+    if (k < 0 || k > n) return 0;
+    return fact[n] * ifact[k] * ifact[n - k];
+  }
+};
+
+// // nCk where n can be large but min(k, n-k) is small.
+// template <typename T = Mint>
+// T comb(i64 n, i64 k) {
+//   if (k < 0 or k > n) return 0;
+//   if (k > n - k) k = n - k;
+//   T nume = 1, deno = 1;
+//   for (i64 i = 0; i < k; ++i) {
+//     nume *= T(n - i);
+//     deno *= T(i + 1);
+//   }
+//   return nume / deno;
+// }
+
+using namespace std;
+
+Mint solve() {
+  int n, a, b, c;
+  cin >> n >> a >> b >> c;
+  if (a < b) swap(a, b);
+  Factorials fs(2 * n);
+
+  Mint pa = Mint(a) / (a + b);
+  Mint pb = Mint(b) / (a + b);
+  Mint pan = pa.pow(n), pbn = pb.pow(n);
+  Mint pak = 1, pbk = 1;
+  Mint expected = 0;
+  for (int k = n; k <= 2 * n - 1; ++k) {
+    auto co = fs.C(k - 1, n - 1) * k;
+    expected += co * (pbk * pan + pak * pbn);
+    pbk *= pb;
+    pak *= pa;
+  }
+  Mint one_step = Mint(100) / (100 - c);
+  return expected * one_step;
+}
+
+int main() {
+  ios_base::sync_with_stdio(false), cin.tie(nullptr);
+  cout << solve() << "\n";
+}
