530689f780d28f03751a079bf24e1a9ad5787c2d
Keiji Maekawa <m@keijak.com>
2021-04-12 01:10:27 +0200

    cf div2 virtual

---
+++ cf/contest/1513/D/main.cpp
@@ -0,0 +1,165 @@
+#include <bits/stdc++.h>
+#define REP_(i, a_, b_, a, b, ...) \
+  for (int i = (a), _Z_##i = (b); i < _Z_##i; ++i)
+#define REP(i, ...) REP_(i, __VA_ARGS__, __VA_ARGS__, 0, __VA_ARGS__)
+#define ALL(x) std::begin(x), std::end(x)
+using i64 = long long;
+using u64 = unsigned long long;
+
+template <typename T, typename U>
+inline bool chmax(T &a, U b) {
+  return a < b and ((a = std::move(b)), true);
+}
+template <typename T, typename U>
+inline bool chmin(T &a, U b) {
+  return a > b and ((a = std::move(b)), true);
+}
+template <typename T>
+inline int ssize(const T &a) {
+  return (int)std::size(a);
+}
+
+template <typename T>
+std::istream &operator>>(std::istream &is, std::vector<T> &a) {
+  for (auto &x : a) is >> x;
+  return is;
+}
+template <typename Container>
+std::ostream &print_seq(const Container &a, std::string_view sep = " ",
+                        std::string_view ends = "\n",
+                        std::ostream &os = std::cout) {
+  auto b = std::begin(a), e = std::end(a);
+  for (auto it = std::begin(a); it != e; ++it) {
+    if (it != b) os << sep;
+    os << *it;
+  }
+  return os << ends;
+}
+template <typename T, typename = void>
+struct is_iterable : std::false_type {};
+template <typename T>
+struct is_iterable<T, std::void_t<decltype(std::begin(std::declval<T>())),
+                                  decltype(std::end(std::declval<T>()))>>
+    : std::true_type {};
+
+template <typename T, typename = std::enable_if_t<
+                          is_iterable<T>::value &&
+                          !std::is_same<T, std::string_view>::value &&
+                          !std::is_same<T, std::string>::value>>
+std::ostream &operator<<(std::ostream &os, const T &a) {
+  return print_seq(a, ", ", "", (os << "{")) << "}";
+}
+template <typename T, typename U>
+std::ostream &operator<<(std::ostream &os, const std::pair<T, U> &a) {
+  return os << "(" << a.first << ", " << a.second << ")";
+}
+
+#ifdef ENABLE_DEBUG
+template <typename T>
+void pdebug(const T &value) {
+  std::cerr << value;
+}
+template <typename T, typename... Ts>
+void pdebug(const T &value, const Ts &...args) {
+  pdebug(value);
+  std::cerr << ", ";
+  pdebug(args...);
+}
+#define DEBUG(...)                                   \
+  do {                                               \
+    std::cerr << " \033[33m (L" << __LINE__ << ") "; \
+    std::cerr << #__VA_ARGS__ << ":\033[0m ";        \
+    pdebug(__VA_ARGS__);                             \
+    std::cerr << std::endl;                          \
+  } while (0)
+#else
+#define pdebug(...)
+#define DEBUG(...)
+#endif
+
+using namespace std;
+
+struct UnionFind {
+  int n;
+  mutable std::vector<int> parent;  // positive: parent, negative: size
+  int num_roots;
+
+  explicit UnionFind(int sz) : n(sz), parent(sz, -1), num_roots(sz) {}
+
+  bool unite(int x, int y) {
+    x = find(x), y = find(y);
+    if (x == y) return false;
+    if (-parent[x] < -parent[y]) std::swap(x, y);
+    parent[x] += parent[y];
+    parent[y] = x;
+    --num_roots;
+    return true;
+  }
+
+  int find(int v) const {
+    if (parent[v] < 0) return v;
+    return parent[v] = find(parent[v]);
+  }
+
+  int size(int v) const { return -parent[find(v)]; }
+
+  bool same(int x, int y) const { return find(x) == find(y); }
+
+  std::vector<int> roots() const {
+    std::vector<int> res;
+    res.reserve(num_roots);
+    for (int i = 0; i < n; ++i) {
+      if (parent[i] < 0) res.push_back(i);
+    }
+    return res;
+  }
+};
+
+i64 solve() {
+  i64 n, p;
+  cin >> n >> p;
+  vector<i64> a(n);
+  cin >> a;
+  vector<pair<i64, int>> sa(n);
+  REP(i, n) { sa[i] = {a[i], i}; }
+  sort(ALL(sa));
+
+  UnionFind uf(n);
+  i64 cost = 0;
+  REP(i, n) {
+    auto [x, pos] = sa[i];
+    if (x >= p) break;
+    if (uf.size(pos) > 1) continue;
+    int l = pos, r = pos;
+    while (l - 1 >= 0 and a[l - 1] % x == 0) {
+      --l;
+      DEBUG(pos, l);
+      cost += x;
+      int lsz = uf.size(l);
+      uf.unite(pos, l);
+      if (lsz > 1) break;
+    }
+    while (r + 1 < n and a[r + 1] % x == 0) {
+      ++r;
+      DEBUG(pos, r);
+      cost += x;
+      int rsz = uf.size(r);
+      uf.unite(pos, r);
+      if (rsz > 1) break;
+    }
+  }
+  REP(i, n - 1) {
+    if (uf.same(i, i + 1)) continue;
+    cost += p;
+    uf.unite(i, i + 1);
+  }
+  assert(uf.num_roots == 1);
+  return cost;
+}
+
+int main() {
+  ios_base::sync_with_stdio(false), cin.tie(nullptr);
+  int t;
+  cin >> t;
+  REP(i, t) { cout << solve() << "\n"; }
+}
