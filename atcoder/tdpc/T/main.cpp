739b4af72c8ce4ce24a56483f0d997fd089ac0b5
Keiji Maekawa <m@keijak.com>
2021-04-27 23:38:52 +0200

    tdpc t

--- atcoder/tdpc/T/main.cpp
+++ atcoder/tdpc/T/main.cpp
@@ -1,240 +1,77 @@
 #include <bits/stdc++.h>
 
 #include <atcoder/modint>
-#define REP(i, n) for (int i = 0, REP_N_ = (n); i < REP_N_; ++i)
-#define ALL(x) std::begin(x), std::end(x)
-using i64 = long long;
-using u64 = unsigned long long;
-
-template <class T>
-inline int ssize(const T &a) {
-  return (int)std::size(a);
-}
-template <class T>
-inline bool chmax(T &a, T b) {
-  return a < b and ((a = std::move(b)), true);
-}
-template <class T>
-inline bool chmin(T &a, T b) {
-  return a > b and ((a = std::move(b)), true);
-}
-
-template <typename T>
-std::istream &operator>>(std::istream &is, std::vector<T> &a) {
-  for (auto &x : a) is >> x;
-  return is;
-}
-template <typename Container>
-std::ostream &pprint(const Container &a, std::string_view sep = " ",
-                     std::string_view ends = "\n", std::ostream *os = nullptr) {
-  if (os == nullptr) os = &std::cout;
-  auto b = std::begin(a), e = std::end(a);
-  for (auto it = std::begin(a); it != e; ++it) {
-    if (it != b) *os << sep;
-    *os << *it;
-  }
-  return *os << ends;
-}
-template <typename T, typename = void>
-struct is_iterable : std::false_type {};
-template <typename T>
-struct is_iterable<T, std::void_t<decltype(std::begin(std::declval<T>())),
-                                  decltype(std::end(std::declval<T>()))>>
-    : std::true_type {};
-
-template <typename T, typename = std::enable_if_t<
-                          is_iterable<T>::value &&
-                          !std::is_same<T, std::string_view>::value &&
-                          !std::is_same<T, std::string>::value>>
-std::ostream &operator<<(std::ostream &os, const T &a) {
-  return pprint(a, ", ", "", &(os << "{")) << "}";
-}
-template <typename T, typename U>
-std::ostream &operator<<(std::ostream &os, const std::pair<T, U> &a) {
-  return os << "(" << a.first << ", " << a.second << ")";
-}
-
-#ifdef ENABLE_DEBUG
-template <typename T>
-void pdebug(const T &value) {
-  std::cerr << value;
-}
-template <typename T, typename... Ts>
-void pdebug(const T &value, const Ts &...args) {
-  pdebug(value);
-  std::cerr << ", ";
-  pdebug(args...);
-}
-#define DEBUG(...)                                   \
-  do {                                               \
-    std::cerr << " \033[33m (L" << __LINE__ << ") "; \
-    std::cerr << #__VA_ARGS__ << ":\033[0m ";        \
-    pdebug(__VA_ARGS__);                             \
-    std::cerr << std::endl;                          \
-  } while (0)
-#else
-#define pdebug(...)
-#define DEBUG(...)
-#endif
-
-using namespace std;
 using Mint = atcoder::modint1000000007;
 std::ostream &operator<<(std::ostream &os, const Mint &m) {
   return os << m.val();
 }
 
-template <class T = Mint>
-struct Matrix {
-  using Row = vector<T>;
-  vector<Row> A;
+// Computes the n-th term of a linear recurrence sequence.
+template <typename T = Mint>
+struct Kitamasa {
+  const int k;
 
-  Matrix() {}
+  // Initial k elements (a[1] .. a[k]).
+  const std::vector<T> a_;
 
-  Matrix(int n, int m) : A(n, Row(m, 0)) {}
+  // Coefficients of the recurrence.
+  //   a[k+1] = coeff[1]*a[1] + coeff[2]*a[2] + ... + coeff[k]*a[k].
+  const std::vector<T> coeff_;
 
-  Matrix(int n) : A(n, Row(n, 0)){};
+  explicit Kitamasa(std::vector<T> a, std::vector<T> c)
+      : k(a.size()), a_(a), coeff_(std::move(c)) {}
 
-  int height() const { return (int)(A.size()); }
-
-  int width() const { return (int)(A[0].size()); }
-
-  inline const Row &operator[](int k) const { return (A.at(k)); }
-
-  inline Row &operator[](int k) { return (A.at(k)); }
-
-  Matrix transpose() const {
-    int n = height(), m = width();
-    Matrix mat(m, n);
-    for (int i = 0; i < n; ++i) {
-      for (int j = 0; j < m; ++j) {
-        mat[j][i] = (*this)[i][j];
-      }
+  T nth(long long n) const {
+    auto x = nth_coeff(n);
+    T res = 0;
+    for (int i = 0; i < k; ++i) {
+      res += x[i] * a_[i];
     }
-    return mat;
+    return res;
   }
 
-  static Matrix I(int n) {
-    Matrix mat(n);
-    for (int i = 0; i < n; i++) mat[i][i] = 1;
-    return mat;
-  }
-
-  static Matrix row(vector<T> v) {
-    Matrix mat(1, v.size());
-    mat[0] = std::move(v);
-    return mat;
-  }
-
-  static Matrix column(const vector<T> &v) {
-    const int n = v.size();
-    Matrix mat(n, 1);
-    for (int i = 0; i < n; ++i) mat[i][0] = v[i];
-    return mat;
-  }
-
-  Matrix &operator+=(const Matrix &B) {
-    size_t n = height(), m = width();
-    assert(n == B.height() && m == B.width());
-    for (int i = 0; i < n; i++)
-      for (int j = 0; j < m; j++) (*this)[i][j] += B[i][j];
-    return (*this);
-  }
-
-  Matrix &operator-=(const Matrix &B) {
-    size_t n = height(), m = width();
-    assert(n == B.height() && m == B.width());
-    for (int i = 0; i < n; i++)
-      for (int j = 0; j < m; j++) (*this)[i][j] -= B[i][j];
-    return (*this);
-  }
-
-  Matrix &operator*=(const Matrix &B) {
-    int n = height(), m = B.width(), p = width();
-    assert(p == B.height());
-    vector<Row> C(n, Row(m, 0));
-    for (int i = 0; i < n; i++)
-      for (int j = 0; j < m; j++)
-        for (int k = 0; k < p; k++)
-          C[i][j] = (C[i][j] + (*this)[i][k] * B[k][j]);
-    A.swap(C);
-    return (*this);
-  }
-
-  Matrix &operator^=(long long k) {
-    Matrix B = Matrix::I(height());
-    while (k > 0) {
-      if (k & 1) B *= *this;
-      *this *= *this;
-      k >>= 1LL;
+ private:
+  // n: 1-indexed.
+  std::vector<T> nth_coeff(long long n) const {
+    if (n <= k) {
+      std::vector<T> res(k, 0);
+      res[n - 1] = a_[n - 1];
+      return res;
     }
-    A.swap(B.A);
-    return (*this);
-  }
-
-  Matrix operator+(const Matrix &B) const { return (Matrix(*this) += B); }
-
-  Matrix operator-(const Matrix &B) const { return (Matrix(*this) -= B); }
-
-  Matrix operator*(const Matrix &B) const { return (Matrix(*this) *= B); }
-
-  Matrix operator^(const long long k) const { return (Matrix(*this) ^= k); }
-
-  Matrix pow(const long long k) const { return (*this) ^ k; }
-
-  friend ostream &operator<<(ostream &os, const Matrix &p) {
-    int n = p.height();
-    for (int i = 0; i < n; i++) {
-      os << p[i] << "\n";
-    }
-    return os;
-  }
-
-  T det() {
-    Matrix B(*this);
-    assert(width() == height());
-    T ret = 1;
-    for (int i = 0; i < width(); i++) {
-      int idx = -1;
-      for (int j = i; j < width(); j++) {
-        if (B[j][i] != 0) idx = j;
-      }
-      if (idx == -1) return (0);
-      if (i != idx) {
-        ret *= -1;
-        swap(B[i], B[idx]);
-      }
-      ret *= B[i][i];
-      T vv = B[i][i];
-      for (int j = 0; j < width(); j++) {
-        B[i][j] /= vv;
-      }
-      for (int j = i + 1; j < width(); j++) {
-        T a = B[j][i];
-        for (int k = 0; k < width(); k++) {
-          B[j][k] -= B[i][k] * a;
+    if (n & 1) {
+      return next(nth_coeff(n - 1));
+    } else {
+      const int h = n / 2;
+      std::vector<T> s = nth_coeff(h);
+      std::vector<T> t = s;
+      std::vector<T> res(k, 0);
+      for (int i = 0; i < k; ++i) {
+        t = next(std::move(t));
+        for (int j = 0; j < k; ++j) {
+          res[j] += t[j] * s[i];
         }
       }
+      return res;
     }
-    return ret;
+  }
+
+  std::vector<T> next(std::vector<T> x) const {
+    std::vector<T> res(k, 0);
+    for (int i = 1; i < k; ++i) {
+      res[i] = x[i - 1];
+    }
+    for (int i = 0; i < k; ++i) {
+      res[i] += x[k - 1] * coeff_[i];
+    }
+    return res;
   }
 };
 
-i64 solve() {
-  int k, n;
-  cin >> k >> n;
-
-  if (n <= k) return 1;
-  vector<Mint> a(k, Mint(1));
-  Matrix<Mint> b = Matrix<Mint>::column(a);
-  Matrix<Mint> m(k);
-  REP(i, k - 1) { m[i][i + 1] = 1; }
-  REP(i, k) { m[k - 1][i] = 1; }
-  m ^= n - k;
-  m *= b;
-  return m[k - 1][0].val();
-}
+using namespace std;
 
 int main() {
-  ios::sync_with_stdio(false), cin.tie(nullptr);
-  cout << solve() << endl;
+  int k, n;
+  cin >> k >> n;
+  Kitamasa<Mint> kitamasa(vector<Mint>(k, 1), vector<Mint>(k, 1));
+  cout << kitamasa.nth(n) << endl;
 }
