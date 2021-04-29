84756c8b4512bb1ff5bf1d282de8c29a2eb6256e
keijak <m@keijak.com>
2021-04-29 11:49:30 +0200

    burn bury

---
+++ aizu/2903/combined.cpp
@@ -0,0 +1,319 @@
+#include <bits/stdc++.h>
+
+
+#include <algorithm>
+#include <cassert>
+#include <limits>
+#include <queue>
+#include <vector>
+
+
+#include <vector>
+
+namespace atcoder {
+
+namespace internal {
+
+template <class T> struct simple_queue {
+    std::vector<T> payload;
+    int pos = 0;
+    void reserve(int n) { payload.reserve(n); }
+    int size() const { return int(payload.size()) - pos; }
+    bool empty() const { return pos == int(payload.size()); }
+    void push(const T& t) { payload.push_back(t); }
+    T& front() { return payload[pos]; }
+    void clear() {
+        payload.clear();
+        pos = 0;
+    }
+    void pop() { pos++; }
+};
+
+}  // namespace internal
+
+}  // namespace atcoder
+
+
+namespace atcoder {
+
+template <class Cap> struct mf_graph {
+  public:
+    mf_graph() : _n(0) {}
+    explicit mf_graph(int n) : _n(n), g(n) {}
+
+    int add_edge(int from, int to, Cap cap) {
+        assert(0 <= from && from < _n);
+        assert(0 <= to && to < _n);
+        assert(0 <= cap);
+        int m = int(pos.size());
+        pos.push_back({from, int(g[from].size())});
+        int from_id = int(g[from].size());
+        int to_id = int(g[to].size());
+        if (from == to) to_id++;
+        g[from].push_back(_edge{to, to_id, cap});
+        g[to].push_back(_edge{from, from_id, 0});
+        return m;
+    }
+
+    struct edge {
+        int from, to;
+        Cap cap, flow;
+    };
+
+    edge get_edge(int i) {
+        int m = int(pos.size());
+        assert(0 <= i && i < m);
+        auto _e = g[pos[i].first][pos[i].second];
+        auto _re = g[_e.to][_e.rev];
+        return edge{pos[i].first, _e.to, _e.cap + _re.cap, _re.cap};
+    }
+    std::vector<edge> edges() {
+        int m = int(pos.size());
+        std::vector<edge> result;
+        for (int i = 0; i < m; i++) {
+            result.push_back(get_edge(i));
+        }
+        return result;
+    }
+    void change_edge(int i, Cap new_cap, Cap new_flow) {
+        int m = int(pos.size());
+        assert(0 <= i && i < m);
+        assert(0 <= new_flow && new_flow <= new_cap);
+        auto& _e = g[pos[i].first][pos[i].second];
+        auto& _re = g[_e.to][_e.rev];
+        _e.cap = new_cap - new_flow;
+        _re.cap = new_flow;
+    }
+
+    Cap flow(int s, int t) {
+        return flow(s, t, std::numeric_limits<Cap>::max());
+    }
+    Cap flow(int s, int t, Cap flow_limit) {
+        assert(0 <= s && s < _n);
+        assert(0 <= t && t < _n);
+        assert(s != t);
+
+        std::vector<int> level(_n), iter(_n);
+        internal::simple_queue<int> que;
+
+        auto bfs = [&]() {
+            std::fill(level.begin(), level.end(), -1);
+            level[s] = 0;
+            que.clear();
+            que.push(s);
+            while (!que.empty()) {
+                int v = que.front();
+                que.pop();
+                for (auto e : g[v]) {
+                    if (e.cap == 0 || level[e.to] >= 0) continue;
+                    level[e.to] = level[v] + 1;
+                    if (e.to == t) return;
+                    que.push(e.to);
+                }
+            }
+        };
+        auto dfs = [&](auto self, int v, Cap up) {
+            if (v == s) return up;
+            Cap res = 0;
+            int level_v = level[v];
+            for (int& i = iter[v]; i < int(g[v].size()); i++) {
+                _edge& e = g[v][i];
+                if (level_v <= level[e.to] || g[e.to][e.rev].cap == 0) continue;
+                Cap d =
+                    self(self, e.to, std::min(up - res, g[e.to][e.rev].cap));
+                if (d <= 0) continue;
+                g[v][i].cap += d;
+                g[e.to][e.rev].cap -= d;
+                res += d;
+                if (res == up) return res;
+            }
+            level[v] = _n;
+            return res;
+        };
+
+        Cap flow = 0;
+        while (flow < flow_limit) {
+            bfs();
+            if (level[t] == -1) break;
+            std::fill(iter.begin(), iter.end(), 0);
+            Cap f = dfs(dfs, t, flow_limit - flow);
+            if (!f) break;
+            flow += f;
+        }
+        return flow;
+    }
+
+    std::vector<bool> min_cut(int s) {
+        std::vector<bool> visited(_n);
+        internal::simple_queue<int> que;
+        que.push(s);
+        while (!que.empty()) {
+            int p = que.front();
+            que.pop();
+            visited[p] = true;
+            for (auto e : g[p]) {
+                if (e.cap && !visited[e.to]) {
+                    visited[e.to] = true;
+                    que.push(e.to);
+                }
+            }
+        }
+        return visited;
+    }
+
+  private:
+    int _n;
+    struct _edge {
+        int to, rev;
+        Cap cap;
+    };
+    std::vector<std::pair<int, int>> pos;
+    std::vector<std::vector<_edge>> g;
+};
+
+}  // namespace atcoder
+
+
+#define REP_(i, a_, b_, a, b, ...) \
+  for (int i = (a), END_##i = (b); i < END_##i; ++i)
+#define REP(i, ...) REP_(i, __VA_ARGS__, __VA_ARGS__, 0, __VA_ARGS__)
+#define ALL(x) std::begin(x), std::end(x)
+using i64 = long long;
+using u64 = unsigned long long;
+
+template <typename T, typename U>
+inline bool chmax(T &a, U b) {
+  return a < b and ((a = std::move(b)), true);
+}
+template <typename T, typename U>
+inline bool chmin(T &a, U b) {
+  return a > b and ((a = std::move(b)), true);
+}
+template <typename T>
+inline int ssize(const T &a) {
+  return (int)std::size(a);
+}
+
+template <typename T>
+std::istream &operator>>(std::istream &is, std::vector<T> &a) {
+  for (auto &x : a) is >> x;
+  return is;
+}
+template <typename Container>
+std::ostream &print_seq(const Container &a, std::string_view sep = " ",
+                        std::string_view ends = "\n",
+                        std::ostream &os = std::cout) {
+  auto b = std::begin(a), e = std::end(a);
+  for (auto it = std::begin(a); it != e; ++it) {
+    if (it != b) os << sep;
+    os << *it;
+  }
+  return os << ends;
+}
+template <typename T, typename = void>
+struct is_iterable : std::false_type {};
+template <typename T>
+struct is_iterable<T, std::void_t<decltype(std::begin(std::declval<T>())),
+                                  decltype(std::end(std::declval<T>()))>>
+    : std::true_type {};
+
+template <typename T, typename = std::enable_if_t<
+                          is_iterable<T>::value &&
+                          !std::is_same<T, std::string_view>::value &&
+                          !std::is_same<T, std::string>::value>>
+std::ostream &operator<<(std::ostream &os, const T &a) {
+  return print_seq(a, ", ", "", (os << "{")) << "}";
+}
+template <typename T, typename U>
+std::ostream &operator<<(std::ostream &os, const std::pair<T, U> &a) {
+  return os << "(" << a.first << ", " << a.second << ")";
+}
+
+#ifdef ENABLE_DEBUG
+template <typename T>
+void pdebug(const T &value) {
+  std::cerr << value;
+}
+template <typename T, typename... Ts>
+void pdebug(const T &value, const Ts &...args) {
+  pdebug(value);
+  std::cerr << ", ";
+  pdebug(args...);
+}
+#define DEBUG(...)                                   \
+  do {                                               \
+    std::cerr << " \033[33m (L" << __LINE__ << ") "; \
+    std::cerr << #__VA_ARGS__ << ":\033[0m ";        \
+    pdebug(__VA_ARGS__);                             \
+    std::cerr << std::endl;                          \
+  } while (0)
+#else
+#define pdebug(...)
+#define DEBUG(...)
+#endif
+
+using namespace std;
+
+i64 solve() {
+  int R, C;
+  cin >> R >> C;
+  vector<string> grid(R);
+  vector<vector<int>> node_id(R, vector<int>(C, -1));
+  int n = 0;
+  REP(i, R) {
+    cin >> grid[i];
+    assert(ssize(grid[i]) == C);
+    REP(j, C) {
+      if (grid[i][j] == '#') {
+        node_id[i][j] = n++;
+      }
+    }
+  }
+
+  vector<pair<int, int>> hor, ver;
+  REP(i, R) REP(j, C) {
+    if (grid[i][j] != '#') continue;
+    const int v = node_id[i][j];
+    if (j + 1 < C and grid[i][j + 1] == '#') {
+      const int u = node_id[i][j + 1];
+      hor.emplace_back(v, u);
+    }
+    if (i + 1 < R and grid[i + 1][j] == '#') {
+      const int u = node_id[i + 1][j];
+      ver.emplace_back(v, u);
+    }
+  }
+
+  const i64 BIG = 1000;
+  const int nh = ssize(hor), nv = ssize(ver);
+  atcoder::mf_graph<i64> g(n + nh + nv + 2);
+  const int source = n + nh + nv, sink = n + nh + nv + 1;
+  REP(i, n) {
+    g.add_edge(source, i, 1);
+    g.add_edge(i, sink, 1);
+  }
+  i64 total_cost = -nh - nv;
+  REP(i, nh) {
+    const int x = n + i;
+    auto [u, v] = hor[i];
+    g.add_edge(source, x, 1);
+    g.add_edge(x, u, BIG);
+    g.add_edge(x, v, BIG);
+  }
+  REP(i, nv) {
+    const int x = n + nh + i;
+    auto [u, v] = ver[i];
+    g.add_edge(x, sink, 1);
+    g.add_edge(u, x, BIG);
+    g.add_edge(v, x, BIG);
+  }
+  i64 res = g.flow(source, sink);
+  total_cost += res;
+  DEBUG(res, total_cost);
+  return total_cost;
+}
+
+int main() {
+  ios_base::sync_with_stdio(false), cin.tie(nullptr);
+  cout << solve() << "\n";
+}
---
+++ aizu/2903/main.cpp
@@ -0,0 +1,147 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/maxflow>
+
+#define REP_(i, a_, b_, a, b, ...) \
+  for (int i = (a), END_##i = (b); i < END_##i; ++i)
+#define REP(i, ...) REP_(i, __VA_ARGS__, __VA_ARGS__, 0, __VA_ARGS__)
+#define ALL(x) std::begin(x), std::end(x)
+using i64 = long long;
+using u64 = unsigned long long;
+
+template <typename T, typename U>
+inline bool chmax(T &a, U b) {
+  return a < b and ((a = std::move(b)), true);
+}
+template <typename T, typename U>
+inline bool chmin(T &a, U b) {
+  return a > b and ((a = std::move(b)), true);
+}
+template <typename T>
+inline int ssize(const T &a) {
+  return (int)std::size(a);
+}
+
+template <typename T>
+std::istream &operator>>(std::istream &is, std::vector<T> &a) {
+  for (auto &x : a) is >> x;
+  return is;
+}
+template <typename Container>
+std::ostream &print_seq(const Container &a, std::string_view sep = " ",
+                        std::string_view ends = "\n",
+                        std::ostream &os = std::cout) {
+  auto b = std::begin(a), e = std::end(a);
+  for (auto it = std::begin(a); it != e; ++it) {
+    if (it != b) os << sep;
+    os << *it;
+  }
+  return os << ends;
+}
+template <typename T, typename = void>
+struct is_iterable : std::false_type {};
+template <typename T>
+struct is_iterable<T, std::void_t<decltype(std::begin(std::declval<T>())),
+                                  decltype(std::end(std::declval<T>()))>>
+    : std::true_type {};
+
+template <typename T, typename = std::enable_if_t<
+                          is_iterable<T>::value &&
+                          !std::is_same<T, std::string_view>::value &&
+                          !std::is_same<T, std::string>::value>>
+std::ostream &operator<<(std::ostream &os, const T &a) {
+  return print_seq(a, ", ", "", (os << "{")) << "}";
+}
+template <typename T, typename U>
+std::ostream &operator<<(std::ostream &os, const std::pair<T, U> &a) {
+  return os << "(" << a.first << ", " << a.second << ")";
+}
+
+#ifdef ENABLE_DEBUG
+template <typename T>
+void pdebug(const T &value) {
+  std::cerr << value;
+}
+template <typename T, typename... Ts>
+void pdebug(const T &value, const Ts &...args) {
+  pdebug(value);
+  std::cerr << ", ";
+  pdebug(args...);
+}
+#define DEBUG(...)                                   \
+  do {                                               \
+    std::cerr << " \033[33m (L" << __LINE__ << ") "; \
+    std::cerr << #__VA_ARGS__ << ":\033[0m ";        \
+    pdebug(__VA_ARGS__);                             \
+    std::cerr << std::endl;                          \
+  } while (0)
+#else
+#define pdebug(...)
+#define DEBUG(...)
+#endif
+
+using namespace std;
+
+i64 solve() {
+  int R, C;
+  cin >> R >> C;
+  vector<string> grid(R);
+  vector<vector<int>> node_id(R, vector<int>(C, -1));
+  int n = 0;
+  REP(i, R) {
+    cin >> grid[i];
+    assert(ssize(grid[i]) == C);
+    REP(j, C) {
+      if (grid[i][j] == '#') {
+        node_id[i][j] = n++;
+      }
+    }
+  }
+
+  vector<pair<int, int>> hor, ver;
+  REP(i, R) REP(j, C) {
+    if (grid[i][j] != '#') continue;
+    const int v = node_id[i][j];
+    if (j + 1 < C and grid[i][j + 1] == '#') {
+      const int u = node_id[i][j + 1];
+      hor.emplace_back(v, u);
+    }
+    if (i + 1 < R and grid[i + 1][j] == '#') {
+      const int u = node_id[i + 1][j];
+      ver.emplace_back(v, u);
+    }
+  }
+
+  const i64 BIG = 1000;
+  const int nh = ssize(hor), nv = ssize(ver);
+  atcoder::mf_graph<i64> g(n + nh + nv + 2);
+  const int source = n + nh + nv, sink = n + nh + nv + 1;
+  REP(i, n) {
+    g.add_edge(source, i, 1);
+    g.add_edge(i, sink, 1);
+  }
+  i64 total_cost = -nh - nv;
+  REP(i, nh) {
+    const int x = n + i;
+    auto [u, v] = hor[i];
+    g.add_edge(source, x, 1);
+    g.add_edge(x, u, BIG);
+    g.add_edge(x, v, BIG);
+  }
+  REP(i, nv) {
+    const int x = n + nh + i;
+    auto [u, v] = ver[i];
+    g.add_edge(x, sink, 1);
+    g.add_edge(u, x, BIG);
+    g.add_edge(v, x, BIG);
+  }
+  i64 res = g.flow(source, sink);
+  total_cost += res;
+  DEBUG(res, total_cost);
+  return total_cost;
+}
+
+int main() {
+  ios_base::sync_with_stdio(false), cin.tie(nullptr);
+  cout << solve() << "\n";
+}
