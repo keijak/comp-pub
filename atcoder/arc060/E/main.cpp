b0f03ccbc50560679d7b2ccb53a78173e855a1fa
keijak <m@keijak.com>
2021-04-30 23:18:20 +0200

    binary lifting

---
+++ atcoder/arc060/E/main.cpp
@@ -0,0 +1,168 @@
+#include <bits/stdc++.h>
+#define REP_(i, a_, b_, a, b, ...) \
+  for (int i = (a), END_##i = (b); i < END_##i; ++i)
+#define REP(i, ...) REP_(i, __VA_ARGS__, __VA_ARGS__, 0, __VA_ARGS__)
+#define ALL(x) std::begin(x), std::end(x)
+using i64 = long long;
+using u64 = unsigned long long;
+
+template <typename T, typename U>
+inline bool chmax(T &a, U b) {
+  return a < b and ((a = std::move(b)), true);
+}
+template <typename T, typename U>
+inline bool chmin(T &a, U b) {
+  return a > b and ((a = std::move(b)), true);
+}
+template <typename T>
+inline int ssize(const T &a) {
+  return (int)std::size(a);
+}
+
+template <typename T>
+std::istream &operator>>(std::istream &is, std::vector<T> &a) {
+  for (auto &x : a) is >> x;
+  return is;
+}
+template <typename Container>
+std::ostream &print_seq(const Container &a, std::string_view sep = " ",
+                        std::string_view ends = "\n",
+                        std::ostream &os = std::cout) {
+  auto b = std::begin(a), e = std::end(a);
+  for (auto it = std::begin(a); it != e; ++it) {
+    if (it != b) os << sep;
+    os << *it;
+  }
+  return os << ends;
+}
+template <typename T, typename = void>
+struct is_iterable : std::false_type {};
+template <typename T>
+struct is_iterable<T, std::void_t<decltype(std::begin(std::declval<T>())),
+                                  decltype(std::end(std::declval<T>()))>>
+    : std::true_type {};
+
+template <typename T, typename = std::enable_if_t<
+                          is_iterable<T>::value &&
+                          !std::is_same<T, std::string_view>::value &&
+                          !std::is_same<T, std::string>::value>>
+std::ostream &operator<<(std::ostream &os, const T &a) {
+  return print_seq(a, ", ", "", (os << "{")) << "}";
+}
+template <typename T, typename U>
+std::ostream &operator<<(std::ostream &os, const std::pair<T, U> &a) {
+  return os << "(" << a.first << ", " << a.second << ")";
+}
+
+#ifdef ENABLE_DEBUG
+template <typename T>
+void pdebug(const T &value) {
+  std::cerr << value;
+}
+template <typename T, typename... Ts>
+void pdebug(const T &value, const Ts &...args) {
+  pdebug(value);
+  std::cerr << ", ";
+  pdebug(args...);
+}
+#define DEBUG(...)                                   \
+  do {                                               \
+    std::cerr << " \033[33m (L" << __LINE__ << ") "; \
+    std::cerr << #__VA_ARGS__ << ":\033[0m ";        \
+    pdebug(__VA_ARGS__);                             \
+    std::cerr << std::endl;                          \
+  } while (0)
+#else
+#define pdebug(...)
+#define DEBUG(...)
+#endif
+
+using namespace std;
+
+struct SimpleFunctionalGraph {
+ private:
+  static const int kMaxBits = 60;
+
+  // number of nodes.
+  int size;
+
+  // next_pos[d][i] := starting from i, what's the position after 2^d steps.
+  std::vector<std::vector<int>> next_pos;
+
+  bool build_done_;
+
+ public:
+  explicit SimpleFunctionalGraph(int n)
+      : size(n),
+        next_pos(kMaxBits, std::vector<int>(n, -1)),
+        build_done_(false) {}
+
+  // Sets next position of node `u`.
+  void set_next(int u, int v) { next_pos[0][u] = v; }
+
+  // Builds the transition table.
+  void build() {
+    for (int d = 0; d + 1 < kMaxBits; d++) {
+      for (int i = 0; i < size; i++) {
+        if (int p = next_pos[d][i]; p != -1) {
+          next_pos[d + 1][i] = next_pos[d][p];
+        }
+      }
+    }
+    build_done_ = true;
+  }
+
+  // Starting from `start`, `steps` times goes forward and returns where it
+  // ends up.
+  int go(int start, long long steps) const {
+    assert(build_done_);
+    // steps >= 2^kMaxBits is not supported.
+    assert(steps < (1LL << kMaxBits));
+
+    int i = start;
+    for (int d = kMaxBits - 1; d >= 0; d--) {
+      if ((steps >> d) & 1) {
+        i = next_pos[d][i];
+      }
+    }
+    return i;
+  }
+};
+
+int main() {
+  ios_base::sync_with_stdio(false), cin.tie(nullptr);
+  int n;
+  cin >> n;
+  vector<i64> X(n);
+  cin >> X;
+  i64 L;
+  cin >> L;
+  FunctionalGraph g(n);
+  REP(i, n) {
+    auto it = upper_bound(ALL(X), X[i] + L);
+    --it;
+    int nex = it - X.begin();
+    g.set_next(i, nex);
+  }
+  g.build();
+
+  int Q;
+  cin >> Q;
+  REP(i, Q) {
+    int a, b;
+    cin >> a >> b;
+    --a, --b;
+    if (a > b) swap(a, b);
+    i64 fv = 0, tv = b - a;
+    while (tv - fv > 1) {
+      i64 mid = (fv + tv) / 2;
+      int x = g.go(a, mid);
+      if (x >= b) {
+        tv = mid;
+      } else {
+        fv = mid;
+      }
+    }
+    cout << tv << "\n";
+  }
+}
