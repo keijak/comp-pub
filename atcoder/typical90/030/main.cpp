6251480a5b96dd3c6f6a8c30592d669a12b22f4d
keijak <m@keijak.com>
2021-05-03 11:42:44 +0200

    opt

--- atcoder/typical90/030/main.cpp
+++ atcoder/typical90/030/main.cpp
@@ -1,41 +1,29 @@
 #include <cstdio>
 
-int spf[10'000'001];
-int dp[10'000'001];
-
-void sieve_smallest_prime_factors(int n) {
-  for (int i = 1; i <= n; ++i) spf[i] = i;
-  for (int i = 2; i <= n; i += 2) spf[i] = 2;
-  for (int i = 3; i * i <= n; i += 2) {
-    if (spf[i] != i) continue;
-    for (int j = i * i; j <= n; j += i) {
-      if (spf[j] == j) spf[j] = i;
-    }
-  }
-}
+unsigned count[10'000'001];
 
 using namespace std;
 
 int main() {
-  int n, k;
-  scanf("%d%d", &n, &k);
+  unsigned n, k;
+  scanf("%u%u", &n, &k);
   if (k == 1) {
-    printf("%d\n", n - 1);
+    printf("%u\n", n - 1);
     return 0;
   }
-  sieve_smallest_prime_factors(n);
-
-  int ans = 0;
-  for (int x = 2; x <= n; ++x) {
-    const int p = spf[x];
-    int m = x;
-    do {
-      m /= p;
-    } while (m % p == 0);
-    dp[x] = dp[m] + 1;
-    if (dp[x] >= k) {
-      ++ans;
+  for (unsigned i = 4; i <= n; i += 2) {
+    ++count[i];
+  }
+  const unsigned m = (n + 1) / 2;
+  for (unsigned i = 3; i <= m; i += 2) {
+    if (count[i]) continue;
+    for (unsigned j = i << 1; j <= n; j += i) {
+      ++count[j];
     }
   }
-  printf("%d\n", ans);
+  unsigned ans = 0;
+  for (unsigned i = 2; i <= n; ++i) {
+    if (count[i] >= k) ++ans;
+  }
+  printf("%u\n", ans);
 }
