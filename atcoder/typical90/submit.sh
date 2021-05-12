59aa83701e3840ad3d44ca42062e86cdb5699532
keijak <m@keijak.com>
2021-05-13 01:10:06 +0200

    subm

--- atcoder/typical90/submit.sh
+++ atcoder/typical90/submit.sh
@@ -2,9 +2,9 @@
 
 while :
 do
-  atcoder_dl_test_submit.py https://atcoder.jp/contests/typical90/tasks/typical90_ak --cmd='./a.out' --src='main.cpp'
+  date
+  atcoder_dl_test_submit.py https://atcoder.jp/contests/typical90/tasks/typical90_am --cmd='./a.out' --src='main.cpp'
   echo '.'
   echo '.'
-  echo '.'
-  sleep $(($RANDOM % 10 + 5))
+  sleep $(($RANDOM % 5 + 5))
 done
