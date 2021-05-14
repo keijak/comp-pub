58ffad544c163438b5b0cb8e62469b81cec962c7
keijak <m@keijak.com>
2021-05-14 08:47:15 +0200

    dp

---
+++ atcoder/arc116/D/main.cpp
@@ -0,0 +1,144 @@
+#include <bits/stdc++.h>
+
+#include <optional>
+
+#define REP_(i, a_, b_, a, b, ...) \
+  for (int i = (a), END_##i = (b); i < END_##i; ++i)
+#define REP(i, ...) REP_(i, __VA_ARGS__, __VA_ARGS__, 0, __VA_ARGS__)
+#define ALL(x) std::begin(x), std::end(x)
+using i64 = long long;
+
+#include <atcoder/modint>
+using Mint = atcoder::modint998244353;
+std::ostream &operator<<(std::ostream &os, const Mint &m) {
+  return os << m.val();
+}
+
+template <typename T, typename U>
+inline bool chmax(T &a, U b) {
+  return a < b and ((a = std::move(b)), true);
+}
+template <typename T, typename U>
+inline bool chmin(T &a, U b) {
+  return a > b and ((a = std::move(b)), true);
+}
+template <typename T>
+inline int ssize(const T &a) {
+  return (int)std::size(a);
+}
+
+template <typename T>
+std::istream &operator>>(std::istream &is, std::vector<T> &a) {
+  for (auto &x : a) is >> x;
+  return is;
+}
+template <typename T, typename U>
+std::ostream &operator<<(std::ostream &os, const std::pair<T, U> &a) {
+  return os << "(" << a.first << ", " << a.second << ")";
+}
+template <typename Container>
+std::ostream &print_seq(const Container &a, std::string_view sep = " ",
+                        std::string_view ends = "\n",
+                        std::ostream &os = std::cout) {
+  auto b = std::begin(a), e = std::end(a);
+  for (auto it = std::begin(a); it != e; ++it) {
+    if (it != b) os << sep;
+    os << *it;
+  }
+  return os << ends;
+}
+template <typename T, typename = void>
+struct is_iterable : std::false_type {};
+template <typename T>
+struct is_iterable<T, std::void_t<decltype(std::begin(std::declval<T>())),
+                                  decltype(std::end(std::declval<T>()))>>
+    : std::true_type {};
+
+template <typename T, typename = std::enable_if_t<
+                          is_iterable<T>::value &&
+                          !std::is_same<T, std::string_view>::value &&
+                          !std::is_same<T, std::string>::value>>
+std::ostream &operator<<(std::ostream &os, const T &a) {
+  return print_seq(a, ", ", "", (os << "{")) << "}";
+}
+
+#ifdef ENABLE_DEBUG
+#include "debug_dump.hpp"
+#else
+#define DUMP(...)
+#endif
+
+using namespace std;
+using u32 = unsigned;
+using u64 = unsigned long long;
+
+int bit_width(u32 x) {
+  static const int U32_WIDTH = __builtin_clz(1U) + 1;
+  return (x == 0) ? 0 : U32_WIDTH - __builtin_clz(x);
+}
+
+int mssb_pos(unsigned x) {
+  static const int CLZ_WIDTH = __builtin_clz(1);
+  assert(x != 0);
+  return CLZ_WIDTH - __builtin_clz(x);
+}
+inline u32 bit_floor(u32 x) { return u32(1) << mssb_pos(x); }
+
+// mod: prime
+template <class T = Mint>
+struct Factorials {
+  // factorials and inverse factorials.
+  std::vector<T> fact, ifact;
+
+  // n: max cached value.
+  Factorials(int n) : fact(n + 1), ifact(n + 1) {
+    assert(n >= 0);
+    assert(n < T::mod());
+    fact[0] = 1;
+    for (int i = 1; i <= n; ++i) {
+      fact[i] = fact[i - 1] * i;
+    }
+    ifact[n] = fact[n].inv();
+    for (int i = n; i >= 1; --i) {
+      ifact[i - 1] = ifact[i] * i;
+    }
+  }
+
+  // Combination (nCk)
+  T C(int n, int k) const {
+    if (k < 0 || k > n) return 0;
+    return fact[n] * ifact[k] * ifact[n - k];
+  }
+};
+
+auto solve() {
+  int n, m;
+  cin >> n >> m;
+  const int L = bit_width(m);
+
+  Factorials fs(n);
+  auto dp = vector(L, vector(m + 1, std::optional<Mint>(nullopt)));
+  auto f = [&](auto &f, int i, int s) -> Mint {
+    if (s == 0) return 1;
+    if (i == L) return 0;
+    auto &memo = dp[i][s];
+    if (memo.has_value()) {
+      return memo.value();
+    }
+    Mint res = 0;
+    for (int j = 0; j <= min(n, m); j += 2) {
+      i64 x = j * (1LL << i);
+      if (x > s) continue;
+      Mint sub = f(f, i + 1, s - x);
+      res += fs.C(n, j) * sub;
+    }
+    memo = res;
+    return res;
+  };
+  return f(f, 0, m);
+}
+
+int main() {
+  ios_base::sync_with_stdio(false), cin.tie(nullptr);
+  cout << solve() << "\n";
+}
