64d08b64a31d74bd57a047650d8a3ce7294d5888
keijak <m@keijak.com>
2021-05-19 14:42:23 +0200

    bfs trim

---
+++ atcoder/typical90/043/bfs.cpp
@@ -0,0 +1,123 @@
+#include <bits/stdc++.h>
+#define REP_(i, a_, b_, a, b, ...) \
+  for (int i = (a), END_##i = (b); i < END_##i; ++i)
+#define REP(i, ...) REP_(i, __VA_ARGS__, __VA_ARGS__, 0, __VA_ARGS__)
+#define ALL(x) std::begin(x), std::end(x)
+using i64 = long long;
+
+template <typename T, typename U>
+inline bool chmax(T &a, U b) {
+  return a < b and ((a = std::move(b)), true);
+}
+template <typename T, typename U>
+inline bool chmin(T &a, U b) {
+  return a > b and ((a = std::move(b)), true);
+}
+template <typename T>
+inline int ssize(const T &a) {
+  return (int)std::size(a);
+}
+
+template <typename T>
+std::istream &operator>>(std::istream &is, std::vector<T> &a) {
+  for (auto &x : a) is >> x;
+  return is;
+}
+template <typename T, typename U>
+std::ostream &operator<<(std::ostream &os, const std::pair<T, U> &a) {
+  return os << "(" << a.first << ", " << a.second << ")";
+}
+template <typename Container>
+std::ostream &print_seq(const Container &a, std::string_view sep = " ",
+                        std::string_view ends = "\n",
+                        std::ostream &os = std::cout) {
+  auto b = std::begin(a), e = std::end(a);
+  for (auto it = std::begin(a); it != e; ++it) {
+    if (it != b) os << sep;
+    os << *it;
+  }
+  return os << ends;
+}
+template <typename T, typename = void>
+struct is_iterable : std::false_type {};
+template <typename T>
+struct is_iterable<T, std::void_t<decltype(std::begin(std::declval<T>())),
+                                  decltype(std::end(std::declval<T>()))>>
+    : std::true_type {};
+
+template <typename T, typename = std::enable_if_t<
+                          is_iterable<T>::value &&
+                          !std::is_same<T, std::string_view>::value &&
+                          !std::is_same<T, std::string>::value>>
+std::ostream &operator<<(std::ostream &os, const T &a) {
+  return print_seq(a, ", ", "", (os << "{")) << "}";
+}
+
+#ifdef ENABLE_DEBUG
+#include "debug_dump.hpp"
+#else
+#define DUMP(...)
+#endif
+
+using namespace std;
+
+const int BIG = numeric_limits<int>::max() >> 1;
+
+struct GridState {
+  int cost;
+  int r;
+  int c;
+};
+
+const array<int, 4> dr = {-1, 0, 1, 0};
+const array<int, 4> dc = {0, -1, 0, 1};
+
+int solve() {
+  int H, W, rs, cs, rt, ct;
+  cin >> H >> W >> rs >> cs >> rt >> ct;
+  --rs, --cs, --rt, --ct;
+  vector<string> cell(H);
+  REP(i, H) cin >> cell[i];
+  if (rs == rt and cs == ct) {
+    return 0;
+  }
+
+  auto mincost = vector(H, vector(W, BIG));
+  deque<GridState> que;
+  mincost[rs][cs] = -1;
+  que.push_back({-1, rs, cs});
+
+  auto push = [&](int new_cost, int r, int c) -> bool {
+    if (r < 0 or r >= H or c < 0 or c >= W) return false;
+    if (cell[r][c] == '#') return false;
+    if (mincost[r][c] < new_cost) return false;
+    if (mincost[r][c] == new_cost) return true;
+    mincost[r][c] = new_cost;
+    que.push_back({new_cost, r, c});
+    return true;
+  };
+
+  while (not que.empty()) {
+    GridState cur = move(que.front());
+    que.pop_front();
+    if (cur.r == rt and cur.c == ct) {
+      return cur.cost;
+    }
+    const int new_cost = cur.cost + 1;
+    for (int i = 0; i < 4; ++i) {
+      int nr = cur.r;
+      int nc = cur.c;
+      while (true) {
+        nr += dr[i];
+        nc += dc[i];
+        if (!push(new_cost, nr, nc)) break;
+      }
+    }
+  }
+  assert(false);
+}
+
+int main() {
+  ios_base::sync_with_stdio(false), cin.tie(nullptr);
+  cout << solve() << "\n";
+}
