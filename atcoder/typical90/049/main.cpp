ff8c2b60492e3ba1843dd306366da94fb82d9e17
keijak <m@keijak.com>
2021-05-28 09:21:30 +0200

    typical

---
+++ atcoder/typical90/049/main.cpp
@@ -0,0 +1,102 @@
+#include <bits/stdc++.h>
+#define REP_(i, a_, b_, a, b, ...) \
+  for (int i = (a), END_##i = (b); i < END_##i; ++i)
+#define REP(i, ...) REP_(i, __VA_ARGS__, __VA_ARGS__, 0, __VA_ARGS__)
+#define ALL(x) std::begin(x), std::end(x)
+using i64 = long long;
+
+template <typename T, typename U>
+inline bool chmax(T &a, U b) {
+  return a < b and ((a = std::move(b)), true);
+}
+template <typename T, typename U>
+inline bool chmin(T &a, U b) {
+  return a > b and ((a = std::move(b)), true);
+}
+template <typename T>
+inline int ssize(const T &a) {
+  return (int)std::size(a);
+}
+
+template <typename T>
+std::istream &operator>>(std::istream &is, std::vector<T> &a) {
+  for (auto &x : a) is >> x;
+  return is;
+}
+template <typename T, typename U>
+std::ostream &operator<<(std::ostream &os, const std::pair<T, U> &a) {
+  return os << "(" << a.first << ", " << a.second << ")";
+}
+template <typename Container>
+std::ostream &print_seq(const Container &a, std::string_view sep = " ",
+                        std::string_view ends = "\n",
+                        std::ostream &os = std::cout) {
+  auto b = std::begin(a), e = std::end(a);
+  for (auto it = std::begin(a); it != e; ++it) {
+    if (it != b) os << sep;
+    os << *it;
+  }
+  return os << ends;
+}
+template <typename T, typename = void>
+struct is_iterable : std::false_type {};
+template <typename T>
+struct is_iterable<T, std::void_t<decltype(std::begin(std::declval<T>())),
+                                  decltype(std::end(std::declval<T>()))>>
+    : std::true_type {};
+
+template <typename T, typename = std::enable_if_t<
+                          is_iterable<T>::value &&
+                          !std::is_same<T, std::string_view>::value &&
+                          !std::is_same<T, std::string>::value>>
+std::ostream &operator<<(std::ostream &os, const T &a) {
+  return print_seq(a, ", ", "", (os << "{")) << "}";
+}
+
+#ifdef ENABLE_DEBUG
+#include "debug_dump.hpp"
+#else
+#define DUMP(...)
+#endif
+
+using namespace std;
+
+i64 solve() {
+  int n, m;
+  cin >> n >> m;
+  if (m < n) return -1;
+  vector<array<i64, 3>> items(m);
+  REP(i, m) {
+    i64 c, l, r;
+    cin >> c >> l >> r;
+    items[i] = {c, l - 1, r};
+  }
+  sort(items.begin(), items.end());
+  vector<optional<i64>> basis(n);
+  i64 basis_count = 0, total_cost = 0;
+  for (auto [cost, l, r] : items) {
+    while (l < n) {
+      if (not basis[l]) {
+        basis[l] = r;
+        basis_count += 1;
+        total_cost += cost;
+        break;
+      }
+      const i64 p = *basis[l];
+      if (p == r) break;
+      if (p < r) {
+        l = p;
+      } else {
+        l = r;
+        r = p;
+      }
+    }
+    if (basis_count == n) return total_cost;
+  }
+  return -1;
+}
+
+int main() {
+  ios_base::sync_with_stdio(false), cin.tie(nullptr);
+  cout << solve() << "\n";
+}
