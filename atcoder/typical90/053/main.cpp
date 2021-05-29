392d6a008017703ea5cb587585b2dbb4b1a92ecc
keijak <m@keijak.com>
2021-05-29 11:09:06 +0200

    typical

---
+++ atcoder/typical90/053/main.cpp
@@ -0,0 +1,122 @@
+#include <bits/stdc++.h>
+#define REP_(i, a_, b_, a, b, ...) \
+  for (int i = (a), END_##i = (b); i < END_##i; ++i)
+#define REP(i, ...) REP_(i, __VA_ARGS__, __VA_ARGS__, 0, __VA_ARGS__)
+#define ALL(x) std::begin(x), std::end(x)
+using i64 = long long;
+
+template <typename T, typename U>
+inline bool chmax(T &a, U b) {
+  return a < b and ((a = std::move(b)), true);
+}
+template <typename T, typename U>
+inline bool chmin(T &a, U b) {
+  return a > b and ((a = std::move(b)), true);
+}
+template <typename T>
+inline int ssize(const T &a) {
+  return (int)std::size(a);
+}
+
+template <typename T>
+std::istream &operator>>(std::istream &is, std::vector<T> &a) {
+  for (auto &x : a) is >> x;
+  return is;
+}
+template <typename T, typename U>
+std::ostream &operator<<(std::ostream &os, const std::pair<T, U> &a) {
+  return os << "(" << a.first << ", " << a.second << ")";
+}
+template <typename Container>
+std::ostream &print_seq(const Container &a, std::string_view sep = " ",
+                        std::string_view ends = "\n",
+                        std::ostream &os = std::cout) {
+  auto b = std::begin(a), e = std::end(a);
+  for (auto it = std::begin(a); it != e; ++it) {
+    if (it != b) os << sep;
+    os << *it;
+  }
+  return os << ends;
+}
+template <typename T, typename = void>
+struct is_iterable : std::false_type {};
+template <typename T>
+struct is_iterable<T, std::void_t<decltype(std::begin(std::declval<T>())),
+                                  decltype(std::end(std::declval<T>()))>>
+    : std::true_type {};
+
+template <typename T, typename = std::enable_if_t<
+                          is_iterable<T>::value &&
+                          !std::is_same<T, std::string_view>::value &&
+                          !std::is_same<T, std::string>::value>>
+std::ostream &operator<<(std::ostream &os, const T &a) {
+  return print_seq(a, ", ", "", (os << "{")) << "}";
+}
+
+#ifdef ENABLE_DEBUG
+#include "debug_dump.hpp"
+#else
+#define DUMP(...)
+#endif
+
+using namespace std;
+
+template <class T = i64, class Compare = std::less<T>,
+          class F = std::function<T(i64)>>
+i64 find_max_golden_section_search(i64 low, i64 high, F f) {
+  Compare compare;
+  --low;             // Make it an open interval: (low, high).
+  i64 l = 1, r = 1;  // Left and right offsets from `low`.
+  while (l + r < high - low) {
+    l += r;
+    std::swap(l, r);
+  }
+  auto lval = f(low + l), rval = f(low + r);
+  while (l < r) {
+    r -= l;
+    std::swap(l, r);
+    if (compare(lval, rval)) {
+      rval = std::move(lval);
+      lval = f(low + l);
+    } else {
+      low += r;
+      if (low + r < high) {
+        lval = std::move(rval);
+        rval = f(low + r);
+      } else {
+        r -= l;
+        std::swap(l, r);
+        lval = f(low + l);
+      }
+    }
+  }
+  return low + 1;
+}
+
+map<i64, i64> cache;
+
+i64 query(i64 i) {
+  if (auto it = cache.find(i); it != cache.end()) {
+    return it->second;
+  }
+  cout << "? " << i + 1 << endl;
+  int res;
+  cin >> res;
+  assert(res >= 0);
+  cache[i] = res;
+  return res;
+}
+
+void solve() {
+  cache.clear();
+  int N;
+  cin >> N;
+  i64 j = find_max_golden_section_search<i64, greater<i64>>(0, N, query);
+  cout << "! " << query(j) << endl;
+}
+
+int main() {
+  int T;
+  cin >> T;
+  REP(i, T) { solve(); }
+}
