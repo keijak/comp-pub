9c322c082de8e22624805116c7f3e3458c0f0366
keijak <m@keijak.com>
2021-06-07 10:34:23 +0200

    rational

--- cf/contest/1536/C/main.cpp
+++ cf/contest/1536/C/main.cpp
@@ -60,35 +60,70 @@ std::ostream &operator<<(std::ostream &os, const T &a) {
 #endif
 
 using namespace std;
+#include <bits/stdc++.h>
 
+template <typename T = long long,
+          typename = std::enable_if_t<std::is_integral<T>::value>>
 struct Rational {
-  i64 nume, deno;
+ private:
+  using BigInt = __int128_t;
+
+ public:
+  T nume, deno;
 
   Rational() : nume(0), deno(1) {}
-  explicit Rational(i64 n) : nume(n), deno(1) {}
-  Rational(i64 n, i64 d) : nume(n), deno(d) {
+
+  explicit Rational(T n) : nume(n), deno(1) {}
+
+  Rational(T n, T d) : nume(n), deno(d) {
     assert(deno != 0);
     if (nume == 0) {
       deno = 1;
     } else {
-      i64 g = std::gcd(nume, deno);
+      int sign = 1;
+      if ((nume < 0) xor (deno < 0)) {
+        sign = -1;
+      }
+      nume = abs(nume);
+      deno = abs(deno);
+      auto g = std::gcd(nume, deno);
       if (g > 1) {
         nume /= g;
         deno /= g;
       }
+      nume *= sign;
     }
   }
-  Rational(const Rational &) = default;
-  Rational(Rational &&) = default;
-  Rational &operator=(const Rational &) = default;
-  Rational &operator=(Rational &&) = default;
+
+  operator double() { return (double)(nume) / deno; }
+  operator long double() { return (long double)(nume) / deno; }
+
+  friend bool operator<(const Rational &x, const Rational &y) {
+    return (BigInt)(x.nume) * y.deno < (BigInt)(y.nume) * x.deno;
+  }
+
+  friend bool operator==(const Rational &x, const Rational &y) {
+    return std::tie(x.nume, x.deno) == std::tie(y.nume, y.deno);
+  }
+
+  friend Rational operator+(const Rational &x, const Rational &y) {
+    auto g = std::gcd(x.deno, y.deno);
+    auto zn = x.nume * (y.deno / g) + y.nume * (x.deno / g);
+    auto zd = (x.deno / g) * y.deno;
+    return Rational(zn, zd);
+  }
+
+  friend Rational operator*(const Rational &x, const Rational &y) {
+    auto g1 = std::gcd(std::abs(x.nume), y.deno);
+    auto g2 = std::gcd(std::abs(y.nume), x.deno);
+    return Rational((x.nume / g1) * (y.nume / g2),
+                    (x.deno / g2) * (y.deno / g1));
+  }
+
+  friend std::ostream &operator<<(std::ostream &os, const Rational &x) {
+    return os << x.nume << "/" << x.deno;
+  }
 };
-bool operator<(const Rational &x, const Rational &y) {
-  return __int128_t(x.nume) * y.deno < __int128_t(y.nume) * x.deno;
-}
-ostream &operator<<(ostream &os, const Rational &x) {
-  return os << x.nume << "/" << x.deno;
-}
 
 vector<i64> solve() {
   int n;
@@ -99,9 +134,9 @@ vector<i64> solve() {
   REP(i, n) { dcum[i + 1] = dcum[i] + int(s[i] == 'D'); }
 
   vector<i64> ans(n, 1);
-  map<Rational, int> mp;
+  map<Rational<>, int> mp;
   REP(i, n) {
-    Rational r(dcum[i + 1], i64(i + 1));
+    Rational<> r(dcum[i + 1], i64(i + 1));
     int c = ++mp[r];
     chmax(ans[i], c);
   }
