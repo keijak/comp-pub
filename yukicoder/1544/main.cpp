987caee7299a1388733f1751ce307f289978fb68
keijak <m@keijak.com>
2021-06-11 18:29:15 +0200

    crt

--- yukicoder/1544/main.cpp
+++ yukicoder/1544/main.cpp
@@ -1,110 +1,21 @@
 #include <bits/stdc++.h>
 
-#include <atcoder/math>
 #define REP_(i, a_, b_, a, b, ...) \
   for (int i = (a), END_##i = (b); i < END_##i; ++i)
 #define REP(i, ...) REP_(i, __VA_ARGS__, __VA_ARGS__, 0, __VA_ARGS__)
-#define ALL(x) std::begin(x), std::end(x)
 using i64 = long long;
-
-template <typename T, typename U>
-inline bool chmax(T &a, U b) {
-  return a < b and ((a = std::move(b)), true);
-}
-template <typename T, typename U>
-inline bool chmin(T &a, U b) {
-  return a > b and ((a = std::move(b)), true);
-}
-template <typename T>
-inline int ssize(const T &a) {
-  return (int)std::size(a);
-}
-
-template <typename T>
-std::istream &operator>>(std::istream &is, std::vector<T> &a) {
-  for (auto &x : a) is >> x;
-  return is;
-}
-template <typename T, typename U>
-std::ostream &operator<<(std::ostream &os, const std::pair<T, U> &a) {
-  return os << "(" << a.first << ", " << a.second << ")";
-}
-template <typename Container>
-std::ostream &print_seq(const Container &a, std::string_view sep = " ",
-                        std::string_view ends = "\n",
-                        std::ostream &os = std::cout) {
-  auto b = std::begin(a), e = std::end(a);
-  for (auto it = std::begin(a); it != e; ++it) {
-    if (it != b) os << sep;
-    os << *it;
-  }
-  return os << ends;
-}
-template <typename T, typename = void>
-struct is_iterable : std::false_type {};
-template <typename T>
-struct is_iterable<T, std::void_t<decltype(std::begin(std::declval<T>())),
-                                  decltype(std::end(std::declval<T>()))>>
-    : std::true_type {};
-
-template <typename T, typename = std::enable_if_t<
-                          is_iterable<T>::value &&
-                          !std::is_same<T, std::string_view>::value &&
-                          !std::is_same<T, std::string>::value>>
-std::ostream &operator<<(std::ostream &os, const T &a) {
-  return print_seq(a, ", ", "", (os << "{")) << "}";
-}
-
-#ifdef ENABLE_DEBUG
-#include "debug_dump.hpp"
-#else
-#define DUMP(...)
-#endif
-
 using namespace std;
-
-template <int sign = 1>
-struct Infinity {
-  template <typename T>
-  constexpr operator T() const {
-    static_assert(sign == 1 or not std::is_unsigned_v<T>,
-                  "must be positive in an unsigned type");
-    if constexpr (std::numeric_limits<T>::has_infinity) {
-      return T(sign) * std::numeric_limits<T>::infinity();
-    } else {
-      return T(sign) * (std::numeric_limits<T>::max() / T(2));
-    }
-  }
-  constexpr Infinity<sign * -1> operator-() const { return {}; }
-  template <typename T>
-  friend bool operator==(const T &x, const Infinity &y) {
-    return x == T(y);
-  }
-  template <typename T>
-  friend bool operator!=(const T &x, const Infinity &y) {
-    return x != T(y);
-  }
-};
-constexpr Infinity<> INF;
 
 i64 solve() {
   int n, m;
   cin >> n >> m;
   vector<int> A(n), B(m);
-  cin >> A >> B;
-  i64 ans = INF;
-  REP(i, n) REP(j, m) {
-    if (A[i] != B[j]) continue;
-    vector<i64> xr = {i, j};
-    vector<i64> xm = {n, m};
-    auto [y, z] = atcoder::crt(xr, xm);
-    if (y == 0 and z == 0) continue;
-    chmin(ans, y);
+  REP(i, n) cin >> A[i];
+  REP(i, m) cin >> B[i];
+  REP(i, std::lcm(n, m)) {
+    if (A[i % n] == B[i % m]) return i + 1;
   }
-  if (ans == INF) {
-    return -1;
-  }
-  return ans + 1;
+  return -1;
 }
 
 int main() {
