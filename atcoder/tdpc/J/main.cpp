f9714945ba387f43a5a09220922dbca17f1383fe
keijak <m@keijak.com>
2021-06-14 11:35:04 +0200

    tdpc

---
+++ atcoder/tdpc/J/main.cpp
@@ -0,0 +1,125 @@
+#include <bits/stdc++.h>
+#define REP_(i, a_, b_, a, b, ...) \
+  for (int i = (a), END_##i = (b); i < END_##i; ++i)
+#define REP(i, ...) REP_(i, __VA_ARGS__, __VA_ARGS__, 0, __VA_ARGS__)
+#define ALL(x) std::begin(x), std::end(x)
+using i64 = long long;
+
+template <typename T, typename U>
+inline bool chmax(T &a, U b) {
+  return a < b and ((a = std::move(b)), true);
+}
+template <typename T, typename U>
+inline bool chmin(T &a, U b) {
+  return a > b and ((a = std::move(b)), true);
+}
+template <typename T>
+inline int ssize(const T &a) {
+  return (int)std::size(a);
+}
+
+template <typename T>
+std::istream &operator>>(std::istream &is, std::vector<T> &a) {
+  for (auto &x : a) is >> x;
+  return is;
+}
+template <typename T, typename U>
+std::ostream &operator<<(std::ostream &os, const std::pair<T, U> &a) {
+  return os << "(" << a.first << ", " << a.second << ")";
+}
+template <typename Container>
+std::ostream &print_seq(const Container &a, std::string_view sep = " ",
+                        std::string_view ends = "\n",
+                        std::ostream &os = std::cout) {
+  auto b = std::begin(a), e = std::end(a);
+  for (auto it = std::begin(a); it != e; ++it) {
+    if (it != b) os << sep;
+    os << *it;
+  }
+  return os << ends;
+}
+template <typename T, typename = void>
+struct is_iterable : std::false_type {};
+template <typename T>
+struct is_iterable<T, std::void_t<decltype(std::begin(std::declval<T>())),
+                                  decltype(std::end(std::declval<T>()))>>
+    : std::true_type {};
+
+template <typename T, typename = std::enable_if_t<
+                          is_iterable<T>::value &&
+                          !std::is_same<T, std::string_view>::value &&
+                          !std::is_same<T, std::string>::value>>
+std::ostream &operator<<(std::ostream &os, const T &a) {
+  return print_seq(a, ", ", "", (os << "{")) << "}";
+}
+
+#ifdef ENABLE_DEBUG
+#include "debug_dump.hpp"
+#else
+#define DUMP(...)
+#endif
+
+using namespace std;
+using Real = long double;
+using Uint = unsigned;
+
+template <int sign = 1>
+struct Infinity {
+  template <typename T>
+  constexpr operator T() const {
+    static_assert(sign == 1 or not std::is_unsigned<T>::value,
+                  "must be positive in an unsigned type");
+    return (std::numeric_limits<T>::max() / T(2)) * T(sign);
+  }
+  constexpr Infinity<sign * -1> operator-() const { return {}; }
+  template <typename T>
+  friend bool operator==(const T &x, const Infinity &y) {
+    return x == T(y);
+  }
+  template <typename T>
+  friend bool operator!=(const T &x, const Infinity &y) {
+    return x != T(y);
+  }
+};
+const Infinity<> INF;
+
+Real solve() {
+  int n;
+  cin >> n;
+  Uint X = 0;
+  REP(i, n) {
+    int x;
+    cin >> x;
+    X |= 1u << x;
+  }
+  auto dp = vector(1u << 16, Real(INF));
+  dp[0] = 0;
+  for (Uint bits = 1; bits <= X; ++bits) {
+    if ((bits & X) != bits) continue;
+    Real best = INF;
+    for (int t = 1; t <= 14; ++t) {
+      Real e = 0.0;
+      int stay = 0;
+      for (int d = -1; d <= +1; ++d) {
+        Uint sub = bits & ~(1u << (t + d));
+        if (sub == bits) {
+          ++stay;
+          e += 1.0;
+        } else {
+          e += 1.0 + dp[sub];
+        }
+      }
+      e *= 1 / Real(3 - stay);
+      chmin(best, e);
+    }
+    dp[bits] = best;
+  }
+  assert(dp[X] != INF);
+  return dp[X];
+}
+
+int main() {
+  ios_base::sync_with_stdio(false), cin.tie(nullptr);
+  cout << std::fixed << std::setprecision(15);
+  cout << solve() << "\n";
+}
