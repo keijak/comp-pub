e941b73dad9452a5282b9f9a71a22e3bdc3a82aa
keijak <m@keijak.com>
2021-06-21 05:03:27 +0200

    yuki

---
+++ yukicoder/1553/main.cpp
@@ -0,0 +1,218 @@
+#include <bits/stdc++.h>
+#define REP_(i, a_, b_, a, b, ...) \
+  for (int i = (a), END_##i = (b); i < END_##i; ++i)
+#define REP(i, ...) REP_(i, __VA_ARGS__, __VA_ARGS__, 0, __VA_ARGS__)
+#define ALL(x) std::begin(x), std::end(x)
+using i64 = long long;
+
+template <typename T, typename U>
+inline bool chmax(T &a, U b) {
+  return a < b and ((a = std::move(b)), true);
+}
+template <typename T, typename U>
+inline bool chmin(T &a, U b) {
+  return a > b and ((a = std::move(b)), true);
+}
+template <typename T>
+inline int ssize(const T &a) {
+  return (int)std::size(a);
+}
+
+template <typename T>
+std::istream &operator>>(std::istream &is, std::vector<T> &a) {
+  for (auto &x : a) is >> x;
+  return is;
+}
+template <typename T, typename U>
+std::ostream &operator<<(std::ostream &os, const std::pair<T, U> &a) {
+  return os << "(" << a.first << ", " << a.second << ")";
+}
+
+void println() { std::cout << "\n"; }
+template <class T>
+void println(const T &x) {
+  std::cout << x << "\n";
+}
+template <typename Head, typename... Tail>
+void println(const Head &head, Tail... tail) {
+  std::cout << head << " ";
+  println(tail...);
+}
+
+template <typename Container>
+std::ostream &print_seq(const Container &a, std::string_view sep = " ",
+                        std::string_view ends = "\n",
+                        std::ostream &os = std::cout) {
+  auto b = std::begin(a), e = std::end(a);
+  for (auto it = std::begin(a); it != e; ++it) {
+    if (it != b) os << sep;
+    os << *it;
+  }
+  return os << ends;
+}
+template <typename T, typename = void>
+struct is_iterable : std::false_type {};
+template <typename T>
+struct is_iterable<T, std::void_t<decltype(std::begin(std::declval<T>())),
+                                  decltype(std::end(std::declval<T>()))>>
+    : std::true_type {};
+
+template <typename T, typename = std::enable_if_t<
+                          is_iterable<T>::value &&
+                          !std::is_same<T, std::string_view>::value &&
+                          !std::is_same<T, std::string>::value>>
+std::ostream &operator<<(std::ostream &os, const T &a) {
+  return print_seq(a, ", ", "", (os << "{")) << "}";
+}
+
+#ifdef ENABLE_DEBUG
+#include "debug_dump.hpp"
+#else
+#define DUMP(...)
+#endif
+
+using namespace std;
+
+struct UnionFind {
+  int n;
+  mutable std::vector<int> parent;  // positive: parent, negative: size
+  int num_roots;
+
+  explicit UnionFind(int sz) : n(sz), parent(sz, -1), num_roots(sz) {}
+
+  bool unite(int x, int y) {
+    x = find(x), y = find(y);
+    if (x == y) return false;
+    if (-parent[x] < -parent[y]) std::swap(x, y);
+    parent[x] += parent[y];
+    parent[y] = x;
+    --num_roots;
+    return true;
+  }
+
+  int find(int v) const {
+    if (parent[v] < 0) return v;
+    return parent[v] = find(parent[v]);
+  }
+
+  int size(int v) const { return -parent[find(v)]; }
+
+  bool same(int x, int y) const { return find(x) == find(y); }
+
+  std::vector<int> roots() const {
+    std::vector<int> res;
+    res.reserve(num_roots);
+    for (int i = 0; i < n; ++i) {
+      if (parent[i] < 0) res.push_back(i);
+    }
+    return res;
+  }
+};
+
+// Returns the topogically sorted list of nodes.
+// Nodes with zero indegree come first.
+// Returns nullopt if the input graph is not a DAG.
+// Args:
+//   g[a] = {b, c, d}; // directed adjacency list {a->b, a->c, a->d}
+optional<vector<int>> topological_sort(const vector<vector<int>> &g) {
+  const int n = g.size();
+  vector<int> indeg(n);
+  for (int v = 0; v < n; ++v) {
+    for (auto u : g[v]) ++indeg[u];
+  }
+  deque<int> q;
+  for (int v = 0; v < n; ++v) {
+    if (indeg[v] == 0) {
+      q.emplace_back(v);
+    }
+  }
+  vector<int> res;
+  while (not q.empty()) {
+    auto v = q.front();
+    q.pop_front();
+    res.push_back(v);
+    for (auto u : g[v]) {
+      if (--indeg[u] == 0) {
+        q.push_back(u);
+      }
+    }
+  }
+  if (int(res.size()) != n) {
+    return nullopt;  // not a DAG
+  }
+  return res;
+}
+
+template <typename T>
+struct Compressed {
+  std::vector<T> values;
+
+  explicit Compressed(std::vector<T> v) : values(v) {
+    std::sort(values.begin(), values.end());
+    values.erase(std::unique(values.begin(), values.end()), values.end());
+  }
+
+  int size() const { return values.size(); }
+
+  int index(T x) const {
+    return std::lower_bound(values.begin(), values.end(), x) - values.begin();
+  }
+
+  const T &value(int i) const { return values[i]; }
+};
+
+auto solve() -> void {
+  int n, m;
+  cin >> n >> m;
+  vector<vector<int>> g(n);
+  UnionFind uf(n);
+  REP(i, m) {
+    int u, v;
+    cin >> u >> v;
+    --u, --v;
+    g[u].push_back(v);
+    uf.unite(u, v);
+  }
+
+  map<int, vector<int>> components;
+  REP(i, n) {
+    int r = uf.find(i);
+    components[r].push_back(i);
+  }
+  DUMP(components);
+
+  vector<pair<int, int>> edges;
+  for (const auto &[r, nodes] : components) {
+    if (ssize(nodes) <= 1) continue;
+    Compressed<int> cnodes(nodes);
+
+    vector<vector<int>> p(cnodes.size());
+    for (auto u : nodes) {
+      int v = cnodes.index(u);
+      for (auto w : g[u]) {
+        p[v].push_back(cnodes.index(w));
+      }
+    }
+    auto topo = topological_sort(p);
+    if (topo.has_value()) {
+      const auto &vs = topo.value();
+      REP(j, 1, ssize(vs)) {
+        edges.emplace_back(cnodes.value(vs[j - 1]), cnodes.value(vs[j]));
+      }
+    } else {
+      REP(j, 1, ssize(nodes)) { edges.emplace_back(nodes[j - 1], nodes[j]); }
+      edges.emplace_back(nodes.back(), nodes.front());
+    }
+  }
+
+  cout << ssize(edges) << "\n";
+  for (const auto &[a, b] : edges) {
+    cout << a + 1 << " " << b + 1 << "\n";
+  }
+}
+
+int main() {
+  ios_base::sync_with_stdio(false), cin.tie(nullptr);
+  int t = 1;
+  REP(test_case, t) { solve(); }
+}
