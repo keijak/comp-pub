7fb18c116ec02f88f46b8c20981cf25a8456dd39
keijak <m@keijak.com>
2021-06-22 23:07:12 +0200

    matrix

---
+++ atcoder/typical90/073/main.cpp
@@ -0,0 +1,267 @@
+#include <bits/stdc++.h>
+#define REP_(i, a_, b_, a, b, ...) \
+  for (int i = (a), END_##i = (b); i < END_##i; ++i)
+#define REP(i, ...) REP_(i, __VA_ARGS__, __VA_ARGS__, 0, __VA_ARGS__)
+#define ALL(x) std::begin(x), std::end(x)
+using i64 = long long;
+
+#include <atcoder/modint>
+using Mint = atcoder::modint1000000007;
+std::ostream &operator<<(std::ostream &os, const Mint &m) {
+  return os << m.val();
+}
+
+template <typename T, typename U>
+inline bool chmax(T &a, U b) {
+  return a < b and ((a = std::move(b)), true);
+}
+template <typename T, typename U>
+inline bool chmin(T &a, U b) {
+  return a > b and ((a = std::move(b)), true);
+}
+template <typename T>
+inline int ssize(const T &a) {
+  return (int)std::size(a);
+}
+
+template <typename T>
+std::istream &operator>>(std::istream &is, std::vector<T> &a) {
+  for (auto &x : a) is >> x;
+  return is;
+}
+template <typename T, typename U>
+std::ostream &operator<<(std::ostream &os, const std::pair<T, U> &a) {
+  return os << "(" << a.first << ", " << a.second << ")";
+}
+template <typename Container>
+std::ostream &print_seq(const Container &a, std::string_view sep = " ",
+                        std::string_view ends = "\n",
+                        std::ostream &os = std::cout) {
+  auto b = std::begin(a), e = std::end(a);
+  for (auto it = std::begin(a); it != e; ++it) {
+    if (it != b) os << sep;
+    os << *it;
+  }
+  return os << ends;
+}
+template <typename T, typename = void>
+struct is_iterable : std::false_type {};
+template <typename T>
+struct is_iterable<T, std::void_t<decltype(std::begin(std::declval<T>())),
+                                  decltype(std::end(std::declval<T>()))>>
+    : std::true_type {};
+
+template <typename T, typename = std::enable_if_t<
+                          is_iterable<T>::value &&
+                          !std::is_same<T, std::string_view>::value &&
+                          !std::is_same<T, std::string>::value>>
+std::ostream &operator<<(std::ostream &os, const T &a) {
+  return print_seq(a, ", ", "", (os << "{")) << "}";
+}
+
+void print() { std::cout << "\n"; }
+template <class T>
+void print(const T &x) {
+  std::cout << x << "\n";
+}
+template <typename Head, typename... Tail>
+void print(const Head &head, Tail... tail) {
+  std::cout << head << " ";
+  print(tail...);
+}
+
+void read_from_cin() {}
+template <typename T, typename... Ts>
+void read_from_cin(T &value, Ts &...args) {
+  std::cin >> value;
+  read_from_cin(args...);
+}
+#define INPUT(type, ...) \
+  type __VA_ARGS__;      \
+  read_from_cin(__VA_ARGS__);
+
+#ifdef ENABLE_DEBUG
+#include "debug_dump.hpp"
+#else
+#define DUMP(...)
+#endif
+
+using namespace std;
+
+template <class T = Mint>
+struct Matrix {
+  using Row = std::vector<T>;
+  std::vector<Row> data;
+
+  Matrix() {}
+  Matrix(int n, int m) : data(n, Row(m, 0)) {}
+  explicit Matrix(int n) : data(n, Row(n, 0)){};
+  explicit Matrix(std::vector<Row> a) : data(std::move(a)) {}
+  Matrix(std::initializer_list<Row> a) : data(std::move(a)) {}
+
+  inline int height() const { return (int)(data.size()); }
+
+  inline int width() const { return (int)(data[0].size()); }
+
+  inline const Row &operator[](int k) const { return data[k]; }
+
+  inline Row &operator[](int k) { return data[k]; }
+
+  static Matrix I(int n) {
+    Matrix mat(n);
+    for (int i = 0; i < n; i++) mat[i][i] = 1;
+    return mat;
+  }
+
+  static Matrix row(std::vector<T> v) {
+    Matrix mat(1, v.size());
+    mat[0] = std::move(v);
+    return mat;
+  }
+
+  static Matrix column(const std::vector<T> &v) {
+    const int n = v.size();
+    Matrix mat(n, 1);
+    for (int i = 0; i < n; ++i) mat[i][0] = v[i];
+    return mat;
+  }
+
+  Matrix transpose() const {
+    int n = height(), m = width();
+    Matrix mat(m, n);
+    for (int i = 0; i < n; ++i) {
+      for (int j = 0; j < m; ++j) {
+        mat[j][i] = (*this)[i][j];
+      }
+    }
+    return mat;
+  }
+
+  Matrix &operator+=(const Matrix &B) {
+    int n = height(), m = width();
+    assert(n == B.height() and m == B.width());
+    for (int i = 0; i < n; ++i)
+      for (int j = 0; j < m; ++j) (*this)[i][j] += B[i][j];
+    return *this;
+  }
+
+  Matrix &operator-=(const Matrix &B) {
+    int n = height(), m = width();
+    assert(n == B.height() and m == B.width());
+    for (int i = 0; i < n; ++i)
+      for (int j = 0; j < m; ++j) (*this)[i][j] -= B[i][j];
+    return *this;
+  }
+
+  Matrix &operator*=(const Matrix &B) {
+    int n = height(), m = B.width(), p = width();
+    assert(p == B.height());
+    std::vector<Row> C(n, Row(m, 0));
+    for (int i = 0; i < n; i++)
+      for (int j = 0; j < m; j++)
+        for (int k = 0; k < p; k++)
+          C[i][j] = (C[i][j] + (*this)[i][k] * B[k][j]);
+    std::swap(data, C);
+    return *this;
+  }
+
+  Matrix &operator^=(long long k) {
+    Matrix B = Matrix::I(height());
+    while (k > 0) {
+      if (k & 1) B *= *this;
+      *this *= *this;
+      k >>= 1LL;
+    }
+    data.swap(B.data);
+    return *this;
+  }
+
+  Matrix operator+(const Matrix &B) const { return (Matrix(*this) += B); }
+
+  Matrix operator-(const Matrix &B) const { return (Matrix(*this) -= B); }
+
+  Matrix operator*(const Matrix &B) const { return (Matrix(*this) *= B); }
+
+  Matrix operator^(const long long k) const { return (Matrix(*this) ^= k); }
+
+  Matrix pow(const long long k) const { return (*this) ^ k; }
+
+  friend std::ostream &operator<<(std::ostream &os, const Matrix &p) {
+    int n = p.height();
+    for (int i = 0; i < n; i++) {
+      os << p[i] << "\n";
+    }
+    return os;
+  }
+
+  T det() {
+    Matrix B(*this);
+    assert(width() == height());
+    T ret = 1;
+    for (int i = 0; i < width(); i++) {
+      int idx = -1;
+      for (int j = i; j < width(); j++) {
+        if (B[j][i] != 0) idx = j;
+      }
+      if (idx == -1) return 0;
+      if (i != idx) {
+        ret *= -1;
+        swap(B[i], B[idx]);
+      }
+      ret *= B[i][i];
+      T vv = B[i][i];
+      for (int j = 0; j < width(); j++) {
+        B[i][j] /= vv;
+      }
+      for (int j = i + 1; j < width(); j++) {
+        T a = B[j][i];
+        for (int k = 0; k < width(); k++) {
+          B[j][k] -= B[i][k] * a;
+        }
+      }
+    }
+    return ret;
+  }
+};
+
+void solve() {
+  INPUT(int, n);
+  vector<char> C(n);
+  REP(i, n) cin >> C[i];
+
+  vector<vector<int>> g(n);
+  REP(i, n - 1) {
+    INPUT(int, u, v);
+    --u, --v;
+    g[u].push_back(v);
+    g[v].push_back(u);
+  }
+
+  // Returns a 1x3 matrix.
+  // x[0] := count patterns where v's component contains both A and B.
+  // x[1] := count patterns where v's component contains only A.
+  // x[2] := count patterns where v's component contains only B.
+  auto dfs = [&](auto &dfs, int v, int parent = -1) -> Matrix<Mint> {
+    auto x = Matrix<Mint>(1, 3);
+    x[0][1] = (C[v] == 'a') ? 1 : 0;
+    x[0][2] = (C[v] == 'b') ? 1 : 0;
+    for (auto u : g[v]) {
+      if (u == parent) continue;
+      auto y = dfs(dfs, u, v)[0];
+      auto A = Matrix<Mint>(3, 3);
+      A[0][0] = (y[0] + y[1]) + (y[0] + y[2]);
+      A[1][1] = A[2][0] = (y[0] + y[1]);
+      A[2][2] = A[1][0] = (y[0] + y[2]);
+      x *= A;
+    }
+    return x;
+  };
+  auto result = dfs(dfs, 0);
+  print(result[0][0]);
+}
+
+int main() {
+  ios_base::sync_with_stdio(false), cin.tie(nullptr);
+  int t = 1;
+  REP(test_case, t) { solve(); }
+}
