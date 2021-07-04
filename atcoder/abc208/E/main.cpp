ceafef8d204c15000c6d69227b76730ea86330d1
keijak <m@keijak.com>
2021-07-04 20:36:12 +0200

    abc

---
+++ atcoder/abc208/E/main.cpp
@@ -0,0 +1,138 @@
+#include <bits/stdc++.h>
+#define REP_(i, a_, b_, a, b, ...) \
+  for (int i = (a), END_##i = (b); i < END_##i; ++i)
+#define REP(i, ...) REP_(i, __VA_ARGS__, __VA_ARGS__, 0, __VA_ARGS__)
+#define ALL(x) std::begin(x), std::end(x)
+using i64 = long long;
+
+#include <boost/multiprecision/cpp_int.hpp>
+// using i128 = boost::multiprecision::checked_int128_t;
+// using u128 = boost::multiprecision::checked_uint128_t;
+using Int = boost::multiprecision::checked_cpp_int;
+// using Rational = boost::multiprecision::checked_cpp_rational;
+namespace multip = boost::multiprecision;
+
+template<typename T, typename U>
+inline bool chmax(T &a, U b) {
+  return a < b and ((a = std::move(b)), true);
+}
+template<typename T, typename U>
+inline bool chmin(T &a, U b) {
+  return a > b and ((a = std::move(b)), true);
+}
+template<typename T>
+inline int ssize(const T &a) {
+  return (int) std::size(a);
+}
+
+template<typename T>
+std::istream &operator>>(std::istream &is, std::vector<T> &a) {
+  for (auto &x : a) is >> x;
+  return is;
+}
+template<typename T, typename U>
+std::ostream &operator<<(std::ostream &os, const std::pair<T, U> &a) {
+  return os << "(" << a.first << ", " << a.second << ")";
+}
+template<typename Container>
+std::ostream &print_seq(const Container &a, std::string_view sep = " ",
+                        std::string_view ends = "\n",
+                        std::ostream &os = std::cout) {
+  auto b = std::begin(a), e = std::end(a);
+  for (auto it = std::begin(a); it != e; ++it) {
+    if (it != b) os << sep;
+    os << *it;
+  }
+  return os << ends;
+}
+template<typename T, typename = void>
+struct is_iterable : std::false_type {};
+template<typename T>
+struct is_iterable<T, std::void_t<decltype(std::begin(std::declval<T>())),
+                                  decltype(std::end(std::declval<T>()))>>
+    : std::true_type {
+};
+
+template<typename T, typename = std::enable_if_t<
+    is_iterable<T>::value &&
+        !std::is_same<T, std::string_view>::value &&
+        !std::is_same<T, std::string>::value>>
+std::ostream &operator<<(std::ostream &os, const T &a) {
+  return print_seq(a, ", ", "", (os << "{")) << "}";
+}
+
+void print() { std::cout << "\n"; }
+template<class T>
+void print(const T &x) {
+  std::cout << x << "\n";
+}
+template<typename Head, typename... Tail>
+void print(const Head &head, Tail... tail) {
+  std::cout << head << " ";
+  print(tail...);
+}
+
+void read_from_cin() {}
+template<typename T, typename... Ts>
+void read_from_cin(T &value, Ts &...args) {
+  std::cin >> value;
+  read_from_cin(args...);
+}
+#define INPUT(type, ...) \
+  type __VA_ARGS__;      \
+  read_from_cin(__VA_ARGS__)
+
+#ifdef ENABLE_DEBUG
+#include "debug_dump.hpp"
+#else
+#define DUMP(...)
+#endif
+
+using namespace std;
+
+auto solve() {
+  INPUT(i64, N, K);
+  string S = std::to_string(N);
+  const int n = S.size();
+  auto dp = vector(n + 1, vector(2, vector(2, map<Int, i64>())));
+  dp[0][0][0][1] = 1;
+  const i64 overK = 2000000000LL;
+
+  REP(i, n) {
+    int curd = S[i] - '0';
+    REP(smaller, 2) REP(nonzero, 2) REP(d, 10) {
+          bool smaller2 = smaller;
+          bool nonzero2 = nonzero;
+          if (not smaller and d > curd) continue;
+          if (not smaller and d < curd) smaller2 = true;
+          if (not nonzero and d != 0) nonzero2 = true;
+          for (auto[prod, count] : dp[i][smaller][nonzero]) {
+            if (d == 0 and not nonzero2 and prod == 1) {
+              dp[i + 1][smaller2][nonzero2][1] += count;
+            } else {
+              Int prod2 = prod * d;
+              if (prod2 > K) prod2 = overK;
+              dp[i + 1][smaller2][nonzero2][prod2] += count;
+            }
+          }
+        }
+  }
+  Int ans = 0;
+  REP(smaller, 2) {
+    for (auto[prod, count] : dp[n][smaller][1]) {
+      if (prod <= K) {
+        ans += count;
+      }
+    }
+  }
+  return ans;
+}
+
+int main() {
+  ios_base::sync_with_stdio(false), cin.tie(nullptr);
+  int t = 1;
+  REP(test_case, t) {
+    auto ans = solve();
+    print(ans);
+  }
+}
