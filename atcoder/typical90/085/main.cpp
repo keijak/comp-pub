36fc8b5d3f6aa917abb228c52e1a413fb7d6f43e
keijak <m@keijak.com>
2021-07-06 12:48:38 +0200

    typical

---
+++ atcoder/typical90/085/main.cpp
@@ -0,0 +1,153 @@
+#include <bits/stdc++.h>
+#define REP_(i, a_, b_, a, b, ...) \
+  for (int i = (a), END_##i = (b); i < END_##i; ++i)
+#define REP(i, ...) REP_(i, __VA_ARGS__, __VA_ARGS__, 0, __VA_ARGS__)
+#define ALL(x) std::begin(x), std::end(x)
+using i64 = long long;
+
+#include <boost/multiprecision/cpp_int.hpp>
+using i128 = boost::multiprecision::checked_int128_t;
+// using u128 = boost::multiprecision::checked_uint128_t;
+// using Int = boost::multiprecision::checked_cpp_int;
+// using Rational = boost::multiprecision::checked_cpp_rational;
+namespace multip = boost::multiprecision;
+
+template<typename T, typename U>
+inline bool chmax(T &a, U b) {
+  return a < b and ((a = std::move(b)), true);
+}
+template<typename T, typename U>
+inline bool chmin(T &a, U b) {
+  return a > b and ((a = std::move(b)), true);
+}
+template<typename T>
+inline int ssize(const T &a) {
+  return (int) std::size(a);
+}
+
+template<typename T>
+std::istream &operator>>(std::istream &is, std::vector<T> &a) {
+  for (auto &x : a) is >> x;
+  return is;
+}
+template<typename T, typename U>
+std::ostream &operator<<(std::ostream &os, const std::pair<T, U> &a) {
+  return os << "(" << a.first << ", " << a.second << ")";
+}
+template<typename Container>
+std::ostream &print_seq(const Container &a, std::string_view sep = " ",
+                        std::string_view ends = "\n",
+                        std::ostream &os = std::cout) {
+  auto b = std::begin(a), e = std::end(a);
+  for (auto it = std::begin(a); it != e; ++it) {
+    if (it != b) os << sep;
+    os << *it;
+  }
+  return os << ends;
+}
+template<typename T, typename = void>
+struct is_iterable : std::false_type {};
+template<typename T>
+struct is_iterable<T, std::void_t<decltype(std::begin(std::declval<T>())),
+                                  decltype(std::end(std::declval<T>()))>>
+    : std::true_type {
+};
+
+template<typename T, typename = std::enable_if_t<
+    is_iterable<T>::value &&
+        !std::is_same<T, std::string_view>::value &&
+        !std::is_same<T, std::string>::value>>
+std::ostream &operator<<(std::ostream &os, const T &a) {
+  return print_seq(a, ", ", "", (os << "{")) << "}";
+}
+
+void print() { std::cout << "\n"; }
+template<class T>
+void print(const T &x) {
+  std::cout << x << "\n";
+}
+template<typename Head, typename... Tail>
+void print(const Head &head, Tail... tail) {
+  std::cout << head << " ";
+  print(tail...);
+}
+
+void read_from_cin() {}
+template<typename T, typename... Ts>
+void read_from_cin(T &value, Ts &...args) {
+  std::cin >> value;
+  read_from_cin(args...);
+}
+#define INPUT(type, ...) \
+  type __VA_ARGS__;      \
+  read_from_cin(__VA_ARGS__)
+
+#ifdef ENABLE_DEBUG
+#include "debug_dump.hpp"
+#else
+#define DUMP(...)
+#endif
+
+using namespace std;
+
+void enumerate_divisors(i64 n, std::vector<i64> &divisors) {
+  std::vector<std::pair<i64, int>> fac;
+  for (i64 k = 2; k * k <= n; ++k) {
+    if (n % k != 0) continue;
+    int count = 0;
+    do {
+      n /= k;
+      ++count;
+    } while (n % k == 0);
+    fac.emplace_back(k, count);
+  }
+  if (n > 1) {
+    fac.emplace_back(n, 1);
+  }
+  const int m = fac.size();
+  auto rec = [&](auto &rec, int i, i64 val) -> void {
+    if (i == m) {
+      divisors.push_back(val);
+      return;
+    }
+    const auto &[p, k] = fac[i];
+    for (int j = 0; j <= k; ++j) {
+      rec(rec, i + 1, val);
+      val *= p;
+    }
+  };
+  rec(rec, 0, 1LL);
+}
+
+auto solve() {
+  INPUT(i64, K);
+  int ans = 0;
+  vector<i64> divs;
+  divs.reserve(7000);
+  enumerate_divisors(K, divs);
+  sort(ALL(divs));
+  const int m = divs.size();
+  REP(i, m) {
+    i128 a = divs[i];
+    if (a * a * a > K) break;
+    for (int j = i; j < m; ++j) {
+      i128 b = divs[j];
+      i128 ab = a * b;
+      if (b * ab > K) break;
+      if (K % ab == 0) {
+        ++ans;
+      }
+    }
+  }
+  return ans;
+}
+
+int main() {
+  ios_base::sync_with_stdio(false), cin.tie(nullptr);
+  cout << std::fixed << std::setprecision(15);
+  int t = 1;
+  REP(test_case, t) {
+    auto ans = solve();
+    print(ans);
+  }
+}
