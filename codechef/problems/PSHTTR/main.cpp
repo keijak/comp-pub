add71edf8a61e5c6229741857538e60a13c89603
keijak <m@keijak.com>
2021-07-08 22:16:52 +0200

    segtree offline

---
+++ codechef/problems/PSHTTR/main.cpp
@@ -0,0 +1,231 @@
+#include <bits/stdc++.h>
+#define REP_(i, a_, b_, a, b, ...) \
+  for (int i = (a), END_##i = (b); i < END_##i; ++i)
+#define REP(i, ...) REP_(i, __VA_ARGS__, __VA_ARGS__, 0, __VA_ARGS__)
+#define ALL(x) std::begin(x), std::end(x)
+using i64 = long long;
+
+template<typename T, typename U>
+inline bool chmax(T &a, U b) {
+  return a < b and ((a = std::move(b)), true);
+}
+template<typename T, typename U>
+inline bool chmin(T &a, U b) {
+  return a > b and ((a = std::move(b)), true);
+}
+template<typename T>
+inline int ssize(const T &a) {
+  return (int) a.size();
+}
+
+template<typename T>
+std::istream &operator>>(std::istream &is, std::vector<T> &a) {
+  for (auto &x : a) is >> x;
+  return is;
+}
+template<typename T, typename U>
+std::ostream &operator<<(std::ostream &os, const std::pair<T, U> &a) {
+  return os << "(" << a.first << ", " << a.second << ")";
+}
+template<typename Container>
+std::ostream &print_seq(const Container &a, const char *sep = " ",
+                        const char *ends = "\n",
+                        std::ostream &os = std::cout) {
+  auto b = std::begin(a), e = std::end(a);
+  for (auto it = std::begin(a); it != e; ++it) {
+    if (it != b) os << sep;
+    os << *it;
+  }
+  return os << ends;
+}
+
+void print() { std::cout << "\n"; }
+template<class T>
+void print(const T &x) {
+  std::cout << x << "\n";
+}
+template<typename Head, typename... Tail>
+void print(const Head &head, Tail... tail) {
+  std::cout << head << " ";
+  print(tail...);
+}
+
+void read_from_cin() {}
+template<typename T, typename... Ts>
+void read_from_cin(T &value, Ts &...args) {
+  std::cin >> value;
+  read_from_cin(args...);
+}
+#define INPUT(type, ...) \
+  type __VA_ARGS__;      \
+  read_from_cin(__VA_ARGS__)
+
+#ifdef ENABLE_DEBUG
+#include "debug_dump.hpp"
+#else
+#define DUMP(...)
+#endif
+
+using namespace std;
+
+template<typename Monoid>
+struct SegTree {
+  using T = typename Monoid::T;
+
+  int n_;                // number of valid leaves.
+  int offset_;           // where leaves start
+  std::vector<T> data_;  // data size: 2*offset_
+
+  inline int size() const { return n_; }
+  inline int offset() const { return offset_; }
+
+  explicit SegTree(int n) : n_(n) {
+    offset_ = 1;
+    while (offset_ < n_) offset_ <<= 1;
+    data_.assign(2 * offset_, Monoid::id());
+  }
+
+  explicit SegTree(const std::vector<T> &leaves) : n_(leaves.size()) {
+    offset_ = 1;
+    while (offset_ < n_) offset_ <<= 1;
+    data_.assign(2 * offset_, Monoid::id());
+    for (int i = 0; i < n_; ++i) {
+      data_[offset_ + i] = leaves[i];
+    }
+    for (int i = offset_ - 1; i > 0; --i) {
+      data_[i] = Monoid::op(data_[i * 2], data_[i * 2 + 1]);
+    }
+  }
+
+  // Sets i-th value (0-indexed) to x.
+  void set(int i, const T &x) {
+    int k = offset_ + i;
+    data_[k] = x;
+    // Update its ancestors.
+    while (k > 1) {
+      k >>= 1;
+      data_[k] = Monoid::op(data_[k * 2], data_[k * 2 + 1]);
+    }
+  }
+
+  // Queries by [l,r) range (0-indexed, half-open interval).
+  T fold(int l, int r) const {
+    l = std::max(l, 0) + offset_;
+    r = std::min(r, offset_) + offset_;
+    T vleft = Monoid::id(), vright = Monoid::id();
+    for (; l < r; l >>= 1, r >>= 1) {
+      if (l & 1) vleft = Monoid::op(vleft, data_[l++]);
+      if (r & 1) vright = Monoid::op(data_[--r], vright);
+    }
+    return Monoid::op(vleft, vright);
+  }
+
+  T fold_all() const { return data_[1]; }
+
+  // Returns i-th value (0-indexed).
+  T operator[](int i) const { return data_[offset_ + i]; }
+
+  friend std::ostream &operator<<(std::ostream &os, const SegTree &st) {
+    os << "[";
+    for (int i = 0; i < st.size(); ++i) {
+      if (i != 0) os << ", ";
+      const auto &x = st[i];
+      os << x;
+    }
+    return os << "]";
+  }
+};
+
+struct XorOp {
+  using T = unsigned;
+  static T op(const T &x, const T &y) { return x ^ y; }
+  static constexpr T id() { return 0; }
+};
+
+template<typename T>
+struct Compressed {
+  std::vector<T> values;
+
+  explicit Compressed(std::vector<T> v) : values(v) {
+    std::sort(values.begin(), values.end());
+    values.erase(std::unique(values.begin(), values.end()), values.end());
+  }
+
+  int size() const { return values.size(); }
+
+  int index(T x) const {
+    return std::lower_bound(values.begin(), values.end(), x) - values.begin();
+  }
+
+  const T &value(int i) const { return values[i]; }
+};
+
+struct Edge {
+  int to;
+  unsigned c;
+};
+
+auto solve() {
+  INPUT(int, n);
+  vector<vector<Edge>> g(n);
+  vector<unsigned> magic;
+  REP(i, n - 1) {
+    INPUT(int, u, v, c);
+    --u, --v;
+    g[u].push_back(Edge{v, (unsigned) c});
+    g[v].push_back(Edge{u, (unsigned) c});
+    magic.push_back(c);
+  }
+  INPUT(int, m);
+  vector<array<unsigned, 3>> queries(m);
+  vector<map<int, unsigned>> nodeq(n);
+  REP(i, m) {
+    INPUT(unsigned, u, v, k);
+    --u, --v;
+    queries[i] = {u, v, k};
+    nodeq[u][k] = 0;
+    nodeq[v][k] = 0;
+    magic.push_back(k);
+  }
+  Compressed<unsigned> cm(magic);
+  const int P = cm.size();
+
+  SegTree<XorOp> seg(P);
+  auto dfs = [&](auto &rec, int v, int p) -> void {
+    auto &nq = nodeq[v];
+    for (auto it = nq.begin(); it != nq.end(); ++it) {
+      unsigned k = it->first;
+      int j = cm.index(k + 1);
+      unsigned attr = seg.fold(0, j);
+      it->second = attr;
+    }
+    for (const Edge &e : g[v]) {
+      if (e.to == p) continue;
+      int ci = cm.index(e.c);
+      unsigned pv = seg[ci];
+      seg.set(ci, pv ^ e.c);
+      rec(rec, e.to, v);
+      seg.set(ci, pv);
+    }
+  };
+  dfs(dfs, 0, -1);
+
+  REP(i, m) {
+    unsigned u = queries[i][0];
+    unsigned v = queries[i][1];
+    unsigned k = queries[i][2];
+    auto a1 = nodeq[u][k];
+    auto a2 = nodeq[v][k];
+    print(a1 ^ a2);
+  }
+}
+
+int main() {
+  ios_base::sync_with_stdio(false), cin.tie(nullptr);
+  cout << std::fixed << std::setprecision(15);
+  int t = 1;
+  cin >> t;
+  REP(test_case, t) {
+    solve();
+  }
+}
