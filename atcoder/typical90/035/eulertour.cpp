1355f142341bdd4f79dacfa7e0499adba0c14079
keijak <m@keijak.com>
2021-07-12 16:25:58 +0200

    hld distance

--- atcoder/typical90/035/eulertour.cpp
+++ atcoder/typical90/035/eulertour.cpp
@@ -40,15 +40,14 @@ inline void rd(char &c) {
   if (pil + 32 > pir) load();
   c = ibuf[pil++];
 }
-template<typename T>
+template <typename T>
 inline void rd(T &x) {
   if (pil + 32 > pir) load();
   char c;
   do c = ibuf[pil++];
   while (c < '-');
   [[maybe_unused]] bool minus = false;
-  if constexpr(std::is_signed<T>::value == true)
-  {
+  if constexpr (std::is_signed<T>::value == true) {
     if (c == '-') minus = true, c = ibuf[pil++];
   }
   x = 0;
@@ -56,13 +55,12 @@ inline void rd(T &x) {
     x = x * 10 + (c & 15);
     c = ibuf[pil++];
   }
-  if constexpr(std::is_signed<T>::value == true)
-  {
+  if constexpr (std::is_signed<T>::value == true) {
     if (minus) x = -x;
   }
 }
 inline void rd() {}
-template<typename Head, typename... Tail>
+template <typename Head, typename... Tail>
 inline void rd(Head &head, Tail &...tail) {
   rd(head);
   rd(tail...);
@@ -89,8 +87,7 @@ struct Pre {
       }
     }
   }
-} constexpr
-pre;
+} constexpr pre;
 
 struct Post {
   Post() { std::atexit(flush); }
@@ -104,15 +101,14 @@ inline void wt(bool b) {
   if (por > SZ - 32) flush();
   obuf[por++] = b ? '1' : '0';
 }
-template<typename T>
+template <typename T>
 inline void wt(T x) {
   if (por > SZ - 32) flush();
   if (!x) {
     obuf[por++] = '0';
     return;
   }
-  if constexpr(std::is_signed<T>::value == true)
-  {
+  if constexpr (std::is_signed<T>::value == true) {
     if (x < 0) obuf[por++] = '-', x = -x;
   }
   int i = 12;
@@ -147,12 +143,12 @@ inline void wt(T x) {
 }
 
 inline void wt() {}
-template<typename Head, typename... Tail>
+template <typename Head, typename... Tail>
 inline void wt(Head &&head, Tail &&...tail) {
   wt(head);
   wt(std::forward<Tail>(tail)...);
 }
-template<typename... Args>
+template <typename... Args>
 inline void wtn(Args &&...x) {
   wt(std::forward<Args>(x)...);
   wt('\n');
@@ -165,7 +161,7 @@ using fastio::wtn;
 
 using namespace std;
 
-template<class SemiLattice>
+template <class SemiLattice>
 struct SparseTable {
   using T = typename SemiLattice::T;
 
@@ -213,127 +209,77 @@ struct SparseTable {
 struct Min {
   using T = int;
   static T op(const T &x, const T &y) { return std::min(x, y); }
-  static constexpr T
-  id() { return std::numeric_limits<T>::max(); }
+  static constexpr T id() { return std::numeric_limits<T>::max(); }
 };
 
-// Heavy-Light Decomposition
-struct HLDecomp {
-  // Whether the given adjacency list only contains children or may also contain
-  // parents. If true, skips removing parent from the graph for speedup.
-  static constexpr bool kChildrenOnlyGraph = false;
+// tour: preorder node ids
+// The interval [begin[v], end[v]) represents a subtree whose
+// root is v.
+struct EulerTour {
+  using G = vector<vector<int>>;
 
-  using NodeID = int;                       // [0, n)
-  using G = std::vector<std::vector<int>>;  // undirected graph
-  // half-open intervals of preorder indices of nodes.
-  using IntervalVec = std::vector<std::pair<int, int>>;
+  const int n;  // number of nodes
+  G g;
 
-  int n;                       // number of nodes in the tree
-  NodeID root;                 // root of the tree
-  G child;                     // children node ids
-  std::vector<NodeID> parent;  // parent node id (or -1 on the root node)
-  std::vector<int> subsize;    // subtree size
-  // "ord" is preorder index in DFS traversal. [0, n)
-  std::vector<int> node_to_ord;     // node id to preorder index
-  std::vector<NodeID> ord_to_node;  // preorder index to node id
-  std::vector<NodeID> comp_root;    // node id to its heavy path component
-  int ord_counter_;
+  // Euler Tour on nodes.
+  vector<int> tour;   // depth
+  vector<int> begin;  // index in the tour on entering the subtree of v
+  vector<int> end;    // index in the tour on exiting the subtree of v
+  vector<int> depth;
 
-  explicit HLDecomp(G g, NodeID root = 0)
-      : n(int(g.size())),
-        root(root),
-        child(g),
-        parent(n, -1),
-        subsize(n, 1),
-        node_to_ord(n, -1),
-        ord_to_node(n, -1),
-        comp_root(n, -1),
-        ord_counter_(0) {
-    dfs_subsize(root);
-    comp_root[root] = root;
-    dfs_hld(root);
-  }
-
-  // Distance (= number of edges) of the path between two nodes.
-  int distance(NodeID u, NodeID v) const {
-    int dist = 0;
-    for (;;) {
-      if (node_to_ord[u] > node_to_ord[v]) std::swap(u, v);
-      NodeID crv = comp_root[v];
-      if (comp_root[u] == crv) {
-        if (u != v) dist += node_to_ord[v] - node_to_ord[u];
-        break;
-      }
-      dist += node_to_ord[v] - node_to_ord[crv] + 1;
-      v = parent[crv];
-    }
-    return dist;
+  explicit EulerTour(G g, int root = 0)
+      : n((int)g.size()), g(move(g)), begin(n, -1), end(n, -1), depth(n, -1) {
+    tour.reserve(n * 2);
+    depth[root] = 0;
+    dfs(root, -1);
   }
 
  private:
-  // Fills `parent` and `subsize`.
-  void dfs_subsize(NodeID v) {
-    auto &edges = child[v];
-    if constexpr(not kChildrenOnlyGraph)
-    {
-      if (parent[v] != -1) {
-        // Remove the parent from `child[v]`. Amortized O(N).
-        auto it = std::find(edges.begin(), edges.end(), parent[v]);
-        if (it != edges.end()) {
-          std::swap(*it, edges.back());
-          edges.pop_back();
-        }
-      }
+  void dfs(int v, int p) {
+    begin[v] = int(tour.size());
+    if (p >= 0) depth[v] = depth[p] + 1;
+    tour.push_back(depth[v]);
+    for (auto u : g[v]) {
+      if (u == p) continue;
+      dfs(u, v);
+      tour.push_back(depth[v]);
     }
-    for (NodeID u : edges) {
-      parent[u] = v;
-      dfs_subsize(u);
-      subsize[v] += subsize[u];
-      if (subsize[u] > subsize[edges[0]]) {
-        std::swap(u, edges[0]);
-      }
-    }
-  }
-
-  // Fills `node_to_ord`, `ord_to_node`, and `comp_root`.
-  void dfs_hld(NodeID v) {
-    ord_to_node[ord_counter_] = v;
-    node_to_ord[v] = ord_counter_++;
-    for (NodeID u : child[v]) {
-      comp_root[u] = (u == child[v][0] ? comp_root[v] : u);
-      dfs_hld(u);
-    }
+    end[v] = int(tour.size());
   }
 };
 
 int main() {
-  unsigned n;
+  int n;
   rd(n);
   vector<vector<int>> g(n);
   REP(i, n - 1) {
-    unsigned a, b;
+    int a, b;
     rd(a, b);
     --a, --b;
     g[a].push_back(b);
     g[b].push_back(a);
   }
-  HLDecomp hld(std::move(g));
+  EulerTour et(g);
+  SparseTable<Min> st(et.tour);
   vector<int> ord;
-  ord.reserve(n);
-  unsigned Q, k, v, res;
+  ord.reserve(200005);
+  int Q;
   rd(Q);
   REP(_, Q) {
+    int k;
     rd(k);
     ord.resize(k);
     REP(i, k) {
+      int v;
       rd(v);
-      ord[i] = hld.node_to_ord[--v];
+      --v;
+      ord[i] = et.begin[v];
     }
     sort(ALL(ord));
-    res = hld.distance(hld.ord_to_node[ord[0]], hld.ord_to_node[ord[k - 1]]);
-    for (unsigned i = 1; i < k; ++i) {
-      res += hld.distance(hld.ord_to_node[ord[i]], hld.ord_to_node[ord[i - 1]]);
+    int res = et.tour[ord[0]] - st.fold(ord.front(), ord.back() + 1);
+    for (int i = 1; i < k; ++i) {
+      res += et.tour[ord[i]] - st.fold(ord[i - 1], ord[i] + 1);
     }
-    wtn(res / 2);
+    wtn(res);
   }
 }
