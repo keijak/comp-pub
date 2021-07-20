ca7ae05fd9f5f247c7b560e6b9806f4e261590b9
keijak <m@keijak.com>
2021-07-20 02:31:57 +0200

    insertion dp

---
+++ cf/contest/0840/C/main.cpp
@@ -0,0 +1,343 @@
+#include <bits/stdc++.h>
+#define REP_(i, a_, b_, a, b, ...) \
+  for (int i = (a), END_##i = (b); i < END_##i; ++i)
+#define REP(i, ...) REP_(i, __VA_ARGS__, __VA_ARGS__, 0, __VA_ARGS__)
+#define ALL(x) std::begin(x), std::end(x)
+using i64 = long long;
+
+template<unsigned M>
+struct ModInt {
+  constexpr ModInt(long long val = 0) : _v(0) {
+    if (val < 0) {
+      long long k = (std::abs(val) + M - 1) / M;
+      val += k * M;
+    }
+    assert(val >= 0);
+    _v = val % M;
+  }
+
+  static constexpr int mod() { return M; }
+  static constexpr unsigned umod() { return M; }
+  inline unsigned val() const { return _v; }
+
+  ModInt &operator++() {
+    _v++;
+    if (_v == umod()) _v = 0;
+    return *this;
+  }
+  ModInt &operator--() {
+    if (_v == 0) _v = umod();
+    _v--;
+    return *this;
+  }
+  ModInt operator++(int) {
+    auto result = *this;
+    ++*this;
+    return result;
+  }
+  ModInt operator--(int) {
+    auto result = *this;
+    --*this;
+    return result;
+  }
+
+  constexpr ModInt operator-() const { return ModInt(umod() - _v); }
+
+  constexpr ModInt &operator+=(const ModInt &a) {
+    if ((_v += a._v) >= M) _v -= M;
+    return *this;
+  }
+  constexpr ModInt &operator-=(const ModInt &a) {
+    if ((_v += M - a._v) >= M) _v -= M;
+    return *this;
+  }
+  constexpr ModInt &operator*=(const ModInt &a) {
+    _v = ((unsigned long long) (_v) * a._v) % M;
+    return *this;
+  }
+  constexpr ModInt pow(unsigned long long t) const {
+    ModInt base = *this;
+    ModInt res = 1;
+    while (t) {
+      if (t & 1) res *= base;
+      base *= base;
+      t >>= 1;
+    }
+    return res;
+  }
+
+  constexpr ModInt inv() const {
+    // Inverse by Extended Euclidean algorithm.
+    // M doesn't need to be prime, but x and M must be coprime.
+    assert(_v != 0);
+    auto[g, x, y] = ext_gcd(_v, M);
+    assert(g == 1);  // The GCD must be 1.
+    return x;
+
+    // Inverse by Fermat's little theorem. M must be prime.
+    //
+    //     return pow(M - 2);
+  }
+  constexpr ModInt &operator/=(const ModInt &a) { return *this *= a.inv(); }
+
+  friend constexpr ModInt operator+(const ModInt &a, const ModInt &b) {
+    return ModInt(a) += b;
+  }
+  friend constexpr ModInt operator-(const ModInt &a, const ModInt &b) {
+    return ModInt(a) -= b;
+  }
+  friend constexpr ModInt operator*(const ModInt &a, const ModInt &b) {
+    return ModInt(a) *= b;
+  }
+  friend constexpr ModInt operator/(const ModInt &a, const ModInt &b) {
+    return ModInt(a) /= b;
+  }
+  friend constexpr bool operator==(const ModInt &a, const ModInt &b) {
+    return a._v == b._v;
+  }
+  friend constexpr bool operator!=(const ModInt &a, const ModInt &b) {
+    return a._v != b._v;
+  }
+  friend std::istream &operator>>(std::istream &is, ModInt &a) {
+    return is >> a._v;
+  }
+  friend std::ostream &operator<<(std::ostream &os, const ModInt &a) {
+    return os << a._v;
+  }
+
+ private:
+  // Extended Euclidean algorithm.
+  // Returns [g, x, y] where g = a*x + b*y = GCD(a, b).
+  static constexpr std::array<long long, 3> ext_gcd(int a, int b) {
+    if (b == 0) return {a, 1, 0};
+    auto res = ext_gcd(b, a % b);  // = (g, x, y)
+    res[1] -= (a / b) * res[2];
+    std::swap(res[1], res[2]);
+    return res;  // = (g, y, x - (a/b)*y)
+  }
+
+  unsigned _v;  // raw value
+};
+const unsigned MOD = int(1e9) + 7;
+using Mint = ModInt<MOD>;
+
+template<typename T, typename U>
+inline bool chmax(T &a, U b) {
+  return a < b and ((a = std::move(b)), true);
+}
+template<typename T, typename U>
+inline bool chmin(T &a, U b) {
+  return a > b and ((a = std::move(b)), true);
+}
+template<typename T>
+inline int ssize(const T &a) {
+  return (int) std::size(a);
+}
+
+template<typename T>
+std::istream &operator>>(std::istream &is, std::vector<T> &a) {
+  for (auto &x : a) is >> x;
+  return is;
+}
+template<typename T, typename U>
+std::ostream &operator<<(std::ostream &os, const std::pair<T, U> &a) {
+  return os << "(" << a.first << ", " << a.second << ")";
+}
+template<typename Container>
+std::ostream &print_seq(const Container &a, std::string_view sep = " ",
+                        std::string_view ends = "\n",
+                        std::ostream &os = std::cout) {
+  auto b = std::begin(a), e = std::end(a);
+  for (auto it = std::begin(a); it != e; ++it) {
+    if (it != b) os << sep;
+    os << *it;
+  }
+  return os << ends;
+}
+template<typename T, typename = void>
+struct is_iterable : std::false_type {};
+template<typename T>
+struct is_iterable<T, std::void_t<decltype(std::begin(std::declval<T>())),
+                                  decltype(std::end(std::declval<T>()))>>
+    : std::true_type {
+};
+
+template<typename T, typename = std::enable_if_t<
+    is_iterable<T>::value &&
+        !std::is_same<T, std::string_view>::value &&
+        !std::is_same<T, std::string>::value>>
+std::ostream &operator<<(std::ostream &os, const T &a) {
+  return print_seq(a, ", ", "", (os << "{")) << "}";
+}
+
+void print() { std::cout << "\n"; }
+template<class T>
+void print(const T &x) {
+  std::cout << x << "\n";
+}
+template<typename Head, typename... Tail>
+void print(const Head &head, Tail... tail) {
+  std::cout << head << " ";
+  print(tail...);
+}
+
+void read_from_cin() {}
+template<typename T, typename... Ts>
+void read_from_cin(T &value, Ts &...args) {
+  std::cin >> value;
+  read_from_cin(args...);
+}
+#define INPUT(type, ...) \
+  type __VA_ARGS__;      \
+  read_from_cin(__VA_ARGS__)
+
+#ifdef MY_DEBUG
+#include "debug_dump.hpp"
+#else
+#define DUMP(...)
+#endif
+
+using namespace std;
+
+// Returns floor(sqrt(x)).
+i64 floor_sqrt(i64 x) {
+  assert(x >= 0);
+  if (x <= 1) return x;
+  i64 r = std::floor(sqrtl((long double) x));
+  while (r * r < x) ++r;
+  while (r * r > x) --r;
+  return r;
+}
+
+struct UnionFind {
+  int n;
+  mutable std::vector<int> parent;  // positive: parent, negative: size
+  int num_roots;
+
+  explicit UnionFind(int sz) : n(sz), parent(sz, -1), num_roots(sz) {}
+
+  bool unite(int x, int y) {
+    x = find(x), y = find(y);
+    if (x == y) return false;
+    if (-parent[x] < -parent[y]) std::swap(x, y);
+    parent[x] += parent[y];
+    parent[y] = x;
+    --num_roots;
+    return true;
+  }
+
+  int find(int v) const {
+    if (parent[v] < 0) return v;
+    return parent[v] = find(parent[v]);
+  }
+
+  int size(int v) const { return -parent[find(v)]; }
+
+  bool same(int x, int y) const { return find(x) == find(y); }
+
+  std::vector<int> roots() const {
+    std::vector<int> res;
+    res.reserve(num_roots);
+    for (int i = 0; i < n; ++i) {
+      if (parent[i] < 0) res.push_back(i);
+    }
+    return res;
+  }
+};
+
+// mod: prime
+template<class T = Mint>
+struct Factorials {
+  // factorials and inverse factorials.
+  std::vector<T> fact, ifact;
+
+  // n: max cached value.
+  Factorials(int n) : fact(n + 1), ifact(n + 1) {
+    assert(n >= 0);
+    assert(n < T::mod());
+    fact[0] = 1;
+    for (int i = 1; i <= n; ++i) {
+      fact[i] = fact[i - 1] * i;
+    }
+    ifact[n] = fact[n].inv();
+    for (int i = n; i >= 1; --i) {
+      ifact[i - 1] = ifact[i] * i;
+    }
+  }
+
+  // Combination (nCk)
+  T C(int n, int k) const {
+    if (k < 0 || k > n) return 0;
+    return fact[n] * ifact[k] * ifact[n - k];
+  }
+};
+
+auto solve() {
+  INPUT(int, n);
+  vector<i64> a(n);
+  cin >> a;
+
+  UnionFind uf(n);
+  REP(i, n) {
+    REP(j, i) {
+      i64 x = a[i] * a[j];
+      i64 y = floor_sqrt(x);
+      if (y * y == x) {
+        uf.unite(i, j);
+      }
+    }
+  }
+  vector<int> comp;
+  i64 total = 0;
+  REP(i, n) {
+    if (uf.find(i) == i) {
+      int x = uf.size(i);
+      comp.push_back(x);
+      total += x;
+    }
+  }
+  DUMP(comp);
+  const int m = comp.size();
+  Factorials fs(total + 10);
+
+  assert(m > 0);
+  int slots = comp[0] + 1;
+  auto dp = vector(m, vector(total + 5, Mint(0)));
+  dp[0][slots - 2] = 1;
+  for (int i = 1; i < m; ++i) {
+    // fill dp[i].
+    for (int j = 0; j < total + 5; ++j) {
+      if (dp[i - 1][j].val() == 0) continue;
+      for (int p = 0; p <= min(j, comp[i]); ++p) {
+        for (int q = 0; p + q <= min(slots, comp[i]); ++q) {
+          if (p == 0 and q == 0) continue;
+          int nj = j - p + (comp[i] - (p + q));
+          if (nj >= total + 5) continue;
+          Mint co = 1;
+          co *= fs.C(j, p);
+          co *= fs.C(comp[i] - 1, p + q - 1);
+          co *= fs.C(slots - j, q);
+          dp[i][nj] += dp[i - 1][j] * co;
+        }
+      }
+    }
+    slots += comp[i];
+  }
+  Mint ans = dp[m - 1][0];
+  Mint mult = 1;
+  REP(i, m) {
+    mult *= fs.fact[comp[i]];
+  }
+  ans *= mult;
+  return ans;
+}
+
+int main() {
+  ios_base::sync_with_stdio(false), cin.tie(nullptr);
+  cout << std::fixed << std::setprecision(15);
+  int t = 1;
+  REP(test_case, t) {
+    auto ans = solve();
+    print(ans);
+  }
+}
