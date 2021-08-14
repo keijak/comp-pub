93f8ba0531f4986c6b39034fa1a9bf18cea6903e
keijak <m@keijak.com>
2021-08-15 01:30:47 +0200

    cf

---
+++ cf/contest/1557/B/main.cpp
@@ -0,0 +1,121 @@
+#include <bits/stdc++.h>
+#define REP_(i, a_, b_, a, b, ...) \
+  for (int i = (a), END_##i = (b); i < END_##i; ++i)
+#define REP(i, ...) REP_(i, __VA_ARGS__, __VA_ARGS__, 0, __VA_ARGS__)
+#define ALL(x) std::begin(x), std::end(x)
+using i64 = long long;
+
+template<typename T, typename U>
+inline bool chmax(T &a, U b) {
+  return a < b and ((a = std::move(b)), true);
+}
+template<typename T, typename U>
+inline bool chmin(T &a, U b) {
+  return a > b and ((a = std::move(b)), true);
+}
+template<typename T>
+inline int ssize(const T &a) {
+  return (int) std::size(a);
+}
+
+template<typename T>
+std::istream &operator>>(std::istream &is, std::vector<T> &a) {
+  for (auto &x : a) is >> x;
+  return is;
+}
+template<typename T, typename U>
+std::ostream &operator<<(std::ostream &os, const std::pair<T, U> &a) {
+  return os << "(" << a.first << ", " << a.second << ")";
+}
+template<typename Container>
+std::ostream &print_seq(const Container &a, std::string_view sep = " ",
+                        std::string_view ends = "\n",
+                        std::ostream &os = std::cout) {
+  auto b = std::begin(a), e = std::end(a);
+  for (auto it = std::begin(a); it != e; ++it) {
+    if (it != b) os << sep;
+    os << *it;
+  }
+  return os << ends;
+}
+template<typename T, typename = void>
+struct is_iterable : std::false_type {};
+template<typename T>
+struct is_iterable<T, std::void_t<decltype(std::begin(std::declval<T>())),
+                                  decltype(std::end(std::declval<T>()))>>
+    : std::true_type {
+};
+
+template<typename T, typename = std::enable_if_t<
+    is_iterable<T>::value &&
+        !std::is_same<T, std::string_view>::value &&
+        !std::is_same<T, std::string>::value>>
+std::ostream &operator<<(std::ostream &os, const T &a) {
+  return print_seq(a, ", ", "", (os << "{")) << "}";
+}
+
+void print() { std::cout << "\n"; }
+template<class T>
+void print(const T &x) {
+  std::cout << x << "\n";
+}
+template<typename Head, typename... Tail>
+void print(const Head &head, Tail... tail) {
+  std::cout << head << " ";
+  print(tail...);
+}
+
+void read_from_cin() {}
+template<typename T, typename... Ts>
+void read_from_cin(T &value, Ts &...args) {
+  std::cin >> value;
+  read_from_cin(args...);
+}
+#define INPUT(type, ...) \
+  type __VA_ARGS__;      \
+  read_from_cin(__VA_ARGS__)
+
+#ifdef MY_DEBUG
+#include "debug_dump.hpp"
+#else
+#define DUMP(...)
+#endif
+
+using namespace std;
+
+auto solve() {
+  INPUT(int, n, K);
+  vector<i64> a(n);
+  cin >> a;
+  vector<int> ix(n);
+  REP(i, n) ix[i] = i;
+  sort(ALL(ix), [&](int i, int j) {
+    return a[i] < a[j];
+  });
+  DUMP(ix);
+  vector<int> rank(n);
+  REP(i, n) {
+    rank[ix[i]] = i;
+  }
+  DUMP(rank);
+  int c = 1;
+  for (int i = 1; i < n; ++i) {
+    if (rank[i] == rank[i - 1] + 1) {
+      continue;
+    }
+    ++c;
+  }
+  DUMP(c, K);
+  return c <= K;
+}
+
+int main() {
+  ios_base::sync_with_stdio(false), cin.tie(nullptr);
+  cout << std::fixed << std::setprecision(15);
+  int t = 1;
+  cin >> t;
+  REP(test_case, t) {
+    auto ans = solve();
+    print(ans ? "Yes" : "No");
+  }
+}
---
+++ cf/contest/1557/D/main.cpp
@@ -0,0 +1,317 @@
+#include <bits/stdc++.h>
+#define REP_(i, a_, b_, a, b, ...) \
+  for (int i = (a), END_##i = (b); i < END_##i; ++i)
+#define REP(i, ...) REP_(i, __VA_ARGS__, __VA_ARGS__, 0, __VA_ARGS__)
+#define ALL(x) std::begin(x), std::end(x)
+using i64 = long long;
+
+template<typename T, typename U>
+inline bool chmax(T &a, U b) {
+  return a < b and ((a = std::move(b)), true);
+}
+template<typename T, typename U>
+inline bool chmin(T &a, U b) {
+  return a > b and ((a = std::move(b)), true);
+}
+template<typename T>
+inline int ssize(const T &a) {
+  return (int) std::size(a);
+}
+
+template<typename T>
+std::istream &operator>>(std::istream &is, std::vector<T> &a) {
+  for (auto &x : a) is >> x;
+  return is;
+}
+template<typename T, typename U>
+std::ostream &operator<<(std::ostream &os, const std::pair<T, U> &a) {
+  return os << "(" << a.first << ", " << a.second << ")";
+}
+template<typename Container>
+std::ostream &print_seq(const Container &a, std::string_view sep = " ",
+                        std::string_view ends = "\n",
+                        std::ostream &os = std::cout) {
+  auto b = std::begin(a), e = std::end(a);
+  for (auto it = std::begin(a); it != e; ++it) {
+    if (it != b) os << sep;
+    os << *it;
+  }
+  return os << ends;
+}
+template<typename T, typename = void>
+struct is_iterable : std::false_type {};
+template<typename T>
+struct is_iterable<T, std::void_t<decltype(std::begin(std::declval<T>())),
+                                  decltype(std::end(std::declval<T>()))>>
+    : std::true_type {
+};
+
+template<typename T, typename = std::enable_if_t<
+    is_iterable<T>::value &&
+        !std::is_same<T, std::string_view>::value &&
+        !std::is_same<T, std::string>::value>>
+std::ostream &operator<<(std::ostream &os, const T &a) {
+  return print_seq(a, ", ", "", (os << "{")) << "}";
+}
+
+void print() { std::cout << "\n"; }
+template<class T>
+void print(const T &x) {
+  std::cout << x << "\n";
+}
+template<typename Head, typename... Tail>
+void print(const Head &head, Tail... tail) {
+  std::cout << head << " ";
+  print(tail...);
+}
+
+void read_from_cin() {}
+template<typename T, typename... Ts>
+void read_from_cin(T &value, Ts &...args) {
+  std::cin >> value;
+  read_from_cin(args...);
+}
+#define INPUT(type, ...) \
+  type __VA_ARGS__;      \
+  read_from_cin(__VA_ARGS__)
+
+#ifdef MY_DEBUG
+#include "debug_dump.hpp"
+#else
+#define DUMP(...)
+#endif
+
+using namespace std;
+
+template<typename LazyMonoid>
+struct LazySegTree {
+  using T = typename LazyMonoid::T;
+  using F = typename LazyMonoid::F;
+
+  inline int size() const { return n_; }
+  inline int offset() const { return offset_; }
+
+  explicit LazySegTree(int n)
+      : LazySegTree(std::vector<T>(n, LazyMonoid::id())) {}
+
+  explicit LazySegTree(const std::vector<T> &v) : n_(int(v.size())) {
+    offset_ = 1;
+    for (bits_ = 0; offset_ < n_; ++bits_) {
+      offset_ <<= 1;
+    }
+    data_.assign(2 * offset_, LazyMonoid::id());
+    lazy_ = std::vector<F>(offset_, LazyMonoid::f_id());
+    for (int i = 0; i < n_; i++) data_[offset_ + i] = v[i];
+    for (int i = offset_ - 1; i >= 1; i--) {
+      update(i);
+    }
+  }
+
+  void set(int p, T x) {
+    assert(0 <= p && p < n_);
+    p += offset_;
+    // Update the leaf.
+    for (int i = bits_; i >= 1; i--) push(p >> i);
+    data_[p] = x;
+    // Update its ancestors.
+    for (int i = 1; i <= bits_; i++) update(p >> i);
+  }
+
+  const T &operator[](int p) const {
+    assert(0 <= p && p < n_);
+    p += offset_;
+    for (int i = bits_; i >= 1; i--) push(p >> i);
+    return data_[p];
+  }
+
+  T fold(int l, int r) const {
+    assert(0 <= l && l <= r && r <= n_);
+    if (l == r) return LazyMonoid::id();
+
+    l += offset_;
+    r += offset_;
+
+    for (int i = bits_; i >= 1; i--) {
+      if (((l >> i) << i) != l) push(l >> i);
+      if (((r >> i) << i) != r) push(r >> i);
+    }
+
+    T sml = LazyMonoid::id(), smr = LazyMonoid::id();
+    while (l < r) {
+      if (l & 1) sml = LazyMonoid::op(sml, data_[l++]);
+      if (r & 1) smr = LazyMonoid::op(data_[--r], smr);
+      l >>= 1;
+      r >>= 1;
+    }
+
+    return LazyMonoid::op(sml, smr);
+  }
+
+  T fold_all() const { return data_[1]; }
+
+  void apply(int p, F f) {
+    assert(0 <= p && p < n_);
+    p += offset_;
+    for (int i = bits_; i >= 1; i--) push(p >> i);
+    data_[p] = LazyMonoid::apply(f, data_[p]);
+    for (int i = 1; i <= bits_; i++) update(p >> i);
+  }
+  void apply(int l, int r, F f) {
+    assert(0 <= l && l <= r && r <= n_);
+    if (l == r) return;
+
+    l += offset_;
+    r += offset_;
+
+    for (int i = bits_; i >= 1; i--) {
+      if (((l >> i) << i) != l) push(l >> i);
+      if (((r >> i) << i) != r) push((r - 1) >> i);
+    }
+
+    {
+      int l2 = l, r2 = r;
+      while (l < r) {
+        if (l & 1) apply_all(l++, f);
+        if (r & 1) apply_all(--r, f);
+        l >>= 1;
+        r >>= 1;
+      }
+      l = l2;
+      r = r2;
+    }
+
+    for (int i = 1; i <= bits_; i++) {
+      if (((l >> i) << i) != l) update(l >> i);
+      if (((r >> i) << i) != r) update((r - 1) >> i);
+    }
+  }
+
+  friend std::ostream &operator<<(std::ostream &os, const LazySegTree &st) {
+    os << "[";
+    for (int i = 0; i < st.size(); ++i) {
+      if (i != 0) os << ", ";
+      const auto &x = st[i];
+      os << x;
+    }
+    return os << "]";
+  }
+
+  void update(int k) {
+    data_[k] = LazyMonoid::op(data_[2 * k], data_[2 * k + 1]);
+  }
+  void apply_all(int k, F f) const {
+    data_[k] = LazyMonoid::f_apply(f, data_[k]);
+    if (k < offset_) lazy_[k] = LazyMonoid::f_compose(f, lazy_[k]);
+  }
+  void push(int k) const {
+    apply_all(2 * k, lazy_[k]);
+    apply_all(2 * k + 1, lazy_[k]);
+    lazy_[k] = LazyMonoid::f_id();
+  }
+
+  int n_, offset_, bits_;
+  mutable std::vector<T> data_;
+  mutable std::vector<F> lazy_;
+};
+
+struct MaxMaxOp {
+  using T = pair<int, int>;
+  using F = pair<int, int>;
+
+  // Fold: Max
+  static T op(const T &x, const T &y) {
+    return std::max(x, y);
+  }
+  static constexpr T id() { return {0, -1}; }
+
+  // Update: Max
+  static T f_apply(const F &f, const T &x) {
+    return std::max(f, x);
+  }
+  static F f_compose(const F &f, const F &g) {
+    return std::max(f, g);
+  }
+  static constexpr F f_id() { return {-1, -1}; }
+};
+
+template<typename T>
+struct Compressed {
+  std::vector<T> values;
+
+  explicit Compressed(std::vector<T> v) : values(v) {
+    std::sort(values.begin(), values.end());
+    values.erase(std::unique(values.begin(), values.end()), values.end());
+  }
+
+  int size() const { return values.size(); }
+
+  int index(T x) const {
+    return std::lower_bound(values.begin(), values.end(), x) - values.begin();
+  }
+
+  const T &value(int i) const { return values[i]; }
+};
+
+int main() {
+  ios_base::sync_with_stdio(false), cin.tie(nullptr);
+
+  INPUT(int, n, m);
+  vector<i64> coords;
+  coords.push_back(-1);
+  coords.push_back(int(1e9) + 5);
+  vector<vector<pair<i64, i64>>> rows(n);
+  REP(j, m) {
+    INPUT(i64, i, l, r);
+    --i;
+    --l;
+    coords.push_back(l);
+    coords.push_back(r);
+    rows[i].push_back({l, r});
+  }
+  Compressed<i64> cc(coords);
+  REP(i, n) {
+    auto &r = rows[i];
+    sort(ALL(r));
+    REP(j, r.size()) {
+      r[j].first = cc.index(r[j].first);
+      r[j].second = cc.index(r[j].second);
+    }
+  }
+  LazySegTree<MaxMaxOp> segtree(cc.size());
+  vector<int> pre(n, -1);
+  int amax = 0;
+  int ami = -1;
+  REP(i, n) {
+    i64 maxval = 0;
+    int mi = -1;
+    for (auto[l, r] : rows[i]) {
+      auto[val, ix] = segtree.fold(l, r);
+      if (chmax(maxval, val)) {
+        mi = ix;
+      }
+    }
+    pre[i] = mi;
+    maxval++;
+    if (chmax(amax, maxval)) {
+      ami = i;
+    }
+    for (auto[l, r] : rows[i]) {
+      segtree.apply(l, r, {maxval, i});
+    }
+  }
+  print(n - amax);
+  if (amax != n) {
+    set<int> rem;
+    REP(i, n) rem.insert(i);
+    while (ami != -1) {
+      rem.erase(ami);
+      ami = pre[ami];
+    }
+    assert(ssize(rem) == n - amax);
+    vector<int> ans;
+    for (auto x : rem) {
+      ans.push_back(x + 1);
+    }
+    print_seq(ans);
+  }
+}
