423e0c1fc8db4c83ba891ab25cd0a4ed0d0db88a
keijak <m@keijak.com>
2021-08-19 13:45:28 +0200

    divisor inclusion and exclusion

---
+++ atcoder/abc020/D/main.cpp
@@ -0,0 +1,134 @@
+#include <bits/stdc++.h>
+#define REP_(i, a_, b_, a, b, ...) \
+  for (int i = (a), END_##i = (b); i < END_##i; ++i)
+#define REP(i, ...) REP_(i, __VA_ARGS__, __VA_ARGS__, 0, __VA_ARGS__)
+#define ALL(x) std::begin(x), std::end(x)
+using i64 = long long;
+
+#include <atcoder/modint>
+using Mint = atcoder::modint1000000007;
+std::ostream &operator<<(std::ostream &os, const Mint &m) {
+  return os << m.val();
+}
+
+template<typename T, typename U>
+inline bool chmax(T &a, U b) {
+  return a < b and ((a = std::move(b)), true);
+}
+template<typename T, typename U>
+inline bool chmin(T &a, U b) {
+  return a > b and ((a = std::move(b)), true);
+}
+template<typename T>
+inline int ssize(const T &a) {
+  return (int) std::size(a);
+}
+
+template<typename T>
+std::istream &operator>>(std::istream &is, std::vector<T> &a) {
+  for (auto &x : a) is >> x;
+  return is;
+}
+template<typename T, typename U>
+std::ostream &operator<<(std::ostream &os, const std::pair<T, U> &a) {
+  return os << "(" << a.first << ", " << a.second << ")";
+}
+template<typename Container>
+std::ostream &print_seq(const Container &a, std::string_view sep = " ",
+                        std::string_view ends = "\n",
+                        std::ostream &os = std::cout) {
+  auto b = std::begin(a), e = std::end(a);
+  for (auto it = std::begin(a); it != e; ++it) {
+    if (it != b) os << sep;
+    os << *it;
+  }
+  return os << ends;
+}
+template<typename T, typename = void>
+struct is_iterable : std::false_type {};
+template<typename T>
+struct is_iterable<T, std::void_t<decltype(std::begin(std::declval<T>())),
+                                  decltype(std::end(std::declval<T>()))>>
+    : std::true_type {
+};
+
+template<typename T, typename = std::enable_if_t<
+    is_iterable<T>::value &&
+        !std::is_same<T, std::string_view>::value &&
+        !std::is_same<T, std::string>::value>>
+std::ostream &operator<<(std::ostream &os, const T &a) {
+  return print_seq(a, ", ", "", (os << "{")) << "}";
+}
+
+void print() { std::cout << "\n"; }
+template<class T>
+void print(const T &x) {
+  std::cout << x << "\n";
+}
+template<typename Head, typename... Tail>
+void print(const Head &head, Tail... tail) {
+  std::cout << head << " ";
+  print(tail...);
+}
+
+void read_from_cin() {}
+template<typename T, typename... Ts>
+void read_from_cin(T &value, Ts &...args) {
+  std::cin >> value;
+  read_from_cin(args...);
+}
+#define INPUT(type, ...) \
+  type __VA_ARGS__;      \
+  read_from_cin(__VA_ARGS__)
+
+#ifdef MY_DEBUG
+#include "debug_dump.hpp"
+#else
+#define DUMP(...)
+#endif
+
+using namespace std;
+
+// Returns all divisors of n. O(sqrt(n)) + sorting.
+std::vector<i64> divisors(i64 n) {
+  std::vector<i64> res;
+  for (i64 k = 1; k * k <= n; ++k) {
+    if (n % k != 0) continue;
+    res.push_back(k);
+    i64 q = n / k;
+    if (q != k) res.push_back(q);
+  }
+  std::sort(res.begin(), res.end());
+  return res;
+}
+
+auto solve() {
+  INPUT(i64, N, K);
+  auto dk = divisors(K);
+  int m = ssize(dk);
+  auto dp = vector(m, Mint(0));
+  for (int i = m - 1; i >= 0; --i) {
+    i64 g = dk[i];
+    i64 c = N / g;
+    dp[i] = Mint(c * (c + 1) / 2) * g;
+    for (int j = i + 1; j < m; ++j) {
+      if (dk[j] % g == 0) dp[i] -= dp[j];
+    }
+  }
+  Mint ans = 0;
+  for (int i = 0; i < m; ++i) {
+    i64 g = dk[i];
+    ans += dp[i] * (K / g);
+  }
+  return ans;
+}
+
+int main() {
+  ios_base::sync_with_stdio(false), cin.tie(nullptr);
+  cout << std::fixed << std::setprecision(15);
+  int t = 1;
+  REP(test_case, t) {
+    auto ans = solve();
+    print(ans);
+  }
+}
