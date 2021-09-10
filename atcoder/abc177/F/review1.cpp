ce3ea9a0fb35e6c3576ecdff174813d5f171c369
keijak <m@keijak.com>
2021-09-10 13:05:57 +0200

    map

---
+++ atcoder/abc177/F/review1.cpp
@@ -0,0 +1,145 @@
+#include <bits/stdc++.h>
+#define REP_(i, a_, b_, a, b, ...) \
+  for (int i = (a), END_##i = (b); i < END_##i; ++i)
+#define REP(i, ...) REP_(i, __VA_ARGS__, __VA_ARGS__, 0, __VA_ARGS__)
+#define ALL(x) std::begin(x), std::end(x)
+using i64 = long long;
+
+template<typename T, typename U>
+inline bool chmax(T &a, U b) {
+  return a < b and ((a = std::move(b)), true);
+}
+template<typename T, typename U>
+inline bool chmin(T &a, U b) {
+  return a > b and ((a = std::move(b)), true);
+}
+template<typename T>
+inline int ssize(const T &a) {
+  return (int) std::size(a);
+}
+
+template<typename T>
+std::istream &operator>>(std::istream &is, std::vector<T> &a) {
+  for (auto &x: a) is >> x;
+  return is;
+}
+template<typename T, typename U>
+std::ostream &operator<<(std::ostream &os, const std::pair<T, U> &a) {
+  return os << "(" << a.first << ", " << a.second << ")";
+}
+template<typename Container>
+std::ostream &print_seq(const Container &a, std::string_view sep = " ",
+                        std::string_view ends = "\n",
+                        std::ostream &os = std::cout) {
+  auto b = std::begin(a), e = std::end(a);
+  for (auto it = std::begin(a); it != e; ++it) {
+    if (it != b) os << sep;
+    os << *it;
+  }
+  return os << ends;
+}
+template<typename T, typename = void>
+struct is_iterable : std::false_type {};
+template<typename T>
+struct is_iterable<T, std::void_t<decltype(std::begin(std::declval<T>())),
+                                  decltype(std::end(std::declval<T>()))>>
+    : std::true_type {
+};
+
+template<typename T, typename = std::enable_if_t<
+    is_iterable<T>::value &&
+        !std::is_same<T, std::string_view>::value &&
+        !std::is_same<T, std::string>::value>>
+std::ostream &operator<<(std::ostream &os, const T &a) {
+  return print_seq(a, ", ", "", (os << "{")) << "}";
+}
+
+void print() { std::cout << "\n"; }
+template<class T>
+void print(const T &x) {
+  std::cout << x << "\n";
+}
+template<typename Head, typename... Tail>
+void print(const Head &head, Tail... tail) {
+  std::cout << head << " ";
+  print(tail...);
+}
+
+struct Input {
+  template<typename T>
+  operator T() const {
+    T x;
+    std::cin >> x;
+    return x;
+  }
+} in;
+
+#ifdef MY_DEBUG
+#include "debug_dump.hpp"
+#else
+#define DUMP(...)
+#endif
+
+using namespace std;
+
+template<int sign = 1>
+struct Infinity {
+  template<typename T>
+  constexpr operator T() const {
+    static_assert(sign == 1 or not std::is_unsigned_v<T>,
+                  "must be positive in an unsigned type");
+    if constexpr (std::numeric_limits<T>::has_infinity) {
+      return T(sign) * std::numeric_limits<T>::infinity();
+    } else {
+      return T(sign) * (std::numeric_limits<T>::max() / T(2));
+    }
+  }
+  constexpr Infinity<sign * -1> operator-() const { return {}; }
+  template<typename T>
+  friend constexpr bool operator==(const T &x, const Infinity &y) {
+    return x == T(y);
+  }
+  template<typename T>
+  friend constexpr bool operator!=(const T &x, const Infinity &y) {
+    return x != T(y);
+  }
+};
+constexpr Infinity<> kBig;
+
+int main() {
+  ios_base::sync_with_stdio(false), cin.tie(nullptr);
+  const int H = in, W = in;
+  map<int, int> e2s;
+  multiset<int> hs;
+  REP(i, W) {
+    e2s[i] = i;
+    hs.insert(0);
+  }
+  e2s[-1] = -1;
+  e2s[W] = -1;
+  hs.insert(int(kBig));
+  DUMP(e2s, hs);
+
+  REP(i, H) {
+    int l = in, r = in;
+    --l;
+    auto rit = e2s.upper_bound(r);
+    --rit;
+    if (not e2s.count(r)) {
+      if (rit->second != -1) {
+        hs.insert(r - rit->second);
+      }
+      e2s[r] = rit->second;
+    }
+    auto eit = e2s.find(r);
+    for (auto it = e2s.lower_bound(l); it != eit;) {
+      if (it->second != -1) {
+        hs.erase(hs.find(it->first - it->second));
+      }
+      it = e2s.erase(it);
+    }
+    DUMP(e2s, hs);
+    int minval = *hs.begin();
+    print(minval == kBig ? -1 : (minval + i + 1));
+  }
+}
---
+++ atcoder/abc177/F/segtree_arithmetic_progression.cpp
@@ -0,0 +1,318 @@
+#include <bits/stdc++.h>
+#define REP_(i, a_, b_, a, b, ...) \
+  for (int i = (a), END_##i = (b); i < END_##i; ++i)
+#define REP(i, ...) REP_(i, __VA_ARGS__, __VA_ARGS__, 0, __VA_ARGS__)
+#define ALL(x) std::begin(x), std::end(x)
+using i64 = long long;
+
+template<typename T, typename U>
+inline bool chmax(T &a, U b) {
+  return a < b and ((a = std::move(b)), true);
+}
+template<typename T, typename U>
+inline bool chmin(T &a, U b) {
+  return a > b and ((a = std::move(b)), true);
+}
+template<typename T>
+inline int ssize(const T &a) {
+  return (int) std::size(a);
+}
+
+template<typename T>
+std::istream &operator>>(std::istream &is, std::vector<T> &a) {
+  for (auto &x: a) is >> x;
+  return is;
+}
+template<typename T, typename U>
+std::ostream &operator<<(std::ostream &os, const std::pair<T, U> &a) {
+  return os << "(" << a.first << ", " << a.second << ")";
+}
+template<typename Container>
+std::ostream &print_seq(const Container &a, std::string_view sep = " ",
+                        std::string_view ends = "\n",
+                        std::ostream &os = std::cout) {
+  auto b = std::begin(a), e = std::end(a);
+  for (auto it = std::begin(a); it != e; ++it) {
+    if (it != b) os << sep;
+    os << *it;
+  }
+  return os << ends;
+}
+template<typename T, typename = void>
+struct is_iterable : std::false_type {};
+template<typename T>
+struct is_iterable<T, std::void_t<decltype(std::begin(std::declval<T>())),
+                                  decltype(std::end(std::declval<T>()))>>
+    : std::true_type {
+};
+
+template<typename T, typename = std::enable_if_t<
+    is_iterable<T>::value &&
+        !std::is_same<T, std::string_view>::value &&
+        !std::is_same<T, std::string>::value>>
+std::ostream &operator<<(std::ostream &os, const T &a) {
+  return print_seq(a, ", ", "", (os << "{")) << "}";
+}
+
+void print() { std::cout << "\n"; }
+template<class T>
+void print(const T &x) {
+  std::cout << x << "\n";
+}
+template<typename Head, typename... Tail>
+void print(const Head &head, Tail... tail) {
+  std::cout << head << " ";
+  print(tail...);
+}
+
+struct Input {
+  template<typename T>
+  operator T() const {
+    T x;
+    std::cin >> x;
+    return x;
+  }
+} in;
+
+#ifdef MY_DEBUG
+#include "debug_dump.hpp"
+#else
+#define DUMP(...)
+#endif
+
+using namespace std;
+
+template<typename LazyMonoid>
+struct LazySegTree {
+  using T = typename LazyMonoid::T;
+  using F = typename LazyMonoid::F;
+
+  inline int size() const { return n_; }
+  inline int offset() const { return offset_; }
+
+  explicit LazySegTree(int n)
+      : LazySegTree(std::vector<T>(n, LazyMonoid::id())) {}
+
+  explicit LazySegTree(const std::vector<T> &v) : n_(int(v.size())) {
+    offset_ = 1;
+    for (bits_ = 0; offset_ < n_; ++bits_) {
+      offset_ <<= 1;
+    }
+    data_.assign(2 * offset_, LazyMonoid::id());
+    lazy_ = std::vector<F>(offset_, LazyMonoid::f_id());
+    for (int i = 0; i < n_; i++) data_[offset_ + i] = v[i];
+    for (int i = offset_ - 1; i >= 1; i--) {
+      update(i);
+    }
+  }
+
+  void set(int p, T x) {
+    assert(0 <= p && p < n_);
+    p += offset_;
+    // Update the leaf.
+    for (int i = bits_; i >= 1; i--) push(p >> i);
+    data_[p] = x;
+    // Update its ancestors.
+    for (int i = 1; i <= bits_; i++) update(p >> i);
+  }
+
+  const T &operator[](int p) const {
+    assert(0 <= p && p < n_);
+    p += offset_;
+    for (int i = bits_; i >= 1; i--) push(p >> i);
+    return data_[p];
+  }
+
+  T fold(int l, int r) const {
+    assert(0 <= l && l <= r && r <= n_);
+    if (l == r) return LazyMonoid::id();
+
+    l += offset_;
+    r += offset_;
+
+    for (int i = bits_; i >= 1; i--) {
+      if (((l >> i) << i) != l) push(l >> i);
+      if (((r >> i) << i) != r) push(r >> i);
+    }
+
+    T sml = LazyMonoid::id(), smr = LazyMonoid::id();
+    while (l < r) {
+      if (l & 1) sml = LazyMonoid::op(sml, data_[l++]);
+      if (r & 1) smr = LazyMonoid::op(data_[--r], smr);
+      l >>= 1;
+      r >>= 1;
+    }
+
+    return LazyMonoid::op(sml, smr);
+  }
+
+  T fold_all() const { return data_[1]; }
+
+  void apply(int p, F f) {
+    assert(0 <= p && p < n_);
+    p += offset_;
+    for (int i = bits_; i >= 1; i--) push(p >> i);
+    data_[p] = LazyMonoid::apply(f, data_[p]);
+    for (int i = 1; i <= bits_; i++) update(p >> i);
+  }
+  void apply(int l, int r, F f) {
+    assert(0 <= l && l <= r && r <= n_);
+    if (l == r) return;
+
+    l += offset_;
+    r += offset_;
+
+    for (int i = bits_; i >= 1; i--) {
+      if (((l >> i) << i) != l) push(l >> i);
+      if (((r >> i) << i) != r) push((r - 1) >> i);
+    }
+
+    {
+      int l2 = l, r2 = r;
+      while (l < r) {
+        if (l & 1) apply_all(l++, f);
+        if (r & 1) apply_all(--r, f);
+        l >>= 1;
+        r >>= 1;
+      }
+      l = l2;
+      r = r2;
+    }
+
+    for (int i = 1; i <= bits_; i++) {
+      if (((l >> i) << i) != l) update(l >> i);
+      if (((r >> i) << i) != r) update((r - 1) >> i);
+    }
+  }
+
+  friend std::ostream &operator<<(std::ostream &os, const LazySegTree &st) {
+    os << "[";
+    for (int i = 0; i < st.size(); ++i) {
+      if (i != 0) os << ", ";
+      const auto &x = st[i];
+      os << x;
+    }
+    return os << "]";
+  }
+
+  void update(int k) {
+    data_[k] = LazyMonoid::op(data_[2 * k], data_[2 * k + 1]);
+  }
+  void apply_all(int k, F f) const {
+    data_[k] = LazyMonoid::f_apply(f, data_[k]);
+    if (k < offset_) lazy_[k] = LazyMonoid::f_compose(f, lazy_[k]);
+  }
+  void push(int k) const {
+    apply_all(2 * k, lazy_[k]);
+    apply_all(2 * k + 1, lazy_[k]);
+    lazy_[k] = LazyMonoid::f_id();
+  }
+
+  int n_, offset_, bits_;
+  mutable std::vector<T> data_;
+  mutable std::vector<F> lazy_;
+};
+
+template<int sign = 1>
+struct Infinity {
+  template<typename T>
+  constexpr operator T() const {
+    static_assert(sign == 1 or not std::is_unsigned_v<T>,
+                  "must be positive in an unsigned type");
+    if constexpr (std::numeric_limits<T>::has_infinity) {
+      return T(sign) * std::numeric_limits<T>::infinity();
+    } else {
+      return T(sign) * (std::numeric_limits<T>::max() / T(2));
+    }
+  }
+  constexpr Infinity<sign * -1> operator-() const { return {}; }
+  template<typename T>
+  friend constexpr bool operator==(const T &x, const Infinity &y) {
+    return x == T(y);
+  }
+  template<typename T>
+  friend constexpr bool operator!=(const T &x, const Infinity &y) {
+    return x != T(y);
+  }
+};
+constexpr Infinity<> kBig;
+
+struct Line {  // a*x + b
+  i64 a;
+  i64 b;
+};
+
+// ref. https://null-mn.hatenablog.com/entry/2021/08/22/064325
+struct AssignArithmeticProgressionOp {
+  using i64 = long long;
+  static constexpr i64 kBig = std::numeric_limits<i64>::max() / 2;
+
+  struct T {
+    i64 min;
+    i64 max;
+    i64 sum;
+    // closed interval [l, r]
+    int l;
+    int r;
+  };
+  using F = std::optional<Line>;
+
+  // Fold: Max, Min, Sum
+  static T op(const T &x, const T &y) {
+    return T{
+        std::min(x.min, y.min), std::max(x.max, y.max),
+        std::min(x.sum + y.sum, (i64) kBig), x.l, y.r,
+    };
+  }
+
+  static T id() { return {kBig, -kBig, 0LL, -1, -1}; }
+
+  // Update: Assign a*x + b
+  static T f_apply(const F &f, const T &x) {
+    if (not f) return x;
+    auto[a, b] = *f;
+    if (b == kBig) {
+      return T{kBig, kBig, kBig, x.l, x.r};
+    }
+    i64 width = x.r - x.l;
+    i64 lval = a * x.l + b;
+    i64 rval = a * x.r + b;
+    return T{std::min(lval, rval), std::max(lval, rval),
+             (width + 1) * (lval + rval) / 2, x.l, x.r};
+  }
+  static F f_compose(const F &f, const F &g) { return f ? f : g; }
+  static F f_id() { return std::nullopt; }
+};
+std::ostream &operator<<(std::ostream &os,
+                         const AssignArithmeticProgressionOp::T &a) {
+  return os << "{min:" << a.min << ", max:" << a.max << ", sum:" << a.sum
+            << ", l:" << a.l << ", r:" << a.r << "}";
+}
+
+int main() {
+  ios_base::sync_with_stdio(false), cin.tie(nullptr);
+  const int H = in, W = in;
+  vector<AssignArithmeticProgressionOp::T> init(W);
+  REP(i, W) {
+    init[i] = {0, 0, 0, i, i};
+  }
+  LazySegTree<AssignArithmeticProgressionOp> seg(init);
+  REP(i, H) {
+    int l = in, r = in;
+    --l;
+    i64 x = kBig;
+    if (l > 0) {
+      i64 y = seg[l - 1].sum;
+      if (y < i64(kBig)) {
+        x = y + 1 - l;
+      }
+    }
+    seg.apply(l, r, Line{1, x});
+    i64 hor = seg.fold_all().min;
+    if (hor == kBig) {
+      print(-1);
+    } else {
+      print(i + 1 + hor);
+    }
+  }
+}
