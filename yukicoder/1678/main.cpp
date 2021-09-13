fcb3e1119b424a47e1fb33f02f5d0bcca3016c91
keijak <m@keijak.com>
2021-09-13 09:11:23 +0200

    min cost flow in dag

--- yukicoder/1678/main.cpp
+++ yukicoder/1678/main.cpp
@@ -83,31 +83,60 @@ struct Input {
 
 using namespace std;
 
-template<typename CapType, typename CostType>
+template<typename Cap, typename Cost>
 class MinCostFlowDAG {
  public:
-  using Cat = CapType;
-  using Cot = CostType;
-  using pti = pair<Cot, int>;
-  struct edge {
+  static constexpr Cost INF = std::numeric_limits<Cost>::max();
+
+  struct Edge {
     int to, rev;
-    Cat cap;
-    Cot cost;
+    Cap cap;
+    Cost cost;
   };
+
   const int V;
-  const Cot inf;
-  vector<vector<edge> > G;
-  vector<Cot> h, dist;
-  vector<int> deg, ord, prevv, preve;
-  MinCostFlowDAG(const int node_size) : V(node_size), inf(numeric_limits<Cot>::max()),
-                                        G(V), h(V, inf), dist(V), deg(V, 0), prevv(V), preve(V) {}
-  void add_edge(const int from, const int to, const Cat cap, const Cot cost) {
+  std::vector<vector<Edge>> G;
+  std::vector<Cost> h, dist;
+  std::vector<int> deg, ord, prevv, preve;
+
+  explicit MinCostFlowDAG(int node_count)
+      : V(node_count),
+        G(V),
+        h(V, INF),
+        dist(V, 0),
+        deg(V, 0),
+        prevv(V),
+        preve(V) {}
+
+  void add_edge(const int from, const int to, const Cap cap, const Cost cost) {
     if (cap == 0) return;
-    G[from].push_back((edge) {to, (int) G[to].size(), cap, cost});
-    G[to].push_back((edge) {from, (int) G[from].size() - 1, 0, -cost});
+    G[from].push_back(Edge{to, (int) G[to].size(), cap, cost});
+    G[to].push_back(Edge{from, (int) G[from].size() - 1, 0, -cost});
     ++deg[to];
   }
-  bool tsort() {
+
+  std::optional<Cost> flow(const int s, const int t, Cap f) {
+    assert(topological_sort());  // must be a DAG.
+    calc_potential(s);
+    Cost res = 0;
+    while (f > 0) {
+      dijkstra(s);
+      if (dist[t] == INF) return std::nullopt;
+      update(s, t, f, res);
+    }
+    return res;
+  }
+
+ private:
+  struct DijkstraState {
+    Cost dist;
+    int node;
+  };
+  friend bool operator>(const DijkstraState &x, const DijkstraState &y) {
+    return x.dist > y.dist;
+  }
+
+  bool topological_sort() {
     queue<int> que;
     for (int i = 0; i < V; ++i) {
       if (deg[i] == 0) que.push(i);
@@ -122,61 +151,56 @@ class MinCostFlowDAG {
     }
     return (*max_element(deg.begin(), deg.end()) == 0);
   }
+
   void calc_potential(const int s) {
     h[s] = 0;
     for (const int v: ord) {
-      if (h[v] == inf) continue;
-      for (const edge &e: G[v]) {
+      if (h[v] == INF) continue;
+      for (const Edge &e: G[v]) {
         if (e.cap > 0) h[e.to] = min(h[e.to], h[v] + e.cost);
       }
     }
   }
-  void Dijkstra(const int s) {
-    priority_queue<pti, vector<pti>, greater<pti> > que;
-    fill(dist.begin(), dist.end(), inf);
+
+  void dijkstra(const int s) {
+    using Heap = std::priority_queue<DijkstraState, std::vector<DijkstraState>,
+                                     std::greater<DijkstraState>>;
+    Heap heap;
+    fill(dist.begin(), dist.end(), INF);
     dist[s] = 0;
-    que.push(pti(0, s));
-    while (!que.empty()) {
-      pti p = que.top();
-      que.pop();
-      const int v = p.second;
-      if (dist[v] < p.first) continue;
+    heap.push(DijkstraState{0, s});
+    while (not heap.empty()) {
+      const auto cur = heap.top();
+      heap.pop();
+      const int v = cur.node;
+      if (dist[v] < cur.dist) continue;
       for (int i = 0; i < (int) G[v].size(); ++i) {
-        edge &e = G[v][i];
-        if (e.cap > 0 && dist[e.to] > dist[v] + e.cost + h[v] - h[e.to]) {
+        Edge &e = G[v][i];
+        if (e.cap > 0 and dist[e.to] > dist[v] + e.cost + h[v] - h[e.to]) {
           dist[e.to] = dist[v] + e.cost + h[v] - h[e.to];
-          prevv[e.to] = v, preve[e.to] = i;
-          que.push(pti(dist[e.to], e.to));
+          prevv[e.to] = v;
+          preve[e.to] = i;
+          heap.push(DijkstraState{dist[e.to], e.to});
         }
       }
     }
   }
-  void update(const int s, const int t, Cat &f, Cot &res) {
+
+  void update(const int s, const int t, Cap &f, Cost &res) {
     for (int i = 0; i < V; i++) {
-      if (dist[i] != inf) h[i] += dist[i];
+      if (dist[i] != INF) h[i] += dist[i];
     }
-    Cat d = f;
+    Cap d = f;
     for (int v = t; v != s; v = prevv[v]) {
       d = min(d, G[prevv[v]][preve[v]].cap);
     }
     f -= d;
     res += h[t] * d;
     for (int v = t; v != s; v = prevv[v]) {
-      edge &e = G[prevv[v]][preve[v]];
+      Edge &e = G[prevv[v]][preve[v]];
       e.cap -= d;
       G[v][e.rev].cap += d;
     }
-  }
-  Cot solve(const int s, const int t, Cat f) {
-    if (!tsort()) assert(false); // not DAG
-    calc_potential(s);
-    Cot res = 0;
-    while (f > 0) {
-      Dijkstra(s);
-      if (dist[t] == inf) return -inf;
-      update(s, t, f, res);
-    }
-    return res;
   }
 };
 
@@ -199,7 +223,9 @@ auto solve() {
   REP(i, n - 1) {
     g.add_edge(i, i + 1, K, 0);
   }
-  return -g.solve(0, n - 1, K);
+  auto ans = g.flow(0, n - 1, K);
+  assert(ans.has_value());
+  return -ans.value();
 }
 
 int main() {
