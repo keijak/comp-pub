76e4eedd9c28a7cda25598d4773632d1b601cc33
keijak <m@keijak.com>
2021-09-13 06:43:55 +0200

    min cost flow

--- yukicoder/1678/main.cpp
+++ yukicoder/1678/main.cpp
@@ -1,4 +1,5 @@
 #include <bits/stdc++.h>
+#include <atcoder/mincostflow>
 #define REP_(i, a_, b_, a, b, ...) \
   for (int i = (a), END_##i = (b); i < END_##i; ++i)
 #define REP(i, ...) REP_(i, __VA_ARGS__, __VA_ARGS__, 0, __VA_ARGS__)
@@ -82,57 +83,127 @@ struct Input {
 
 using namespace std;
 
+template<typename CapType, typename CostType>
+class MinCostFlowDAG {
+ public:
+  using Cat = CapType;
+  using Cot = CostType;
+  using pti = pair<Cot, int>;
+  struct edge {
+    int to, rev;
+    Cat cap;
+    Cot cost;
+  };
+  const int V;
+  const Cot inf;
+  vector<vector<edge> > G;
+  vector<Cot> h, dist;
+  vector<int> deg, ord, prevv, preve;
+  MinCostFlowDAG(const int node_size) : V(node_size), inf(numeric_limits<Cot>::max()),
+                                        G(V), h(V, inf), dist(V), deg(V, 0), prevv(V), preve(V) {}
+  void add_edge(const int from, const int to, const Cat cap, const Cot cost) {
+    if (cap == 0) return;
+    G[from].push_back((edge) {to, (int) G[to].size(), cap, cost});
+    G[to].push_back((edge) {from, (int) G[from].size() - 1, 0, -cost});
+    ++deg[to];
+  }
+  bool tsort() {
+    queue<int> que;
+    for (int i = 0; i < V; ++i) {
+      if (deg[i] == 0) que.push(i);
+    }
+    while (!que.empty()) {
+      const int p = que.front();
+      que.pop();
+      ord.push_back(p);
+      for (auto &e: G[p]) {
+        if (e.cap > 0 && --deg[e.to] == 0) que.push(e.to);
+      }
+    }
+    return (*max_element(deg.begin(), deg.end()) == 0);
+  }
+  void calc_potential(const int s) {
+    h[s] = 0;
+    for (const int v: ord) {
+      if (h[v] == inf) continue;
+      for (const edge &e: G[v]) {
+        if (e.cap > 0) h[e.to] = min(h[e.to], h[v] + e.cost);
+      }
+    }
+  }
+  void Dijkstra(const int s) {
+    priority_queue<pti, vector<pti>, greater<pti> > que;
+    fill(dist.begin(), dist.end(), inf);
+    dist[s] = 0;
+    que.push(pti(0, s));
+    while (!que.empty()) {
+      pti p = que.top();
+      que.pop();
+      const int v = p.second;
+      if (dist[v] < p.first) continue;
+      for (int i = 0; i < (int) G[v].size(); ++i) {
+        edge &e = G[v][i];
+        if (e.cap > 0 && dist[e.to] > dist[v] + e.cost + h[v] - h[e.to]) {
+          dist[e.to] = dist[v] + e.cost + h[v] - h[e.to];
+          prevv[e.to] = v, preve[e.to] = i;
+          que.push(pti(dist[e.to], e.to));
+        }
+      }
+    }
+  }
+  void update(const int s, const int t, Cat &f, Cot &res) {
+    for (int i = 0; i < V; i++) {
+      if (dist[i] != inf) h[i] += dist[i];
+    }
+    Cat d = f;
+    for (int v = t; v != s; v = prevv[v]) {
+      d = min(d, G[prevv[v]][preve[v]].cap);
+    }
+    f -= d;
+    res += h[t] * d;
+    for (int v = t; v != s; v = prevv[v]) {
+      edge &e = G[prevv[v]][preve[v]];
+      e.cap -= d;
+      G[v][e.rev].cap += d;
+    }
+  }
+  Cot solve(const int s, const int t, Cat f) {
+    if (!tsort()) assert(false); // not DAG
+    calc_potential(s);
+    Cot res = 0;
+    while (f > 0) {
+      Dijkstra(s);
+      if (dist[t] == inf) return -inf;
+      update(s, t, f, res);
+    }
+    return res;
+  }
+};
+
 auto solve() {
   const int n = in, K = in;
-  vector<vector<int>> g(n), bk(n + 1);
   vector<i64> a(n);
+  MinCostFlowDAG<int, i64> g(n);
   REP(i, n) {
     a[i] = in;
     int m = in;
     REP(j, m) {
       int b = in;
       --b;
-      g[b].push_back(i);
-      bk[i].push_back(b);
-    }
-  }
-
-  auto dp = vector(n + 1, vector(n + 1, vector(K + 1, vector(n, optional<i64>()))));
-  auto f = [&](auto &f, int l, int r, int k, int p) -> i64 {
-    if (l == r) return 0;
-    if (dp[l][r][k][p]) return *dp[l][r][k][p];
-    i64 ret = chmax(ret, f(f, l, r - 1, k, bk[r - 1].size()));
-    if (k > 0) {
-      for (int u = 0; u < p; ++u) {
-        // l -> j -> r
-        int j = bk[r][u];
-        assert(j < r);
-        if (j < l) continue;
-        i64 gain = a[r] - a[j];
-        if (gain <= 0) continue;
-        i64 sub1 = 0;
-        if (l < j) {
-          sub1 += f(f, l, j, k, bk[j].size());
-        }
-        i64 sub2 = 0;
-        if (j < r) {
-          sub2 = f(f, j, r, k - 1, u);
-        }
-        chmax(ret, sub1 + sub2 + gain);
+      i64 cost = a[b] - a[i];
+      if (cost < 0) {
+        g.add_edge(b, i, 1, cost);
       }
     }
-    dp[l][r][k][p] = ret;
-    return ret;
-  };
-  return f(f, 0, n - 1, K, bk[n - 1].size());
+  }
+  REP(i, n - 1) {
+    g.add_edge(i, i + 1, K, 0);
+  }
+  return -g.solve(0, n - 1, K);
 }
 
 int main() {
   ios_base::sync_with_stdio(false), cin.tie(nullptr);
-  cout << std::fixed << std::setprecision(18);
-  const int t = 1;
-  REP(test_case, t) {
-    auto ans = solve();
-    print(ans);
-  }
+  auto ans = solve();
+  print(ans);
 }
