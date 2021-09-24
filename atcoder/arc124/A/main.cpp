76f3950979b0b9ba0ca426f8975e437161b0679b
keijak <m@keijak.com>
2021-09-24 09:48:35 +0200

    adhoc

---
+++ atcoder/arc124/A/main.cpp
@@ -0,0 +1,132 @@
+#include <bits/stdc++.h>
+#define REP_(i, a_, b_, a, b, ...) \
+  for (int i = (a), END_##i = (b); i < END_##i; ++i)
+#define REP(i, ...) REP_(i, __VA_ARGS__, __VA_ARGS__, 0, __VA_ARGS__)
+#define ALL(x) std::begin(x), std::end(x)
+using i64 = long long;
+
+#include <atcoder/modint>
+using Mint = atcoder::modint998244353;
+std::ostream &operator<<(std::ostream &os, const Mint &m) {
+  return os << m.val();
+}
+
+template<typename T, typename U>
+inline bool chmax(T &a, U b) {
+  return a < b and ((a = std::move(b)), true);
+}
+template<typename T, typename U>
+inline bool chmin(T &a, U b) {
+  return a > b and ((a = std::move(b)), true);
+}
+template<typename T>
+inline int ssize(const T &a) {
+  return (int) a.size();
+}
+
+void print() { std::cout << "\n"; }
+template<class T>
+void print(const T &x) {
+  if constexpr (std::is_same_v<T, bool>) {
+    std::cout << (x ? "Yes" : "No") << "\n";
+  } else {
+    std::cout << x << "\n";
+  }
+}
+template<typename Head, typename... Tail>
+void print(const Head &head, Tail... tail) {
+  std::cout << head << " ";
+  print(tail...);
+}
+
+template<typename Container>
+std::ostream &print_seq(const Container &a, std::string_view sep = " ",
+                        std::string_view ends = "\n",
+                        std::ostream &os = std::cout) {
+  auto b = std::begin(a), e = std::end(a);
+  for (auto it = std::begin(a); it != e; ++it) {
+    if (it != b) os << sep;
+    os << *it;
+  }
+  return os << ends;
+}
+
+template<typename T, typename = void>
+struct is_iterable : std::false_type {};
+template<typename T>
+struct is_iterable<T, std::void_t<decltype(std::begin(std::declval<T>())),
+                                  decltype(std::end(std::declval<T>()))>>
+    : std::true_type {
+};
+
+template<typename T, typename = std::enable_if_t<
+    is_iterable<T>::value && !std::is_same<T, std::string>::value>>
+std::ostream &operator<<(std::ostream &os, const T &a) {
+  return print_seq(a, ", ", "", (os << "{")) << "}";
+}
+
+struct Input {
+  template<typename T>
+  operator T() const {
+    T x;
+    std::cin >> x;
+    return x;
+  }
+  struct SizedInput {
+    std::size_t n;
+    template<typename T>
+    operator T() const {
+      T x(n);
+      for (auto &e: x) std::cin >> e;
+      return x;
+    }
+  };
+  SizedInput operator()(std::size_t n) const { return {n}; }
+} const in;
+
+#ifdef MY_DEBUG
+#include "debug_dump.hpp"
+#else
+#define DUMP(...)
+#endif
+
+using namespace std;
+
+auto solve() {
+  const int n = in, K = in;
+  auto t = vector(n, vector(K + 1, 1));
+  REP(i, n) t[i][0] = false;
+  REP(i, 1, K + 1) {
+    const char c = in;
+    const int k = int(in) - 1;
+    REP(j, K + 1) {
+      if (j != i) t[k][j] = false;
+    }
+    if (c == 'L') {
+      REP(j, k) t[j][i] = false;
+    } else {
+      REP(j, k + 1, n) {
+        t[j][i] = false;
+      }
+    }
+  }
+  Mint ans = 1;
+  REP(i, n) {
+    int c = 0;
+    REP(j, 1, K + 1) {
+      if (t[i][j]) ++c;
+    }
+    ans *= c;
+  }
+  return ans;
+}
+
+int main() {
+  ios_base::sync_with_stdio(false), cin.tie(nullptr);
+  cout << std::fixed << std::setprecision(18);
+  const int t = 1;
+  REP(test_case, t) {
+    auto ans = solve();
+    print(ans);
+  }
+}
