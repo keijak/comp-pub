450c8fcd7be170c855959c6f5a944456da150348
keijak <m@keijak.com>
2021-10-10 22:58:44 +0200

    codechef

---
+++ codechef/contests/OCT21B/MEXOR/main.cpp
@@ -0,0 +1,128 @@
+#include <bits/stdc++.h>
+#define REP_(i, a_, b_, a, b, ...) \
+  for (int i = (a), END_##i = (b); i < END_##i; ++i)
+#define REP(i, ...) REP_(i, __VA_ARGS__, __VA_ARGS__, 0, __VA_ARGS__)
+#define ALL(x) std::begin(x), std::end(x)
+using i64 = long long;
+
+template<typename T, typename U>
+inline bool chmax(T &a, U b) {
+  return a < b and ((a = std::move(b)), true);
+}
+template<typename T, typename U>
+inline bool chmin(T &a, U b) {
+  return a > b and ((a = std::move(b)), true);
+}
+template<typename T>
+inline int ssize(const T &a) {
+  return (int) a.size();
+}
+
+template<class T>
+inline std::ostream &print_one(const T &x, char endc) {
+  if constexpr (std::is_same_v<T, bool>) {
+    return std::cout << (x ? "Yes" : "No") << endc;
+  } else {
+    return std::cout << x << endc;
+  }
+}
+template<class T>
+inline std::ostream &print(const T &x) { return print_one(x, '\n'); }
+template<typename T, typename... Ts>
+std::ostream &print(const T &head, Ts... tail) {
+  return print_one(head, ' '), print(tail...);
+}
+inline std::ostream &print() { return std::cout << '\n'; }
+
+template<typename Container>
+std::ostream &print_seq(const Container &a, std::string_view sep = " ",
+                        std::string_view ends = "\n",
+                        std::ostream &os = std::cout) {
+  auto b = std::begin(a), e = std::end(a);
+  for (auto it = std::begin(a); it != e; ++it) {
+    if (it != b) os << sep;
+    os << *it;
+  }
+  return os << ends;
+}
+
+template<typename T, typename = void>
+struct is_iterable : std::false_type {};
+template<typename T>
+struct is_iterable<T, std::void_t<decltype(std::begin(std::declval<T>())),
+                                  decltype(std::end(std::declval<T>()))>>
+    : std::true_type {
+};
+
+template<typename T, typename = std::enable_if_t<
+    is_iterable<T>::value && !std::is_same<T, std::string>::value>>
+std::ostream &operator<<(std::ostream &os, const T &a) {
+  return print_seq(a, ", ", "", (os << "{")) << "}";
+}
+
+struct VersatileInput {
+  template<typename T>
+  operator T() const {
+    T x;
+    std::cin >> x;
+    return x;
+  }
+  struct Sized {
+    std::size_t n;
+    template<typename T>
+    operator T() const {
+      T x(n);
+      for (auto &e: x) std::cin >> e;
+      return x;
+    }
+  };
+  Sized operator()(std::size_t n) const { return {n}; }
+} const in;
+
+inline void check(bool cond, const char *message = "!ERROR!") {
+  if (not cond) throw std::runtime_error(message);
+}
+
+#ifdef MY_DEBUG
+#include "debug_dump.hpp"
+#else
+#define DUMP(...)
+#define cerr if(false)std::cerr
+#endif
+
+using namespace std;
+
+// Most significant bit that is set.
+// - msb(x) = bit_floor(x)
+// - msb_log(x) = bit_width(x) - 1
+inline int msb_log(unsigned x) {
+  check(x != 0);
+  return std::numeric_limits<unsigned>::digits - __builtin_clz(x) - 1;
+}
+template<typename T, typename U = std::make_unsigned_t<T>>
+inline U msb(T x) {
+  if (x == 0) return 0;
+  return U(1) << msb_log(U(x));
+}
+
+inline bool has_single_bit(unsigned x) { return __builtin_popcount(x) == 1; }
+
+auto solve() -> unsigned {
+  unsigned X = in;
+  if (X == 0) return 1;
+  if (X == 1) return 2;
+  if (has_single_bit(X + 1)) {
+    return msb(X + 1);
+  }
+  return msb(X);
+}
+
+int main() {
+  ios_base::sync_with_stdio(false), cin.tie(nullptr);
+  cout << std::fixed << std::setprecision(18);
+  const int T = in;
+  REP(t, T) {
+    auto ans = solve();
+    print(ans);
+  }
+}
