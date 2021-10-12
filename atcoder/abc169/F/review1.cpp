b24201b1db491be9a2952064aaa73bba7f0a9d27
keijak <m@keijak.com>
2021-10-13 00:00:46 +0200

    fps

---
+++ atcoder/abc169/F/review1.cpp
@@ -0,0 +1,311 @@
+#include <bits/stdc++.h>
+#define REP_(i, a_, b_, a, b, ...) \
+  for (int i = (a), END_##i = (b); i < END_##i; ++i)
+#define REP(i, ...) REP_(i, __VA_ARGS__, __VA_ARGS__, 0, __VA_ARGS__)
+#define ALL(x) std::begin(x), std::end(x)
+using i64 = long long;
+#include <atcoder/modint>
+using Mint = atcoder::modint998244353;
+std::ostream &operator<<(std::ostream &os, const Mint &m) {
+  return os << m.val();
+}
+
+template<typename T, typename U>
+inline bool chmax(T &a, U b) {
+  return a < b and ((a = std::move(b)), true);
+}
+template<typename T, typename U>
+inline bool chmin(T &a, U b) {
+  return a > b and ((a = std::move(b)), true);
+}
+template<typename T>
+inline int ssize(const T &a) {
+  return (int) a.size();
+}
+
+template<class T>
+inline std::ostream &print_one(const T &x, char endc) {
+  if constexpr (std::is_same_v<T, bool>) {
+    return std::cout << (x ? "Yes" : "No") << endc;
+  } else {
+    return std::cout << x << endc;
+  }
+}
+template<class T>
+inline std::ostream &print(const T &x) { return print_one(x, '\n'); }
+template<typename T, typename... Ts>
+std::ostream &print(const T &head, Ts... tail) {
+  return print_one(head, ' '), print(tail...);
+}
+inline std::ostream &print() { return std::cout << '\n'; }
+
+template<typename Container>
+std::ostream &print_seq(const Container &a, std::string_view sep = " ",
+                        std::string_view ends = "\n",
+                        std::ostream &os = std::cout) {
+  auto b = std::begin(a), e = std::end(a);
+  for (auto it = std::begin(a); it != e; ++it) {
+    if (it != b) os << sep;
+    os << *it;
+  }
+  return os << ends;
+}
+
+template<typename T, typename = void>
+struct is_iterable : std::false_type {};
+template<typename T>
+struct is_iterable<T, std::void_t<decltype(std::begin(std::declval<T>())),
+                                  decltype(std::end(std::declval<T>()))>>
+    : std::true_type {
+};
+
+template<typename T, typename = std::enable_if_t<
+    is_iterable<T>::value && !std::is_same<T, std::string>::value>>
+std::ostream &operator<<(std::ostream &os, const T &a) {
+  return print_seq(a, ", ", "", (os << "{")) << "}";
+}
+
+struct VersatileInput {
+  template<typename T>
+  operator T() const {
+    T x;
+    std::cin >> x;
+    return x;
+  }
+  struct Sized {
+    std::size_t n;
+    template<typename T>
+    operator T() const {
+      T x(n);
+      for (auto &e: x) std::cin >> e;
+      return x;
+    }
+  };
+  Sized operator()(std::size_t n) const { return {n}; }
+} const in;
+
+inline void check(bool cond, const char *message = "!ERROR!") {
+  if (not cond) throw std::runtime_error(message);
+}
+
+#ifdef MY_DEBUG
+#include "debug_dump.hpp"
+#else
+#define DUMP(...)
+#define cerr if(false)std::cerr
+#endif
+
+using namespace std;
+
+template<typename T, int DMAX>
+struct NaiveMult {
+  using value_type = T;
+  static constexpr int dmax() { return DMAX; }
+
+  static std::vector<T> multiply(const std::vector<T> &x,
+                                 const std::vector<T> &y) {
+    const int n = std::min<int>(x.size() + y.size() - 1, DMAX + 1);
+    const int mi = std::min<int>(x.size(), n);
+    std::vector<T> res(n);
+    for (int i = 0; i < mi; ++i) {
+      for (int j = 0; j < int(y.size()); ++j) {
+        if (i + j >= n) break;
+        res[i + j] += x[i] * y[j];
+      }
+    }
+    return res;
+  }
+
+  static std::vector<T> invert(const std::vector<T> &x) {
+    std::vector<T> res(DMAX + 1);
+    res[0] = x[0].inv();
+    for (int i = 1; i <= DMAX; ++i) {
+      T s = 0;
+      const int mj = std::min<int>(i + 1, x.size());
+      for (int j = 1; j < mj; ++j) {
+        s += x[j] * res[i - j];
+      }
+      res[i] = -res[0] * s;
+    }
+    return res;
+  }
+};
+
+// Formal Power Series (dense format).
+template<typename Mult>
+struct DenseFPS {
+  using T = typename Mult::value_type;
+  static constexpr int dmax() { return Mult::dmax(); }
+
+  // Coefficients of terms from x^0 to x^DMAX.
+  std::vector<T> coeff_;
+
+  DenseFPS() : coeff_(1, 0) {}  // = 0 * x^0
+
+  explicit DenseFPS(std::vector<T> c) : coeff_(std::move(c)) {
+    while (size() > dmax() + 1) coeff_.pop_back();
+    assert(size() > 0);
+  }
+  DenseFPS(std::initializer_list<T> init) : coeff_(init.begin(), init.end()) {
+    while (size() > dmax() + 1) coeff_.pop_back();
+    assert(size() > 0);
+  }
+
+  DenseFPS(const DenseFPS &other) : coeff_(other.coeff_) {}
+  DenseFPS(DenseFPS &&other) : coeff_(std::move(other.coeff_)) {}
+  DenseFPS &operator=(const DenseFPS &other) {
+    coeff_ = other.coeff_;
+    return *this;
+  }
+  DenseFPS &operator=(DenseFPS &&other) {
+    coeff_ = std::move(other.coeff_);
+    return *this;
+  }
+
+  // size <= dmax + 1
+  inline int size() const { return static_cast<int>(coeff_.size()); }
+
+  // Returns the coefficient of x^k.
+  inline T operator[](int k) const { return (k >= size()) ? 0 : coeff_[k]; }
+
+  DenseFPS &operator+=(const T &scalar) {
+    coeff_[0] += scalar;
+    return *this;
+  }
+  friend DenseFPS operator+(const DenseFPS &x, const T &scalar) {
+    return DenseFPS(x) += scalar;
+  }
+  DenseFPS &operator+=(const DenseFPS &other) {
+    if (size() < other.size()) {
+      coeff_.resize(other.size());
+    }
+    for (int i = 0; i < other.size(); ++i) coeff_[i] += other[i];
+    return *this;
+  }
+  friend DenseFPS operator+(const DenseFPS &x, const DenseFPS &y) {
+    return DenseFPS(x) += y;
+  }
+
+  DenseFPS &operator-=(const DenseFPS &other) {
+    if (size() < other.size()) {
+      coeff_.resize(other.size());
+    }
+    for (int i = 0; i < other.size(); ++i) coeff_[i] -= other[i];
+    return *this;
+  }
+  friend DenseFPS operator-(const DenseFPS &x, const DenseFPS &y) {
+    return DenseFPS(x) -= y;
+  }
+
+  DenseFPS &operator*=(const T &scalar) {
+    for (auto &x: coeff_) x *= scalar;
+    return *this;
+  }
+  friend DenseFPS operator*(const DenseFPS &x, const T &scalar) {
+    return DenseFPS(x) *= scalar;
+  }
+  friend DenseFPS operator*(const T &scalar, const DenseFPS &y) {
+    return DenseFPS{scalar} *= y;
+  }
+  DenseFPS &operator*=(const DenseFPS &other) {
+    return *this =
+               DenseFPS(Mult::multiply(std::move(this->coeff_), other.coeff_));
+  }
+  friend DenseFPS operator*(const DenseFPS &x, const DenseFPS &y) {
+    return DenseFPS(Mult::multiply(x.coeff_, y.coeff_));
+  }
+
+  DenseFPS &operator/=(const T &scalar) {
+    for (auto &x: coeff_) x /= scalar;
+    return *this;
+  }
+  friend DenseFPS operator/(const DenseFPS &x, const T &scalar) {
+    return DenseFPS(x) /= scalar;
+  }
+  friend DenseFPS operator/(const T &scalar, const DenseFPS &y) {
+    return DenseFPS{scalar} /= y;
+  }
+  DenseFPS &operator/=(const DenseFPS &other) {
+    return *this *= DenseFPS(Mult::invert(other.coeff_));
+  }
+  friend DenseFPS operator/(const DenseFPS &x, const DenseFPS &y) {
+    return x * DenseFPS(Mult::invert(y.coeff_));
+  }
+
+  DenseFPS pow(i64 t) const {
+    assert(t >= 0);
+    DenseFPS res = {1}, base = *this;
+    while (t) {
+      if (t & 1) res *= base;
+      base *= base;
+      t >>= 1;
+    }
+    return res;
+  }
+
+  // Multiplies by (1 + c * x^k).
+  void multiply2_inplace(int k, int c) {
+    assert(k > 0);
+    if (size() <= dmax()) {
+      coeff_.resize(min(size() + k, dmax() + 1), 0);
+    }
+    for (int i = size() - 1; i >= k; --i) {
+      coeff_[i] += coeff_[i - k] * c;
+    }
+  }
+  // Multiplies by (1 + c * x^k).
+  DenseFPS multiply2(int k, int c) const {
+    DenseFPS res = *this;
+    res.multiply2_inplace(k, c);
+    return res;
+  }
+
+  // Divides by (1 + c * x^k).
+  void divide2_inplace(int k, int c) {
+    assert(k > 0);
+    for (int i = k; i < size(); ++i) {
+      coeff_[i] -= coeff_[i - k] * c;
+    }
+  }
+  // Divides by (1 + c * x^k).
+  DenseFPS divide2(int k, int c) const {
+    DenseFPS res = *this;
+    res.divide2_inplace(k, c);
+    return res;
+  }
+
+  T eval(const T &a) const {
+    T res = 0, x = 1;
+    for (auto c: coeff_) {
+      res += c * x;
+      x *= a;
+    }
+    return res;
+  }
+};
+
+const int D = 3000;
+using DF = DenseFPS<NaiveMult<Mint, D>>;
+
+template<typename T>
+inline bool has_bit(const T &x, int i) { return (x >> i) & 1; }
+
+auto solve() {
+  const int n = in, S = in;
+  vector<int> a = in(n);
+  DF f = {1};
+  REP(i, n) {
+    f += f.multiply2(a[i], 1);
+  }
+  return f[S];
+}
+
+int main() {
+  ios_base::sync_with_stdio(false), cin.tie(nullptr);
+  cout << std::fixed << std::setprecision(18);
+  const int T = 1;//in;
+  REP(t, T) {
+    auto ans = solve();
+    print(ans);
+  }
+}
