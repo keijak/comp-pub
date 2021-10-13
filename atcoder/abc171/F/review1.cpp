a229fde99af81863b56a96daacd90eaab69755b6
keijak <m@keijak.com>
2021-10-13 22:22:14 +0200

    fps

---
+++ atcoder/abc171/F/review1.cpp
@@ -0,0 +1,162 @@
+#include <bits/stdc++.h>
+#define REP_(i, a_, b_, a, b, ...) \
+  for (int i = (a), END_##i = (b); i < END_##i; ++i)
+#define REP(i, ...) REP_(i, __VA_ARGS__, __VA_ARGS__, 0, __VA_ARGS__)
+#define ALL(x) std::begin(x), std::end(x)
+using i64 = long long;
+#include <atcoder/modint>
+using Mint = atcoder::modint1000000007;
+std::ostream &operator<<(std::ostream &os, const Mint &m) {
+  return os << m.val();
+}
+
+template<typename T, typename U>
+inline bool chmax(T &a, U b) {
+  return a < b and ((a = std::move(b)), true);
+}
+template<typename T, typename U>
+inline bool chmin(T &a, U b) {
+  return a > b and ((a = std::move(b)), true);
+}
+template<typename T>
+inline int ssize(const T &a) {
+  return (int) a.size();
+}
+
+template<class T>
+inline std::ostream &print_one(const T &x, char endc) {
+  if constexpr(std::is_same_v < T, bool > )
+  {
+    return std::cout << (x ? "Yes" : "No") << endc;
+  } else {
+    return std::cout << x << endc;
+  }
+}
+template<class T>
+inline std::ostream &print(const T &x) { return print_one(x, '\n'); }
+template<typename T, typename... Ts>
+std::ostream &print(const T &head, Ts... tail) {
+  return print_one(head, ' '), print(tail...);
+}
+inline std::ostream &print() { return std::cout << '\n'; }
+
+template<typename Container>
+std::ostream &print_seq(const Container &a, std::string_view sep = " ",
+                        std::string_view ends = "\n",
+                        std::ostream &os = std::cout) {
+  auto b = std::begin(a), e = std::end(a);
+  for (auto it = std::begin(a); it != e; ++it) {
+    if (it != b) os << sep;
+    os << *it;
+  }
+  return os << ends;
+}
+
+template<typename T, typename = void>
+struct is_iterable : std::false_type {};
+template<typename T>
+struct is_iterable<T, std::void_t < decltype(std::begin(std::declval<T>())),
+                   decltype(std::end(std::declval<T>()))>>
+: std::true_type {
+};
+
+template<typename T, typename = std::enable_if_t<
+    is_iterable<T>::value && !std::is_same<T, std::string>::value>>
+std::ostream &operator<<(std::ostream &os, const T &a) {
+  return print_seq(a, ", ", "", (os << "{")) << "}";
+}
+
+struct VersatileInput {
+  template<typename T>
+  operator T() const {
+    T x;
+    std::cin >> x;
+    return x;
+  }
+  struct Sized {
+    std::size_t n;
+    template<typename T>
+    operator T() const {
+      T x(n);
+      for (auto &e: x) std::cin >> e;
+      return x;
+    }
+  };
+  Sized operator()(std::size_t n) const { return {n}; }
+} const in;
+
+inline void check(bool cond, const char *message = "!ERROR!") {
+  if (not cond) throw std::runtime_error(message);
+}
+
+#ifdef MY_DEBUG
+#include "debug_dump.hpp"
+#else
+#define DUMP(...)
+#define cerr if(false)std::cerr
+#endif
+
+using namespace std;
+template<class T = Mint>
+struct Factorials {
+  // factorials and inverse factorials.
+  std::vector<T> fact, ifact;
+
+  // n: max cached value.
+  Factorials(int n) : fact(n + 1), ifact(n + 1) {
+    assert(n >= 0);
+    assert(n < T::mod());
+    fact[0] = 1;
+    for (int i = 1; i <= n; ++i) {
+      fact[i] = fact[i - 1] * i;
+    }
+    ifact[n] = fact[n].inv();
+    for (int i = n; i >= 1; --i) {
+      ifact[i - 1] = ifact[i] * i;
+    }
+  }
+
+  // Combination (nCk)
+  T C(int n, int k) const {
+    if (k < 0 || k > n) return 0;
+    return fact[n] * ifact[k] * ifact[n - k];
+  }
+};
+
+template<typename T>
+std::vector<T> pow_seq(int n, int base = 2) {
+  assert(n >= 0 and base > 0);
+  std::vector<T> p(n + 1);
+  p[0] = 1;
+  for (int i = 0; i < n; ++i) {
+    p[i + 1] = p[i] * base;
+  }
+  return p;
+}
+
+auto solve() {
+  int K = in;
+  string S = in;
+  int n = ssize(S);
+  Factorials<Mint> fs(n + K);
+  auto pow25 = pow_seq<Mint>(K, 25);
+  auto pow26 = pow_seq<Mint>(K, 26);
+  Mint ans;
+  for (int i = 0; i <= K; ++i) {
+    Mint now = pow26[i];
+    now *= fs.C(n - 1 + K - i, n - 1);
+    now *= pow25[K - i];
+    ans += now;
+  }
+  return ans;
+}
+
+int main() {
+  ios_base::sync_with_stdio(false), cin.tie(nullptr);
+  cout << std::fixed << std::setprecision(18);
+  const int T = 1;//in;
+  REP(t, T) {
+    auto ans = solve();
+    print(ans);
+  }
+}
