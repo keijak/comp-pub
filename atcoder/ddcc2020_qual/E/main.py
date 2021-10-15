c4154498dbef2b2c98e4a81cdb08fd27c5682b2c
keijak <m@keijak.com>
2021-10-15 21:13:35 +0200

    interactive

--- atcoder/ddcc2020_qual/E/main.py
+++ atcoder/ddcc2020_qual/E/main.py
@@ -1,4 +1,5 @@
 import sys
+from typing import *
 
 n = int(input())
 n2 = n * 2
@@ -16,24 +17,63 @@ def query(a):
         exit(-1)
 
 
-ans = [None] * (n2 + 1)
+ans: List[Optional[bool]] = [None] * (n2 + 1)
 
 
 def answer():
-    print('!', *ans[1:], flush=True)
+    s = ['BR'[x] for x in ans[1:]]
+    print('! ' + ''.join(s), flush=True)
     exit(0)
 
 
 ini = query(list(range(1, n + 1)))
+l = 1
 r = n + 1
-while r <= n2:
-    res = query(list(range(r - n + 1, r + 1)))
+while r - l > 1:
+    mid = (r + l) // 2
+    res = query(list(range(mid, mid + n)))
     if res == ini:
-        r += 1
+        l = mid
+    else:
+        r = mid
+
+ans[l] = ini
+ans[l + n] = not ini
+
+for i in range(1, n2 + 1):
+    if ans[i] is not None:
         continue
-    ans[r] = res
-    ans[r - n] = ini
-    print('ok:', ans, file=sys.stderr)
-    for i in range(1, n2+1):
-        
-l = r - n  # ini color
+    if l <= i <= l + n:
+        continue
+    a = list(range(l + 1, l + n))
+    a.append(i)
+    a.sort()
+    res = query(a)
+    if res == ini:
+        ans[i] = ini
+    else:
+        ans[i] = not ini
+
+for i in range(1, n2 + 1):
+    if ans[i] is not None:
+        continue
+    a = [i]
+    c1, c2 = 0, 0
+    for j in range(1, n2 + 1):
+        if j == i or ans[j] is None:
+            continue
+        if ans[j] is True and c1 < n // 2:
+            a.append(j)
+            c1 += 1
+        if ans[j] is False and c2 < n // 2:
+            a.append(j)
+            c2 += 1
+    assert len(a) == n
+    a.sort()
+    res = query(a)
+    if res == ini:
+        ans[i] = ini
+    else:
+        ans[i] = not ini
+
+answer()
