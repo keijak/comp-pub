4aaeab42759c1a329e400d26725fa6b969208bd6
keijak <m@keijak.com>
2021-10-25 11:32:06 +0200

    cf div1

---
+++ cf/contest/1601/A/main_A.cpp
@@ -0,0 +1,150 @@
+#include <bits/stdc++.h>
+#define REP_(i, a_, b_, a, b, ...) \
+  for (int i = (a), END_##i = (b); i < END_##i; ++i)
+#define REP(i, ...) REP_(i, __VA_ARGS__, __VA_ARGS__, 0, __VA_ARGS__)
+#define ALL(x) std::begin(x), std::end(x)
+using Int = long long;
+using Uint = unsigned long long;
+using Real = long double;
+
+template<typename T, typename U>
+inline bool chmax(T &a, U b) {
+  return a < b and ((a = std::move(b)), true);
+}
+template<typename T, typename U>
+inline bool chmin(T &a, U b) {
+  return a > b and ((a = std::move(b)), true);
+}
+template<typename T>
+inline int ssize(const T &a) {
+  return (int) a.size();
+}
+inline void check(bool cond, const char *message = "!ERROR!") {
+  if (not cond) {
+    std::cout.flush(), std::cerr.flush();
+    throw std::runtime_error(message);
+  }
+}
+
+template<class T>
+inline std::ostream &print_one(const T &x, char endc) {
+  if constexpr (std::is_same_v<T, bool>) {
+    return std::cout << (x ? "Yes" : "No") << endc;
+  } else {
+    return std::cout << x << endc;
+  }
+}
+template<class T>
+inline std::ostream &print(const T &x) { return print_one(x, '\n'); }
+template<typename T, typename... Ts>
+std::ostream &print(const T &head, Ts... tail) {
+  return print_one(head, ' '), print(tail...);
+}
+inline std::ostream &print() { return std::cout << '\n'; }
+
+template<typename Container>
+std::ostream &print_seq(const Container &a, const char *sep = " ",
+                        const char *ends = "\n",
+                        std::ostream &os = std::cout) {
+  auto b = std::begin(a), e = std::end(a);
+  for (auto it = std::begin(a); it != e; ++it) {
+    if (it != b) os << sep;
+    os << *it;
+  }
+  return os << ends;
+}
+
+template<typename T, typename = void>
+struct is_iterable : std::false_type {};
+template<typename T>
+struct is_iterable<T, std::void_t<decltype(std::begin(std::declval<T>())),
+                                  decltype(std::end(std::declval<T>()))>>
+    : std::true_type {
+};
+
+template<typename T, typename = std::enable_if_t<
+    is_iterable<T>::value && !std::is_same<T, std::string>::value>>
+std::ostream &operator<<(std::ostream &os, const T &a) {
+  return print_seq(a, ", ", "", (os << "{")) << "}";
+}
+
+struct CastInput {
+  template<typename T>
+  operator T() const {
+    T x;
+    std::cin >> x;
+    return x;
+  }
+  struct Sized {
+    std::size_t n;
+    template<typename T>
+    operator T() const {
+      T x(n);
+      for (auto &e: x) std::cin >> e;
+      return x;
+    }
+  };
+  Sized operator()(std::size_t n) const { return {n}; }
+} in;
+
+#ifdef MY_DEBUG
+#include "debug_dump.hpp"
+#include "backward.hpp"
+backward::SignalHandling kSignalHandling;
+#else
+#define DUMP(...)
+#define cerr if(false)std::cerr
+#endif
+
+using namespace std;
+
+template<typename T>
+inline bool has_bit(const T &x, int i) { return (x >> i) & 1; }
+
+// Returns all divisors of n. O(sqrt(n)) + sorting.
+std::vector<Int> divisors(Int n) {
+  std::vector<Int> res;
+  for (Int k = 1; k * k <= n; ++k) {
+    if (n % k != 0) continue;
+    res.push_back(k);
+    Int q = n / k;
+    if (q != k) res.push_back(q);
+  }
+  std::sort(res.begin(), res.end());
+  return res;
+}
+
+auto solve() -> vector<Int> {
+  int n = in;
+  vector<unsigned> a = in(n);
+  if (all_of(ALL(a), [&](unsigned x) { return x == 0; })) {
+    vector<Int> ans(n);
+    REP(i, n) ans[i] = i + 1;
+    return ans;
+  }
+  int g = -1;
+  REP(i, 30) {
+    int cnt = 0;
+    REP(j, n) {
+      if (has_bit(a[j], i)) ++cnt;
+    }
+    //if (cnt > 0) DUMP(i, cnt);
+    if (cnt > 0) {
+      if (g == -1) g = cnt;
+      else g = gcd(g, cnt);
+    }
+  }
+  check(g != -1);
+  return divisors(g);
+}
+
+int main() {
+  ios_base::sync_with_stdio(false), cin.tie(nullptr);
+  cout << std::fixed << std::setprecision(18);
+  const int T = in;
+  REP(t, T) {
+    DUMP("test", t);
+    auto ans = solve();
+    print_seq(ans);
+  }
+}
