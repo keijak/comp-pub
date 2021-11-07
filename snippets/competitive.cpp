431f98415917541260fe2925e69924a197658b77
keijak <m@keijak.com>
2021-11-07 23:12:14 +0100

    competitive template

--- snippets/competitive.cpp
+++ snippets/competitive.cpp
@@ -3,42 +3,52 @@
   for (int i = (a), END_##i = (b); i < END_##i; ++i)
 #define REP(i, ...) REP_(i, __VA_ARGS__, __VA_ARGS__, 0, __VA_ARGS__)
 #define ALL(x) std::begin(x), std::end(x)
-using i64 = long long;
+using Int = long long;
+using Uint = unsigned long long;
+using Real = long double;
 
-template <typename T, typename U>
+template<typename T, typename U>
 inline bool chmax(T &a, U b) {
   return a < b and ((a = std::move(b)), true);
 }
-template <typename T, typename U>
+template<typename T, typename U>
 inline bool chmin(T &a, U b) {
   return a > b and ((a = std::move(b)), true);
 }
-template <typename T>
+template<typename T>
 inline int ssize(const T &a) {
-  return (int)a.size();
+  return (int) a.size();
+}
+inline void check(bool cond, const char *message = "!ERROR!") {
+  if (not cond) {
+    std::cout.flush(), std::cerr.flush();
+    throw std::runtime_error(message);
+  }
 }
 
-template <class T>
+struct Void {};  // No print.
+
+template<class T>
 inline std::ostream &print_one(const T &x, char endc) {
-  if constexpr (std::is_same_v<T, bool>) {
+  if constexpr (std::is_same_v<T, Void>) {
+    return std::cout;  // Nothing.
+  } else if constexpr (std::is_same_v<T, bool>) {
     return std::cout << (x ? "Yes" : "No") << endc;
   } else {
     return std::cout << x << endc;
   }
 }
-template <class T>
-inline std::ostream &print(const T &x) {
-  return print_one(x, '\n');
-}
-template <typename T, typename... Ts>
+template<class T>
+inline std::ostream &print(const T &x) { return print_one(x, '\n'); }
+template<typename T, typename... Ts>
 std::ostream &print(const T &head, Ts... tail) {
   return print_one(head, ' '), print(tail...);
 }
 inline std::ostream &print() { return std::cout << '\n'; }
 
-template <typename Container>
-std::ostream &print_seq(const Container &a, std::string_view sep = " ",
-                        std::string_view ends = "\n",
+template<typename Container>
+std::ostream &print_seq(const Container &a, const char *sep = " ",
+                        const char *ends = "\n",
                         std::ostream &os = std::cout) {
   auto b = std::begin(a), e = std::end(a);
   for (auto it = std::begin(a); it != e; ++it) {
@@ -48,22 +58,22 @@ std::ostream &print_seq(const Container &a, std::string_view sep = " ",
   return os << ends;
 }
 
-template <typename T, typename = void>
+template<typename T, typename = void>
 struct is_iterable : std::false_type {};
-template <typename T>
+template<typename T>
 struct is_iterable<T, std::void_t<decltype(std::begin(std::declval<T>())),
                                   decltype(std::end(std::declval<T>()))>>
-    : std::true_type {};
+    : std::true_type {
+};
 
-template <typename T,
-          typename = std::enable_if_t<is_iterable<T>::value &&
-                                      !std::is_same<T, std::string>::value>>
+template<typename T, typename = std::enable_if_t<
+    is_iterable<T>::value && !std::is_same<T, std::string>::value>>
 std::ostream &operator<<(std::ostream &os, const T &a) {
   return print_seq(a, ", ", "", (os << "{")) << "}";
 }
 
-struct VersatileInput {
-  template <typename T>
+struct CastInput {
+  template<typename T>
   operator T() const {
     T x;
     std::cin >> x;
@@ -71,35 +81,37 @@ struct VersatileInput {
   }
   struct Sized {
     std::size_t n;
-    template <typename T>
+    template<typename T>
     operator T() const {
       T x(n);
-      for (auto &e : x) std::cin >> e;
+      for (auto &e: x) std::cin >> e;
       return x;
     }
   };
   Sized operator()(std::size_t n) const { return {n}; }
-} const in;
-
-inline void check(bool cond, const char *message = "!ERROR!") {
-  if (not cond) throw std::runtime_error(message);
-}
+} in;
 
 #ifdef MY_DEBUG
 #include "debug_dump.hpp"
+#include "backward.hpp"
+backward::SignalHandling kSignalHandling;
 #else
 #define DUMP(...)
+#define cerr if(false)std::cerr
 #endif
 
 using namespace std;
 
-auto solve() { return -42; }
+auto solve() {
+
+  return Void{};
+}
 
 int main() {
   ios_base::sync_with_stdio(false), cin.tie(nullptr);
   cout << std::fixed << std::setprecision(18);
-  const int t = 1;
-  REP(test_case, t) {
+  const int T = 1;//in;
+  REP(t, T) {
     auto ans = solve();
     print(ans);
   }
