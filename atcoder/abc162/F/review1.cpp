ade48c7e871b34ea6745c385f4c59baf2bfaf72e
keijak <m@keijak.com>
2021-11-09 10:28:03 +0100

    dp

---
+++ atcoder/abc162/F/review1.cpp
@@ -0,0 +1,148 @@
+#include <bits/stdc++.h>
+#define REP_(i, a_, b_, a, b, ...) \
+  for (int i = (a), END_##i = (b); i < END_##i; ++i)
+#define REP(i, ...) REP_(i, __VA_ARGS__, __VA_ARGS__, 0, __VA_ARGS__)
+#define ALL(x) std::begin(x), std::end(x)
+using Int = long long;
+using Uint = unsigned long long;
+using Real = long double;
+
+template<typename T, typename U>
+inline bool chmax(T &a, U b) {
+  return a < b and ((a = std::move(b)), true);
+}
+template<typename T, typename U>
+inline bool chmin(T &a, U b) {
+  return a > b and ((a = std::move(b)), true);
+}
+template<typename T>
+inline int ssize(const T &a) {
+  return (int) a.size();
+}
+inline void check(bool cond, const char *message = "!ERROR!") {
+  if (not cond) {
+    std::cout.flush(), std::cerr.flush();
+    throw std::runtime_error(message);
+  }
+}
+
+struct Void {};  // No print.
+
+template<class T>
+inline std::ostream &print_one(const T &x, char endc) {
+  if constexpr (std::is_same_v<T, Void>) {
+    return std::cout;  // Nothing.
+  } else if constexpr (std::is_same_v<T, bool>) {
+    return std::cout << (x ? "Yes" : "No") << endc;
+  } else {
+    return std::cout << x << endc;
+  }
+}
+template<class T>
+inline std::ostream &print(const T &x) { return print_one(x, '\n'); }
+template<typename T, typename... Ts>
+std::ostream &print(const T &head, Ts... tail) {
+  return print_one(head, ' '), print(tail...);
+}
+inline std::ostream &print() { return std::cout << '\n'; }
+
+template<typename Container>
+std::ostream &print_seq(const Container &a, const char *sep = " ",
+                        const char *ends = "\n",
+                        std::ostream &os = std::cout) {
+  auto b = std::begin(a), e = std::end(a);
+  for (auto it = std::begin(a); it != e; ++it) {
+    if (it != b) os << sep;
+    os << *it;
+  }
+  return os << ends;
+}
+
+template<typename T, typename = void>
+struct is_iterable : std::false_type {};
+template<typename T>
+struct is_iterable<T, std::void_t<decltype(std::begin(std::declval<T>())),
+                                  decltype(std::end(std::declval<T>()))>>
+    : std::true_type {
+};
+
+template<typename T, typename = std::enable_if_t<
+    is_iterable<T>::value && !std::is_same<T, std::string>::value>>
+std::ostream &operator<<(std::ostream &os, const T &a) {
+  return print_seq(a, ", ", "", (os << "{")) << "}";
+}
+
+struct CastInput {
+  template<typename T>
+  operator T() const {
+    T x;
+    std::cin >> x;
+    return x;
+  }
+  struct Sized {
+    std::size_t n;
+    template<typename T>
+    operator T() const {
+      T x(n);
+      for (auto &e: x) std::cin >> e;
+      return x;
+    }
+  };
+  Sized operator()(std::size_t n) const { return {n}; }
+} in;
+
+#ifdef MY_DEBUG
+#include "debug_dump.hpp"
+#include "backward.hpp"
+backward::SignalHandling kSignalHandling;
+#else
+#define DUMP(...)
+#define cerr if(false)std::cerr
+#endif
+
+using namespace std;
+
+const Int kBig = (Int) 2e18;
+
+auto solve() {
+  int n = in;
+  vector<Int> a = in(n);
+  int T = (n & 1) ? 2 : 1;
+  Int dp[2][3][2];
+  REP(i, 2) REP(j, 3) REP(pre, 2) dp[i][j][pre] = -kBig;
+  dp[0][0][0] = 0;
+  REP(i, n) {
+    const auto x = a[i];
+    REP(j, T + 1) {
+      REP(pre, 2) {
+        const auto dpval = dp[0][j][pre];
+        if (dpval == -kBig) continue;
+        if (pre) {
+          chmax(dp[1][j][0], dpval);
+        } else {
+          chmax(dp[1][j][1], dpval + x);
+          if (j + 1 <= T) chmax(dp[1][j + 1][0], dpval);
+        }
+      }
+    }
+    swap(dp[0], dp[1]);
+    REP(j, T + 1) REP(pre, 2) dp[1][j][pre] = -kBig;
+  }
+  Int ans = -kBig;
+  REP(j, (n & 1) ? 1 : 0, T + 1) {
+    REP(pre, 2) {
+      chmax(ans, dp[0][j][pre]);
+    }
+  }
+  return ans;
+}
+
+int main() {
+  ios_base::sync_with_stdio(false), cin.tie(nullptr);
+  cout << std::fixed << std::setprecision(18);
+  const int T = 1;//in;
+  REP(t, T) {
+    auto ans = solve();
+    print(ans);
+  }
+}
