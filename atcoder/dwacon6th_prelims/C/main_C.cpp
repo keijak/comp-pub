a923002d37467f35e70ce07c00b59a08388520f8
keijak <m@keijak.com>
2021-12-13 01:29:11 +0100

    cookie distribution

--- atcoder/dwacon6th_prelims/C/main_C.cpp
+++ atcoder/dwacon6th_prelims/C/main_C.cpp
@@ -5,7 +5,10 @@
 #define ALL(x) std::begin(x), std::end(x)
 using Int = long long;
 using Uint = unsigned long long;
-using Real = long double;
+using Real = double;
+
+#include <atcoder/convolution>
+#include <atcoder/math>
 #include <atcoder/modint>
 using Mint = atcoder::modint1000000007;
 std::ostream &operator<<(std::ostream &os, const Mint &m) {
@@ -31,9 +34,13 @@ inline void check(bool cond, const char *message = "!ERROR!") {
   }
 }
 
+struct Void {};
+
 template<class T>
 inline std::ostream &print_one(const T &x, char endc) {
-  if constexpr (std::is_same_v<T, bool>) {
+  if constexpr (std::is_same<T, Void>::value) {
+    return std::cout;  // print nothing
+  } else if constexpr (std::is_same<T, bool>::value) {
     return std::cout << (x ? "Yes" : "No") << endc;
   } else {
     return std::cout << x << endc;
@@ -48,12 +55,13 @@ std::ostream &print(const T &head, Ts... tail) {
 inline std::ostream &print() { return std::cout << '\n'; }
 
 template<typename Container>
-std::ostream &print_seq(const Container &a, const char *sep = " ",
+std::ostream &print_seq(const Container &seq,
+                        const char *sep = " ",
                         const char *ends = "\n",
                         std::ostream &os = std::cout) {
-  auto b = std::begin(a), e = std::end(a);
-  for (auto it = std::begin(a); it != e; ++it) {
-    if (it != b) os << sep;
+  const auto itl = std::begin(seq), itr = std::end(seq);
+  for (auto it = itl; it != itr; ++it) {
+    if (it != itl) os << sep;
     os << *it;
   }
   return os << ends;
@@ -79,36 +87,59 @@ struct CastInput {
 } in;
 
 #ifdef MY_DEBUG
-#include "debug_dump.hpp"
+//#include "debug_dump.hpp"
 #include "backward.hpp"
 backward::SignalHandling kSignalHandling;
 #else
 #define DUMP(...)
-#define cerr if(false)std::cerr
+#define cerr if(false)cerr
 #endif
 
 using namespace std;
 
-Mint factorial(int x) {
-  static std::vector<Mint> facts = {1, 1, 2};
-  facts.reserve(x + 1);
-  while ((int) facts.size() <= x) {
-    facts.push_back(facts.back() * facts.size());
-  }
-  return facts[x];
-}
+template<class T = Mint>
+struct Factorials {
+  // factorials and inverse factorials.
+  std::vector<T> fact, ifact;
 
-auto solve() {
-  const int n = in;
-  const vector<Int> X = in(n);
-  Mint ans = 0;
-  Mint contrib = 0;
-  REP(i, n - 1) {
-    Mint d = X[i + 1] - X[i];
-    contrib += Mint(1) / (i + 1);
-    ans += contrib * d;
+  // n: max cached value.
+  Factorials(int n) : fact(n + 1), ifact(n + 1) {
+    assert(n >= 0);
+    assert(n < T::mod());
+    fact[0] = 1;
+    for (int i = 1; i <= n; ++i) {
+      fact[i] = fact[i - 1] * i;
+    }
+    ifact[n] = fact[n].inv();
+    for (int i = n; i >= 1; --i) {
+      ifact[i - 1] = ifact[i] * i;
+    }
   }
-  return ans * factorial(n - 1);
+
+  // Combination (nCk)
+  T C(int n, int k) const {
+    if (k < 0 || k > n) return 0;
+    return fact[n] * ifact[k] * ifact[n - k];
+  }
+};
+
+auto solve() -> Mint {
+  int n = in, K = in;
+  vector<int> a = in(K);
+  Factorials fs(n);
+
+  auto dp = vector(K + 1, vector(n + 1, Mint(0)));
+  dp[0][0] = 1;
+  REP(i, K) {
+    REP(j, n + 1) {
+      if (dp[i][j].val() == 0) continue;
+      int r = n - j;
+      for (int t = 0; t <= min(r, a[i]); ++t) {
+        dp[i + 1][j + t] += dp[i][j] * fs.C(r, t) * fs.C(n - t, a[i] - t);
+      }
+    }
+  }
+  return dp[K][n];
 }
 
 int main() {
