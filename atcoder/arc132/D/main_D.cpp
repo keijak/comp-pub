54fc6cfac986e05b1602761d7401d59aac34f72a
keijak <m@keijak.com>
2021-12-27 20:53:20 +0100

    greedy

--- atcoder/arc132/D/main_D.cpp
+++ atcoder/arc132/D/main_D.cpp
@@ -90,98 +90,50 @@ void flip(string &s) {
   }
 }
 
-Int beauty(const string &s) {
-  int n = s.size();
-  Int ret = 0;
-  REP(i, 1, n) {
-    if (s[i] == s[i - 1]) {
-      ++ret;
-    }
-  }
-  return ret;
-}
-
 auto solve() {
   int n = in, m = in;
   string S = in;
   string T = in;
   Int ans = 0;
-//  chmax(ans, beauty(S));
-//  chmax(ans, beauty(T));
-  REP(ite, 1) {
-    if (n < m) {
+  REP(ite, 2) {
+    if (ite == 1) {
       swap(n, m);
       flip(S);
       flip(T);
     }
     vector<int> si, ti;
     REP(i, n + m) {
-      if (S[i] == '1') {
-        si.push_back(i);
-      }
-      if (T[i] == '1') {
-        ti.push_back(i);
-      }
+      if (S[i] == '1') si.push_back(i);
+      if (T[i] == '1') ti.push_back(i);
     }
-    DUMP(si);
-    DUMP(ti);
     assert(ssize(si) == ssize(ti));
     const int Z = ssize(si);
 
-    vector<unordered_map<int, Int>> memo(Z);
-
-    auto f = [&](auto &f, int i, int px) -> Int {
-      if (i == Z) {
-        return max<Int>((n + m - 1) - px - 1, 0);
-      }
-      if (auto it = memo[i].find(px); it != memo[i].end()) {
-        return it->second;
-      }
+    Int res = 0;
+    int px = -1;
+    REP(i, Z) {
       int sx = si[i];
       int tx = ti[i];
-      Int res = 0;
-      //string branch;
-      if (sx == tx) {
-        Int r = f(f, i + 1, sx);
-        if (px + 3 <= sx) r += sx - px - 2;
-        if (i > 0 and px + 1 == sx) ++r;
-        chmax(res, r);
-        //branch = "eq";
-      } else if (sx < tx) {
-        Int r = f(f, i + 1, tx);
-        if (px + 3 <= tx) r += tx - px - 2;
-        if (i > 0 and px + 1 == tx) ++r;
-        chmax(res, r);
-        //branch = "less";
-        if (px + 1 >= sx) {
-          assert(px + 1 <= tx);
-          r = f(f, i + 1, px + 1);
-          if (i > 0) ++r;
-          if (chmax(res, r)) {
-            //  branch += '+';
-          }
+      if (sx > tx) {
+        if (i > 0 and tx <= px + 1 and px + 1 <= sx) {
+          ++res;
+          px = px + 1;
+        } else {
+          res += max(sx - px - 2, 0);
+          px = sx;
         }
       } else {
-        Int r = f(f, i + 1, sx);
-        if (px + 3 <= sx) r += sx - px - 2;
-        if (i > 0 and px + 1 == sx) ++r;
-        chmax(res, r);
-        //branch = "gt";
-        if (tx <= px + 1 and px + 1 <= sx) {
-          r = f(f, i + 1, px + 1);
-          if (i > 0) ++r;
-          if (chmax(res, r)) {
-            // branch += '+';
-          }
+        if (i > 0 and sx <= px + 1 and px + 1 <= tx) {
+          ++res;
+          px = px + 1;
+        } else {
+          res += max(tx - px - 2, 0);
+          px = tx;
         }
       }
-//    DUMP(i, px, res, branch);
-      memo[i][px] = res;
-      return res;
-    };
-    DUMP(S);
-    DUMP(T);
-    chmax(ans, f(f, 0, -1));
+    }
+    res += max<Int>((n + m - 1) - px - 1, 0);
+    chmax(ans, res);
   }
   return ans;
 }
