ffd4eefc33d0c37fb8bc10865e5b7be9f0bc5a25
keijak <m@keijak.com>
2022-01-03 09:54:06 +0100

    cf

--- cf/contest/1623/D/main_D.cpp
+++ cf/contest/1623/D/main_D.cpp
@@ -6,6 +6,125 @@ using Int = long long;
 using Uint = unsigned long long;
 using Real = long double;
 
+template<unsigned M>
+struct ModInt {
+  constexpr ModInt(long long val = 0) : _v(0) {
+    if (val < 0) {
+      long long k = (std::abs(val) + M - 1) / M;
+      val += k * M;
+    }
+    assert(val >= 0);
+    _v = val % M;
+  }
+
+  static constexpr int mod() { return M; }
+  static constexpr unsigned umod() { return M; }
+  inline unsigned val() const { return _v; }
+
+  ModInt &operator++() {
+    _v++;
+    if (_v == umod()) _v = 0;
+    return *this;
+  }
+  ModInt &operator--() {
+    if (_v == 0) _v = umod();
+    _v--;
+    return *this;
+  }
+  ModInt operator++(int) {
+    auto result = *this;
+    ++*this;
+    return result;
+  }
+  ModInt operator--(int) {
+    auto result = *this;
+    --*this;
+    return result;
+  }
+
+  constexpr ModInt operator-() const { return ModInt(umod() - _v); }
+
+  constexpr ModInt &operator+=(const ModInt &a) {
+    if ((_v += a._v) >= M) _v -= M;
+    return *this;
+  }
+  constexpr ModInt &operator-=(const ModInt &a) {
+    if ((_v += M - a._v) >= M) _v -= M;
+    return *this;
+  }
+  constexpr ModInt &operator*=(const ModInt &a) {
+    _v = ((unsigned long long) (_v) * a._v) % M;
+    return *this;
+  }
+  constexpr ModInt pow(unsigned long long t) const {
+    if (_v == 0) {
+      return 0;  // corner case: 0^0 = ?
+    }
+    ModInt base = *this;
+    ModInt res = 1;
+    while (t) {
+      if (t & 1) res *= base;
+      base *= base;
+      t >>= 1;
+    }
+    return res;
+  }
+
+  constexpr ModInt inv() const {
+    // Inverse by Extended Euclidean algorithm.
+    // M doesn't need to be prime, but x and M must be coprime.
+    assert(_v != 0);
+    auto[g, x, y] = ext_gcd(_v, M);
+    assert(g == 1);  // The GCD must be 1.
+    return x;
+
+    // Inverse by Fermat's little theorem. M must be prime.
+    //
+    //     return pow(M - 2);
+  }
+  constexpr ModInt &operator/=(const ModInt &a) { return *this *= a.inv(); }
+
+  friend constexpr ModInt operator+(const ModInt &a, const ModInt &b) {
+    return ModInt(a) += b;
+  }
+  friend constexpr ModInt operator-(const ModInt &a, const ModInt &b) {
+    return ModInt(a) -= b;
+  }
+  friend constexpr ModInt operator*(const ModInt &a, const ModInt &b) {
+    return ModInt(a) *= b;
+  }
+  friend constexpr ModInt operator/(const ModInt &a, const ModInt &b) {
+    return ModInt(a) /= b;
+  }
+  friend constexpr bool operator==(const ModInt &a, const ModInt &b) {
+    return a._v == b._v;
+  }
+  friend constexpr bool operator!=(const ModInt &a, const ModInt &b) {
+    return a._v != b._v;
+  }
+  friend std::istream &operator>>(std::istream &is, ModInt &a) {
+    return is >> a._v;
+  }
+  friend std::ostream &operator<<(std::ostream &os, const ModInt &a) {
+    return os << a._v;
+  }
+
+ private:
+  // Extended Euclidean algorithm.
+  // Returns [g, x, y] where g = a*x + b*y = GCD(a, b).
+  static constexpr std::array<long long, 3> ext_gcd(int a, int b) {
+    if (b == 0) return {a, 1, 0};
+    auto res = ext_gcd(b, a % b);  // = (g, x, y)
+    res[1] -= (a / b) * res[2];
+    std::swap(res[1], res[2]);
+    return res;  // = (g, y, x - (a/b)*y)
+  }
+
+  unsigned _v;  // raw value
+};
+const unsigned MOD = int(1e9) + 7;
+using Mint = ModInt<MOD>;
+
 template<typename T, typename U>
 inline bool chmax(T &a, U b) {
   return a < b and ((a = std::move(b)), true);
@@ -82,16 +201,73 @@ backward::SignalHandling kSignalHandling;
 
 using namespace std;
 
-auto solve() {
+template<typename T>
+void sort_unique(std::vector<T> &v) {
+  std::sort(v.begin(), v.end());
+  v.erase(std::unique(v.begin(), v.end()), v.end());
+}
 
-  return Void{};
+auto solve() {
+  int n = in, m = in, rb = in, cb = in, rd = in, cd = in, P = in;
+  --rb, --cb, --rd, --cd;
+  set<int> rx, cx;
+  rx.insert((rd - rb + 2 * (n - 1)) % (2 * (n - 1)));
+  if (rd != 0 and rd != n - 1) {
+    rx.insert((2 * (n - 1) - rb - rd) % (2 * (n - 1)));
+  }
+  cx.insert((cd - cb + 2 * (m - 1)) % (2 * (m - 1)));
+  if (cd != 0 and cd != m - 1) {
+    cx.insert((2 * (m - 1) - cb - cd) % (2 * (m - 1)));
+  }
+  DUMP(rx, cx);
+  Int g = std::gcd((Int) n - 1, (Int) m - 1);
+  Int period = lcm(2 * (n - 1), 2 * (m - 1));
+  vector<Int> lx;
+  for (int x: rx) {
+    x %= period;
+    Int offset = 0;
+    REP(i, (m - 1) / g) {
+      lx.push_back(offset + x);
+      offset += 2 * (n - 1);
+    }
+  }
+  for (int x: cx) {
+    x %= period;
+    Int offset = 0;
+    REP(i, (n - 1) / g) {
+      lx.push_back(offset + x);
+      offset += 2 * (m - 1);
+    }
+  }
+  sort_unique(lx);
+  DUMP(n, m, period, lx.size());
+  if (lx.size() < 10) DUMP(lx);
+  assert(not lx.empty());
+  Mint q = Mint(100 - P) / 100;
+
+  auto f = [&](auto &f, int i) -> pair<Mint, Mint> {
+    if (i == ssize(lx) - 1) {
+      Int x = (lx[0] - lx[i] - 1 + period) % period;
+      return {Mint(1 + q * x), Mint(q)};
+    } else {
+      auto[x, y] = f(f, i + 1);
+      x += (lx[i + 1] - lx[i] - 1);
+      x *= q;
+      y *= q;
+      x += 1;
+      return {x, y};
+    }
+  };
+  auto[x, y] = f(f, 0);
+  return Mint(lx[0]) + (x / (1 - y)) - 1;
 }
 
 int main() {
   std::ios::sync_with_stdio(false), cin.tie(nullptr);
   cout << std::fixed << std::setprecision(18);
-  const int T = 1;//in;
+  const int T = in;
   REP(t, T) {
+    DUMP("#", t);
     auto ans = solve();
     print(ans);
   }
