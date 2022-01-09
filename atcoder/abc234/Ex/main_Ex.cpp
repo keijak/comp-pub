8e261d81f0d5c431adc995005cdf25d6eae260f9
keijak <m@keijak.com>
2022-01-09 14:19:33 +0100

    range tree

--- atcoder/abc234/Ex/main_Ex.cpp
+++ atcoder/abc234/Ex/main_Ex.cpp
@@ -1,93 +1,63 @@
 #include <bits/stdc++.h>
-#define REP_(i, a_, b_, a, b, ...) for (int i = (a), END_##i = (b); i < END_##i; ++i)
-#define REP(i, ...) REP_(i, __VA_ARGS__, __VA_ARGS__, 0, __VA_ARGS__)
-#define ALL(x) std::begin(x), std::end(x)
+using namespace std;
 using Int = long long;
 
-struct Point {
-  using T = long long;
-  T x, y;
-  int index;
-};
+// https://kopricky.github.io/code/SegmentTrees/orthogonal_range_report.html
 
-template<class P>
-struct KdTree2d {
-  using T = typename P::T;
-  std::vector<P> points_;
+#define all(v) (v).begin(),(v).end()
 
-  struct Node {
-    T xmin, xmax, ymin, ymax;
-    std::vector<int> indices;
-    Node *l, *r;
-
-    Node(typename std::vector<P>::iterator begin,
-         typename std::vector<P>::iterator end)
-        : xmin(std::numeric_limits<T>::max()),
-          xmax(std::numeric_limits<T>::lowest()),
-          ymin(std::numeric_limits<T>::max()),
-          ymax(std::numeric_limits<T>::lowest()) {
-      if (begin == end) return;
-
-      {
-        auto[mn, mx] = std::minmax_element(
-            begin, end, [](const P &a, const P &b) { return a.x < b.x; });
-        xmin = mn->x;
-        xmax = mx->x;
+template<typename CandidateType>
+class OrthogonalRangeReport {
+ private:
+  using CT = CandidateType;
+  using pcc = pair<CT, CT>;
+  using pci = pair<CT, int>;
+  int n, sz;
+  //座標, インデックス
+  vector<pair<pcc, int> > sorted;
+  //x座標
+  vector<CT> xs;
+  //y座標, インデックス
+  vector<vector<pci> > ys;
+  void query(int lxid, int rxid, CT ly, CT ry, vector<int> &report, int k, int l, int r) {
+    if (r <= lxid || rxid <= l) return;
+    if (lxid <= l && r <= rxid) {
+      auto st = lower_bound(all(ys[k]), pci(ly, -1)), ed = upper_bound(all(ys[k]), pci(ry, -1));
+      for (auto it = st; it != ed; ++it) {
+        report.push_back(it->second);
       }
-      {
-        auto[mn, mx] = std::minmax_element(
-            begin, end, [](const P &a, const P &b) { return a.y < b.y; });
-        ymin = mn->y;
-        ymax = mx->y;
-      }
-      const int n = int(end - begin);
-      indices.reserve(n);
-      for (auto it = begin; it != end; ++it) {
-        indices.emplace_back(it->index);
-      }
-      if (n == 1) return;
-
-      auto mid = begin + (n / 2);
-      if (xmax - xmin > ymax - ymin) {
-        std::nth_element(begin, mid, end, [](const P &a, const P &b) { return a.x < b.x; });
-      } else {
-        std::nth_element(begin, mid, end, [](const P &a, const P &b) { return a.y < b.y; });
-      }
-      l = new Node(begin, mid);
-      r = new Node(mid, end);
+    } else {
+      query(lxid, rxid, ly, ry, report, 2 * k + 1, l, (l + r) / 2);
+      query(lxid, rxid, ly, ry, report, 2 * k + 2, (l + r) / 2, r);
     }
-
-    template<class F>
-    void search_rect(T x_low, T y_low, T x_high, T y_high, F process_index) {
-      static_assert(std::is_invocable_v<F, int>);
-      // The node range is completely contained by the query range.
-      if (x_low <= xmin and xmax <= x_high and y_low <= ymin and ymax <= y_high) {
-        for (int i: indices) process_index(i);
-        return;
-      }
-      // The node range is completely outside of the query range.
-      if (xmax < x_low or x_high < xmin or ymax < y_low or y_high < ymin) {
-        return;
-      }
-      // Otherwise
-      if (l) l->search_rect(x_low, y_low, x_high, y_high, process_index);
-      if (r) r->search_rect(x_low, y_low, x_high, y_high, process_index);
+  }
+ public:
+  OrthogonalRangeReport(const vector<pcc> &cand) : n(1), sz((int) cand.size()), sorted(sz), xs(sz) {
+    while (n < sz) n *= 2;
+    for (int i = 0; i < sz; i++) {
+      sorted[i] = make_pair(cand[i], i);
     }
-  };
-  Node root_;
-
-  explicit KdTree2d(std::vector<P> points)
-      : points_(std::move(points)), root_(points_.begin(), points_.end()) {}
-
-  // Visit each point in (top-left x bottom-right) rectangle (boundary
-  // inclusive).
-  template<class F>
-  void search_rect(T x_low, T y_low, T x_high, T y_high, F process_index) {
-    root_.search_rect(x_low, y_low, x_high, y_high, process_index);
+    sort(sorted.begin(), sorted.end());
+    ys.resize(2 * n - 1);
+    for (int i = 0; i < sz; i++) {
+      xs[i] = (sorted[i].first).first;
+      ys[i + n - 1] = {pci((sorted[i].first).second, sorted[i].second)};
+    }
+    for (int i = n - 2; i >= 0; i--) {
+      ys[i].resize((int) ys[2 * i + 1].size() + (int) ys[2 * i + 2].size());
+      merge(all(ys[2 * i + 1]), all(ys[2 * i + 2]), ys[i].begin(), [&](pci &a, pci &b) {
+        return a.first < b.first;
+      });
+    }
+  }
+  // [lx,rx)×[ly,ry)の長方形領域の範囲内の点のインデックスを報告する
+  void query(const CT lx, const CT rx, const CT ly, const CT ry, vector<int> &report) {
+    const int lxid = lower_bound(all(xs), lx) - xs.begin();
+    const int rxid = upper_bound(all(xs), rx - 1) - xs.begin();
+    if (lxid >= rxid) return;
+    query(lxid, rxid, ly, ry, report, 0, 0, n);
   }
 };
-
-using namespace std;
 
 int main() {
   std::ios::sync_with_stdio(false), cin.tie(nullptr);
@@ -95,26 +65,28 @@ int main() {
   int n, K;
   cin >> n >> K;
   const Int K2 = Int(K) * K;
-  vector<Point> points(n);
-  REP(i, n) {
-    int x, y;
+
+  vector<pair<Int, Int>> points(n);
+  for (auto&[x, y]: points) {
     cin >> x >> y;
-    points[i] = {(Int) x, (Int) y, i};
   }
-  KdTree2d tree(points);
+
+  OrthogonalRangeReport<Int> tree(points);
   vector<pair<int, int>> ans;
-  REP(i, 1, n) {
-    Int x = points[i].x, y = points[i].y;
-    tree.search_rect(x - K, y - K, x + K, y + K, [&](int j) {
+  for (int i = 1; i < n; ++i) {
+    auto[x, y] = points[i];
+    vector<int> res;
+    tree.query(x - K, x + K + 1, y - K, y + K + 1, res);
+    for (int j: res) {
       if (j < i) {
-        Int dx = x - points[j].x, dy = y - points[j].y;
+        Int dx = x - points[j].first, dy = y - points[j].second;
         if (dx * dx + dy * dy <= K2) {
           ans.emplace_back(j, i);
         }
       }
-    });
+    }
   }
-  sort(ALL(ans));
+  sort(ans.begin(), ans.end());
   cout << ans.size() << '\n';
   for (auto[p, q]: ans) {
     cout << (p + 1) << ' ' << (q + 1) << '\n';
