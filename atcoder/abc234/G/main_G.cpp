ba0e8f0d852974348ec3e3b0cf65d7d3d39c085b
keijak <m@keijak.com>
2022-01-11 10:18:59 +0100

    simplify min/max range

--- atcoder/abc234/G/main_G.cpp
+++ atcoder/abc234/G/main_G.cpp
@@ -84,64 +84,38 @@ backward::SignalHandling kSignalHandling;
 
 template<typename T>
 std::vector<std::pair<int, int>> min_range(const std::vector<T> &a) {
-  static const T kSentinel = std::numeric_limits<T>::lowest();
   const int n = a.size();
   std::vector<std::pair<int, int>> res(n);
-  std::deque<int> q;
-
   // left bound
-  for (int i = n - 1; i >= -1; --i) {
-    const auto &x = i >= 0 ? a[i] : kSentinel;
-    while (not q.empty() and a[q.back()] >= x) {
-      int j = q.back();
-      res[j].first = i + 1;
-      q.pop_back();
-    }
-    q.push_back(i);
+  for (int i = 0; i < n; ++i) {
+    int j = i - 1;
+    while (j >= 0 and a[j] > a[i]) j = res[j].first - 1;
+    res[i].first = j + 1;
   }
-  q.clear();
-
   // right bound
-  for (int i = 0; i <= n; ++i) {
-    const auto &x = i < n ? a[i] : kSentinel;
-    while (not q.empty() and a[q.back()] > x) {
-      int j = q.back();
-      res[j].second = i;
-      q.pop_back();
-    }
-    q.push_back(i);
+  for (int i = n - 1; i >= 0; --i) {
+    int j = i + 1;
+    while (j < n and a[j] >= a[i]) j = res[j].second;
+    res[i].second = j;
   }
   return res;
 }
 
 template<typename T>
 std::vector<std::pair<int, int>> max_range(const std::vector<T> &a) {
-  static const T kSentinel = std::numeric_limits<T>::max();
   const int n = a.size();
   std::vector<std::pair<int, int>> res(n);
-  std::deque<int> q;
-
   // left bound
-  for (int i = n - 1; i >= -1; --i) {
-    const auto &x = i >= 0 ? a[i] : kSentinel;
-    while (not q.empty() and a[q.back()] < x) {
-      int j = q.back();
-      res[j].first = i + 1;
-      q.pop_back();
-    }
-    q.push_back(i);
+  for (int i = 0; i < n; ++i) {
+    int j = i - 1;
+    while (j >= 0 and a[j] <= a[i]) j = res[j].first - 1;
+    res[i].first = j + 1;
   }
-  q.clear();
-
   // right bound
-  for (int i = 0; i <= n; ++i) {
-    const auto &x = i < n ? a[i] : kSentinel;
-    while (not q.empty() and a[q.back()] <= x) {
-      int j = q.back();
-      res[j].second = i;
-      q.pop_back();
-    }
-    q.push_back(i);
+  for (int i = n - 1; i >= 0; --i) {
+    int j = i + 1;
+    while (j < n and a[j] < a[i]) j = res[j].second;
+    res[i].second = j;
   }
   return res;
 }
