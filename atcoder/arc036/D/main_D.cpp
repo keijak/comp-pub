609bf162ed278fd3c69928e66aa14a4210af2a47
keijak <m@keijak.com>
2022-01-11 21:56:10 +0100

    union find

---
+++ atcoder/arc036/D/main_D.cpp
@@ -0,0 +1,141 @@
+#include <bits/stdc++.h>
+#define REP_(i, a_, b_, a, b, ...) for (int i = (a), END_##i = (b); i < END_##i; ++i)
+#define REP(i, ...) REP_(i, __VA_ARGS__, __VA_ARGS__, 0, __VA_ARGS__)
+#define ALL(x) std::begin(x), std::end(x)
+using Int = long long;
+using Uint = unsigned long long;
+using Real = long double;
+
+template<typename T, typename U>
+inline bool chmax(T &a, U b) { return a < b and ((a = b), true); }
+template<typename T, typename U>
+inline bool chmin(T &a, U b) { return a > b and ((a = b), true); }
+template<typename T>
+inline int ssize(const T &a) { return (int) a.size(); }
+template<typename T>
+constexpr T kBigVal = std::numeric_limits<T>::max() / 2;
+
+struct Void {};
+
+template<typename T>
+inline std::ostream &print_one(const T &x, char endc) {
+  if constexpr (std::is_same<T, Void>::value) {
+    return std::cout;  // print nothing
+  } else if constexpr (std::is_same<T, bool>::value) {
+    return std::cout << (x ? "YES" : "NO") << endc;
+  } else {
+    return std::cout << x << endc;
+  }
+}
+template<typename T>
+inline std::ostream &print(const T &x) { return print_one(x, '\n'); }
+template<typename T, typename... Ts>
+std::ostream &print(const T &head, Ts... tail) {
+  return print_one(head, ' '), print(tail...);
+}
+inline std::ostream &print() { return std::cout << '\n'; }
+
+template<typename Container>
+std::ostream &print_seq(const Container &seq,
+                        const char *sep = " ",
+                        const char *ends = "\n",
+                        std::ostream &os = std::cout) {
+  const auto itl = std::begin(seq), itr = std::end(seq);
+  for (auto it = itl; it != itr; ++it) {
+    if (it != itl) os << sep;
+    os << *it;
+  }
+  return os << ends;
+}
+
+struct CastInput {
+  template<typename T>
+  operator T() const {
+    T x;
+    std::cin >> x;
+    return x;
+  }
+  struct Sized {
+    std::size_t n;
+    template<typename T>
+    operator T() const {
+      T x(n);
+      for (auto &e: x) std::cin >> e;
+      return x;
+    }
+  };
+  Sized operator()(std::size_t n) const { return {n}; }
+} in;
+
+#ifdef MY_DEBUG
+#include "debug_dump.hpp"
+#include "backward.hpp"
+backward::SignalHandling kSignalHandling;
+#else
+#define DUMP(...)
+#define cerr if(false)cerr
+#endif
+
+using namespace std;
+
+template<bool keep_roots = false>
+struct UnionFind {
+  int n;
+  mutable std::vector<int> parent;  // positive: parent, negative: size
+  std::set<int> roots_;
+
+  explicit UnionFind(int sz) : n(sz), parent(sz, -1) {
+    if constexpr (keep_roots) {
+      for (int i = 0; i < sz; ++i) roots_.insert(i);
+    }
+  }
+
+  bool unite(int x, int y) {
+    x = find(x), y = find(y);
+    if (x == y) return false;
+    if (-parent[x] < -parent[y]) std::swap(x, y);
+    parent[x] += parent[y];
+    parent[y] = x;
+    if constexpr (keep_roots) {
+      roots_.erase(y);
+    }
+    return true;
+  }
+
+  int find(int v) const {
+    if (parent[v] < 0) return v;
+    return parent[v] = find(parent[v]);
+  }
+
+  int size(int v) const { return -parent[find(v)]; }
+
+  bool same(int x, int y) const { return find(x) == find(y); }
+
+  const std::set<int> &roots() const {
+    static_assert(keep_roots);
+    return roots_;
+  }
+};
+
+int main() {
+  std::ios::sync_with_stdio(false), cin.tie(nullptr);
+  const int n = in, Q = in;
+  UnionFind uf(n * 2);
+  REP(i, Q) {
+    int w = in, x = in, y = in, z = in;
+    --x, --y;
+    if (w == 1) {
+      // Add an edge.
+      if (z & 1) {
+        uf.unite(x, y + n);
+        uf.unite(x + n, y);
+      } else {
+        uf.unite(x, y);
+        uf.unite(x + n, y + n);
+      }
+    } else {
+      // Answer the question.
+      print(uf.same(x, y));
+    }
+  }
+}
