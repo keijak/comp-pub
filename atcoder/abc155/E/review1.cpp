08d1af85cf5cd31ef34d9da18133dec9aa67c32e
keijak <m@keijak.com>
2022-01-17 11:35:38 +0100

    digit dp

--- atcoder/abc155/E/review1.cpp
+++ atcoder/abc155/E/review1.cpp
@@ -1,127 +1,17 @@
-#include <bits/stdc++.h>
-#define REP_(i, a_, b_, a, b, ...) \
-  for (int i = (a), END_##i = (b); i < END_##i; ++i)
-#define REP(i, ...) REP_(i, __VA_ARGS__, __VA_ARGS__, 0, __VA_ARGS__)
-#define ALL(x) std::begin(x), std::end(x)
-using Int = long long;
-using Uint = unsigned long long;
-using Real = long double;
+#include <cstdio>
+#include <algorithm>
 
-template<typename T, typename U>
-inline bool chmax(T &a, U b) {
-  return a < b and ((a = std::move(b)), true);
-}
-template<typename T, typename U>
-inline bool chmin(T &a, U b) {
-  return a > b and ((a = std::move(b)), true);
-}
-template<typename T>
-inline int ssize(const T &a) {
-  return (int) a.size();
-}
-inline void check(bool cond, const char *message = "!ERROR!") {
-  if (not cond) {
-    std::cout.flush(), std::cerr.flush();
-    throw std::runtime_error(message);
-  }
-}
-
-struct Void {};
-
-template<class T>
-inline std::ostream &print_one(const T &x, char endc) {
-  if constexpr (std::is_same<T, Void>::value) {
-    return std::cout;  // print nothing
-  } else if constexpr (std::is_same<T, bool>::value) {
-    return std::cout << (x ? "Yes" : "No") << endc;
-  } else {
-    return std::cout << x << endc;
-  }
-}
-template<class T>
-inline std::ostream &print(const T &x) { return print_one(x, '\n'); }
-template<typename T, typename... Ts>
-std::ostream &print(const T &head, Ts... tail) {
-  return print_one(head, ' '), print(tail...);
-}
-inline std::ostream &print() { return std::cout << '\n'; }
-
-template<typename Container>
-std::ostream &print_seq(const Container &seq,
-                        const char *sep = " ",
-                        const char *ends = "\n",
-                        std::ostream &os = std::cout) {
-  const auto itl = std::begin(seq), itr = std::end(seq);
-  for (auto it = itl; it != itr; ++it) {
-    if (it != itl) os << sep;
-    os << *it;
-  }
-  return os << ends;
-}
-
-struct CastInput {
-  template<typename T>
-  operator T() const {
-    T x;
-    std::cin >> x;
-    return x;
-  }
-  struct Sized {
-    std::size_t n;
-    template<typename T>
-    operator T() const {
-      T x(n);
-      for (auto &e: x) std::cin >> e;
-      return x;
-    }
-  };
-  Sized operator()(std::size_t n) const { return {n}; }
-} in;
-
-#ifdef MY_DEBUG
-#include "debug_dump.hpp"
-#include "backward.hpp"
-backward::SignalHandling kSignalHandling;
-#else
-#define DUMP(...)
-#define cerr if(false)cerr
-#endif
-
-using namespace std;
-
-auto solve() {
-  string N = in;
-  int n = ssize(N);
-
-  vector<optional<Int>> memo(n * 2);
-
-  auto f = [&](auto &f, int i, int c) -> Int {
-    if (i == n) return c;
-    int mkey = 2 * i + c;
-    auto &cached = memo[mkey];
-    if (cached.has_value()) return *cached;
-
-    int ds = (N[n - 1 - i] - '0' + c);
-    int c2 = ds / 10;
-    int p = ds % 10;
-    Int ret = f(f, i + 1, c2) + p;
-    if (ds < 10) {
-      c2 = 1;
-      p = 10 - ds;
-      chmin(ret, f(f, i + 1, c2) + p);
-    }
-    cached = ret;
-    return ret;
-  };
-  return f(f, 0, 0);
-}
+char buf[1'000'010];
 
 int main() {
-  ios_base::sync_with_stdio(false), cin.tie(nullptr);
-  cout << std::fixed << std::setprecision(18);
-  const int T = 1;//in;
-  REP(t, T) {
-    auto ans = solve();
-    print(ans);
+  int a = 0, b = 1, i = 0;
+  char c;
+  fgets_unlocked(buf, 1'000'005, stdin);
+  while ((c = buf[i++]) != '\n') {
+    char d = c - '0';
+    int a_ = std::min<int>(a + d, b + 10 - d);
+    b = std::min<int>(a + d + 1, b + 9 - d);
+    a = a_;
   }
+  printf("%d\n", a);
 }
