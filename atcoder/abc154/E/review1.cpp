bed60621941292df565f35ec2563d212d4723f97
keijak <m@keijak.com>
2022-01-17 10:45:41 +0100

    digit dp

---
+++ atcoder/abc154/E/review1.cpp
@@ -0,0 +1,128 @@
+#include <bits/stdc++.h>
+#define REP_(i, a_, b_, a, b, ...) for (int i = (a), END_##i = (b); i < END_##i; ++i)
+#define REP(i, ...) REP_(i, __VA_ARGS__, __VA_ARGS__, 0, __VA_ARGS__)
+#define ALL(x) std::begin(x), std::end(x)
+using Int = long long;
+using Uint = unsigned long long;
+using Real = long double;
+
+template<typename T, typename U>
+inline bool chmax(T &a, U b) { return a < b and ((a = b), true); }
+template<typename T, typename U>
+inline bool chmin(T &a, U b) { return a > b and ((a = b), true); }
+template<typename T>
+inline int ssize(const T &a) { return (int) a.size(); }
+template<typename T>
+constexpr T kBigVal = std::numeric_limits<T>::max() / 2;
+
+struct Void {};
+
+template<typename T>
+inline std::ostream &print_one(const T &x, char endc) {
+  if constexpr (std::is_same<T, Void>::value) {
+    return std::cout;  // print nothing
+  } else if constexpr (std::is_same<T, bool>::value) {
+    return std::cout << (x ? "Yes" : "No") << endc;
+  } else {
+    return std::cout << x << endc;
+  }
+}
+template<typename T>
+inline std::ostream &print(const T &x) { return print_one(x, '\n'); }
+template<typename T, typename... Ts>
+std::ostream &print(const T &head, Ts... tail) {
+  return print_one(head, ' '), print(tail...);
+}
+inline std::ostream &print() { return std::cout << '\n'; }
+
+template<typename Container>
+std::ostream &print_seq(const Container &seq,
+                        const char *sep = " ",
+                        const char *ends = "\n",
+                        std::ostream &os = std::cout) {
+  const auto itl = std::begin(seq), itr = std::end(seq);
+  for (auto it = itl; it != itr; ++it) {
+    if (it != itl) os << sep;
+    os << *it;
+  }
+  return os << ends;
+}
+
+struct CastInput {
+  template<typename T>
+  operator T() const {
+    T x;
+    std::cin >> x;
+    return x;
+  }
+  struct Sized {
+    std::size_t n;
+    template<typename T>
+    operator T() const {
+      T x(n);
+      for (auto &e: x) std::cin >> e;
+      return x;
+    }
+  };
+  Sized operator()(std::size_t n) const { return {n}; }
+} in;
+
+#ifdef MY_DEBUG
+#include "debug_dump.hpp"
+#include "backward.hpp"
+backward::SignalHandling kSignalHandling;
+#else
+#define DUMP(...)
+#define cerr if(false)cerr
+#endif
+
+using namespace std;
+
+#include <boost/multiprecision/cpp_int.hpp>
+using BigInt = boost::multiprecision::cpp_int;
+namespace multip = boost::multiprecision;
+
+auto solve() {
+  string N = in;
+  int K = in;
+
+  stringstream ss;
+  ss << N;
+  BigInt NB;
+  ss >> NB;
+
+  map<pair<BigInt, int>, Int> memo;
+  auto f = [&](auto &f, const BigInt &x, int k) -> Int {
+    if (k < 0) return 0;
+    if (x == 0) {
+      return (k == 0) ? 1 : 0;
+    }
+    if (auto it = memo.find({x, k}); it != memo.end()) {
+      return it->second;
+    }
+    int cur_d = int(x % 10);
+    Int ret = 0;
+    ret += f(f, x / 10, k);
+
+    REP(d, 1, 10) {
+      if (d <= cur_d) {
+        ret += f(f, (x - d) / 10, k - 1);
+      } else if (x - d >= 0) {
+        ret += f(f, (x - d) / 10, k - 1);
+      }
+    }
+    memo[{x, k}] = ret;
+    return ret;
+  };
+  return f(f, NB, K);
+}
+
+int main() {
+  std::ios::sync_with_stdio(false), cin.tie(nullptr);
+  cout << std::fixed << std::setprecision(18);
+  const int T = 1;//in;
+  REP(t, T) {
+    auto ans = solve();
+    print(ans);
+  }
+}
