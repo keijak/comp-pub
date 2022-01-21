e00d2464c4221bba92c69ac45515d6b53e09000c
keijak <m@keijak.com>
2022-01-21 01:09:12 +0100

    asakatsu

---
+++ vcon/asa20220120/C/main_C.cpp
@@ -0,0 +1,116 @@
+#include <bits/stdc++.h>
+#define REP_(i, a_, b_, a, b, ...) for (int i = (a), END_##i = (b); i < END_##i; ++i)
+#define REP(i, ...) REP_(i, __VA_ARGS__, __VA_ARGS__, 0, __VA_ARGS__)
+#define ALL(x) std::begin(x), std::end(x)
+using Int = long long;
+using Uint = unsigned long long;
+using Real = long double;
+
+#include <atcoder/modint>
+using Mint = atcoder::modint998244353;
+std::ostream &operator<<(std::ostream &os, const Mint &m) {
+  return os << m.val();
+}
+template<typename T, typename U>
+inline bool chmax(T &a, U b) { return a < b and ((a = b), true); }
+template<typename T, typename U>
+inline bool chmin(T &a, U b) { return a > b and ((a = b), true); }
+template<typename T>
+inline int ssize(const T &a) { return (int) a.size(); }
+template<typename T>
+constexpr T kBigVal = std::numeric_limits<T>::max() / 2;
+
+struct Void {};
+
+template<typename T>
+inline std::ostream &print_one(const T &x, char endc) {
+  if constexpr (std::is_same<T, Void>::value) {
+    return std::cout;  // print nothing
+  } else if constexpr (std::is_same<T, bool>::value) {
+    return std::cout << (x ? "Yes" : "No") << endc;
+  } else {
+    return std::cout << x << endc;
+  }
+}
+template<typename T>
+inline std::ostream &print(const T &x) { return print_one(x, '\n'); }
+template<typename T, typename... Ts>
+std::ostream &print(const T &head, Ts... tail) {
+  return print_one(head, ' '), print(tail...);
+}
+inline std::ostream &print() { return std::cout << '\n'; }
+
+template<typename Container>
+std::ostream &print_seq(const Container &seq,
+                        const char *sep = " ",
+                        const char *ends = "\n",
+                        std::ostream &os = std::cout) {
+  const auto itl = std::begin(seq), itr = std::end(seq);
+  for (auto it = itl; it != itr; ++it) {
+    if (it != itl) os << sep;
+    os << *it;
+  }
+  return os << ends;
+}
+
+struct CastInput {
+  template<typename T>
+  operator T() const {
+    T x;
+    std::cin >> x;
+    return x;
+  }
+  struct Sized {
+    std::size_t n;
+    template<typename T>
+    operator T() const {
+      T x(n);
+      for (auto &e: x) std::cin >> e;
+      return x;
+    }
+  };
+  Sized operator()(std::size_t n) const { return {n}; }
+} in;
+
+#ifdef MY_DEBUG
+#include "debug_dump.hpp"
+#include "backward.hpp"
+backward::SignalHandling kSignalHandling;
+#else
+#define DUMP(...)
+#define cerr if(false)cerr
+#endif
+
+using namespace std;
+
+auto solve() {
+  int n = in;
+  vector<int> a = in(n), b = in(n);
+  auto dp = vector(n, vector(3010, Mint(0)));
+  for (int i = a[0]; i <= b[0]; ++i) {
+    dp[0][i] = 1;
+  }
+  REP(i, 1, n) {
+    vector<Mint> acc(3011);
+    for (int j = 0; j < 3010; ++j) {
+      acc[j + 1] = acc[j] + dp[i - 1][j];
+    }
+    for (int c = a[i]; c <= b[i]; ++c) {
+      dp[i][c] += acc[c + 1];
+//      for (int j = 0; j <= c; ++j) {
+//        dp[i][c] += dp[i - 1][j];
+//      }
+    }
+  }
+  return accumulate(ALL(dp[n - 1]), Mint(0));
+}
+
+int main() {
+  std::ios::sync_with_stdio(false), cin.tie(nullptr);
+  cout << std::fixed << std::setprecision(18);
+  const int T = 1;//in;
+  REP(t, T) {
+    auto ans = solve();
+    print(ans);
+  }
+}
---
+++ vcon/asa20220120/F/main_F.cpp
@@ -0,0 +1,123 @@
+#include <bits/stdc++.h>
+#define REP_(i, a_, b_, a, b, ...) for (int i = (a), END_##i = (b); i < END_##i; ++i)
+#define REP(i, ...) REP_(i, __VA_ARGS__, __VA_ARGS__, 0, __VA_ARGS__)
+#define ALL(x) std::begin(x), std::end(x)
+using Int = long long;
+using Uint = unsigned long long;
+using Real = long double;
+
+template<typename T, typename U>
+inline bool chmax(T &a, U b) { return a < b and ((a = b), true); }
+template<typename T, typename U>
+inline bool chmin(T &a, U b) { return a > b and ((a = b), true); }
+template<typename T>
+inline int ssize(const T &a) { return (int) a.size(); }
+template<typename T>
+constexpr T kBigVal = std::numeric_limits<T>::max() / 2;
+
+struct Void {};
+
+template<typename T>
+inline std::ostream &print_one(const T &x, char endc) {
+  if constexpr (std::is_same<T, Void>::value) {
+    return std::cout;  // print nothing
+  } else if constexpr (std::is_same<T, bool>::value) {
+    return std::cout << (x ? "Yes" : "No") << endc;
+  } else {
+    return std::cout << x << endc;
+  }
+}
+template<typename T>
+inline std::ostream &print(const T &x) { return print_one(x, '\n'); }
+template<typename T, typename... Ts>
+std::ostream &print(const T &head, Ts... tail) {
+  return print_one(head, ' '), print(tail...);
+}
+inline std::ostream &print() { return std::cout << '\n'; }
+
+template<typename Container>
+std::ostream &print_seq(const Container &seq,
+                        const char *sep = " ",
+                        const char *ends = "\n",
+                        std::ostream &os = std::cout) {
+  const auto itl = std::begin(seq), itr = std::end(seq);
+  for (auto it = itl; it != itr; ++it) {
+    if (it != itl) os << sep;
+    os << *it;
+  }
+  return os << ends;
+}
+
+struct CastInput {
+  template<typename T>
+  operator T() const {
+    T x;
+    std::cin >> x;
+    return x;
+  }
+  struct Sized {
+    std::size_t n;
+    template<typename T>
+    operator T() const {
+      T x(n);
+      for (auto &e: x) std::cin >> e;
+      return x;
+    }
+  };
+  Sized operator()(std::size_t n) const { return {n}; }
+} in;
+
+#ifdef MY_DEBUG
+#include "debug_dump.hpp"
+#include "backward.hpp"
+backward::SignalHandling kSignalHandling;
+#else
+#define DUMP(...)
+#define cerr if(false)cerr
+#endif
+
+using namespace std;
+
+template<typename T>
+struct Array2d {
+  std::vector<T> data_;
+  size_t h_, w_;
+
+  Array2d(size_t h, size_t w) : data_(h * w, T{}), h_(h), w_(w) {}
+
+  inline T &get(size_t i0, size_t i1) { return data_[i0 * w_ + i1]; }
+
+  // Fills one row with the specified value.
+  inline void fill(size_t i0, T val) {
+    std::fill(data_.begin() + i0 * w_, data_.begin() + (i0 + 1) * w_, val);
+  }
+};
+
+template<class T>
+using MinHeap = priority_queue<T, vector<T>, greater<T>>;
+
+auto solve() {
+  const int n = in;
+  const int n2 = n * 2;
+  vector<Int> V = in(n2);
+  MinHeap<Int> heap;
+  Int s = 0;
+  for (int i = 1; i <= n; ++i) {
+    int l = n - i, r = n + i - 1;
+    heap.push(V[l]);
+    heap.push(V[r]);
+    s += heap.top();
+    heap.pop();
+  }
+  return accumulate(ALL(V), 0LL) - s;
+}
+
+int main() {
+  std::ios::sync_with_stdio(false), cin.tie(nullptr);
+  cout << std::fixed << std::setprecision(18);
+  const int T = 1;//in;
+  REP(t, T) {
+    auto ans = solve();
+    print(ans);
+  }
+}
