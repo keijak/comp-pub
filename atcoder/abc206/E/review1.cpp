e1210ed8d56e0a910ae0aa779487708426259404
keijak <m@keijak.com>
2022-03-07 09:53:05 +0100

    moebius

---
+++ atcoder/abc206/E/review1.cpp
@@ -0,0 +1,153 @@
+#include <bits/stdc++.h>
+#define REP_(i, a_, b_, a, b, ...) for (int i = (a), END_##i = (b); i < END_##i; ++i)
+#define REP(i, ...) REP_(i, __VA_ARGS__, __VA_ARGS__, 0, __VA_ARGS__)
+#define ALL(x) std::begin(x), std::end(x)
+using Int = long long;
+using Uint = unsigned long long;
+using Real = long double;
+
+template<typename T, typename U>
+inline bool chmax(T &a, U b) { return a < b and ((a = b), true); }
+template<typename T, typename U>
+inline bool chmin(T &a, U b) { return a > b and ((a = b), true); }
+template<typename T>
+inline int ssize(const T &a) { return (int) a.size(); }
+template<typename T>
+constexpr T kBigVal = std::numeric_limits<T>::max() / 2;
+
+struct Void {};
+
+template<typename T>
+inline std::ostream &print_one(const T &x, char endc) {
+  if constexpr (std::is_same<T, Void>::value) {
+    return std::cout;  // print nothing
+  } else if constexpr (std::is_same<T, bool>::value) {
+    return std::cout << (x ? "Yes" : "No") << endc;
+  } else {
+    return std::cout << x << endc;
+  }
+}
+template<typename T>
+inline std::ostream &print(const T &x) { return print_one(x, '\n'); }
+template<typename T, typename... Ts>
+std::ostream &print(const T &head, Ts... tail) {
+  return print_one(head, ' '), print(tail...);
+}
+inline std::ostream &print() { return std::cout << '\n'; }
+
+template<typename Container>
+std::ostream &print_seq(const Container &seq,
+                        const char *sep = " ",
+                        const char *ends = "\n",
+                        std::ostream &os = std::cout) {
+  const auto itl = std::begin(seq), itr = std::end(seq);
+  for (auto it = itl; it != itr; ++it) {
+    if (it != itl) os << sep;
+    os << *it;
+  }
+  return os << ends;
+}
+
+struct CastInput {
+  template<typename T>
+  operator T() const {
+    T x;
+    std::cin >> x;
+    return x;
+  }
+  struct Sized {
+    int n;
+    template<typename T>
+    operator T() const {
+      T xs(n);
+      for (auto &x: xs) std::cin >> x;
+      return xs;
+    }
+  };
+  Sized operator()(int n) const { return {n}; }
+} in;
+
+#ifdef MY_DEBUG
+#include "debug_dump.hpp"
+#include "backward.hpp"
+backward::SignalHandling kSignalHandling;
+#else
+#define DUMP(...)
+#define cerr if(false)cerr
+#endif
+
+using namespace std;
+
+template<class T>
+T floor_div(T x, T y) {
+  assert(y != 0);
+  return x / y - (((x ^ y) < 0) and (x % y));
+}
+
+template<class T>
+T ceil_div(T x, T y) {
+  assert(y != 0);
+  return x / y + (((x ^ y) >= 0) and (x % y));
+}
+
+struct PrimeSieve {
+  std::vector<int> spf;  // smallest prime factors table.
+  std::vector<int> primes;
+
+  explicit PrimeSieve(int n) : spf(n + 1) {
+    // O(n)
+    for (int i = 2; i <= n; ++i) {
+      if (spf[i] == 0) {
+        spf[i] = i;
+        primes.push_back(i);
+      }
+      for (const auto &p: primes) {
+        if (i * p > n or p > spf[i]) break;
+        spf[i * p] = p;
+      }
+    }
+  }
+
+  // Möbius function.
+  int moebius(int n) const {
+    assert(0 < n and n < int(spf.size()));
+    int res = 1;
+    while (n > 1) {
+      const int p = spf[n];
+      n /= p;
+      if (n % p == 0) return 0;
+      res *= -1;
+    }
+    return res;
+  }
+};
+
+auto solve() {
+  Int L = in, R = in;
+  PrimeSieve sieve(R + 1);
+  vector<Int> dp(R + 1);
+  Int ans = 0;
+  for (Int g = R; g >= 2; --g) {
+    for (Int d = 1; d <= R / g; ++d) {
+      int mu = sieve.moebius(d);
+      if (mu == 0) continue;
+      Int s = floor_div(R, g * d) - ceil_div(L, g * d) + 1;
+      ans += mu * (s * (s - 1) / 2);
+    }
+    if (g >= L) {
+      ans -= R / g - 1;
+    }
+  }
+  ans *= 2;
+  return ans;
+}
+
+int main() {
+  std::ios::sync_with_stdio(false), cin.tie(nullptr);
+  cout << std::fixed << std::setprecision(18);
+  const int T = 1;//in;
+  REP(t, T) {
+    auto ans = solve();
+    print(ans);
+  }
+}
