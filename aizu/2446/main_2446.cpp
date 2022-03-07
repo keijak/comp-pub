d6e3c71980e1952f1c87362712e4f629c678c14c
keijak <m@keijak.com>
2022-03-07 13:52:43 +0100

    fast mobius transform

--- aizu/2446/main_2446.cpp
+++ aizu/2446/main_2446.cpp
@@ -94,6 +94,19 @@ void subset_zeta_transform(std::vector<T> &f) {
   }
 }
 
+template<typename T>
+void subset_mobius_transform(std::vector<T> &f) {
+  const int n = f.size();
+  assert((n & (n - 1)) == 0);  // n must be power of 2.
+  for (int i = 1; i < n; i <<= 1) {
+    for (int j = 0; j < n; j++) {
+      if ((j & i) == 0) {
+        f[j | i] -= f[j];
+      }
+    }
+  }
+}
+
 using i128 = __int128_t;
 
 auto solve() {
@@ -132,8 +145,7 @@ auto solve() {
   subset_zeta_transform(cont);
   Real ans = 0;
   REP(bits, 1, 1 << n) {
-    assert(cont[bits] >= 0);
-    ans += prob[bits] * Real(cont[bits]);
+    ans += prob[bits] * abs(Real(cont[bits]));
   }
   return ans;
 }
