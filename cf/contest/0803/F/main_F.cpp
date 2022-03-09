075b7921993ba8a7d6b5dfba9639bc6869481ed2
keijak <m@keijak.com>
2022-03-09 11:01:48 +0100

    inclusion exclusion

---
+++ cf/contest/0803/F/main_F.cpp
@@ -0,0 +1,260 @@
+#include <bits/stdc++.h>
+#define REP_(i, a_, b_, a, b, ...) for (int i = (a), END_##i = (b); i < END_##i; ++i)
+#define REP(i, ...) REP_(i, __VA_ARGS__, __VA_ARGS__, 0, __VA_ARGS__)
+#define ALL(x) std::begin(x), std::end(x)
+using Int = long long;
+using Uint = unsigned long long;
+using Real = long double;
+
+template<unsigned M>
+struct ModInt {
+  constexpr ModInt() : _v(0) {}
+  constexpr ModInt(long long val) {
+    if (val < 0) {
+      long long k = (std::abs(val) + M - 1) / M;
+      val += k * M;
+    }
+    assert(val >= 0);
+    _v = val % M;
+  }
+
+  static constexpr int mod() { return M; }
+  static constexpr unsigned umod() { return M; }
+  inline unsigned val() const { return _v; }
+
+  ModInt &operator++() {
+    _v++;
+    if (_v == umod()) _v = 0;
+    return *this;
+  }
+  ModInt &operator--() {
+    if (_v == 0) _v = umod();
+    _v--;
+    return *this;
+  }
+  ModInt operator++(int) {
+    auto result = *this;
+    ++*this;
+    return result;
+  }
+  ModInt operator--(int) {
+    auto result = *this;
+    --*this;
+    return result;
+  }
+
+  constexpr ModInt operator-() const { return ModInt(umod() - _v); }
+
+  constexpr ModInt &operator+=(const ModInt &a) {
+    if ((_v += a._v) >= M) _v -= M;
+    return *this;
+  }
+  constexpr ModInt &operator-=(const ModInt &a) {
+    if ((_v += M - a._v) >= M) _v -= M;
+    return *this;
+  }
+  constexpr ModInt &operator*=(const ModInt &a) {
+    _v = ((unsigned long long) (_v) * a._v) % M;
+    return *this;
+  }
+  constexpr ModInt pow(unsigned long long t) const {
+    if (_v == 0) {
+      return 0;  // corner case: 0^0 = ?
+    }
+    ModInt base = *this;
+    ModInt res = 1;
+    while (t) {
+      if (t & 1) res *= base;
+      base *= base;
+      t >>= 1;
+    }
+    return res;
+  }
+
+  // https://qiita.com/Mitarushi/items/8d7fb52e8a80e8008463
+  constexpr ModInt inv() const {
+    long long b = 1, a = _v;
+    while (a > 1) {
+      long long q = M / a;
+      a = M - a * q;
+      b = -b * q % M;
+    }
+    assert(a == 1);  // if a = 0, _v and M are not coprime.
+    if (b < 0) b += M;
+    ModInt ret;
+    ret._v = (unsigned) b;
+    return ret;
+  }
+  constexpr ModInt &operator/=(const ModInt &a) { return *this *= a.inv(); }
+
+  friend constexpr ModInt operator+(const ModInt &a, const ModInt &b) {
+    return ModInt(a) += b;
+  }
+  friend constexpr ModInt operator-(const ModInt &a, const ModInt &b) {
+    return ModInt(a) -= b;
+  }
+  friend constexpr ModInt operator*(const ModInt &a, const ModInt &b) {
+    return ModInt(a) *= b;
+  }
+  friend constexpr ModInt operator/(const ModInt &a, const ModInt &b) {
+    return ModInt(a) /= b;
+  }
+  friend constexpr bool operator==(const ModInt &a, const ModInt &b) {
+    return a._v == b._v;
+  }
+  friend constexpr bool operator!=(const ModInt &a, const ModInt &b) {
+    return a._v != b._v;
+  }
+  friend std::istream &operator>>(std::istream &is, ModInt &a) {
+    return is >> a._v;
+  }
+  friend std::ostream &operator<<(std::ostream &os, const ModInt &a) {
+    return os << a._v;
+  }
+
+ private:
+  unsigned _v;  // raw value
+};
+const unsigned MOD = int(1e9) + 7;
+using Mint = ModInt<MOD>;
+
+template<typename T, typename U>
+inline bool chmax(T &a, U b) { return a < b and ((a = b), true); }
+template<typename T, typename U>
+inline bool chmin(T &a, U b) { return a > b and ((a = b), true); }
+template<typename T>
+inline int ssize(const T &a) { return (int) a.size(); }
+template<typename T>
+constexpr T kBigVal = std::numeric_limits<T>::max() / 2;
+
+struct Void {};
+
+template<typename T>
+inline std::ostream &print_one(const T &x, char endc) {
+  if constexpr (std::is_same<T, Void>::value) {
+    return std::cout;  // print nothing
+  } else if constexpr (std::is_same<T, bool>::value) {
+    return std::cout << (x ? "Yes" : "No") << endc;
+  } else {
+    return std::cout << x << endc;
+  }
+}
+template<typename T>
+inline std::ostream &print(const T &x) { return print_one(x, '\n'); }
+template<typename T, typename... Ts>
+std::ostream &print(const T &head, Ts... tail) {
+  return print_one(head, ' '), print(tail...);
+}
+inline std::ostream &print() { return std::cout << '\n'; }
+
+template<typename Container>
+std::ostream &print_seq(const Container &seq,
+                        const char *sep = " ",
+                        const char *ends = "\n",
+                        std::ostream &os = std::cout) {
+  const auto itl = std::begin(seq), itr = std::end(seq);
+  for (auto it = itl; it != itr; ++it) {
+    if (it != itl) os << sep;
+    os << *it;
+  }
+  return os << ends;
+}
+
+struct CastInput {
+  template<typename T>
+  operator T() const {
+    T x;
+    std::cin >> x;
+    return x;
+  }
+  struct Sized {
+    int n;
+    template<typename T>
+    operator T() const {
+      T xs(n);
+      for (auto &x: xs) std::cin >> x;
+      return xs;
+    }
+  };
+  Sized operator()(int n) const { return {n}; }
+} in;
+
+#ifdef MY_DEBUG
+#include "debug_dump.hpp"
+#include "backward.hpp"
+backward::SignalHandling kSignalHandling;
+#else
+#define DUMP(...)
+#define cerr if(false)cerr
+#endif
+
+using namespace std;
+
+struct LinearSieve {
+  std::vector<int> spf;  // smallest prime factors table.
+  std::vector<int> mu;   // Moebius function
+  std::vector<int> primes;
+
+  explicit LinearSieve(int n) : spf(n + 1), mu(n + 1) {
+    mu[1] = 1;
+    for (int i = 2; i <= n; ++i) {
+      if (spf[i] == 0) {
+        spf[i] = i;
+        mu[i] = -1;
+        primes.push_back(i);
+      }
+      for (const auto &p: primes) {
+        if (i * p > n) break;
+        spf[i * p] = p;
+        if (i % p == 0) {
+          mu[i * p] = 0;
+          break;
+        } else {
+          mu[i * p] = -mu[i];
+        }
+      }
+    }
+  }
+};
+
+template<typename T>
+std::vector<T> pow_seq(int n, int base = 2) {
+  assert(n >= 0 and base > 0);
+  std::vector<T> p(n + 1);
+  p[0] = 1;
+  for (int i = 0; i < n; ++i) {
+    p[i + 1] = p[i] * base;
+  }
+  return p;
+}
+
+auto solve() {
+  int n = in;
+  vector<int> a = in(n);
+  const int amax = *max_element(ALL(a));
+  vector<int> acnt(100005);
+  for (int x: a) acnt[x]++;
+  LinearSieve sieve(100005);
+  auto pow2 = pow_seq<Mint>(n + 5, 2);
+  Mint ans = 0;
+  for (int k = 1; k <= amax; ++k) {
+    int mu = sieve.mu[k];
+    if (mu == 0) continue;
+    int pcnt = 0;
+    for (int j = k; j <= amax; j += k) {
+      pcnt += acnt[j];
+    }
+    ans += mu * (pow2[pcnt] - 1);
+  }
+  return ans;
+}
+
+int main() {
+  std::ios::sync_with_stdio(false), cin.tie(nullptr);
+  cout << std::fixed << std::setprecision(18);
+  const int T = 1;//in;
+  REP(t, T) {
+    auto ans = solve();
+    print(ans);
+  }
+}
