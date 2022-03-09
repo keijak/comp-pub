2d1cf13e178a8e926d445a11d5803d7f83db80e9
keijak <m@keijak.com>
2022-03-09 11:15:34 +0100

    pie

---
+++ hackerrank/university_codesprint_5/cube_loving_numbers/main_cube_loving_numbers.cpp
@@ -0,0 +1,130 @@
+#include <bits/stdc++.h>
+#define REP_(i, a_, b_, a, b, ...) for (int i = (a), END_##i = (b); i < END_##i; ++i)
+#define REP(i, ...) REP_(i, __VA_ARGS__, __VA_ARGS__, 0, __VA_ARGS__)
+#define ALL(x) std::begin(x), std::end(x)
+using Int = long long;
+using Uint = unsigned long long;
+using Real = long double;
+
+template<typename T, typename U>
+inline bool chmax(T &a, U b) { return a < b and ((a = b), true); }
+template<typename T, typename U>
+inline bool chmin(T &a, U b) { return a > b and ((a = b), true); }
+template<typename T>
+inline int ssize(const T &a) { return (int) a.size(); }
+template<typename T>
+constexpr T kBigVal = std::numeric_limits<T>::max() / 2;
+
+struct Void {};
+
+template<typename T>
+inline std::ostream &print_one(const T &x, char endc) {
+  if constexpr (std::is_same<T, Void>::value) {
+    return std::cout;  // print nothing
+  } else if constexpr (std::is_same<T, bool>::value) {
+    return std::cout << (x ? "Yes" : "No") << endc;
+  } else {
+    return std::cout << x << endc;
+  }
+}
+template<typename T>
+inline std::ostream &print(const T &x) { return print_one(x, '\n'); }
+template<typename T, typename... Ts>
+std::ostream &print(const T &head, Ts... tail) {
+  return print_one(head, ' '), print(tail...);
+}
+inline std::ostream &print() { return std::cout << '\n'; }
+
+template<typename Container>
+std::ostream &print_seq(const Container &seq,
+                        const char *sep = " ",
+                        const char *ends = "\n",
+                        std::ostream &os = std::cout) {
+  const auto itl = std::begin(seq), itr = std::end(seq);
+  for (auto it = itl; it != itr; ++it) {
+    if (it != itl) os << sep;
+    os << *it;
+  }
+  return os << ends;
+}
+
+struct CastInput {
+  template<typename T>
+  operator T() const {
+    T x;
+    std::cin >> x;
+    return x;
+  }
+  struct Sized {
+    int n;
+    template<typename T>
+    operator T() const {
+      T xs(n);
+      for (auto &x: xs) std::cin >> x;
+      return xs;
+    }
+  };
+  Sized operator()(int n) const { return {n}; }
+} in;
+
+#ifdef MY_DEBUG
+#include "debug_dump.hpp"
+#include "backward.hpp"
+backward::SignalHandling kSignalHandling;
+#else
+#define DUMP(...)
+#define cerr if(false)cerr
+#endif
+
+using namespace std;
+
+struct LinearSieve {
+  std::vector<int> spf;  // smallest prime factors.
+  std::vector<int> mu;   // Moebius function
+  std::vector<int> primes;
+
+  explicit LinearSieve(int n) : spf(n + 1), mu(n + 1) {
+    mu[1] = 1;
+    for (int i = 2; i <= n; ++i) {
+      if (spf[i] == 0) {
+        spf[i] = i;
+        mu[i] = -1;
+        primes.push_back(i);
+      }
+      for (const auto &p: primes) {
+        if (i * p > n) break;
+        spf[i * p] = p;
+        if (i % p == 0) {
+          mu[i * p] = 0;
+          break;
+        } else {
+          mu[i * p] = -mu[i];
+        }
+      }
+    }
+  }
+} sieve(1000005);
+
+auto solve() {
+  Int n = in;
+  Int ans = 0;
+  for (Int k = 2; k <= 1000000; ++k) {
+    int mu = sieve.mu[k];
+    if (mu == 0) continue;
+    Int cube = k * k * k;
+    if (cube > n) break;
+    Int co = n / cube;
+    ans -= mu * co;
+  }
+  return ans;
+}
+
+int main() {
+  std::ios::sync_with_stdio(false), cin.tie(nullptr);
+  cout << std::fixed << std::setprecision(18);
+  const int T = in;
+  REP(t, T) {
+    auto ans = solve();
+    print(ans);
+  }
+}
