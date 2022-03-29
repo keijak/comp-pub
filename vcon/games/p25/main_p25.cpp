055c24312f66dc1707d7adcc6f8ddc51549d2ef7
keijak <m@keijak.com>
2022-03-30 01:40:33 +0200

    games

---
+++ vcon/games/p25/main_p25.cpp
@@ -0,0 +1,146 @@
+#include <bits/stdc++.h>
+#define REP_(i, a_, b_, a, b, ...) for (int i = (a), END_##i = (b); i < END_##i; ++i)
+#define REP(i, ...) REP_(i, __VA_ARGS__, __VA_ARGS__, 0, __VA_ARGS__)
+#define ALL(x) std::begin(x), std::end(x)
+using Int = long long;
+using Uint = unsigned long long;
+using Real = long double;
+
+template<typename T, typename U>
+inline bool chmax(T &a, U b) { return a < b and ((a = b), true); }
+template<typename T, typename U>
+inline bool chmin(T &a, U b) { return a > b and ((a = b), true); }
+template<typename T>
+inline int ssize(const T &a) { return (int) a.size(); }
+template<typename T>
+constexpr T kBigVal = std::numeric_limits<T>::max() / 2;
+
+struct CastInput {
+  template<typename T>
+  operator T() const {
+    T x;
+    std::cin >> x;
+    return x;
+  }
+  struct Sized {
+    int n;
+    template<typename T>
+    operator T() const {
+      T xs(n);
+      for (auto &x: xs) std::cin >> x;
+      return xs;
+    }
+  };
+  Sized operator()(int n) const { return {n}; }
+} in;
+
+template<typename Container>
+std::ostream &print_seq(const Container &seq,
+                        const char *sep = " ",
+                        const char *ends = "\n",
+                        std::ostream &os = std::cout) {
+  const auto itl = std::begin(seq), itr = std::end(seq);
+  for (auto it = itl; it != itr; ++it) {
+    if (it != itl) os << sep;
+    os << *it;
+  }
+  return os << ends;
+}
+
+template<typename T>
+inline std::ostream &print_one(const T &x, char endc) {
+  if constexpr (std::is_same<T, bool>::value) {
+    return std::cout << (x ? "First" : "Second") << endc;
+  } else {
+    return std::cout << x << endc;
+  }
+}
+template<typename T>
+inline std::ostream &print(const T &x) { return print_one(x, '\n'); }
+template<typename T, typename... Ts>
+std::ostream &print(const T &head, Ts... tail) {
+  return print_one(head, ' '), print(tail...);
+}
+inline std::ostream &print() { return std::cout << '\n'; }
+
+#ifdef MY_DEBUG
+#include "debug_dump.hpp"
+#include "backward.hpp"
+backward::SignalHandling kSignalHandling;
+#else
+#define DUMP(...)
+#define cerr if(false)cerr
+#endif
+
+using namespace std;
+
+struct UnionFind {
+  int n_;
+  std::vector<int> parent_;  // negative: size
+
+  explicit UnionFind(int n) : n_(n), parent_(n, -1) {}
+
+  bool unite(int x, int y) {
+    x = find(x), y = find(y);
+    if (x == y) return false;
+    if (-parent_[x] < -parent_[y]) std::swap(x, y);
+    parent_[x] += parent_[y];
+    parent_[y] = x;
+    return true;
+  }
+
+  int find(int v) {
+    if (parent_[v] < 0) return v;
+    return parent_[v] = find(parent_[v]);
+  }
+
+  int size(int v) { return -parent_[find(v)]; }
+
+  bool same(int x, int y) { return find(x) == find(y); }
+};
+
+auto solve() {
+  int n = in, m = in;
+  UnionFind uf(n);
+  vector<vector<int>> g(n);
+  REP(i, m) {
+    int u = in, v = in;
+    --u, --v;
+    g[u].push_back(v);
+    g[v].push_back(u);
+    uf.unite(u, v);
+  }
+  Int perfect = Int(n) * (n - 1) / 2;
+  Int rest = perfect - m;
+  //DUMP(perfect, m, rest);
+  int oddc = 0, compoc = 0;
+  REP(v, n) {
+    if (uf.find(v) != v) continue;
+    ++compoc;
+    if (uf.size(v) % 2 == 1) {
+      ++oddc;
+    }
+  }
+  int base_odd = (uf.size(0) % 2) + (uf.size(n - 1) % 2);
+  oddc -= base_odd;
+
+  if (rest % 2 == 1) {
+    //DUMP("want even", base_odd, oddc);
+    if (base_odd == 2 and oddc % 2 == 0) return false;
+    return true;
+  } else {
+    //DUMP("want odd", base_odd, oddc);
+    if (base_odd == 1 and oddc % 2 == 1) return true;
+    if (base_odd == 2 and oddc % 2 == 0) return true;
+    return false;
+  }
+}
+
+int main() {
+  std::ios::sync_with_stdio(false), cin.tie(nullptr);
+  cout << std::fixed << std::setprecision(18);
+  const int T = in;
+  REP(t, T) {
+    print(solve());
+  }
+}
