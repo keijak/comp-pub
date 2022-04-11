28ce9cb19e1c7b468de87b346ad298b0abaab494
keijak <m@keijak.com>
2022-04-12 06:30:50 +0900

    inv

---
+++ atcoder/typical90/089/review_089.cpp
@@ -0,0 +1,339 @@
+#include <bits/stdc++.h>
+#define REP_(i, a_, b_, a, b, ...) for (int i = (a), END_##i = (b); i < END_##i; ++i)
+#define REP(i, ...) REP_(i, __VA_ARGS__, __VA_ARGS__, 0, __VA_ARGS__)
+#define ALL(x) std::begin(x), std::end(x)
+using Int = long long;
+using Uint = unsigned long long;
+using Real = long double;
+
+#include <atcoder/modint>
+using Mint = atcoder::modint1000000007;
+std::ostream &operator<<(std::ostream &os, const Mint &m) {
+  return os << m.val();
+}
+
+template<typename T, typename U>
+inline bool chmax(T &a, U b) { return a < b and ((a = b), true); }
+template<typename T, typename U>
+inline bool chmin(T &a, U b) { return a > b and ((a = b), true); }
+template<typename T>
+inline int ssize(const T &a) { return (int) a.size(); }
+template<typename T>
+constexpr T kBigVal = std::numeric_limits<T>::max() / 2;
+
+struct CastInput {
+  template<typename T>
+  operator T() const {
+    T x;
+    std::cin >> x;
+    return x;
+  }
+  struct Sized {
+    int n;
+    template<typename T>
+    operator T() const {
+      T xs(n);
+      for (auto &x: xs) std::cin >> x;
+      return xs;
+    }
+  };
+  Sized operator()(int n) const { return {n}; }
+} in;
+
+template<typename Container>
+std::ostream &print_seq(const Container &seq,
+                        const char *sep = " ",
+                        const char *ends = "\n",
+                        std::ostream &os = std::cout) {
+  const auto itl = std::begin(seq), itr = std::end(seq);
+  for (auto it = itl; it != itr; ++it) {
+    if (it != itl) os << sep;
+    os << *it;
+  }
+  return os << ends;
+}
+
+template<typename T>
+inline std::ostream &print_one(const T &x, char endc) {
+  if constexpr (std::is_same<T, bool>::value) {
+    return std::cout << (x ? "Yes" : "No") << endc;
+  } else {
+    return std::cout << x << endc;
+  }
+}
+template<typename T>
+inline std::ostream &print(const T &x) { return print_one(x, '\n'); }
+template<typename T, typename... Ts>
+std::ostream &print(const T &head, Ts... tail) {
+  return print_one(head, ' '), print(tail...);
+}
+inline std::ostream &print() { return std::cout << '\n'; }
+void exit_() { std::cout.flush(), std::cerr.flush(), std::_Exit(0); }
+
+#ifdef MY_DEBUG
+#include "debug_dump.hpp"
+#include "backward.hpp"
+backward::SignalHandling kSignalHandling;
+#else
+#define DUMP(...)
+#define cerr if(false)cerr
+#endif
+
+using namespace std;
+
+struct SuccinctBitVector {
+  int size_;
+  int blocks_;
+  std::vector<unsigned> bit_, sum_;
+
+  SuccinctBitVector() : size_(0), blocks_(0) {};
+
+  explicit SuccinctBitVector(int size)
+      : size_(size),
+        blocks_((size_ + 31) >> 5),
+        bit_(blocks_, 0U),
+        sum_(blocks_, 0U) {}
+
+  void set(int k) { bit_[k >> 5] |= 1U << (k & 31); }
+
+  void build() {
+    sum_[0] = 0U;
+    for (int i = 1; i < blocks_; ++i) {
+      sum_[i] = sum_[i - 1] + __builtin_popcount(bit_[i - 1]);
+    }
+  }
+
+  // O(1)
+  bool operator[](int k) const { return (bit_[k >> 5] >> (k & 31)) & 1; }
+
+  // O(1)
+  int rank(int k) const {
+    return (sum_[k >> 5] +
+        __builtin_popcount(bit_[k >> 5] & ((1U << (k & 31)) - 1)));
+  }
+
+  // O(1)
+  int rank(bool val, int k) const { return (val ? rank(k) : k - rank(k)); }
+
+  // O(log(n))
+  int select(bool val, int k) const {
+    if (k < 0 || rank(val, size_) <= k) return (-1);
+    int low = 0, high = size_;
+    while (high - low > 1) {
+      int mid = (low + high) >> 1;
+      if (rank(val, mid) >= k + 1)
+        high = mid;
+      else
+        low = mid;
+    }
+    return (high - 1);
+  }
+
+  // O(log(n))
+  int select(bool val, int i, int l) const {
+    return select(val, i + rank(val, l));
+  }
+};
+
+template<class T = unsigned, int kBitWidth = std::numeric_limits<T>::digits>
+struct WaveletMatrix {
+  static_assert(std::is_unsigned<T>::value, "Requires unsigned type");
+  static_assert(kBitWidth > 0);
+
+  int size_;
+  std::array<SuccinctBitVector, kBitWidth> matrix;
+  std::array<int, kBitWidth> mid;
+
+  WaveletMatrix() = default;
+
+  explicit WaveletMatrix(std::vector<T> v) : size_(v.size()) {
+    std::vector<T> l(size_), r(size_);
+    for (int level = kBitWidth - 1; level >= 0; --level) {
+      matrix[level] = SuccinctBitVector(size_ + 1);
+      int left = 0, right = 0;
+      for (int i = 0; i < size_; ++i) {
+        if (((v[i] >> level) & 1)) {
+          matrix[level].set(i);
+          r[right++] = v[i];
+        } else {
+          l[left++] = v[i];
+        }
+      }
+      mid[level] = left;
+      matrix[level].build();
+      v.swap(l);
+      for (int i = 0; i < right; ++i) {
+        v[left + i] = r[i];
+      }
+    }
+  }
+
+  inline int size() const { return size_; }
+
+  // access(i): the value at i (0-indexed).
+  T operator[](int i) const {
+    T ret = 0;
+    for (int level = kBitWidth - 1; level >= 0; --level) {
+      bool f = matrix[level][i];
+      if (f) ret |= T(1) << level;
+      i = matrix[level].rank(f, i) + mid[level] * f;
+    }
+    return ret;
+  }
+
+  // count i s.t. (0 <= i < r) && v[i] == x
+  int rank(const T &x, int r) const {
+    int l = 0;
+    for (int level = kBitWidth - 1; level >= 0; --level) {
+      std::tie(l, r) = succ((x >> level) & 1, l, r, level);
+    }
+    return r - l;
+  }
+
+  // k-th(0-indexed) smallest number in v[l,r)
+  T kth_smallest(int l, int r, int k) const {
+    assert(0 <= k && k < r - l);
+    T ret = 0;
+    for (int level = kBitWidth - 1; level >= 0; --level) {
+      int cnt = matrix[level].rank(false, r) - matrix[level].rank(false, l);
+      bool f = cnt <= k;
+      if (f) {
+        ret |= T(1) << level;
+        k -= cnt;
+      }
+      std::tie(l, r) = succ(f, l, r, level);
+    }
+    return ret;
+  }
+
+  // k-th(0-indexed) largest number in v[l,r)
+  T kth_largest(int l, int r, int k) const {
+    return kth_smallest(l, r, r - l - k - 1);
+  }
+
+  // count i s.t. (l <= i < r) && (v[i] < upper)
+  int range_freq(int l, int r, T upper) const {
+    int ret = 0;
+    for (int level = kBitWidth - 1; level >= 0; --level) {
+      bool f = ((upper >> level) & 1);
+      if (f) ret += matrix[level].rank(false, r) - matrix[level].rank(false, l);
+      std::tie(l, r) = succ(f, l, r, level);
+    }
+    return ret;
+  }
+
+  // count i s.t. (l <= i < r) && (lower <= v[i] < upper)
+  int range_freq(int l, int r, T lower, T upper) const {
+    return range_freq(l, r, upper) - range_freq(l, r, lower);
+  }
+
+  // max v[i] s.t. (l <= i < r) && (v[i] < upper)
+  T prev_value(int l, int r, T upper) const {
+    int cnt = range_freq(l, r, upper);
+    return cnt == 0 ? T(-1) : kth_smallest(l, r, cnt - 1);
+  }
+
+  // min v[i] s.t. (l <= i < r) && (lower <= v[i])
+  T next_value(int l, int r, T lower) const {
+    int cnt = range_freq(l, r, lower);
+    return cnt == r - l ? T(-1) : kth_smallest(l, r, cnt);
+  }
+
+ private:
+  std::pair<int, int> succ(bool f, int l, int r, int level) const {
+    return {matrix[level].rank(f, l) + mid[level] * f,
+            matrix[level].rank(f, r) + mid[level] * f};
+  }
+};
+
+// T: can be large nubmers or negative numbers
+// Number of unique values must be less than 2^kBitWidth.
+template<typename T, int kBitWidth = 28>
+struct CompressedWaveletMatrix {
+  std::vector<T> values;
+  WaveletMatrix<size_t, kBitWidth> indices;
+
+  explicit CompressedWaveletMatrix(const std::vector<T> &v) : values(v) {
+    std::sort(values.begin(), values.end());
+    values.erase(std::unique(values.begin(), values.end()), values.end());
+    assert(values.size() < (1ULL << kBitWidth));
+
+    std::vector<size_t> t(v.size());
+    for (size_t i = 0; i < v.size(); ++i) {
+      t[i] = index(v[i]);
+    }
+    indices = WaveletMatrix<size_t, kBitWidth>(t);
+  }
+
+  T operator[](int k) const { return values[indices[k]]; }
+
+  int rank(const T &x, int r) const {
+    auto pos = index(x);
+    if (pos == values.size() || values[pos] != x) return 0;
+    return indices.rank(pos, r);
+  }
+
+  T kth_smallest(int l, int r, int k) const {
+    return values[indices.kth_smallest(l, r, k)];
+  }
+
+  T kth_largest(int l, int r, int k) const {
+    return values[indices.kth_largest(l, r, k)];
+  }
+
+  int range_freq(int l, int r, T upper) const {
+    return indices.range_freq(l, r, index(upper));
+  }
+
+  int range_freq(int l, int r, T lower, T upper) const {
+    return indices.range_freq(l, r, index(lower), index(upper));
+  }
+
+  T prev_value(int l, int r, T upper) const {
+    auto ret = indices.prev_value(l, r, index(upper));
+    return ret == -1 ? T(-1) : values[ret];
+  }
+
+  T next_value(int l, int r, T lower) const {
+    auto ret = indices.next_value(l, r, index(lower));
+    return ret == -1 ? T(-1) : values[ret];
+  }
+
+ private:
+  // value -> index
+  size_t index(const T &x) const {
+    return std::lower_bound(values.begin(), values.end(), x) - values.begin();
+  }
+};
+
+auto solve() {
+  const int n = in;
+  const Int K = in;
+  vector<uint32_t> a = in(n);
+  CompressedWaveletMatrix<uint32_t, 32> wm(a);
+
+  vector<Mint> dp(n + 1), dp_sum(n + 2);
+  dp[0] = 1;
+  dp_sum[1] = 1;
+  int l = 0;
+  Int inversion = 0;
+  REP(r, 1, n + 1) {
+    inversion += wm.range_freq(l, r - 1, a[r - 1] + 1, kBigVal<uint32_t>);
+    while (inversion > K) {
+      inversion -= wm.range_freq(l + 1, r, 0, a[l]);
+      ++l;
+    }
+    assert(l < r);
+    dp[r] += dp_sum[r] - dp_sum[l];
+    dp_sum[r + 1] = dp_sum[r] + dp[r];
+  }
+  print(dp[n]);
+}
+
+int main() {
+  std::ios::sync_with_stdio(false), cin.tie(nullptr);
+  cout << std::fixed << std::setprecision(18);
+  const int T = 1;//in;
+  REP(t, T) { (solve()); }
+  exit_();
+}
