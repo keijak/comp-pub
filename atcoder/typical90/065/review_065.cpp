421caa7f13669a2f5033fade4641487a932edccc
keijak <m@keijak.com>
2022-04-13 16:45:55 +0900

    fps

---
+++ atcoder/typical90/065/review_065.cpp
@@ -0,0 +1,240 @@
+#include <bits/stdc++.h>
+#define REP_(i, a_, b_, a, b, ...) for (int i = (a), END_##i = (b); i < END_##i; ++i)
+#define REP(i, ...) REP_(i, __VA_ARGS__, __VA_ARGS__, 0, __VA_ARGS__)
+#define ALL(x) std::begin(x), std::end(x)
+using Int = long long;
+using Uint = unsigned long long;
+using Real = long double;
+
+#include <atcoder/math>
+#include <atcoder/convolution>
+#include <atcoder/modint>
+using Mint = atcoder::modint998244353;
+std::ostream &operator<<(std::ostream &os, const Mint &m) {
+  return os << m.val();
+}
+
+template<typename T, typename U>
+inline bool chmax(T &a, U b) { return a < b and ((a = b), true); }
+template<typename T, typename U>
+inline bool chmin(T &a, U b) { return a > b and ((a = b), true); }
+template<typename T>
+inline int ssize(const T &a) { return (int) a.size(); }
+template<typename T>
+constexpr T kBigVal = std::numeric_limits<T>::max() / 2;
+
+struct CastInput {
+  template<typename T>
+  operator T() const {
+    T x;
+    std::cin >> x;
+    return x;
+  }
+  struct Sized {
+    int n;
+    template<typename T>
+    operator T() const {
+      T xs(n);
+      for (auto &x: xs) std::cin >> x;
+      return xs;
+    }
+  };
+  Sized operator()(int n) const { return {n}; }
+} in;
+
+template<typename Container>
+std::ostream &print_seq(const Container &seq,
+                        const char *sep = " ",
+                        const char *ends = "\n",
+                        std::ostream &os = std::cout) {
+  const auto itl = std::begin(seq), itr = std::end(seq);
+  for (auto it = itl; it != itr; ++it) {
+    if (it != itl) os << sep;
+    os << *it;
+  }
+  return os << ends;
+}
+
+template<typename T>
+inline std::ostream &print_one(const T &x, char endc) {
+  if constexpr (std::is_same<T, bool>::value) {
+    return std::cout << (x ? "Yes" : "No") << endc;
+  } else {
+    return std::cout << x << endc;
+  }
+}
+template<typename T>
+inline std::ostream &print(const T &x) { return print_one(x, '\n'); }
+template<typename T, typename... Ts>
+std::ostream &print(const T &head, Ts... tail) {
+  return print_one(head, ' '), print(tail...);
+}
+inline std::ostream &print() { return std::cout << '\n'; }
+void exit_() { std::cout.flush(), std::cerr.flush(), std::_Exit(0); }
+
+#ifdef MY_DEBUG
+#include "debug_dump.hpp"
+#include "backward.hpp"
+backward::SignalHandling kSignalHandling;
+#else
+#define DUMP(...)
+#define cerr if(false)cerr
+#endif
+
+using namespace std;
+
+// mod: prime
+template<class T = Mint>
+struct Factorials {
+  // factorials and inverse factorials.
+  std::vector<T> fact, ifact;
+
+  // n: max cached value.
+  explicit Factorials(int n) : fact(n + 1), ifact(n + 1) {
+    assert(n >= 0);
+    assert(n < T::mod());
+    fact[0] = 1;
+    for (int i = 1; i <= n; ++i) {
+      fact[i] = fact[i - 1] * i;
+    }
+    ifact[n] = fact[n].inv();
+    for (int i = n; i >= 1; --i) {
+      ifact[i - 1] = ifact[i] * i;
+    }
+  }
+
+  // Combination (binomial coefficients)
+  T C(Int n, Int k) const {
+    if (k < 0 || k > n) return 0;
+    return fact[n] * ifact[k] * ifact[n - k];
+  }
+};
+
+// T: modint
+template<typename T, int DMAX>
+struct NTTMult {
+  static_assert(atcoder::internal::is_modint<T>::value, "Requires ACL modint.");
+  static_assert(T::mod() == 998244353, "Requires an NTT-friendly mod.");
+
+  using value_type = T;
+  static constexpr int dmax() { return DMAX; }
+
+  static std::vector<T> multiply(const std::vector<T> &x,
+                                 const std::vector<T> &y) {
+    std::vector<T> res = atcoder::convolution(x, y);
+    if (int(res.size()) > DMAX + 1) res.resize(DMAX + 1);  // shrink
+    return res;
+  }
+};
+
+// Formal Power Series (dense format).
+template<typename Mult>
+struct DenseFPS {
+  using T = typename Mult::value_type;
+  static constexpr int dmax() { return Mult::dmax(); }
+
+  // Coefficients of terms from x^0 to x^DMAX.
+  std::vector<T> coeff_;
+
+  DenseFPS() : coeff_(1, 0) {}  // zero
+
+  explicit DenseFPS(std::vector<T> c) : coeff_(std::move(c)) {
+    while (size() > dmax() + 1) coeff_.pop_back();
+    assert(size() > 0);
+  }
+  DenseFPS(std::initializer_list<T> c) : coeff_(c.begin(), c.end()) {
+    while (size() > dmax() + 1) coeff_.pop_back();
+    assert(size() > 0);
+  }
+
+  // size <= dmax + 1
+  inline int size() const { return static_cast<int>(coeff_.size()); }
+
+  // Returns the coefficient of x^k.
+  inline T operator[](int k) const { return (k >= size()) ? 0 : coeff_[k]; }
+
+  // Removes trailing zeros.
+  void shrink() {
+    while (coeff_.size() > 1 and coeff_.back() == T(0)) coeff_.pop_back();
+  }
+
+  DenseFPS &operator+=(const T &scalar) {
+    coeff_[0] += scalar;
+    return *this;
+  }
+  friend DenseFPS operator+(const DenseFPS &f, const T &scalar) {
+    return DenseFPS(f) += scalar;
+  }
+  DenseFPS &operator+=(const DenseFPS &other) {
+    if (size() < other.size()) coeff_.resize(other.size());
+    for (int i = 0; i < other.size(); ++i) coeff_[i] += other[i];
+    return *this;
+  }
+  friend DenseFPS operator+(const DenseFPS &f, const DenseFPS &g) {
+    return DenseFPS(f) += g;
+  }
+
+  DenseFPS &operator-=(const DenseFPS &other) {
+    if (size() < other.size()) coeff_.resize(other.size());
+    for (int i = 0; i < other.size(); ++i) coeff_[i] -= other[i];
+    return *this;
+  }
+  friend DenseFPS operator-(const DenseFPS &f, const DenseFPS &g) {
+    return DenseFPS(f) -= g;
+  }
+
+  DenseFPS operator-() const { return *this * -1; }
+
+  DenseFPS &operator*=(const T &scalar) {
+    for (auto &x: coeff_) x *= scalar;
+    return *this;
+  }
+  friend DenseFPS operator*(const DenseFPS &f, const T &scalar) {
+    return DenseFPS(f) *= scalar;
+  }
+  friend DenseFPS operator*(const T &scalar, const DenseFPS &g) {
+    return DenseFPS{scalar} *= g;
+  }
+  DenseFPS &operator*=(const DenseFPS &other) {
+    return *this = DenseFPS(Mult::multiply(std::move(this->coeff_), other.coeff_));
+  }
+  friend DenseFPS operator*(const DenseFPS &f, const DenseFPS &g) {
+    return DenseFPS(Mult::multiply(f.coeff_, g.coeff_));
+  }
+};
+
+constexpr int DMAX = 200005;
+using DF = DenseFPS<NTTMult<Mint, DMAX>>;
+
+Mint solve() {
+  int R = in, G = in, B = in, K = in;
+  int X = in, Y = in, Z = in;
+  int Rreq = K - Y, Greq = K - Z, Breq = K - X;
+  if (Rreq < 0 or Greq < 0 or Breq < 0) return 0;
+  if (Rreq + Greq + Breq > K) return 0;
+  if (Rreq > R or Greq > G or Breq > B) return 0;
+  Factorials<Mint> fs(DMAX);
+  DF f = {1};
+  vector<Mint> rc(R + 1), gc(G + 1), bc(B + 1);
+  for (int i = Rreq; i <= R; ++i) {
+    rc[i] = fs.C(R, i);
+  }
+  f *= DF(rc);
+  for (int i = Greq; i <= G; ++i) {
+    gc[i] = fs.C(G, i);
+  }
+  f *= DF(gc);
+  for (int i = Breq; i <= B; ++i) {
+    bc[i] = fs.C(B, i);
+  }
+  f *= DF(bc);
+  return f[K];
+}
+
+int main() {
+  std::ios::sync_with_stdio(false), cin.tie(nullptr);
+  cout << std::fixed << std::setprecision(18);
+  const int T = 1;//in;
+  REP(t, T) { print(solve()); }
+  exit_();
+}
