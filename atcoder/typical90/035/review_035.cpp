aadb1d0a2df8ea27c95cf24d0ce19a0253b839bf
keijak <m@keijak.com>
2022-04-13 16:55:41 +0900

    lca many

---
+++ atcoder/typical90/035/review_035.cpp
@@ -0,0 +1,254 @@
+#include <bits/stdc++.h>
+#define REP_(i, a_, b_, a, b, ...) for (int i = (a), END_##i = (b); i < END_##i; ++i)
+#define REP(i, ...) REP_(i, __VA_ARGS__, __VA_ARGS__, 0, __VA_ARGS__)
+#define ALL(x) std::begin(x), std::end(x)
+using Int = long long;
+using Uint = unsigned long long;
+using Real = long double;
+
+template<typename T, typename U>
+inline bool chmax(T &a, U b) { return a < b and ((a = b), true); }
+template<typename T, typename U>
+inline bool chmin(T &a, U b) { return a > b and ((a = b), true); }
+template<typename T>
+inline int ssize(const T &a) { return (int) a.size(); }
+template<typename T>
+constexpr T kBigVal = std::numeric_limits<T>::max() / 2;
+
+struct CastInput {
+  template<typename T>
+  operator T() const {
+    T x;
+    std::cin >> x;
+    return x;
+  }
+  struct Sized {
+    int n;
+    template<typename T>
+    operator T() const {
+      T xs(n);
+      for (auto &x: xs) std::cin >> x;
+      return xs;
+    }
+  };
+  Sized operator()(int n) const { return {n}; }
+} in;
+
+template<typename Container>
+std::ostream &print_seq(const Container &seq,
+                        const char *sep = " ",
+                        const char *ends = "\n",
+                        std::ostream &os = std::cout) {
+  const auto itl = std::begin(seq), itr = std::end(seq);
+  for (auto it = itl; it != itr; ++it) {
+    if (it != itl) os << sep;
+    os << *it;
+  }
+  return os << ends;
+}
+
+template<typename T>
+inline std::ostream &print_one(const T &x, char endc) {
+  if constexpr (std::is_same<T, bool>::value) {
+    return std::cout << (x ? "Yes" : "No") << endc;
+  } else {
+    return std::cout << x << endc;
+  }
+}
+template<typename T>
+inline std::ostream &print(const T &x) { return print_one(x, '\n'); }
+template<typename T, typename... Ts>
+std::ostream &print(const T &head, Ts... tail) {
+  return print_one(head, ' '), print(tail...);
+}
+inline std::ostream &print() { return std::cout << '\n'; }
+void exit_() { std::cout.flush(), std::cerr.flush(), std::_Exit(0); }
+
+#ifdef MY_DEBUG
+#include "debug_dump.hpp"
+#include "backward.hpp"
+backward::SignalHandling kSignalHandling;
+#else
+#define DUMP(...)
+#define cerr if(false)cerr
+#endif
+
+using namespace std;
+
+using G = std::vector<std::vector<int>>;  // undirected graph
+
+// Heavy-Light Decomposition
+struct HLDecomp {
+  // Whether the given adjacency list only contains children or may also contain
+  // parents. If true, skips removing parent from the graph for speedup.
+  static constexpr bool kChildrenOnlyGraph = false;
+
+  using NodeID = int;                       // [0, n)
+  // half-open intervals of preorder indices of nodes.
+  using IntervalVec = std::vector<std::pair<int, int>>;
+
+  int n;                       // number of nodes in the tree
+  NodeID root;                 // root of the tree
+  G child;                     // children node ids
+  std::vector<NodeID> parent;  // parent node id (or -1 on the root node)
+  std::vector<int> subsize;    // subtree size
+  // "ord" is preorder index in DFS traversal. [0, n)
+  std::vector<int> node_to_ord;     // node id to preorder index
+  std::vector<NodeID> ord_to_node;  // preorder index to node id
+  std::vector<NodeID> comp_root;    // node id to its heavy path component
+
+  explicit HLDecomp(G g, NodeID root = 0)
+      : n(int(g.size())),
+        root(root),
+        child(g),
+        parent(n, -1),
+        subsize(n, 1),
+        node_to_ord(n, -1),
+        ord_to_node(n, -1),
+        comp_root(n, -1) {
+    dfs_subsize(root);
+    int counter = 0;
+    comp_root[root] = root;
+    dfs_hld(root, counter);
+  }
+
+  // Lowest Common Ancestor
+  NodeID lca(NodeID u, NodeID v) const {
+    for (;;) {
+      if (node_to_ord[u] > node_to_ord[v]) std::swap(u, v);
+      NodeID crv = comp_root[v];
+      if (comp_root[u] == crv) return u;
+      assert(parent[crv] != -1);
+      v = parent[crv];
+    }
+  }
+
+  // Returns the set of nodes in the u-v path, including both u and v.
+  //
+  // The return value is half-open intervals of the preorder indices of the
+  // nodes. One interval corresponds to one heavy path component.
+  IntervalVec node_ranges_on_path(NodeID u, NodeID v) const {
+    IntervalVec res;
+    for (;;) {
+      if (node_to_ord[u] > node_to_ord[v]) std::swap(u, v);
+      NodeID crv = comp_root[v];
+      res.emplace_back(std::max(node_to_ord[crv], node_to_ord[u]),
+                       node_to_ord[v] + 1);
+      if (comp_root[u] == crv) break;
+      assert(parent[crv] != -1);
+      v = parent[crv];
+    }
+    return res;
+  }
+
+  // Returns the set of edges in the u-v path.
+  //
+  // The return value is half-open intervals of the preorder indices of nodes
+  // corresponding to the deeper end of each edge in the path. Here we identify
+  // a node `v` with `Edge(v, parent[v])`.
+  IntervalVec edge_ranges_on_path(NodeID u, NodeID v) const {
+    IntervalVec res;
+    for (;;) {
+      if (node_to_ord[u] > node_to_ord[v]) std::swap(u, v);
+      NodeID crv = comp_root[v];
+      if (comp_root[u] == crv) {
+        if (u != v) res.emplace_back(node_to_ord[u] + 1, node_to_ord[v] + 1);
+        break;
+      }
+      res.emplace_back(node_to_ord[crv], node_to_ord[v] + 1);
+      assert(parent[crv] != -1);
+      v = parent[crv];
+    }
+    return res;
+  }
+
+  // Distance (= number of edges) of the path between two nodes.
+  int distance(NodeID u, NodeID v) const {
+    int dist = 0;
+    for (const auto &[l, r]: edge_ranges_on_path(u, v)) {
+      dist += r - l;
+    }
+    return dist;
+  }
+
+  // Whether the node x is on the u-v path.
+  bool node_is_on_path(NodeID x, const std::pair<NodeID, NodeID> &path) const {
+    const auto &[u, v] = path;
+    return distance(u, x) + distance(x, v) == distance(u, v);
+  }
+
+ private:
+  // Fills `parent` and `subsize`.
+  void dfs_subsize(NodeID v) {
+    auto &edges = child[v];
+    if constexpr (not kChildrenOnlyGraph) {
+      if (parent[v] != -1) {
+        // Remove the parent from `child[v]`.
+        // O(N) in total for processing the entire tree.
+        auto it = std::find(edges.begin(), edges.end(), parent[v]);
+        if (it != edges.end()) {
+          std::swap(*it, edges.back());
+          edges.pop_back();
+        }
+      }
+    }
+    for (NodeID &u: edges) {
+      parent[u] = v;
+      dfs_subsize(u);
+      subsize[v] += subsize[u];
+      if (subsize[u] > subsize[edges[0]]) {
+        std::swap(u, edges[0]);
+      }
+    }
+  }
+
+  // Fills `node_to_ord`, `ord_to_node`, and `comp_root`.
+  void dfs_hld(NodeID v, int &counter) {
+    node_to_ord[v] = counter++;
+    ord_to_node[node_to_ord[v]] = v;
+    for (NodeID u: child[v]) {
+      comp_root[u] = (u == child[v][0] ? comp_root[v] : u);
+      dfs_hld(u, counter);
+    }
+  }
+};
+
+auto solve() {
+  const int n = in;
+  G g(n);
+  REP(i, n - 1) {
+    int u = in, v = in;
+    --u, --v;
+    g[u].push_back(v);
+    g[v].push_back(u);
+  }
+  HLDecomp hld(g);
+  const int Q = in;
+  REP(qi, Q) {
+    const int K = in;
+    vector<int> V = in(K);
+    for (auto &v: V) --v;
+    vector<int> ord(K);
+    REP(i, K) ord[i] = hld.node_to_ord[V[i]];
+    sort(ALL(ord));
+    int v0 = hld.ord_to_node[ord.front()];
+    int lca_id = hld.lca(v0, hld.ord_to_node[ord.back()]);
+    Int dist = hld.distance(lca_id, v0);
+    REP(i, 1, K) {
+      int v = hld.ord_to_node[ord[i]];
+      dist += hld.distance(v0, v);
+      v0 = v;
+    }
+    dist += hld.distance(v0, lca_id);
+    assert(dist % 2 == 0);
+    print(dist / 2);
+  }
+}
+
+int main() {
+  std::ios::sync_with_stdio(false), cin.tie(nullptr);
+  cout << std::fixed << std::setprecision(18);
+  const int T = 1;//in;
+  REP(t, T) { (solve()); }
+  exit_();
+}
