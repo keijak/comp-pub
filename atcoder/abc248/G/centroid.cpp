109bb177ee85342f1505ae3ee8e006821bbbdde7
keijak <m@keijak.com>
2022-04-16 23:21:02 +0900

    abc

---
+++ atcoder/abc248/G/centroid.cpp
@@ -0,0 +1,431 @@
+#include <bits/stdc++.h>
+#define REP_(i, a_, b_, a, b, ...) for (int i = (a), END_##i = (b); i < END_##i; ++i)
+#define REP(i, ...) REP_(i, __VA_ARGS__, __VA_ARGS__, 0, __VA_ARGS__)
+#define ALL(x) std::begin(x), std::end(x)
+using Int = long long;
+using Uint = unsigned long long;
+using Real = long double;
+
+#include <atcoder/modint>
+using Mint = atcoder::modint998244353;
+std::ostream &operator<<(std::ostream &os, const Mint &m) {
+  return os << m.val();
+}
+
+template<typename T, typename U>
+inline bool chmax(T &a, U b) { return a < b and ((a = b), true); }
+template<typename T, typename U>
+inline bool chmin(T &a, U b) { return a > b and ((a = b), true); }
+template<typename T>
+inline int ssize(const T &a) { return (int) a.size(); }
+template<typename T>
+constexpr T kBigVal = std::numeric_limits<T>::max() / 2;
+
+struct CastInput {
+  template<typename T>
+  operator T() const {
+    T x;
+    std::cin >> x;
+    return x;
+  }
+  struct Sized {
+    int n;
+    template<typename T>
+    operator T() const {
+      T xs(n);
+      for (auto &x: xs) std::cin >> x;
+      return xs;
+    }
+  };
+  Sized operator()(int n) const { return {n}; }
+} in;
+
+template<typename Container>
+std::ostream &print_seq(const Container &seq,
+                        const char *sep = " ",
+                        const char *ends = "\n",
+                        std::ostream &os = std::cout) {
+  const auto itl = std::begin(seq), itr = std::end(seq);
+  for (auto it = itl; it != itr; ++it) {
+    if (it != itl) os << sep;
+    os << *it;
+  }
+  return os << ends;
+}
+
+template<typename T>
+inline std::ostream &print_one(const T &x, char endc) {
+  if constexpr (std::is_same<T, bool>::value) {
+    return std::cout << (x ? "Yes" : "No") << endc;
+  } else {
+    return std::cout << x << endc;
+  }
+}
+template<typename T>
+inline std::ostream &print(const T &x) { return print_one(x, '\n'); }
+template<typename T, typename... Ts>
+std::ostream &print(const T &head, Ts... tail) {
+  return print_one(head, ' '), print(tail...);
+}
+inline std::ostream &print() { return std::cout << '\n'; }
+void exit_() { std::cout.flush(), std::cerr.flush(), std::_Exit(0); }
+
+#ifdef MY_DEBUG
+#include "debug_dump.hpp"
+#include "backward.hpp"
+backward::SignalHandling kSignalHandling;
+#else
+#define DUMP(...)
+#define cerr if(false)cerr
+#endif
+
+using namespace std;
+
+// Heavy-Light Decomposition
+struct HLDecomp {
+  // Whether the given adjacency list only contains children or may also contain
+  // parents. If true, skips removing parent from the graph for speedup.
+  static constexpr bool kChildrenOnlyGraph = false;
+
+  using NodeID = int;                       // [0, n)
+  using G = std::vector<std::vector<int>>;  // undirected graph
+  // half-open intervals of preorder indices of nodes.
+  using IntervalVec = std::vector<std::pair<int, int>>;
+
+  int n;                       // number of nodes in the tree
+  NodeID root;                 // root of the tree
+  G child;                     // children node ids
+  std::vector<NodeID> parent;  // parent node id (or -1 on the root node)
+  std::vector<int> subsize;    // subtree size
+  // "ord" is preorder index in DFS traversal. [0, n)
+  std::vector<int> node_to_ord;     // node id to preorder index
+  std::vector<NodeID> ord_to_node;  // preorder index to node id
+  std::vector<NodeID> comp_root;    // node id to its heavy path component
+
+  explicit HLDecomp(G g, NodeID root = 0)
+      : n(int(g.size())),
+        root(root),
+        child(g),
+        parent(n, -1),
+        subsize(n, 1),
+        node_to_ord(n, -1),
+        ord_to_node(n, -1),
+        comp_root(n, -1) {
+    dfs_subsize(root);
+    int counter = 0;
+    comp_root[root] = root;
+    dfs_hld(root, counter);
+  }
+
+  // Lowest Common Ancestor
+  NodeID lca(NodeID u, NodeID v) const {
+    for (;;) {
+      if (node_to_ord[u] > node_to_ord[v]) std::swap(u, v);
+      NodeID crv = comp_root[v];
+      if (comp_root[u] == crv) return u;
+      assert(parent[crv] != -1);
+      v = parent[crv];
+    }
+  }
+
+  // Returns the set of nodes in the u-v path, including both u and v.
+  //
+  // The return value is half-open intervals of the preorder indices of the
+  // nodes. One interval corresponds to one heavy path component.
+  IntervalVec node_ranges_on_path(NodeID u, NodeID v) const {
+    IntervalVec res;
+    for (;;) {
+      if (node_to_ord[u] > node_to_ord[v]) std::swap(u, v);
+      NodeID crv = comp_root[v];
+      res.emplace_back(std::max(node_to_ord[crv], node_to_ord[u]),
+                       node_to_ord[v] + 1);
+      if (comp_root[u] == crv) break;
+      assert(parent[crv] != -1);
+      v = parent[crv];
+    }
+    return res;
+  }
+
+  // Returns the set of edges in the u-v path.
+  //
+  // The return value is half-open intervals of the preorder indices of nodes
+  // corresponding to the deeper end of each edge in the path. Here we identify
+  // a node `v` with `Edge(v, parent[v])`.
+  IntervalVec edge_ranges_on_path(NodeID u, NodeID v) const {
+    IntervalVec res;
+    for (;;) {
+      if (node_to_ord[u] > node_to_ord[v]) std::swap(u, v);
+      NodeID crv = comp_root[v];
+      if (comp_root[u] == crv) {
+        if (u != v) res.emplace_back(node_to_ord[u] + 1, node_to_ord[v] + 1);
+        break;
+      }
+      res.emplace_back(node_to_ord[crv], node_to_ord[v] + 1);
+      assert(parent[crv] != -1);
+      v = parent[crv];
+    }
+    return res;
+  }
+
+  // Distance (= number of edges) of the path between two nodes.
+  int distance(NodeID u, NodeID v) const {
+    int dist = 0;
+    for (const auto &[l, r]: edge_ranges_on_path(u, v)) {
+      dist += r - l;
+    }
+    return dist;
+  }
+
+  // Whether the node x is on the u-v path.
+  bool node_is_on_path(NodeID x, const std::pair<NodeID, NodeID> &path) const {
+    const auto &[u, v] = path;
+    return distance(u, x) + distance(x, v) == distance(u, v);
+  }
+
+ private:
+  // Fills `parent` and `subsize`.
+  void dfs_subsize(NodeID v) {
+    auto &edges = child[v];
+    if constexpr (not kChildrenOnlyGraph) {
+      if (parent[v] != -1) {
+        // Remove the parent from `child[v]`.
+        // O(N) in total for processing the entire tree.
+        auto it = std::find(edges.begin(), edges.end(), parent[v]);
+        if (it != edges.end()) {
+          std::swap(*it, edges.back());
+          edges.pop_back();
+        }
+      }
+    }
+    for (NodeID &u: edges) {
+      parent[u] = v;
+      dfs_subsize(u);
+      subsize[v] += subsize[u];
+      if (subsize[u] > subsize[edges[0]]) {
+        std::swap(u, edges[0]);
+      }
+    }
+  }
+
+  // Fills `node_to_ord`, `ord_to_node`, and `comp_root`.
+  void dfs_hld(NodeID v, int &counter) {
+    node_to_ord[v] = counter++;
+    ord_to_node[node_to_ord[v]] = v;
+    for (NodeID u: child[v]) {
+      comp_root[u] = (u == child[v][0] ? comp_root[v] : u);
+      dfs_hld(u, counter);
+    }
+  }
+};
+
+struct CentroidTree {
+  int n;                    // number of nodes.
+  int root;                 // root of the centroid tree.
+  std::vector<int> parent;  // parent of each node in the centroid tree.
+  std::vector<std::vector<int>> graph;  // adjacent nodes in the centroid tree.
+
+  explicit CentroidTree(std::vector<std::vector<int>> g)
+      : n(g.size()), root(-1), parent(n, -1), graph(n) {
+    std::vector<std::set<int>> gd(n);
+    std::vector<int> subsize(n, 0);
+    for (int v = 0; v < n; ++v) {
+      gd[v].insert(g[v].begin(), g[v].end());
+    }
+    const int start_node = 0;
+    build(start_node, -1, gd, subsize);
+    for (int v = 0; v < n; ++v) {
+      const int p = parent[v];
+      if (p == -1) {
+        root = v;
+      } else {
+        graph[p].push_back(v);
+        graph[v].push_back(p);
+      }
+    }
+  }
+
+ private:
+  void build(int v, int p, std::vector<std::set<int>> &gd,
+             std::vector<int> &subsize) {
+    const int sz = dfs_subsize(v, p, gd, subsize);  // fill subsize
+    const int c = centroid(v, p, sz, gd, subsize);  // find centroid
+    parent[c] = p;
+
+    std::vector<int> tmp(gd[c].begin(), gd[c].end());
+    for (auto u: tmp) {
+      gd[c].erase(u);
+      gd[u].erase(c);
+      build(u, c, gd, subsize);
+    }
+  }
+
+  int dfs_subsize(int v, int p, const std::vector<std::set<int>> &gd,
+                  std::vector<int> &subsize) {
+    subsize[v] = 1;
+    for (auto u: gd[v]) {
+      if (u == p) continue;
+      subsize[v] += dfs_subsize(u, v, gd, subsize);
+    }
+    return subsize[v];
+  }
+
+  int centroid(int v, int p, int sz, const std::vector<std::set<int>> &gd,
+               const std::vector<int> &subsize) {
+    for (auto u: gd[v]) {
+      if (u == p) continue;
+      if (subsize[u] > sz / 2) {
+        return centroid(u, v, sz, gd, subsize);
+      }
+    }
+    return v;
+  }
+};
+
+template<class SemiLattice>
+struct SparseTable {
+  using T = typename SemiLattice::T;
+
+  explicit SparseTable(const std::vector<T> &vec) { init(vec); }
+
+  // Queries by [l,r) range (0-indexed, half-open interval).
+  T fold(int l, int r) const {
+    l = std::max(l, 0);
+    r = std::min(r, n_);
+    if (l >= r) {
+      return SemiLattice::id();
+    }
+    const T &lval = data_[height_[r - l]][l];
+    const T &rval = data_[height_[r - l]][r - (1 << height_[r - l])];
+    return SemiLattice::op(lval, rval);
+  }
+
+  // Returns i-th value (0-indexed).
+  T operator[](int i) const {
+    assert(0 <= i and i < n_);
+    return data_[height_[1]][i];
+  }
+
+ private:
+  void init(const std::vector<T> &vec) {
+    int n = vec.size(), h = 0;
+    n_ = n;
+    while ((1 << h) <= n) ++h;
+    data_.assign(h, std::vector<T>(1 << h, SemiLattice::id()));
+    height_.assign(n + 1, 0);
+    for (int i = 2; i <= n; i++) {
+      height_[i] = height_[i >> 1] + 1;
+    }
+    for (int i = 0; i < n; ++i) {
+      data_[0][i] = vec[i];
+    }
+    for (int i = 1; i < h; ++i)
+      for (int j = 0; j < n; ++j)
+        data_[i][j] = SemiLattice::op(
+            data_[i - 1][j], data_[i - 1][std::min(j + (1 << (i - 1)), n - 1)]);
+  }
+
+  int n_;  // number of elements.
+  std::vector<std::vector<T>> data_;
+  std::vector<int> height_;
+};
+
+struct GCDOp {
+  using T = int;
+  static T op(const T &x, const T &y) {
+    if (x == id()) return y;
+    if (y == id()) return x;
+    return std::gcd(x, y);
+  }
+  static constexpr T id() { return -1; }
+};
+
+template<class F>
+struct Rec {
+  F f_;
+  explicit Rec(F f) : f_(std::move(f)) {}
+  template<class... Args>
+  decltype(auto) operator()(Args &&... args) {
+    return f_(*this, std::forward<Args>(args)...);
+  }
+};
+template<class F> Rec(F) -> Rec<F>;
+
+auto solve() {
+  int n = in;
+  vector<int> a = in(n);
+  vector<vector<int>> g(n);
+  REP(i, n - 1) {
+    int u = in, v = in;
+    --u, --v;
+    g[u].push_back(v);
+    g[v].push_back(u);
+  }
+
+  CentroidTree ct(g);
+  HLDecomp hld(g, ct.root);
+  vector<int> pathgcds(n);
+  REP(i, n) {
+    int v = hld.ord_to_node[i];
+    pathgcds[i] = a[v];
+  }
+  SparseTable<GCDOp> st(pathgcds);
+
+  Mint ans = 0;
+
+  auto dfs_collect = Rec([&](auto &f, int v, int p, int sr, map<int, pair<Mint, Mint>> &out) -> void {
+    int g = GCDOp::id();
+    for (auto[l, r]: hld.node_ranges_on_path(v, sr)) {
+      g = GCDOp::op(g, st.fold(l, r));
+    }
+    if (g != GCDOp::id()) {
+      auto &res = out[g];
+      res.first += Mint(hld.distance(v, sr));
+      res.second += 1;
+    }
+    for (auto u: ct.graph[v]) {
+      if (u == p) continue;
+      f(u, v, sr, out);
+    }
+  });
+
+  Rec([&](auto &f, int v, int p) -> void {
+    map<int, pair<Mint, Mint>> buf;
+    buf[a[v]] = {1, 1};
+
+    for (int u: ct.graph[v]) {
+      if (u == p) continue;
+      f(u, v);
+    }
+    for (int u: ct.graph[v]) {
+      if (u == p) continue;
+      map<int, pair<Mint, Mint>> sub;
+      dfs_collect(u, v, v, sub);
+      for (const auto&[gb, cntb]: buf) {
+        for (const auto&[gs, cnts]: sub) {
+          int cg = gcd(gb, gs);
+          Mint co = Mint(cg) * (cntb.second * cnts.first + cntb.first * cnts.second);
+          ans += co;
+          DUMP(v, u, gb, gs, cg, cntb, cnts, co, ans);
+        }
+      }
+      if (ssize(buf) < ssize(sub)) {
+        buf.swap(sub);
+      }
+      for (const auto&[gs, cnts]: sub) {
+        auto &r = buf[gs];
+        r.first += cnts.first + cnts.second;
+        r.second += cnts.second;
+      }
+    }
+  })(ct.root, -1);
+
+  print(ans);
+}
+
+int main() {
+  std::ios::sync_with_stdio(false), cin.tie(nullptr);
+  cout << std::fixed << std::setprecision(18);
+  const int T = 1;//in;
+  REP(t, T) { (solve()); }
+  exit_();
+}
---
+++ atcoder/abc248/dl.sh
@@ -0,0 +1,21 @@
+#!/bin/zsh
+
+set -e
+
+contest="abc248"
+tasks=(A B C D E F G H)
+task_prefix=$contest
+
+if [[ "$contest" = "" ]]; then
+    echo "An AtCoder contest name is required."
+    exit 1
+fi
+
+echo "contest: ${contest}"
+echo "tasks: ${tasks}"
+
+for task in $tasks; do
+    oj dl -d "${task}/test" "https://atcoder.jp/contests/${contest}/tasks/${task_prefix}_${task:l}" &
+done
+wait
+echo "downloaded: ${tasks}"
