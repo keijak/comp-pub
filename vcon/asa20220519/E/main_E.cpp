4256dd037d6ff172574659d102cb6f6b6b9f8427
keijak <m@keijak.com>
2022-05-19 01:25:56 +0200

    asakatsu

---
+++ vcon/asa20220519/E/main_E.cpp
@@ -0,0 +1,119 @@
+#include <bits/stdc++.h>
+
+#define REP_(i, a_, b_, a, b, ...) for (int i = (a), END_##i = (b); i < END_##i; ++i)
+#define REP(i, ...) REP_(i, __VA_ARGS__, __VA_ARGS__, 0, __VA_ARGS__)
+#define ALL(x) std::begin(x), std::end(x)
+using Int = long long;
+using Uint = unsigned long long;
+using Real = long double;
+
+template<typename T, typename U>
+inline bool chmax(T &a, U b) { return a < b and ((a = b), true); }
+template<typename T, typename U>
+inline bool chmin(T &a, U b) { return a > b and ((a = b), true); }
+template<typename T>
+inline int ssize(const T &a) { return (int) a.size(); }
+template<typename T>
+constexpr T kBigVal = std::numeric_limits<T>::max() / 2;
+
+struct CastInput {
+  template<typename T>
+  operator T() const {
+    T x;
+    std::cin >> x;
+    return x;
+  }
+  struct Sized {
+    int n;
+    template<typename T>
+    operator T() const {
+      T xs(n);
+      for (auto &x: xs) std::cin >> x;
+      return xs;
+    }
+  };
+  Sized operator()(int n) const { return {n}; }
+} in;
+
+template<typename Container>
+std::ostream &print_seq(const Container &seq,
+                        const char *sep = " ",
+                        const char *ends = "\n",
+                        std::ostream &os = std::cout) {
+  const auto itl = std::begin(seq), itr = std::end(seq);
+  for (auto it = itl; it != itr; ++it) {
+    if (it != itl) os << sep;
+    os << *it;
+  }
+  return os << ends;
+}
+
+template<typename T>
+inline std::ostream &print_one(const T &x, char endc) {
+  if constexpr (std::is_same<T, bool>::value) {
+    return std::cout << (x ? "Yes" : "No") << endc;
+  } else {
+    return std::cout << x << endc;
+  }
+}
+template<typename T>
+inline std::ostream &print(const T &x) { return print_one(x, '\n'); }
+template<typename T, typename... Ts>
+std::ostream &print(const T &head, Ts... tail) {
+  return print_one(head, ' '), print(tail...);
+}
+inline std::ostream &print() { return std::cout << '\n'; }
+void exit_() { std::cout.flush(), std::cerr.flush(), std::_Exit(0); }
+
+#ifdef MY_DEBUG
+#include "debug_dump.hpp"
+#include "backward.hpp"
+backward::SignalHandling kSignalHandling;
+#else
+#define DUMP(...)
+#define cerr if(false)cerr
+#endif
+
+using namespace std;
+
+// Returns x mod y.
+// Guarantees 0 <= r < y (even when x is negative).
+template<class T>
+T floor_mod(T x, T y) {
+  assert(y > 0);
+  auto r = x % y;
+  if (r < 0) r += y;
+  return r;
+}
+
+string solve() {
+  Int N = in;
+  if (N == 0) {
+    return "0";
+  }
+  string ans;
+  for (int i = 0; N != 0; ++i) {
+    Int b = 1ll << i;
+    Int r = floor_mod<Int>(N, b * 2);
+    if (r == 0) {
+      ans += '0';
+      continue;
+    }
+    ans += '1';
+    if (i % 2 == 0) {
+      N -= b;
+    } else {
+      N += b;
+    }
+  }
+  reverse(ALL(ans));
+  return ans;
+}
+
+int main() {
+  std::ios::sync_with_stdio(false), cin.tie(nullptr);
+  cout << std::fixed << std::setprecision(18);
+  const int T = 1;//in;
+  REP(t, T) { print(solve()); }
+  exit_();
+}
