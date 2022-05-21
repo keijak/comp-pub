7dde1d9929a744cd13ba25e7b76fe5c431481cbe
keijak <m@keijak.com>
2022-05-21 13:51:39 +0200

    asakatsu

---
+++ vcon/asa20220520/D/main_D.cpp
@@ -0,0 +1,126 @@
+#include <bits/stdc++.h>
+
+#define REP_(i, a_, b_, a, b, ...) for (int i = (a), END_##i = (b); i < END_##i; ++i)
+#define REP(i, ...) REP_(i, __VA_ARGS__, __VA_ARGS__, 0, __VA_ARGS__)
+#define ALL(x) std::begin(x), std::end(x)
+using Int = long long;
+using Uint = unsigned long long;
+using Real = long double;
+
+template<typename T, typename U>
+inline bool chmax(T &a, U b) { return a < b and ((a = b), true); }
+template<typename T, typename U>
+inline bool chmin(T &a, U b) { return a > b and ((a = b), true); }
+template<typename T>
+inline int ssize(const T &a) { return (int) a.size(); }
+template<typename T>
+constexpr T kBigVal = std::numeric_limits<T>::max() / 2;
+
+struct CastInput {
+  template<typename T>
+  operator T() const {
+    T x;
+    std::cin >> x;
+    return x;
+  }
+  struct Sized {
+    int n;
+    template<typename T>
+    operator T() const {
+      T xs(n);
+      for (auto &x: xs) std::cin >> x;
+      return xs;
+    }
+  };
+  Sized operator()(int n) const { return {n}; }
+} in;
+
+template<typename Container>
+std::ostream &print_seq(const Container &seq,
+                        const char *sep = " ",
+                        const char *ends = "\n",
+                        std::ostream &os = std::cout) {
+  const auto itl = std::begin(seq), itr = std::end(seq);
+  for (auto it = itl; it != itr; ++it) {
+    if (it != itl) os << sep;
+    os << *it;
+  }
+  return os << ends;
+}
+
+template<typename T>
+inline std::ostream &print_one(const T &x, char endc) {
+  if constexpr (std::is_same<T, bool>::value) {
+    return std::cout << (x ? "Yes" : "No") << endc;
+  } else {
+    return std::cout << x << endc;
+  }
+}
+template<typename T>
+inline std::ostream &print(const T &x) { return print_one(x, '\n'); }
+template<typename T, typename... Ts>
+std::ostream &print(const T &head, Ts... tail) {
+  return print_one(head, ' '), print(tail...);
+}
+inline std::ostream &print() { return std::cout << '\n'; }
+void exit_() { std::cout.flush(), std::cerr.flush(), std::_Exit(0); }
+
+#ifdef MY_DEBUG
+#include "debug_dump.hpp"
+#include "backward.hpp"
+backward::SignalHandling kSignalHandling;
+#else
+#define DUMP(...)
+#define cerr if(false)cerr
+#endif
+
+using namespace std;
+
+// Binary search over integers
+template<class T, class F>
+auto bisect(T truthy, T falsy, F pred) -> T {
+  static_assert(std::is_integral_v<T>);
+  static_assert(std::is_invocable_r_v<bool, F, T>);
+  while (std::max(truthy, falsy) - std::min(truthy, falsy) > T(1)) {
+    auto mid = (truthy & falsy) + (truthy ^ falsy) / T(2);
+    auto ok = pred(mid);
+    (ok ? truthy : falsy) = std::move(mid);
+  }
+  return truthy;
+}
+
+template<class T>
+T ceil_div(T x, T y) {
+  assert(y != 0);
+  return x / y + (((x ^ y) >= 0) and (x % y));
+}
+auto solve() {
+  int n = in;
+  vector<int> T = in(n);
+  Int tsum = accumulate(ALL(T), 0LL);
+  auto dp = vector(n + 1, vector(tsum + 1, 0));
+  dp[0][0] = 1;
+  REP(i, n) {
+    dp[i + 1] = dp[i];
+    REP(j, tsum + 1) {
+      int j2 = j + T[i];
+      if (j2 <= tsum and dp[i][j]) {
+        dp[i + 1][j2] = 1;
+      }
+    }
+  }
+  for (Int t = ceil_div(tsum, 2LL); t <= tsum; ++t) {
+    if (dp[n][t]) {
+      return t;
+    }
+  }
+  return tsum;
+}
+
+int main() {
+  std::ios::sync_with_stdio(false), cin.tie(nullptr);
+  cout << std::fixed << std::setprecision(18);
+  const int T = 1;//in;
+  REP(t, T) { print(solve()); }
+  exit_();
+}
