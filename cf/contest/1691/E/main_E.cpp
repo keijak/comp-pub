040e2e9907a4eb7592c134e7552219e789f9393d
keijak <m@keijak.com>
2022-06-01 21:00:39 +0200

    sparse set verify

--- cf/contest/1691/E/main_E.cpp
+++ cf/contest/1691/E/main_E.cpp
@@ -97,6 +97,60 @@ backward::SignalHandling kSignalHandling;
 #endif
 
 using namespace std;
+struct SparseSet {
+  static constexpr int kMaxSize = int(1e5) + 10;
+  static constexpr int kMaxValue = int(1e5) + 10;
+
+  int size_ = 0;
+  std::array<int, kMaxSize> dense_;
+  std::array<int, kMaxValue> sparse_;
+
+  // O(1)
+  bool contains(int x) const {
+    // May access uninitialized memory, but safe.
+    const size_t i = sparse_[x];
+    return i < (size_t) size_ and dense_[i] == x;
+  }
+
+  // O(1)
+  void insert(int x) {
+    if (contains(x)) return;
+    dense_[size_] = x;
+    sparse_[x] = size_;
+    ++size_;
+  }
+
+  // O(1)
+  void erase(int x) {
+    const int i = sparse_[x];
+    if (i < 0 or i >= size_ or dense_[i] != x) {
+      return;  // not a member
+    }
+    auto y = dense_[size_ - 1];
+    dense_[i] = y;
+    sparse_[y] = i;
+    --size_;
+  }
+
+  // O(1)
+  inline void clear() { size_ = 0; }
+  inline int size() const { return size_; }
+  inline bool empty() const { return size_ == 0; }
+
+  // O(size)
+  template<class F>
+  void for_each(F func) {
+    for (int i = 0; i < size_; ++i) {
+      func(dense_[i]);
+    }
+  }
+
+  // For silencing warnings on accessing uninitialized memory.
+  void safe_init() {
+    dense_.fill(0);
+    sparse_.fill(0);
+  }
+};
 
 struct UnionFind {
   int n_;
@@ -142,10 +196,14 @@ auto solve() {
     events.emplace_back(r, true, i);
   }
   sort(ALL(events));
-  array<set<int>, 2> rs;
+  array<SparseSet, 2> rs;
   array<optional<int>, 2> rt;
 
-  for (auto [x, ending, i]: events) {
+  for (const auto &ev: events) {
+    Int x;
+    bool ending;
+    int i;
+    tie(x, ending, i) = ev;
     const auto &si = segs[i];
     const int me = si.c;
     const int other = me ^ 1;
@@ -166,13 +224,13 @@ auto solve() {
       } else if (not rs[other].empty()) {
         int v = -1;
         Int rmax = -1;
-        for (auto j: rs[other]) {
+        rs[other].for_each([&](int j) {
           auto [cj, lj, rj] = segs[j];
           uf.unite(i, j);
           if (chmax(rmax, rj)) {
             v = j;
           }
-        }
+        });
         rs[other].clear();
         rt[other] = v;
         rt[me] = i;
