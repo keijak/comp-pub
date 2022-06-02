1f288837abba84b17460b85c00bc0a2dbefb4b05
keijak <m@keijak.com>
2022-06-02 22:06:52 +0200

    cf 1598

--- cf/contest/1598/C/main.cpp
+++ cf/contest/1598/C/main.cpp
@@ -18,49 +18,7 @@ inline int ssize(const T &a) {
   return (int) a.size();
 }
 
-template<class T>
-inline std::ostream &print_one(const T &x, char endc) {
-  if constexpr (std::is_same_v<T, bool>) {
-    return std::cout << (x ? "Yes" : "No") << endc;
-  } else {
-    return std::cout << x << endc;
-  }
-}
-template<class T>
-inline std::ostream &print(const T &x) { return print_one(x, '\n'); }
-template<typename T, typename... Ts>
-std::ostream &print(const T &head, Ts... tail) {
-  return print_one(head, ' '), print(tail...);
-}
-inline std::ostream &print() { return std::cout << '\n'; }
-
-template<typename Container>
-std::ostream &print_seq(const Container &a, std::string_view sep = " ",
-                        std::string_view ends = "\n",
-                        std::ostream &os = std::cout) {
-  auto b = std::begin(a), e = std::end(a);
-  for (auto it = std::begin(a); it != e; ++it) {
-    if (it != b) os << sep;
-    os << *it;
-  }
-  return os << ends;
-}
-
-template<typename T, typename = void>
-struct is_iterable : std::false_type {};
-template<typename T>
-struct is_iterable<T, std::void_t<decltype(std::begin(std::declval<T>())),
-                                  decltype(std::end(std::declval<T>()))>>
-    : std::true_type {
-};
-
-template<typename T, typename = std::enable_if_t<
-    is_iterable<T>::value && !std::is_same<T, std::string>::value>>
-std::ostream &operator<<(std::ostream &os, const T &a) {
-  return print_seq(a, ", ", "", (os << "{")) << "}";
-}
-
-struct VersatileInput {
+struct CastInput {
   template<typename T>
   operator T() const {
     T x;
@@ -115,6 +73,6 @@ int main() {
   const int t = in;
   REP(test_case, t) {
     auto ans = solve();
-    print(ans);
+    cout << ans << '\n';
   }
 }
