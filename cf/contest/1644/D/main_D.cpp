6df709774b821be2ea8ec95f3c99d107db2ef117
keijak <m@keijak.com>
2022-06-07 00:24:36 +0200

    cf educational 123

---
+++ cf/contest/1644/D/main_D.cpp
@@ -0,0 +1,321 @@
+#include <bits/stdc++.h>
+
+#define REP_(i, a_, b_, a, b, ...) for (int i = (a), END_##i = (b); i < END_##i; ++i)
+#define REP(i, ...) REP_(i, __VA_ARGS__, __VA_ARGS__, 0, __VA_ARGS__)
+#define ALL(x) std::begin(x), std::end(x)
+using Int = long long;
+using Uint = unsigned long long;
+using Real = long double;
+
+template<typename T, typename U>
+inline bool chmax(T &a, U b) { return a < b and ((a = b), true); }
+template<typename T, typename U>
+inline bool chmin(T &a, U b) { return a > b and ((a = b), true); }
+template<typename T>
+constexpr T kBigVal = std::numeric_limits<T>::max() / 2;
+#if __cplusplus < 202002L
+template<typename T>
+inline int ssize(const T &a) { return (int) a.size(); }
+#endif
+
+struct CastInput {
+  template<typename T>
+  operator T() const {
+    T x;
+    assert(std::cin >> x);
+    return x;
+  }
+  struct Sized {
+    int n;
+    template<typename T>
+    operator T() const {
+      T xs(n);
+      for (auto &x: xs) assert(std::cin >> x);
+      return xs;
+    }
+  };
+  Sized operator()(int n) const { return {n}; }
+} in;
+
+template<typename Container>
+std::ostream &print_seq(const Container &seq,
+                        const char *sep = " ",
+                        const char *ends = "\n",
+                        std::ostream &os = std::cout) {
+  const auto itl = std::begin(seq), itr = std::end(seq);
+  for (auto it = itl; it != itr; ++it) {
+    if (it != itl) os << sep;
+    os << *it;
+  }
+  return os << ends;
+}
+
+template<typename T>
+inline std::ostream &print_one(const T &x, char endc) {
+  if constexpr (std::is_same<T, bool>::value) {
+    return std::cout << (x ? "Yes" : "No") << endc;
+  } else {
+    return std::cout << x << endc;
+  }
+}
+template<typename T>
+inline std::ostream &print(const T &x) { return print_one(x, '\n'); }
+template<typename T, typename... Ts>
+std::ostream &print(const T &head, Ts... tail) {
+  return print_one(head, ' '), print(tail...);
+}
+inline std::ostream &print() { return std::cout << '\n'; }
+
+void init_(bool interactive = false) {
+  std::ios::sync_with_stdio(false);
+  if (not interactive) std::cin.tie(nullptr);
+  std::cout << std::fixed << std::setprecision(18);
+}
+
+void exit_() {
+#ifdef MY_DEBUG
+  std::string unused;
+  assert(not(std::cin >> unused));  // No input is left behind.
+#endif
+  std::cout.flush(), std::cerr.flush(), std::_Exit(0);
+}
+
+inline void init_test_case(int t, int T) {
+#ifdef MY_DEBUG
+  if (T > 1) {
+    std::cerr << "\033[35m=== case " << t << " of " << T << " ===\033[0m"
+              << std::endl;
+  }
+#endif
+}
+
+#ifdef MY_DEBUG
+#include "debug_dump.hpp"
+#include "backward.hpp"
+backward::SignalHandling kSignalHandling;
+#else
+#define DUMP(...)
+#define cerr if(false)cerr
+#endif
+
+using namespace std;
+
+template<unsigned M>
+struct ModInt {
+  constexpr ModInt() : _v(0) {}
+  constexpr ModInt(long long val) {
+    if (val < 0) {
+      long long k = (std::abs(val) + M - 1) / M;
+      val += k * M;
+    }
+    assert(val >= 0);
+    _v = val % M;
+  }
+
+  static constexpr int mod() { return M; }
+  static constexpr unsigned umod() { return M; }
+  inline unsigned val() const { return _v; }
+
+  ModInt &operator++() {
+    _v++;
+    if (_v == umod()) _v = 0;
+    return *this;
+  }
+  ModInt &operator--() {
+    if (_v == 0) _v = umod();
+    _v--;
+    return *this;
+  }
+  ModInt operator++(int) {
+    auto result = *this;
+    ++*this;
+    return result;
+  }
+  ModInt operator--(int) {
+    auto result = *this;
+    --*this;
+    return result;
+  }
+
+  constexpr ModInt operator-() const { return ModInt(umod() - _v); }
+
+  constexpr ModInt &operator+=(const ModInt &a) {
+    if ((_v += a._v) >= M) _v -= M;
+    return *this;
+  }
+  constexpr ModInt &operator-=(const ModInt &a) {
+    if ((_v += M - a._v) >= M) _v -= M;
+    return *this;
+  }
+  constexpr ModInt &operator*=(const ModInt &a) {
+    _v = ((unsigned long long) (_v) * a._v) % M;
+    return *this;
+  }
+  constexpr ModInt pow(long long t) const {
+    if (_v == 0) {
+      return 0;  // corner case: 0^0 = ?
+    }
+    if (t < 0) {
+      return this->inv().pow(-t);
+    }
+    ModInt base = *this;
+    ModInt res = 1;
+    while (t) {
+      if (t & 1) res *= base;
+      base *= base;
+      t >>= 1;
+    }
+    return res;
+  }
+
+  // https://qiita.com/Mitarushi/items/8d7fb52e8a80e8008463
+  constexpr ModInt inv() const {
+    long long b = 1, a = _v;
+    while (a > 1) {
+      long long q = M / a;
+      a = M - a * q;
+      b = -b * q % M;
+    }
+    assert(a == 1);  // if a = 0, _v and M are not coprime.
+    if (b < 0) b += M;
+    ModInt ret;
+    ret._v = (unsigned) b;
+    return ret;
+  }
+  constexpr ModInt &operator/=(const ModInt &a) { return *this *= a.inv(); }
+
+  friend constexpr ModInt operator+(const ModInt &a, const ModInt &b) {
+    return ModInt(a) += b;
+  }
+  friend constexpr ModInt operator-(const ModInt &a, const ModInt &b) {
+    return ModInt(a) -= b;
+  }
+  friend constexpr ModInt operator*(const ModInt &a, const ModInt &b) {
+    return ModInt(a) *= b;
+  }
+  friend constexpr ModInt operator/(const ModInt &a, const ModInt &b) {
+    return ModInt(a) /= b;
+  }
+  friend constexpr bool operator==(const ModInt &a, const ModInt &b) {
+    return a._v == b._v;
+  }
+  friend constexpr bool operator!=(const ModInt &a, const ModInt &b) {
+    return a._v != b._v;
+  }
+  friend std::istream &operator>>(std::istream &is, ModInt &a) {
+    return is >> a._v;
+  }
+  friend std::ostream &operator<<(std::ostream &os, const ModInt &a) {
+    return os << a._v;
+  }
+
+ private:
+  unsigned _v;  // raw value
+};
+const unsigned MOD = 998244353;
+using Mint = ModInt<MOD>;
+
+template<int kMaxSize, int kMaxValue>
+struct SparseIntSet {
+  int size_;
+  int dense_[kMaxSize];
+  int sparse_[kMaxValue + 1];
+
+  SparseIntSet() noexcept: size_(0) {}
+
+  // O(1)
+  bool contains(int x) const {
+    // May access uninitialized memory, but safe.
+    const size_t i = sparse_[x];
+    return i < (size_t) size_ and dense_[i] == x;
+  }
+
+  // O(1)
+  void insert(int x) {
+    if (contains(x)) return;
+    dense_[size_] = x;
+    sparse_[x] = size_++;
+  }
+
+  // O(1)
+  void erase(int x) {
+    const int i = sparse_[x];
+    if (i < 0 or i >= size_ or dense_[i] != x) return;
+    const auto y = dense_[--size_];
+    dense_[i] = y;
+    sparse_[y] = i;
+  }
+
+  // O(1)
+  inline void clear() { size_ = 0; }
+  inline int size() const { return size_; }
+  inline bool empty() const { return size_ == 0; }
+
+  // O(size)
+  template<class F>
+  void for_each(F func) {
+    for (int i = 0; i < size_; ++i) {
+      func(dense_[i]);
+    }
+  }
+
+  using iterator = int *;
+  using const_iterator = const int *;
+  iterator begin() { return dense_; }
+  const_iterator begin() const { return dense_; }
+  iterator end() { return dense_ + size_; }
+  const_iterator end() const { return dense_ + size_; }
+
+  // For silencing warnings on accessing uninitialized memory.
+  void safe_init() {
+    dense_.fill(0);
+    sparse_.fill(0);
+  }
+};
+using IntSet = SparseIntSet<200'005, 200'005>;
+
+IntSet rows, cols;
+
+Uint solve() {
+  rows.clear();
+  cols.clear();
+
+  int n = in, m = in, K = in, Q = in;
+  vector<array<int, 2>> ops(Q);
+  REP(i, Q) {
+    int x = in, y = in;
+    --x, --y;
+    ops[i] = {x, y};
+  }
+  Uint ans = 1;
+  for (int i = Q - 1; i >= 0; --i) {
+    auto [x, y] = ops[i];
+    if (rows.contains(x) and cols.contains(y)) {
+      continue;
+    }
+    if (rows.size() < n and cols.size() < m) {
+      ans *= K;
+      ans %= MOD;
+    }
+    if (not rows.contains(x)) {
+      rows.insert(x);
+    }
+    if (not cols.contains(y)) {
+      cols.insert(y);
+    }
+    if (rows.size() == n and cols.size() == m) {
+      break;
+    }
+  }
+  return ans;
+}
+
+int main() {
+  init_();
+  const int T = in;
+  REP(t, T) {
+    init_test_case(t, T);
+    print(solve());
+  }
+  exit_();
+}
---
+++ cf/contest/1644/E/main_E.cpp
@@ -0,0 +1,151 @@
+#include <bits/stdc++.h>
+
+#define REP_(i, a_, b_, a, b, ...) for (int i = (a), END_##i = (b); i < END_##i; ++i)
+#define REP(i, ...) REP_(i, __VA_ARGS__, __VA_ARGS__, 0, __VA_ARGS__)
+#define ALL(x) std::begin(x), std::end(x)
+using Int = long long;
+using Uint = unsigned long long;
+using Real = long double;
+
+template<typename T, typename U>
+inline bool chmax(T &a, U b) { return a < b and ((a = b), true); }
+template<typename T, typename U>
+inline bool chmin(T &a, U b) { return a > b and ((a = b), true); }
+template<typename T>
+constexpr T kBigVal = std::numeric_limits<T>::max() / 2;
+#if __cplusplus < 202002L
+template<typename T>
+inline int ssize(const T &a) { return (int) a.size(); }
+#endif
+
+struct CastInput {
+  template<typename T>
+  operator T() const {
+    T x;
+    assert(std::cin >> x);
+    return x;
+  }
+  struct Sized {
+    int n;
+    template<typename T>
+    operator T() const {
+      T xs(n);
+      for (auto &x: xs) assert(std::cin >> x);
+      return xs;
+    }
+  };
+  Sized operator()(int n) const { return {n}; }
+} in;
+
+template<typename Container>
+std::ostream &print_seq(const Container &seq,
+                        const char *sep = " ",
+                        const char *ends = "\n",
+                        std::ostream &os = std::cout) {
+  const auto itl = std::begin(seq), itr = std::end(seq);
+  for (auto it = itl; it != itr; ++it) {
+    if (it != itl) os << sep;
+    os << *it;
+  }
+  return os << ends;
+}
+
+template<typename T>
+inline std::ostream &print_one(const T &x, char endc) {
+  if constexpr (std::is_same<T, bool>::value) {
+    return std::cout << (x ? "Yes" : "No") << endc;
+  } else {
+    return std::cout << x << endc;
+  }
+}
+template<typename T>
+inline std::ostream &print(const T &x) { return print_one(x, '\n'); }
+template<typename T, typename... Ts>
+std::ostream &print(const T &head, Ts... tail) {
+  return print_one(head, ' '), print(tail...);
+}
+inline std::ostream &print() { return std::cout << '\n'; }
+
+void init_(bool interactive = false) {
+  std::ios::sync_with_stdio(false);
+  if (not interactive) std::cin.tie(nullptr);
+  std::cout << std::fixed << std::setprecision(18);
+}
+
+void exit_() {
+#ifdef MY_DEBUG
+  std::string unused;
+  assert(not(std::cin >> unused));  // No input is left behind.
+#endif
+  std::cout.flush(), std::cerr.flush(), std::_Exit(0);
+}
+
+inline void init_test_case(int t, int T) {
+#ifdef MY_DEBUG
+  if (T > 1) {
+    std::cerr << "\033[35m=== case " << t << " of " << T << " ===\033[0m"
+              << std::endl;
+  }
+#endif
+}
+
+#ifdef MY_DEBUG
+#include "debug_dump.hpp"
+#include "backward.hpp"
+backward::SignalHandling kSignalHandling;
+#else
+#define DUMP(...)
+#define cerr if(false)cerr
+#endif
+
+using namespace std;
+
+Int solve() {
+  const Int n = in;
+  const string s = in;
+  const int m = ssize(s);
+  int total_d = (int) count(s.begin(), s.end(), 'D');
+  int total_r = (int) count(s.begin(), s.end(), 'R');
+  if (total_d == 0 or total_r == 0) {
+    return n;
+  }
+  int x = 0, y = 0;
+  REP(i, m) {
+    if (s[i] == 'D') {
+      x++;
+    } else {
+      y++;
+    }
+  }
+  Int ds = n - x, rs = n - y;
+  Int ans = ds * rs;
+  for (int i = m - 1; i >= 0; --i) {
+    DUMP(i, ds, rs, ans);
+    if (s[i] == 'D') {
+      if (total_r > 0) {
+        ans += rs;
+      } else {
+        ans += 1;
+      }
+      --total_d;
+    } else {
+      if (total_d > 0) {
+        ans += ds;
+      } else {
+        ans += 1;
+      }
+      --total_r;
+    }
+  }
+  return ans;
+}
+
+int main() {
+  init_();
+  const int T = in;
+  REP(t, T) {
+    init_test_case(t, T);
+    print(solve());
+  }
+  exit_();
+}
