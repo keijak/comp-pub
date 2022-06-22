559e340ebf3b28a4bec0050eb11f50114001ecf1
keijak <m@keijak.com>
2022-06-22 22:25:15 +0200

    math

---
+++ atcoder/arc139/B/main_B.cpp
@@ -0,0 +1,138 @@
+// #define NDEBUG
+#include <bits/stdc++.h>
+
+#define REP_(i, a_, b_, a, b, ...) for (int i = (a), END_##i = (b); i < END_##i; ++i)
+#define REP(i, ...) REP_(i, __VA_ARGS__, __VA_ARGS__, 0, __VA_ARGS__)
+#define ALL(x) std::begin(x), std::end(x)
+using Int = long long;
+using Uint = unsigned long long;
+using Real = long double;
+
+#include <boost/multiprecision/cpp_int.hpp>
+namespace multip = boost::multiprecision;
+using BigInt = multip::checked_int256_t;
+//using BigInt = multip::cpp_int;
+
+
+template<typename T, typename U>
+inline bool chmax(T &a, U b) { return a < b and ((a = b), true); }
+template<typename T, typename U>
+inline bool chmin(T &a, U b) { return a > b and ((a = b), true); }
+template<typename T>
+constexpr T kBigVal = std::numeric_limits<T>::max() / 2;
+#if __cplusplus < 202002L
+template<typename T>
+inline int ssize(const T &a) { return (int) a.size(); }
+#endif
+
+struct CastInput {
+  template<typename T>
+  operator T() const {
+    T x;
+    std::cin >> x;
+    assert(bool(std::cin));
+    return x;
+  }
+  struct Sized {
+    int n;
+    template<typename T>
+    operator T() const {
+      T xs(n);
+      for (auto &x: xs) {
+        std::cin >> x;
+        assert(bool(std::cin));
+      }
+      return xs;
+    }
+  };
+  Sized operator()(int n) const { return {n}; }
+} in;
+
+template<typename Container>
+std::ostream &output_seq(
+    const Container &seq, const char *sep = " ", const char *ends = "\n",
+    std::ostream &os = std::cout) {
+  const auto itl = std::begin(seq), itr = std::end(seq);
+  for (auto it = itl; it != itr; ++it) {
+    if (it != itl) os << sep;
+    os << *it;
+  }
+  return os << ends;
+}
+
+template<typename T>
+std::ostream &output_one(const T &x, char endc) {
+  if constexpr (std::is_same<T, bool>::value) {
+    return std::cout << (x ? "Yes" : "No") << endc;
+  } else {
+    return std::cout << x << endc;
+  }
+}
+template<typename T>
+std::ostream &output(const T &x) {
+  return output_one(x, '\n');
+}
+template<typename T, typename... Ts>
+std::ostream &output(const T &head, Ts... tail) {
+  return output_one(head, ' '), output(tail...);
+}
+
+void init_(bool interactive = false) {
+  std::ios::sync_with_stdio(false);
+  if (not interactive) std::cin.tie(nullptr);
+  std::cout << std::fixed << std::setprecision(18);
+}
+
+void exit_() {
+#ifdef MY_DEBUG
+  std::string _;
+  assert((std::cin >> _).fail());
+#endif
+  std::cout.flush(), std::cerr.flush(), std::_Exit(0);
+}
+
+#ifdef MY_DEBUG
+#include "debug_dump.hpp"
+#include "backward.hpp"
+backward::SignalHandling kSignalHandling;
+#else
+#define DUMP(...)
+#define test_case(...)
+#define cerr if(false)cerr
+#endif
+
+using namespace std;
+
+auto solve() -> Int {
+  Int N = in, A = in, B = in, X = in, Y = in, Z = in;
+  if (A * X <= Y) {
+    Y = A * X;
+  }
+  if (B * X <= Z) {
+    Z = B * X;
+  }
+  Int ans = kBigVal<Int>;
+  REP(trial, 2) {
+    REP(i, 100000) {
+      Int q = (N - A * i);
+      if (q < 0) break;
+      q /= B;
+      Int p = N - A * i - B * q;
+      Int r = i * Y + q * Z + p * X;
+      chmin(ans, r);
+    }
+    swap(A, B);
+    swap(Y, Z);
+  }
+  return ans;
+}
+
+int main() {
+  init_();
+  const int T = in;
+  REP(t, T) {
+    test_case(t, T);
+    output(solve());
+  }
+  exit_();
+}
