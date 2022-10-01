66cdbc2a47e56f72e74e778b7bb5a6aa61da5be0
keijak <m@keijak.com>
2022-10-01 23:16:27 +0200

    arc

---
+++ atcoder/arc146/B/main_B.cpp
@@ -0,0 +1,149 @@
+// #define NDEBUG
+#include <bits/stdc++.h>
+
+#define REP_(i, a_, b_, a, b, ...) for (int i = (a), END_##i = (b); i < END_##i; ++i)
+#define REP(i, ...) REP_(i, __VA_ARGS__, __VA_ARGS__, 0, __VA_ARGS__)
+#define ALL(x) std::begin(x), std::end(x)
+using Int = long long;
+using Uint = unsigned long long;
+using Real = long double;
+
+template<typename T, typename U>
+inline bool chmax(T &a, U b) { return a < b and ((a = b), true); }
+template<typename T, typename U>
+inline bool chmin(T &a, U b) { return a > b and ((a = b), true); }
+template<typename T>
+constexpr T kBig = std::numeric_limits<T>::max() / 2;
+#if __cplusplus < 202002L
+template<typename T>
+inline int ssize(const T &a) { return (int) a.size(); }
+#endif
+
+struct CastInput {
+  template<typename T>
+  operator T() const {
+    T x;
+    std::cin >> x;
+    assert(bool(std::cin));
+    return x;
+  }
+  struct Sized {
+    int n;
+    template<typename T>
+    operator T() const {
+      T xs(n);
+      for (auto &x: xs) {
+        std::cin >> x;
+        assert(bool(std::cin));
+      }
+      return xs;
+    }
+  };
+  Sized operator()(int n) const { return {n}; }
+} in;
+
+template<typename Container>
+std::ostream &out_seq(const Container &seq, const char *sep = " ",
+                      const char *ends = "\n", std::ostream &os = std::cout) {
+  const auto itl = std::begin(seq), itr = std::end(seq);
+  for (auto it = itl; it != itr; ++it) {
+    if (it != itl) os << sep;
+    os << *it;
+  }
+  return os << ends;
+}
+
+template<typename T>
+std::ostream &out_one(const T &x, char endc) {
+  if constexpr (std::is_same<T, bool>::value) {
+    return std::cout << (x ? "Yes" : "No") << endc;
+  } else {
+    return std::cout << x << endc;
+  }
+}
+template<typename T>
+std::ostream &out(const T &x) {
+  return out_one(x, '\n');
+}
+template<typename T, typename... Ts>
+std::ostream &out(const T &head, Ts... tail) {
+  return out_one(head, ' '), out(tail...);
+}
+
+void init_(bool interactive = false) {
+  std::ios::sync_with_stdio(false);
+  if (not interactive) std::cin.tie(nullptr);
+  std::cout << std::fixed << std::setprecision(18);
+}
+
+[[noreturn]] void exit_() {
+#ifdef MY_DEBUG
+  std::string _;
+  assert((std::cin >> _).fail());
+#endif
+  std::cout.flush(), std::cerr.flush(), std::_Exit(0);
+}
+
+#ifdef MY_DEBUG
+#include "debug_dump.hpp"
+#include "backward.hpp"
+backward::SignalHandling kSignalHandling;
+#else
+#define DUMP(...)
+#define test_case(...)
+#define cerr if(false)cerr
+#endif
+
+using namespace std;
+
+template<typename T>
+bool has_bit(const T &x, int i) { return (x >> i) & 1; }
+
+auto solve() {
+  int n = in, m = in, K = in;
+  vector<uint32_t> A = in(n);
+  uint32_t ans = 0;
+  for (int i = 30; i >= 0; --i) {
+    sort(A.rbegin(), A.rend());
+    DUMP(A);
+    int cnt = 0;
+    REP(j, n) {
+      if (has_bit(A[j], i)) ++cnt;
+    }
+    if (cnt >= K) {
+      ans |= (1 << i);
+    } else {
+      Int co_sum = 0;
+      vector<int> ix;
+      REP(j, n) {
+        if (has_bit(A[j], i)) continue;
+        assert(A[j] < (1 << i));
+        int co = (1 << i) - A[j];
+        co_sum += co;
+        ix.push_back(j);
+        if (ssize(ix) >= K - cnt) break;
+      }
+      if (co_sum <= m) {
+        ans |= (1 << i);
+        m -= co_sum;
+        for (int j: ix) {
+          A[j] = 1 << i;
+        }
+      }
+    }
+    REP(j, n) {
+      A[j] = A[j] & ~(1 << i);
+    }
+  }
+  out(ans);
+}
+
+int main() {
+  init_();
+  const int T = 1;//in;
+  REP(t, T) {
+    test_case(t, T);
+    (solve());
+  }
+  exit_();
+}
