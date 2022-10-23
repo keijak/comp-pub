5c0ee7c8a7752a85a2d6ef5eb0aaea577b4ed9fd
keijak <m@keijak.com>
2022-10-23 19:21:00 +0200

    min cut

--- atcoder/abc274/G/main_G.cpp
+++ atcoder/abc274/G/main_G.cpp
@@ -97,263 +97,145 @@ backward::SignalHandling kSignalHandling;
 
 using namespace std;
 
+#include <atcoder/maxflow>
 
+// For each node v, make sure:
+// - sum(cost(*, v)) < kBigCost
+// - sum(cost(v, *)) < kBigCost
+static constexpr int kBigCost = 900000;
 
-// https://misawa.github.io/others/flow/library_design.html
-
-enum Objective {
-  MINIMIZE = 1,
-  MAXIMIZE = -1,
-};
-
-enum class Status {
-  OPTIMAL,
-  INFEASIBLE,
-};
-
-using V_id = uint32_t;
-using E_id = uint32_t;
-
-template<class Flow, class Cost, Objective objective = Objective::MINIMIZE>
-class MinCostFlow {
-  class Edge {
-    friend class MinCostFlow;
-
-    V_id src, dst;
-    Flow flow, cap;
-    Cost cost;
-    E_id rev;
-
-   public:
-    Edge() = default;
-
-    Edge(const V_id src, const V_id dst, const Flow cap, const Cost cost,
-         const E_id rev)
-        : src(src), dst(dst), flow(0), cap(cap), cost(cost), rev(rev) {}
-
-    [[nodiscard]] Flow residual_cap() const { return cap - flow; }
-  };
-
-  V_id n;
-  std::vector<std::vector<Edge>> g;
-  std::vector<Flow> b;
-
+// T: flow capacity type (int or long long)
+template<typename T>
+struct ProjectSelection {
  public:
-  class EdgePtr {
-    friend class MinCostFlow;
 
-    const MinCostFlow *instance;
-    V_id v;
-    E_id e;
+  // Assign true to kSource and false to kSink.
+  const int kSource, kSink;
+  const int n_;  // number of initial nodes
+  int m_;        // number of additional nodes
 
-    EdgePtr(const MinCostFlow *const instance, const V_id v, const E_id e)
-        : instance(instance), v(v), e(e) {}
-
-    [[nodiscard]] const Edge &edge() const { return instance->g[v][e]; }
-
-    [[nodiscard]] const Edge &rev() const {
-      const Edge &e = edge();
-      return instance->g[e.dst][e.rev];
-    }
-
-   public:
-    EdgePtr() = default;
-
-    [[nodiscard]] V_id src() const { return v; }
-
-    [[nodiscard]] V_id dst() const { return edge().dst; }
-
-    [[nodiscard]] Flow flow() const { return edge().flow; }
-
-    [[nodiscard]] Flow lower() const { return -rev().cap; }
-
-    [[nodiscard]] Flow upper() const { return edge().cap; }
-
-    [[nodiscard]] Cost cost() const { return edge().cost; }
-
-    [[nodiscard]] Cost gain() const { return -edge().cost; }
+  struct Edge {
+    int from;
+    int to;
+    T cost;
   };
+  std::vector<Edge> edges_;
 
-  MinCostFlow() : n(0) {}
+  T bonus_;
 
-  V_id add_vertex() {
-    ++n;
-    g.resize(n);
-    b.resize(n);
-    return n - 1;
+  std::optional<std::vector<bool>> assignment_;
+
+  explicit ProjectSelection(int n)
+      : kSource(n), kSink(n + 1), n_(n), m_(0), bonus_(0) {}
+
+  // Returns the minimum cost.
+  T min_cost() {
+    atcoder::mf_graph<T> g(n_ + m_ + 2);
+    for (const Edge &e: edges_) {
+      g.add_edge(e.from, e.to, e.cost);
+    }
+    T res = g.flow(kSource, kSink) - bonus_;
+    // Get the assignment.
+    // assignment_ = g.min_cut(kSource);
+    return res;
   }
 
-  std::vector<V_id> add_vertices(const size_t size) {
-    std::vector<V_id> ret(size);
-    std::iota(std::begin(ret), std::end(ret), n);
-    n += size;
-    g.resize(n);
-    b.resize(n);
-    return ret;
+  // Returns the maximum gain.
+  T max_gain() { return -min_cost(); }
+
+  // === if-then API ===
+
+  // {X=1, Y=0} => cost (>= 0)
+  void if_X_and_notY_then_cost(int x, int y, T cost) {
+    if (cost == 0) return;
+    assert(cost > 0);
+    edges_.push_back(Edge{x, y, cost});
   }
 
-  void add_supply(const V_id v, const Flow amount) { b[v] += amount; }
-
-  void add_demand(const V_id v, const Flow amount) { b[v] -= amount; }
-
-  // lower/upper: flow limits (inclusive, non-negative).
-  // cost: cost if the objective is MINIMIZE. gain if the objective is MAXIMIZE.
-  EdgePtr add_edge(const V_id src, const V_id dst, const Flow lower,
-                   const Flow upper, const Cost cost) {
-    const E_id e = g[src].size(), re = src == dst ? e + 1 : g[dst].size();
-    assert(lower <= upper);
-    g[src].emplace_back(Edge{src, dst, upper, cost * objective, re});
-    g[dst].emplace_back(Edge{dst, src, -lower, -cost * objective, e});
-    return EdgePtr{this, src, e};
+  // {X=1} => cost (negative ok)
+  void if_X_then_cost(int x, T cost) {
+    if (cost < 0) return if_X_then_gain(x, -cost);
+    if_X_and_notY_then_cost(x, kSink, cost);
   }
 
-  // Solves the b flow problem.
-  std::pair<Status, Cost> solve() {
-    potential.resize(n);
-    for (auto &es: g) {
-      for (auto &e: es) {
-        const Flow rcap = e.residual_cap();
-        const Cost rcost = residual_cost(e.src, e.dst, e);
-        if (rcost < 0 || rcap < 0) {
-          push(e, rcap);
-          b[e.src] -= rcap;
-          b[e.dst] += rcap;
-        }
-      }
-    }
-    for (V_id v = 0; v < n; ++v) {
-      if (b[v] != 0) {
-        (b[v] > 0 ? excess_vs : deficit_vs).emplace_back(v);
-      }
-    }
-    while (dual()) primal();
-    Cost value = 0;
-    for (const auto &es: g) {
-      for (const auto &e: es) {
-        value += e.flow * e.cost;
-      }
-    }
-    value /= 2;
+  // {X=0} => cost (negative ok)
+  void if_notX_then_cost(int x, T cost) {
+    if (cost < 0) return if_notX_then_gain(x, -cost);
+    if_X_and_notY_then_cost(kSource, x, cost);
+  }
 
-    if (excess_vs.empty() && deficit_vs.empty()) {
-      return {Status::OPTIMAL, value / objective};
+  // {X=1} => gain (negative ok)
+  void if_X_then_gain(int x, T gain) {
+    if (gain < 0) return if_X_then_cost(x, -gain);
+    bonus_ += gain;
+    if_notX_then_cost(x, gain);
+  }
+
+  // {X=0} => gain (negative ok)
+  void if_notX_then_gain(int x, T gain) {
+    if (gain < 0) return if_notX_then_cost(x, -gain);
+    bonus_ += gain;
+    if_X_then_cost(x, gain);
+  }
+
+  // {X=0,Y=1} or {X=1,Y=0} => cost (>= 0)
+  void if_X_not_equal_Y_then_cost(int x, int y, T cost) {
+    if_X_and_notY_then_cost(x, y, cost);
+    if_X_and_notY_then_cost(y, x, cost);
+  }
+
+  // {X=0,Y=0} or {X=1,Y=1} => gain (>= 0)
+  void if_X_equal_Y_then_gain(int x, int y, T gain) {
+    bonus_ += gain;
+    if_X_and_notY_then_cost(x, y, gain);
+    if_X_and_notY_then_cost(y, x, gain);
+  }
+
+  // {X[i]=1 forall i} => gain (>= 0)
+  void if_all_of_X_then_gain(const std::vector<int> &xs, T gain) {
+    const int y = n_ + 2 + m_++;
+    bonus_ += gain;
+    if_X_and_notY_then_cost(kSource, y, gain);
+    for (const auto &x: xs) {
+      if_X_and_notY_then_cost(y, x, kBigCost);
+    }
+  }
+
+  // {X[i]=0 forall i} => gain (>= 0)
+  void if_none_of_X_then_gain(const std::vector<int> &xs, T gain) {
+    const int y = n_ + 2 + m_++;
+    bonus_ += gain;
+    if_X_and_notY_then_cost(y, kSink, gain);
+    for (const auto &x: xs) {
+      if_X_and_notY_then_cost(x, y, kBigCost);
+    }
+  }
+
+  // === constraint API ===
+
+  void constraint(int x, int y, T cost) {
+    if (x >= 0 and y < 0) {
+      assert(cost >= 0);
+      if_X_and_notY_then_cost(x, ~y, cost);
+    } else if (x < 0 and y >= 0) {
+      assert(cost >= 0);
+      if_X_and_notY_then_cost(y, ~x, cost);
+    } else if (x >= 0 and y >= 0) {
+      assert(cost <= 0);
+      if_all_of_X_then_gain({x, y}, -cost);
+    } else if (x < 0 and y < 0) {
+      assert(cost <= 0);
+      if_none_of_X_then_gain({~x, ~y}, -cost);
     } else {
-      return {Status::INFEASIBLE, value / objective};
+      assert(false);
     }
   }
 
-  // Solves the s-t flow problem.
-  //
-  // Computes the maximum flow from s to t, and also returns
-  // the minimum cost when the flow is maximum.
-  // Don't need to call add_supply()/add_demand() in advance.
-  std::tuple<Status, Cost, Flow> solve(const V_id s, const V_id t) {
-    assert(s != t);
-    Flow inf_flow = std::abs(b[s]);
-    for (const auto &e: g[s]) {
-      inf_flow += std::max(e.cap, static_cast<Flow>(0));
-    }
-
-    add_edge(t, s, 0, inf_flow, 0);
-    const auto [status, circulation_value] = solve();
-
-    if (status == Status::INFEASIBLE) {
-      g[s].pop_back();
-      g[t].pop_back();
-      return {status, circulation_value, 0};
-    }
-    inf_flow = std::abs(b[s]);
-    for (const auto &e: g[s]) {
-      inf_flow += e.residual_cap();
-    }
-    b[s] += inf_flow;
-    b[t] -= inf_flow;
-    const auto [mf_status, mf_value] = solve();
-    b[s] -= inf_flow;
-    b[t] += inf_flow;
-    g[s].pop_back();
-    g[t].pop_back();
-    return {Status::OPTIMAL, mf_value, b[t]};
-  }
-
- private:
-  // Variables used in calculation
-  static Cost constexpr unreachable = std::numeric_limits<Cost>::max();
-  Cost farthest;
-  std::vector<Cost> potential;
-  std::vector<Cost> dist;
-  std::vector<Edge *> parent;  // out-forrest.
-  std::priority_queue<std::pair<Cost, int>, std::vector<std::pair<Cost, int>>,
-                      std::greater<>>
-      pq;  // should be empty outside of dual()
-  std::vector<V_id> excess_vs, deficit_vs;
-
-  Edge &rev(const Edge &e) { return g[e.dst][e.rev]; }
-
-  void push(Edge &e, const Flow amount) {
-    e.flow += amount;
-    g[e.dst][e.rev].flow -= amount;
-  }
-
-  Cost residual_cost(const V_id src, const V_id dst, const Edge &e) {
-    return e.cost + potential[src] - potential[dst];
-  }
-
-  bool dual() {
-    dist.assign(n, unreachable);
-    parent.assign(n, nullptr);
-    excess_vs.erase(std::remove_if(std::begin(excess_vs), std::end(excess_vs),
-                                   [&](const V_id v) { return b[v] <= 0; }),
-                    std::end(excess_vs));
-    deficit_vs.erase(
-        std::remove_if(std::begin(deficit_vs), std::end(deficit_vs),
-                       [&](const V_id v) { return b[v] >= 0; }),
-        std::end(deficit_vs));
-    for (const auto v: excess_vs) pq.emplace(dist[v] = 0, v);
-    farthest = 0;
-    std::size_t deficit_count = 0;
-    while (!pq.empty()) {
-      const auto [d, u] = pq.top();
-      pq.pop();
-      if (dist[u] < d) continue;
-      farthest = d;
-      if (b[u] < 0) ++deficit_count;
-      if (deficit_count >= deficit_vs.size()) break;
-      for (auto &e: g[u]) {
-        if (e.residual_cap() <= 0) continue;
-        const auto v = e.dst;
-        const auto new_dist = d + residual_cost(u, v, e);
-        if (new_dist >= dist[v]) continue;
-        pq.emplace(dist[v] = new_dist, v);
-        parent[v] = &e;
-      }
-    }
-    pq = decltype(pq)();  // pq.clear() doesn't exist.
-    for (V_id v = 0; v < n; ++v) {
-      potential[v] += std::min(dist[v], farthest);
-    }
-    return deficit_count > 0;
-  }
-
-  void primal() {
-    for (const auto t: deficit_vs) {
-      if (dist[t] > farthest) continue;
-      Flow f = -b[t];
-      V_id v;
-      for (v = t; parent[v] != nullptr; v = parent[v]->src) {
-        f = std::min(f, parent[v]->residual_cap());
-      }
-      f = std::min(f, b[v]);
-      if (f <= 0) continue;
-      for (v = t; parent[v] != nullptr;) {
-        auto &e = *parent[v];
-        push(e, f);
-        int u = parent[v]->src;
-        if (e.residual_cap() <= 0) parent[v] = nullptr;
-        v = u;
-      }
-      b[t] += f;
-      b[v] -= f;
+  void constraint(int x, T cost) {
+    if (x >= 0) {
+      if_X_then_cost(x, cost);
+    } else {
+      if_notX_then_cost(~x, cost);
     }
   }
 };
@@ -378,49 +260,75 @@ auto solve() {
         vwall[j].insert(i);
       }
     }
-  MinCostFlow<int, int> mcf;
-  map<pair<int, int>, int> vid, uid;
-  const int kSource = mcf.add_vertex();
-  const int kSink = mcf.add_vertex();
 
-  auto add_v = [&](int x, int y) -> int {
-    int id;
-    if (not vid.count(pair{x, y})) {
-      id = mcf.add_vertex();
-      mcf.add_edge(kSource, id, 0, 1, 0);
-      vid[pair{x, y}] = id;
+  map<tuple<int, int, bool>, int> cam_id;
+  vector<tuple<int, int, bool>> cams;
+  map<pair<int, int>, int> space_id;
+
+  auto left_cam = vector(H, vector(W, optional<int>()));
+  auto up_cam = vector(H, vector(W, optional<int>()));
+
+  auto add_camera = [&](int x, int y, bool down) -> int {
+    if (S[x][y] == '#') return -1;
+    if (auto it = cam_id.find({x, y, down}); it != cam_id.end()) {
+      return it->second;
+    }
+    cams.emplace_back(x, y, down);
+    int new_id = ssize(cam_id);
+    cam_id[{x, y, down}] = new_id;
+
+    if (down) {
+      for (int z = x; z < H; ++z) {
+        if (S[z][y] == '#') break;
+        up_cam[z][y] = new_id;
+      }
     } else {
-      id = vid[pair{x, y}];
+      for (int z = y; z < W; ++z) {
+        if (S[x][z] == '#') break;
+        left_cam[x][z] = new_id;
+      }
     }
-    return id;
+    return new_id;
   };
-  REP(i, H) REP(j, W) {
+
+  REP(i, H) {
+    REP(j, W) {
       if (S[i][j] == '#') continue;
-      int u = mcf.add_vertex();
-      uid[pair{i, j}] = u;
-      mcf.add_edge(u, kSink, 0, 1, 0);
-      auto it = hwall[i].lower_bound(j);
-
-      int v = add_v(i, *it);
-      mcf.add_edge(v, u, 0, 1, 1);
-      --it;
-      v = add_v(i, *it);
-      mcf.add_edge(v, u, 0, 1, 1);
-      it = vwall[j].lower_bound(i);
-      v = add_v(*it, j);
-      mcf.add_edge(v, u, 0, 1, 1);
-      --it;
-      v = add_v(*it, j);
-      mcf.add_edge(v, u, 0, 1, 1);
+      int new_id = ssize(space_id);
+      space_id[{i, j}] = new_id;
+      if (i == 0 or S[i - 1][j] == '#') {
+        add_camera(i, j, true);
+      }
+      if (j == 0 or S[i][j - 1] == '#') {
+        add_camera(i, j, false);
+      }
     }
+  }
 
-  int b = ssize(uid);
-  mcf.add_demand(kSink, b);
-  mcf.add_supply(kSource, b);
+  const int n = ssize(cam_id);
+  ProjectSelection<int> ps(n);
+  for (const auto &[key, cid]: cam_id) {
+    auto [x, y, down] = key;
+    if (down) {
+      ps.if_X_then_cost(cid, 1);
+    } else {
+      ps.if_notX_then_cost(cid, 1);
+    }
+  }
 
-  auto [status, cost] = mcf.solve();
-  out(cost);
+  REP(i, H) {
+    REP(j, W) {
+      if (S[i][j] == '#') continue;
+      auto l = left_cam[i][j];
+      auto u = up_cam[i][j];
+      assert(l.has_value() and u.has_value());
+      ps.if_X_and_notY_then_cost(*l, *u, kBigCost);
 
+//      DUMP(i, j, cams[*l], cams[*u]);
+    }
+//    cerr << "---" << endl;
+  }
+  out(ps.min_cost());
 }
 
 int main() {
