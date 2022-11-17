f11e47e0e34bdd133ee106f7aed77d152b2a9a5e
keijak <m@keijak.com>
2022-11-17 01:29:47 +0100

    fps

---
+++ atcoder/abc159/F/review2.cpp
@@ -0,0 +1,206 @@
+// #define NDEBUG
+#include <bits/stdc++.h>
+
+#define REP_(i, a_, b_, a, b, ...) for (int i = (a), END_##i = (b); i < END_##i; ++i)
+#define REP(i, ...) REP_(i, __VA_ARGS__, __VA_ARGS__, 0, __VA_ARGS__)
+#define ALL(x) std::begin(x), std::end(x)
+using Int = long long;
+using Uint = unsigned long long;
+using Real = long double;
+
+#include <atcoder/modint>
+using Mint = atcoder::modint998244353;
+std::ostream &operator<<(std::ostream &os, const Mint &m) {
+  return os << m.val();
+}
+
+template<typename T, typename U>
+inline bool chmax(T &a, U b) { return a < b and ((a = b), true); }
+template<typename T, typename U>
+inline bool chmin(T &a, U b) { return a > b and ((a = b), true); }
+template<typename T>
+constexpr T kBig = std::numeric_limits<T>::max() / 2;
+#if __cplusplus < 202002L
+template<typename T>
+inline int ssize(const T &a) { return (int) a.size(); }
+#endif
+
+template<size_t BufSize>
+class InputReader {
+ public:
+  void skip() {
+    [[maybe_unused]] static const bool lazy_init = [this]() {
+      const size_t len = fread(buf_, 1, sizeof(buf_) - 1, stdin);
+      buf_[len] = '\0';
+      ptr_ = buf_;
+      bufend_ = buf_ + len;
+      return true;
+    }();
+    while (isspace(*ptr_)) ++ptr_;
+  }
+
+  template<typename T>
+  operator T() {
+    T x;
+    skip();
+    assert(not is_eof());  // Couldn't read reached the end of input.
+    read_one(x);
+    return x;
+  }
+
+  struct Sized {
+    InputReader<BufSize> &reader;
+    int n;
+    template<typename T>
+    operator T() const {
+      T xs(n);
+      for (auto &x: xs) {
+        reader.skip();
+        assert(not reader.is_eof());
+        reader.read_one(x);
+      }
+      return xs;
+    }
+  };
+  Sized operator()(int n) { return {*this, n}; }
+
+  bool is_eof() const { return ptr_ >= bufend_; }
+
+ private:
+  template<class T>
+  std::enable_if_t<std::is_integral_v<T>> read_one(T &x) {
+    [[maybe_unused]] int sign = 1;
+    if constexpr (std::is_signed_v<T>) {
+      sign = (*ptr_ == '-') ? (++ptr_, -1) : 1;
+    }
+    x = 0;
+    while (isdigit(*ptr_)) x = x * 10 + (*ptr_++ & 0x0F);
+    if constexpr (std::is_signed_v<T>) {
+      x *= sign;
+    }
+  }
+  void read_one(std::string &s) {
+    char *p0 = ptr_;
+    while (not isspace(*ptr_)) ptr_++;
+    s.assign(p0, ptr_);
+  }
+  void read_one(std::string_view &s) {
+    const char *p0 = ptr_;
+    while (not isspace(*ptr_)) ptr_++;
+    s = std::string_view(p0, ptr_ - p0);
+  }
+
+  static inline char buf_[BufSize];
+  char *ptr_, *bufend_;
+};
+InputReader<(1 << 26)> in;
+
+template<typename Container>
+std::ostream &out_seq(const Container &seq, const char *sep = " ",
+                      const char *ends = "\n", std::ostream &os = std::cout) {
+  const auto itl = std::begin(seq), itr = std::end(seq);
+  for (auto it = itl; it != itr; ++it) {
+    if (it != itl) os << sep;
+    os << *it;
+  }
+  return os << ends;
+}
+
+template<typename T>
+std::ostream &out_one(const T &x, char endc) {
+  if constexpr (std::is_same<T, bool>::value) {
+    return std::cout << (x ? "Yes" : "No") << endc;
+  } else {
+    return std::cout << x << endc;
+  }
+}
+template<typename T>
+std::ostream &out(const T &x) {
+  return out_one(x, '\n');
+}
+template<typename T, typename... Ts>
+std::ostream &out(const T &head, Ts... tail) {
+  return out_one(head, ' '), out(tail...);
+}
+
+void init_() {
+  // std::ios::sync_with_stdio(false);
+  // std::cin.tie(nullptr);
+  std::cout << std::fixed << std::setprecision(18);
+}
+
+[[noreturn]] void exit_() {
+#ifdef MY_DEBUG
+  in.skip();
+  assert(in.is_eof());  // Some input is left.
+#endif
+  fflush(stdout), fflush(stderr);
+  std::cout.flush(), std::cerr.flush();
+  std::_Exit(0);
+}
+
+#ifdef MY_DEBUG
+#include "debug_dump.hpp"
+#include "backward.hpp"
+backward::SignalHandling kSignalHandling;
+#else
+#define DUMP(...)
+#define test_case(...)
+#define cerr if(false)cerr
+#endif
+
+using namespace std;
+
+template<typename T>
+struct Array3d {
+  std::vector<T> data_;
+  size_t b0_ = 0, b1_ = 0;
+
+  Array3d(size_t d0, size_t d1, size_t d2)
+      : data_(d0 * d1 * d2, T{}), b0_(d1 * d2), b1_(d2) {}
+
+  inline T &get(size_t i0, size_t i1, size_t i2) {
+    return data_[i0 * b0_ + i1 * b1_ + i2];
+  }
+  // Fills one row with the specified value.
+  inline void fill(size_t i0, T val) {
+    std::fill(data_.begin() + i0 * b0_, data_.begin() + (i0 + 1) * b0_, val);
+  }
+};
+
+auto solve() {
+  const int N = in, S = in;
+  const vector<int> A = in(N);
+  Array3d<Mint> dp(2, S + 1, 2);
+  dp.get(0, 0, 0) = 1;
+  REP(i, N) {
+    const int i0 = i & 1;
+    const int i1 = i0 ^ 1;
+    REP(j, S + 1) REP(k, 2) dp.get(i1, j, k) = dp.get(i0, j, k);
+    REP(j, S) {
+      if (j + A[i] > S) continue;
+      REP(started, 2) {
+        if (dp.get(i0, j, started).val() == 0) continue;
+        Mint multiplier = 1;
+        if (j + A[i] == S) {
+          multiplier *= N - i;
+        }
+        if (not started) {
+          multiplier *= i + 1;
+        }
+        dp.get(i1, j + A[i], 1) += dp.get(i0, j, started) * multiplier;
+      }
+    }
+  }
+  out(dp.get(N & 1, S, 1));
+}
+
+int main() {
+  init_();
+  const int T = 1;//in;
+  REP(t, T) {
+    test_case(t, T);
+    (solve());
+  }
+  exit_();
+}
